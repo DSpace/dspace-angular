4ddaf450ae2be7d0dd9d75d1985c6e532ab6f49a
Lotte Hofstede <lotte_hofstede@hotmail.com>
2017-01-12 15:26:03 +0100

    Added missing pagenotfound files

---
+++ src/app/pagenotfound/pagenotfound.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'ds-pagenotfound',
+  styleUrls: ['./pagenotfound.component.css'],
+  templateUrl: './pagenotfound.component.html'
+})
+export class PageNotFoundComponent {
+
+  data: any = {};
+
+  constructor() {
+    this.universalInit();
+  }
+
+  universalInit() {
+
+  }
+
+}
