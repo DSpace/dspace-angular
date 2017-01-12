82a379df3c5b0c293a4949e40b07564163615df6
Lotte Hofstede <lotte_hofstede@hotmail.com>
2017-01-12 15:18:15 +0100

    38248: Added PageNotFound component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { PageNotFoundComponent } from './pagenotfound/pagenotfound.component';
 @NgModule({
   imports: [
     RouterModule.forChild([
-      { path: '', redirectTo: '/home', pathMatch: 'full' }
+      { path: '', redirectTo: '/home', pathMatch: 'full' },
+      { path: '**', pathMatch: 'full', component: PageNotFoundComponent},
     ])
   ],
 })
