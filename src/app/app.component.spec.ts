28fd92b9f7fdf8fd0ae6f937a979344960896a13
Giuseppe Digilio <giuseppe.digilio@4science.it>
2017-01-13 18:37:35 +0100

    test issue

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -9,6 +9,7 @@ import {
   CUSTOM_ELEMENTS_SCHEMA,
   DebugElement
 } from "@angular/core";
+import { By } from '@angular/platform-browser';
 import { TranslateModule } from "ng2-translate";
 import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 import { Store } from "@ngrx/store";
@@ -28,7 +29,7 @@ let el:      HTMLElement;
 
 describe('App component', () => {
 
-  beforeEach(() => {
+  beforeEach(async(() => {
     return TestBed.configureTestingModule({
       imports: [ CommonModule, TranslateModule.forRoot(), NgbCollapseModule.forRoot()],
       declarations: [ AppComponent, HeaderComponent ], // declare the test component
@@ -40,10 +41,18 @@ describe('App component', () => {
         }
       ],
       schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
-    }).compileComponents().then(() => {
-      fixture = TestBed.createComponent(AppComponent);
-      fixture.detectChanges();
-    });
+    })
+  }));
+
+  // synchronous beforeEach
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+
+    comp = fixture.componentInstance; // BannerComponent test instance
+
+    // query for the title <p> by CSS element selector
+    de = fixture.debugElement.query(By.css('p'));
+    el = de.nativeElement;
   });
 
   it('should create component', inject([AppComponent], (app: AppComponent) => {
---
+++ src/test-issue.spec.ts
@@ -0,0 +1,97 @@
+import {
+  async,
+  ComponentFixture,
+  inject,
+  TestBed
+} from '@angular/core/testing';
+
+describe('test issue', () => {
+  it('should test 1', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should test 2', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should test 3', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should test 4', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should test 5', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should test 6', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should test 7', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should test 8', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should test 9', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should test 10', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should test 11', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should test 12', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should test 13', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should test 14', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should test 15', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should test 16', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should test 17', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should test 18', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should test 19', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should test 20', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should test 21', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should test 22', () => {
+    expect(true).toBe(true);
+  });
+
+});
