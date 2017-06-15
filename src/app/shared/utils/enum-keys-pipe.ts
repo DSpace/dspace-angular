a253509c5de9db3ab24808daeb12d96335437b27
Lotte Hofstede <lotte_hofstede@hotmail.com>
2017-06-15 13:35:04 +0200

    41914: implemented pagination sort direction/field

---
+++ src/app/shared/utils/enum-keys-pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from "@angular/core";
+@Pipe({ name: 'dsKeys' })
+export class EnumKeysPipe implements PipeTransform {
+    transform(value, args: string[]): any {
+        let keys = [];
+        for (var enumMember in value) {
+            if (!isNaN(parseInt(enumMember, 10))) {
+                keys.push({ key: +enumMember, value: value[enumMember] });
+            }
+        }
+        return keys;
+    }
+}
