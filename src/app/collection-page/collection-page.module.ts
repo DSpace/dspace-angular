5245fd6488a1cc2cf38a1590b01b4ddfe2c35b09
Giuseppe Digilio <giuseppe.digilio@4science.it>
2017-06-20 15:52:58 +0200

    Show default image

--- src/app/collection-page/collection-page.module.ts
+++ src/app/collection-page/collection-page.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { TranslateModule } from "@ngx-translate/core";
+
+import { SharedModule } from '../shared/shared.module';
 
 import { CollectionPageComponent } from './collection-page.component';
 import { FieldWrapperComponent } from './field-wrapper/field-wrapper.component';
@@ -17,6 +18,7 @@ import { CollectionPageRoutingModule } from './collection-page-routing.module';
   imports: [
     CollectionPageRoutingModule,
     CommonModule,
+    SharedModule,
     TranslateModule,
   ],
   declarations: [
