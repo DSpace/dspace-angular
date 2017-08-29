29d04c06b96002e042540bb6ccfc057ef6ca8bb5
Lotte Hofstede <lotte_hofstede@hotmail.com>
2017-08-29 13:21:44 +0200

    44024: search page finalisation

--- src/app/object-list/search-result-list-element/search-result-list-element.component.ts
+++ src/app/object-list/search-result-list-element/search-result-list-element.component.ts
@@ -4,6 +4,8 @@ import { ObjectListElementComponent } from '../object-list-element/object-list-e
 import { ListableObject } from '../listable-object/listable-object.model';
 import { SearchResult } from '../../search/search-result.model';
 import { DSpaceObject } from '../../core/shared/dspace-object.model';
+import { Metadatum } from '../../core/shared/metadatum.model';
+import { isEmpty, hasNoValue } from '../../shared/empty.util';
 
 @Component({
   selector: 'ds-search-result-list-element',
@@ -12,8 +14,44 @@ import { DSpaceObject } from '../../core/shared/dspace-object.model';
 
 export class SearchResultListElementComponent<T extends SearchResult<K>, K extends DSpaceObject> extends ObjectListElementComponent<T> {
   dso: K;
+
   public constructor(@Inject('objectElementProvider') public listable: ListableObject) {
     super(listable);
     this.dso = this.object.dspaceObject;
+  }
+
+  getValues(keys: string[]): string[] {
+    const results: string[] = new Array<string>();
+    this.object.hitHighlights.forEach(
+      (md: Metadatum) => {
+        if (keys.indexOf(md.key) > -1) {
+          results.push(md.value);
+        }
+      }
+    );
+    if (isEmpty(results)) {
+      this.dso.filterMetadata(keys).forEach(
+        (md: Metadatum) => {
+          results.push(md.value);
+        }
+      );
+    }
+    return results;
+  }
+
+  getFirstValue(key: string): string {
+    let result: string;
+    this.object.hitHighlights.some(
+      (md: Metadatum) => {
+        if (key === md.key) {
+          result =  md.value;
+          return true;
+        }
+      }
+    );
+    if (hasNoValue(result)) {
+      result = this.dso.findMetadata(key);
+    }
+    return result;
   }
 }
