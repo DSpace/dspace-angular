ad59cb3276283da702dc62768eec4146149209d9
Lotte Hofstede <lotte_hofstede@hotmail.com>
2017-10-03 15:49:26 +0200

    44878: more unit tests

--- src/app/+search-page/search-page.component.spec.ts
+++ src/app/+search-page/search-page.component.spec.ts
@@ -1,16 +1,19 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { async, ComponentFixture, inject, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { CommunityDataService } from '../core/data/community-data.service';
 import { SearchPageComponent } from './search-page.component';
 import { SearchService } from './search.service';
 import { Community } from '../core/shared/community.model';
+import { PaginationComponentOptions } from '../shared/pagination/pagination-component-options.model';
+import { SortDirection, SortOptions } from '../core/cache/models/sort-options.model';
 
-fdescribe('SearchPageComponent', () => {
+describe('SearchPageComponent', () => {
   let comp: SearchPageComponent;
   let fixture: ComponentFixture<SearchPageComponent>;
-  const mockResults = []; // TODO
+  let searchServiceObject: SearchService;
+  const mockResults = ['test', 'data'];
   const searchServiceStub = {
     search: () => mockResults
   };
@@ -29,20 +32,22 @@ fdescribe('SearchPageComponent', () => {
   };
 
   class RouterStub {
-    navigateByUrl(url: string) { return url; }
+    navigateByUrl(url: string) {
+      return url;
+    }
   }
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       // imports: [ SearchPageModule ],
-      declarations: [ SearchPageComponent ],
+      declarations: [SearchPageComponent],
       providers: [
         { provide: SearchService, useValue: searchServiceStub },
         { provide: ActivatedRoute, useValue: activatedRouteStub },
         { provide: CommunityDataService, useValue: communityDataServiceStub },
         { provide: Router, useClass: RouterStub }
       ],
-      schemas:      [ NO_ERRORS_SCHEMA ]
+      schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   }));
 
@@ -50,6 +55,7 @@ fdescribe('SearchPageComponent', () => {
     fixture = TestBed.createComponent(SearchPageComponent);
     comp = fixture.componentInstance; // SearchPageComponent test instance
     fixture.detectChanges();
+    searchServiceObject = (comp as any).service;
   });
 
   it('should set the scope and query based on the route parameters', () => {
@@ -57,4 +63,48 @@ fdescribe('SearchPageComponent', () => {
     expect((comp as any).scope).toBe(scopeParam);
   });
 
+  describe('when update search results is called', () => {
+    let pagination;
+    let sort;
+    beforeEach(() => {
+      pagination = Object.assign(
+        {},
+        new PaginationComponentOptions(),
+        {
+          currentPage: 5,
+          pageSize: 15
+        }
+      );
+      sort = Object.assign({},
+        new SortOptions(),
+        {
+          direction: SortDirection.Ascending,
+          field: 'test-field'
+        }
+      );
+    });
+
+    it('should call the search function of the search service with the right parameters', () => {
+      spyOn(searchServiceObject, 'search').and.callThrough();
+
+      (comp as any).updateSearchResults({
+        pagination: pagination,
+        sort: sort
+      });
+
+      expect(searchServiceObject.search).toHaveBeenCalledWith(queryParam, scopeParam, {
+        pagination: pagination,
+        sort: sort
+      });
+    });
+
+    it('should update the results', () => {
+      spyOn(searchServiceObject, 'search').and.callThrough();
+
+      (comp as any).updateSearchResults({});
+
+      expect(comp.results as any).toBe(mockResults);
+      });
+
+  });
 });
