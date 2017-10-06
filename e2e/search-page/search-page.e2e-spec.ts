7b6546641d825014a24db40f663891f591abf1d9
Lotte Hofstede <lotte_hofstede@hotmail.com>
2017-10-06 09:27:34 +0200

    Fixed issue with e2e test after merging

--- e2e/search-page/search-page.e2e-spec.ts
+++ e2e/search-page/search-page.e2e-spec.ts
@@ -32,6 +32,7 @@ describe('protractor SearchPage', () => {
     scope.then((scopeString: string) => {
       page.setCurrentScope(scopeString);
       page.submitSearchForm();
+      browser.sleep(10);
       browser.getCurrentUrl().then((url: string) => {
         expect(url.indexOf('scope=' + encodeURI(scopeString))).toBeGreaterThanOrEqual(0);
       });
@@ -42,6 +43,7 @@ describe('protractor SearchPage', () => {
     const queryString = 'Another interesting query string';
     page.setCurrentQuery(queryString);
     page.submitSearchForm();
+    browser.sleep(10);
     browser.getCurrentUrl().then((url: string) => {
       expect(url.indexOf('query=' + encodeURI(queryString))).toBeGreaterThanOrEqual(0);
     });
