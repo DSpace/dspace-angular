727353988fd0870f269509e1908437c9d8ad54fa
Jonas Van Goolen <jonas@atmire.com>
2017-11-03 14:46:33 +0100

    #150 Safety-commit before moving to "shared" package

---
+++ src/app/object-grid/item-grid-element/item-grid-element.component.spec.ts
@@ -0,0 +1,6 @@
+import { ItemGridElementComponent } from './item-grid-element.component';
+
+describe('ItemGridElementComponent',()=>{
+  let itemGridElementComponent: ItemGridElementComponent;
+
+})
