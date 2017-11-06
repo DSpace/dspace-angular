67912fc4a5e2505dc168a9581b195112ac2452d7
Giuseppe Digilio <giuseppe.digilio@4science.it>
2017-11-06 09:27:04 +0100

    Remove form test page

--- src/app/+form-page/app.validators.ts
+++
@@ -1,6 +0,0 @@
-import { AbstractControl, ValidationErrors } from '@angular/forms';
-
-export function customValidator(control: AbstractControl): ValidationErrors | null {
-  const hasError = control.value ? (control.value as string).startsWith('abc') : false;
-  return hasError ? {customValidator: true} : null;
-}
