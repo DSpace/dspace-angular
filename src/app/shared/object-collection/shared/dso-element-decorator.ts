806b7bc5fc0cf82320049b970c08fb30f78ba305
Jonas Van Goolen <jonas@atmire.com>
2017-11-21 13:57:49 +0100

    150 Separate maps per viewMode

--- src/app/shared/object-collection/shared/dso-element-decorator.ts
+++ src/app/shared/object-collection/shared/dso-element-decorator.ts
@@ -3,15 +3,18 @@ import { ListableObject } from './listable-object.model';
 import { ViewMode } from '../../../+search-page/search-options.model';
 
 const dsoElementMap = new Map();
-export function renderElementsFor(listable: GenericConstructor<ListableObject>, viewMode : ViewMode) {
+export function renderElementsFor(listable: GenericConstructor<ListableObject>, viewMode: ViewMode) {
   return function decorator(objectElement: any) {
     if (!objectElement) {
       return;
     }
-    dsoElementMap.set(listable+viewMode, objectElement);
+    if (!dsoElementMap.get(viewMode)) {
+      dsoElementMap.set(viewMode, new Map());
+    }
+    dsoElementMap.get(viewMode).set(listable, objectElement);
   };
 }
 
-export function rendersDSOType(listable: GenericConstructor<ListableObject>, viewMode : ViewMode) {
-  return dsoElementMap.get(listable+viewMode);
+export function rendersDSOType(listable: GenericConstructor<ListableObject>, viewMode: ViewMode) {
+  return dsoElementMap.get(viewMode).get(listable);
 }
