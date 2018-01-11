6993afb7738e64fe4b22c4f9658241afeb737a44
Giuseppe Digilio <giuseppe.digilio@4science.it>
2018-01-11 09:47:46 +0100

    fix dynamic-group.components.ts

--- src/app/shared/form/builder/ds-dynamic-form-ui/models/ds-dynamic-group/dynamic-group.components.ts
+++ src/app/shared/form/builder/ds-dynamic-form-ui/models/ds-dynamic-group/dynamic-group.components.ts
@@ -4,9 +4,7 @@ import { FormGroup } from '@angular/forms';
 import { FormBuilderService } from '../../../form-builder.service';
 import { DynamicFormControlModel, DynamicFormGroupModel, DynamicInputModel } from '@ng-dynamic-forms/core';
 import { SubmissionFormsModel } from '../../../../../../core/shared/config/config-submission-forms.model';
-import { AuthorityModel } from '../../../../../../core/integration/models/authority.model';
 import { FormService } from '../../../../form.service';
-import { Chips } from '../../../../../chips/chips.model';
 import { FormComponent } from '../../../../form.component';
 
 const PLACEHOLDER = '#PLACEHOLDER_PARENT_METADATA_VALUE#';
@@ -36,8 +34,6 @@ export class DsDynamicGroupComponent implements OnInit {
   }
 
   ngOnInit() {
-    console.log('FormConfiguration...');
-    console.log(this.model.formConfiguration);
     const config = {rows: this.model.formConfiguration} as SubmissionFormsModel;
     this.formId = this.formService.getUniqueId(this.model.id);
     this.formModel = this.formBuilderService.modelFromConfiguration(config, {});
@@ -60,23 +56,12 @@ export class DsDynamicGroupComponent implements OnInit {
 
     this.model.chips.add(item);
     this.model.valueUpdates.next(this.model.chips.getItems());
+    // this.formRef.formGroup.reset();
     this.change.emit(event);
-
-    this.formRef.formGroup.reset();
-    /*setTimeout(() => {
-      // Reset the input text after x ms, mandatory or the formatter overwrite it
-      const keys = Object.keys(this.group.controls); // df-row-group-config-18
-      // (this.group.controls[keys[0]] as FormGroup).controls[AUTHOR_KEY].patchValue(null);
-      this.formRef.formGroup.reset();
-    }, 50);
-*/
-    console.log(this.model.chips.getItems());
+    this.formBuilderService.reset(this.formModel);
   }
 
   chipsSelected(event) {
-    console.log('Selected chips : ' + JSON.stringify(this.model.chips.chipsItems[event]));
-    console.log(event);
-
     const selected = this.model.chips.chipsItems[event].item;
     this.formModel.forEach((row) => {
       const modelRow = row as DynamicFormGroupModel;
@@ -95,7 +80,6 @@ export class DsDynamicGroupComponent implements OnInit {
 
   exitEditMode() {
     const keys = Object.keys(this.group.controls);
-    console.log(keys);
 
     // Set ChipsItem's editModel=false
     const item = {};
@@ -145,12 +129,6 @@ export class DsDynamicGroupComponent implements OnInit {
   }
 
   onChange(event) {
-    // AuthorityModel
-    // display: "Salz, Dirk"
-    // id: "no2015021623"
-    // value: "Salz, Dirk"
-    console.log(event);
-    console.log(event.$event);
     this.change.emit(event);
   }
 
