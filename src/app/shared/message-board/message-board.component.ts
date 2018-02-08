16e8edfafb802f1efde66c4fb44d454dfe2ff0b5
Andrea Chiapparelli - 4Science <andrea.chiapparelli@gmail.com>
2018-02-08 17:23:42 +0100

    tmp with messages mock

---
+++ src/app/shared/message-board/message-board.component.ts
@@ -0,0 +1,47 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Bitstream} from '../../core/shared/bitstream.model';
+
+@Component({
+  selector: 'ds-message-board',
+  styleUrls: ['./message-board.component.scss'],
+  templateUrl: './message-board.component.html'
+})
+
+export class MessageBoardComponent {
+  @Input()
+  public messages: Bitstream[];
+  @Output()
+  public close = new EventEmitter<any>();
+  public show = [];
+  public isCreator: boolean;
+
+  ngOnInit() {
+    this.messages.forEach((m) => {
+      this.show.push(false);
+    });
+
+    // TODO Check if actual user is the creator
+
+    // TODO Mark as read only when other read, not the writer
+
+  }
+
+  closeDashboard($event) {
+    this.close.emit(event);
+  }
+
+  toggleDescription(i: number) {
+    this.show[i] = !this.show[i];
+  }
+
+}
+
+interface DsMessage {
+  uuid: string, // bitstream message id
+  title: string,
+  description: string,
+  sent?: string,
+  read?: string, // NULL if not read
+  creatorName?: string, // String, not id
+  type?: string, // INBOUND, OUTBOUND
+}
--- src/app/shared/object-list/my-dspace-result-list-element/wsi-my-dspace-result/wsi-my-dspace-result-list-element.component.ts
+++ src/app/shared/object-list/my-dspace-result-list-element/wsi-my-dspace-result/wsi-my-dspace-result-list-element.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 
 import { renderElementsFor } from '../../../object-collection/shared/dso-element-decorator';
 import { MyDSpaceResultListElementComponent, } from '../my-dspace-result-list-element.component';
@@ -8,30 +8,74 @@ import { WorkspaceitemMyDSpaceResult } from '../../../object-collection/shared/w
 import { Item } from '../../../../core/shared/item.model';
 import { RemoteData } from '../../../../core/data/remote-data';
 import { Observable } from 'rxjs/Observable';
-import { hasNoUndefinedValue, hasNoValue, isEmpty, isNotEmpty, isNotUndefined } from '../../../empty.util';
+import { hasNoUndefinedValue, hasNoValue, isEmpty } from '../../../empty.util';
 import { Metadatum } from '../../../../core/shared/metadatum.model';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ListableObject } from '../../../object-collection/shared/listable-object.model';
+
+import * as data from '../../../../../backend/data/bitstream-messages.json';
+import { Bitstream } from '../../../../core/shared/bitstream.model';
+import { PageInfo } from '../../../../core/shared/page-info.model';
+import { PaginatedList } from '../../../../core/data/paginated-list';
 
 @Component({
   selector: 'ds-workspaceitem-my-dspace-result-list-element',
   styleUrls: ['../my-dspace-result-list-element.component.scss', './wsi-my-dspace-result-list-element.component.scss'],
-  templateUrl: './wsi-my-dspace-result-list-element.component.html'
+  templateUrl: './wsi-my-dspace-result-list-element.component.html',
 })
 
 @renderElementsFor(WorkspaceitemMyDSpaceResult, ViewMode.List)
 @renderElementsFor(Workspaceitem, ViewMode.List)
 export class WorkspaceitemMyDSpaceResultListElementComponent extends MyDSpaceResultListElementComponent<WorkspaceitemMyDSpaceResult, Workspaceitem> {
   public item: Item;
+  public messages: Bitstream[] = [];
+  public showMessageBoard = false;
+  public unRead = 0;
+
+  // public constructor(private modalService: NgbModal, @Inject('objectElementProvider') public listable: ListableObject) {
+  //   super(listable);
+  // }
 
   ngOnInit() {
     (this.dso.item as Observable<RemoteData<Item[]>>)
       .filter((rd: RemoteData<any>) => ((!rd.isRequestPending) && hasNoUndefinedValue(rd.payload)))
       .first()
       .subscribe((rd: RemoteData<any>) => {
-      this.item = rd.payload[0];
-    })
+        this.item = rd.payload[0];
+      });
+
+    // TODO REMOVE WHEN BACK-END WILL MANAGE MESSAGE
+    const bitstreams = 'bitstreams';
+    const messages: Bitstream[] = data[bitstreams];
+    this.item.bitstreams = Observable.of(new RemoteData(
+      false,
+      false,
+      true,
+      undefined,
+      messages));
+    // TODO END REMOVE
+
+    const messagesObs = this.item.getBitstreamsByBundleName('MESSAGE');
+    // TODO Test.... later here I must to create this.messages here
+    messagesObs
+    // .filter()
+      .take(1)
+      .subscribe((m) => {
+        // console.log(m);
+      });
+
+    messages.forEach((m) => {
+      const b = Object.assign(new Bitstream(), m);
+      this.messages.push(b);
+      const accepted = b.findMetadata('dc.date.accepted');
+      if (!accepted || accepted.length === 0) {
+        this.unRead++;
+      }
+    });
+
   }
 
-  getTitle():string {
+  getTitle(): string {
     return this.item.findMetadata('dc.title');
   }
 
@@ -63,7 +107,7 @@ export class WorkspaceitemMyDSpaceResultListElementComponent extends MyDSpaceRes
     this.object.hitHighlights.some(
       (md: Metadatum) => {
         if (key === md.key) {
-          result =  md.value;
+          result = md.value;
           return true;
         }
       }
@@ -73,4 +117,33 @@ export class WorkspaceitemMyDSpaceResultListElementComponent extends MyDSpaceRes
     }
     return result;
   }
+
+  openMessageBoard() {
+    // this.modalService.open('test');
+    this.showMessageBoard = true;
+  }
+
+  hideMessageBoard() {
+    this.showMessageBoard = false;
+  }
+
+// CREAZIONE messaggio
+// - POST /api/messages
+// argomenti
+// - uuid item
+// - subject
+// - description
+//
+// PRESA visione
+// - POST /api/messages/read
+// argomenti
+// - uuid bitsream
+//
+// CANCELLA visione
+// - POST /api/messages/unread
+// argomenti
+// - uuid bitsream
+
 }
+
+
