64ad40b67639d192eec74d2bff317149b4e49296
Giuseppe Digilio <giuseppe.digilio@4science.it>
2018-02-09 12:27:20 +0100

    Added cookie service

---
+++ src/app/shared/services/cookie.service.spec.ts
@@ -0,0 +1,30 @@
+import { PlatformService } from './platform.service'
+import { CookieService, ICookieService } from './cookie.service'
+import { async, TestBed } from '@angular/core/testing'
+import { REQUEST } from '@nguniversal/express-engine/tokens'
+
+describe(CookieService.name, () => {
+  let service: ICookieService;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        CookieService,
+        PlatformService,
+        { provide: REQUEST, useValue: {} }
+      ]
+    })
+  }));
+
+  beforeEach(() => {
+    service = TestBed.get(CookieService)
+  });
+
+  afterEach(() => {
+    TestBed.resetTestingModule()
+  });
+
+  it('should construct', async(() => {
+    expect(service).toBeDefined()
+  }))
+});
