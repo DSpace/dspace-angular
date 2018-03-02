82d504e5803f6fd7da7ed442ec25a42025c7a022
Giuseppe Digilio <giuseppe.digilio@4science.it>
2018-03-02 09:07:15 +0100

    Reverted home-page.component

--- src/app/+home-page/home-page.component.ts
+++ src/app/+home-page/home-page.component.ts
@@ -8,14 +8,4 @@ import { PlatformService } from '../shared/services/platform.service';
   templateUrl: './home-page.component.html'
 })
 export class HomePageComponent {
-  constructor(private gs: GroupEpersonService, private ps: PlatformService) {}
-
-  ngOnInit() {
-    if (this.ps.isBrowser) {
-      this.gs.getDataByUuid('c849bed6-5f55-49c3-a8bb-e998a1145e30')
-        .subscribe((gg) => {
-          console.log(gg);
-        })
-    }
-  }
 }
