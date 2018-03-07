98c24bf190a1c061686f5d88aeb952fbc90b8955
Giuseppe Digilio <giuseppe.digilio@4science.it>
2018-03-07 12:37:01 +0100

    reverted home-page.module

--- src/app/+home-page/home-page.module.ts
+++ src/app/+home-page/home-page.module.ts
@@ -6,8 +6,6 @@ import { HomePageRoutingModule } from './home-page-routing.module';
 
 import { HomePageComponent } from './home-page.component';
 import { TopLevelCommunityListComponent } from './top-level-community-list/top-level-community-list.component';
-import { NotificationComponent } from '../shared/notifications/notification/notification.component';
-import { NotificationsBoardComponent } from '../shared/notifications/notifications-board/notifications-board.component';
 
 @NgModule({
   imports: [
@@ -19,8 +17,6 @@ import { NotificationsBoardComponent } from '../shared/notifications/notificatio
     HomePageComponent,
     TopLevelCommunityListComponent,
     HomeNewsComponent,
-    // NotificationComponent,
-    // NotificationsBoardComponent
   ]
 })
 export class HomePageModule {
