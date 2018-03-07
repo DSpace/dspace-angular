8bfa48467a1256dc83c202549205c5607f96af15
Giuseppe Digilio <giuseppe.digilio@4science.it>
2018-03-07 12:10:11 +0100

    reverted home-page.component

--- src/app/+home-page/home-page.component.ts
+++ src/app/+home-page/home-page.component.ts
@@ -1,6 +1,4 @@
-import { Component, TemplateRef, ViewChild } from '@angular/core';
-import { NotificationsService } from '../shared/notifications/notifications.service';
-import { NotificationOptions } from '../shared/notifications/models/notification-options.model';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'ds-home-page',
@@ -8,33 +6,4 @@ import { NotificationOptions } from '../shared/notifications/models/notification
   templateUrl: './home-page.component.html'
 })
 export class HomePageComponent {
-  @ViewChild('example') example: TemplateRef<any>;
-
-  private htmlNotification;
-
-  constructor(private notificationsService: NotificationsService) {
-  }
-
-  createNotification() {
-    const n1 = this.notificationsService.success('Welcome in DSpace', 'Good choice!',
-      new NotificationOptions(10000, true, 'fromLeft', ['bottom', 'left']));
-  }
-
-  createNotification1() {
-    const n1 = this.notificationsService.warning('Welcome in DSpace', 'Good choice!',
-      new NotificationOptions(10000, false, 'fromLeft', ['bottom', 'left']));
-  }
-
-  createNotification2() {
-    const html = "<h1>Html Alert</h1> <p><button [routerLink]=\"['/mydspace']\">Go to mydspace</button></p>";
-    this.htmlNotification = this.notificationsService.info('Ciao', null, null, html);
-  }
-
-  deleteHtmlNotification() {
-    this.notificationsService.remove(this.htmlNotification);
-  }
-
-  deleteAllNotifications() {
-    this.notificationsService.removeAll();
-  }
 }
