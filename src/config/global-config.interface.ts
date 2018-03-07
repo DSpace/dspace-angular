fad724454e87b7fbf58ab42ad238d41ed5a1ac7a
Andrea Chiapparelli - 4Science <andrea.chiapparelli@gmail.com>
2018-03-07 18:26:10 +0100

    fix for watch:prod

--- src/config/global-config.interface.ts
+++ src/config/global-config.interface.ts
@@ -3,6 +3,7 @@ import { ServerConfig } from './server-config.interface';
 import { CacheConfig } from './cache-config.interface';
 import { UniversalConfig } from './universal-config.interface';
 import { SubmissionConfig } from './submission-config.interface';
+import { INotificationBoardOptions } from './notifications-config.interfaces';
 
 export interface GlobalConfig extends Config {
   ui: ServerConfig;
@@ -10,6 +11,7 @@ export interface GlobalConfig extends Config {
   production: boolean;
   cache: CacheConfig;
   submission: SubmissionConfig;
+  notifications: INotificationBoardOptions;
   universal: UniversalConfig;
   logDirectory: string;
   debug: boolean;
