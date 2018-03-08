d5ee595f8fdc20d2e339fd7b1f902b08285bcf6c
Giuseppe Digilio <giuseppe.digilio@4science.it>
2018-03-08 10:35:37 +0100

    Reverted home-page.component

--- src/app/+home-page/home-page.component.ts
+++ src/app/+home-page/home-page.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'ds-home-page',
   styleUrls: ['./home-page.component.scss'],
   templateUrl: './home-page.component.html'
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent {
 }
