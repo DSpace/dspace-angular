60c1de62d5f4a4c9819da98dd491f38809cbc317
Andrea Chiapparelli - 4Science <andrea.chiapparelli@gmail.com>
2018-03-19 17:43:01 +0100

    save for later

--- src/app/submission/utils/parseSectionErrorPaths.ts
+++ src/app/submission/utils/parseSectionErrorPaths.ts
@@ -5,6 +5,7 @@ export interface SectionErrorPath {
 }
 
 const regex = /\/sections\/(.*)\/(.*)/;
+const regexShort = /\/sections\/(.*)/;
 
 /**
  * the following method accept an array of section path strings and return a path object
@@ -12,13 +13,25 @@ const regex = /\/sections\/(.*)\/(.*)/;
  * @returns {SectionErrorPath[]}
  */
 const parseSectionErrorPaths = (path: string | string[]): SectionErrorPath[] => {
-  const paths = typeof path === 'string' ? [ path ] : path;
+  const paths = typeof path === 'string' ? [path] : path;
 
-  return paths.map((item) => ({
-    sectionId: item.match(regex)[ 1 ],
-    fieldId: item.match(regex)[ 2 ],
-    originalPath: item,
-  }))
+  return paths.map((item) => {
+
+      if (item.match(regex) && item.match(regex).length === 3) {
+        return {
+          sectionId: item.match(regex)[1],
+          fieldId: item.match(regex)[2],
+          originalPath: item,
+        };
+      } else {
+        return {
+          sectionId: item.match(regexShort)[1],
+          originalPath: item,
+        };
+      }
+
+    }
+  );
 };
 
 export default parseSectionErrorPaths;
