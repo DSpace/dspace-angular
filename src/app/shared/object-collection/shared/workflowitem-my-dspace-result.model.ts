79c6b0cc5f3a64f583ada467495ff836a8da66a4
Giuseppe Digilio <giuseppe.digilio@4science.it>
2018-03-20 16:50:03 +0100

    Fixes

--- src/app/shared/object-collection/shared/workflowitem-my-dspace-result.model.ts
+++ src/app/shared/object-collection/shared/workflowitem-my-dspace-result.model.ts
@@ -3,6 +3,6 @@ import { SearchResult } from '../../../+search-page/search-result.model';
 import { MyDSpaceConfigurationType } from '../../../+my-dspace-page/mydspace-configuration-type';
 import { searchResultFor } from '../../../+search-page/search-service/search-result-element-decorator';
 
-@searchResultFor(Workflowitem, MyDSpaceConfigurationType.Workflow)
+@searchResultFor(Workflowitem, MyDSpaceConfigurationType.Workspace)
 export class WorkflowitemMyDSpaceResult extends SearchResult<Workflowitem> {
 }
