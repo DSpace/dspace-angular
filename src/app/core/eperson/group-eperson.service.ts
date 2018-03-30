dc5ffaacedd1cab1de9bc5ca455577cf0eb650cf
Giuseppe Digilio <giuseppe.digilio@4science.it>
2018-03-30 19:39:33 +0200

    Determinate submitter and controller status from rest endpoint

--- src/app/core/eperson/group-eperson.service.ts
+++ src/app/core/eperson/group-eperson.service.ts
@@ -5,6 +5,12 @@ import { ResponseCacheService } from '../cache/response-cache.service';
 import { RequestService } from '../data/request.service';
 import { GLOBAL_CONFIG } from '../../../config';
 import { GlobalConfig } from '../../../config/global-config.interface';
+import { isNotEmpty } from '../../shared/empty.util';
+import { EpersonRequest, GetRequest } from '../data/request.models';
+import { EpersonData } from './eperson-data';
+import { EpersonSuccessResponse, ErrorResponse, RestResponse } from '../cache/response-cache.models';
+import { Observable } from 'rxjs/Observable';
+import { ResponseCacheEntry } from '../cache/response-cache.reducer';
 
 @Injectable()
 export class GroupEpersonService extends EpersonService {
@@ -16,5 +22,33 @@ export class GroupEpersonService extends EpersonService {
     protected requestService: RequestService,
     @Inject(GLOBAL_CONFIG) protected EnvConfig: GlobalConfig) {
     super();
+  }
+
+  protected getSearchHref(endpoint, groupName): string {
+    return `${endpoint}/search/isMemberOf?groupName=${groupName}`;
+  }
+
+  isMemberOf(groupName: string) {
+    return this.getEndpoint()
+      .map((endpoint: string) => this.getSearchHref(endpoint, groupName))
+      .filter((href: string) => isNotEmpty(href))
+      .distinctUntilChanged()
+      .map((endpointURL: string) => new EpersonRequest(this.requestService.generateRequestId(), endpointURL))
+      .do((request: GetRequest) => this.requestService.configure(request))
+      .flatMap((request: GetRequest) => this.getSearch(request))
+      .distinctUntilChanged();
+  }
+
+  protected getSearch(request: GetRequest): Observable<EpersonData> {
+    const [successResponse, errorResponse] =  this.responseCache.get(request.href)
+      .map((entry: ResponseCacheEntry) => entry.response)
+      .partition((response: RestResponse) => response.isSuccessful);
+    return Observable.merge(
+      errorResponse.flatMap((response: ErrorResponse) =>
+        Observable.of(new EpersonData(undefined, undefined))),
+      successResponse
+        .filter((response: EpersonSuccessResponse) => isNotEmpty(response))
+        .map((response: EpersonSuccessResponse) => new EpersonData(response.pageInfo, response.epersonDefinition))
+        .distinctUntilChanged());
   }
 }
