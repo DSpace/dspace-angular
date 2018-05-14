0827208c583818bec3b3f5197b2eb046e4626f0b
Michael Marttila <michael.marttila@georgetown.edu>
2018-05-14 09:39:00 -0400

    Spacing style fix

--- src/app/shared/mocks/mock-admin-guard.service.ts
+++ src/app/shared/mocks/mock-admin-guard.service.ts
@@ -6,18 +6,18 @@ import { hasValue } from '../empty.util';
 @Injectable()
 export class MockAdminGuard implements CanActivate, CanActivateChild {
 
-	canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-		//if being run in browser, enforce 'isAdmin' requirement
-		if (typeof window === 'object' && hasValue(window.localStorage)) {
-			if (window.localStorage.getItem('isAdmin') === 'true') {
-				return true;
-			}
-			return false;
-		}
-		return true;
-	}
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    // if being run in browser, enforce 'isAdmin' requirement
+    if (typeof window === 'object' && hasValue(window.localStorage)) {
+      if (window.localStorage.getItem('isAdmin') === 'true') {
+        return true;
+      }
+      return false;
+    }
+    return true;
+  }
 
-	canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-		return this.canActivate(route, state);
-	}
+  canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    return this.canActivate(route, state);
+  }
 }
