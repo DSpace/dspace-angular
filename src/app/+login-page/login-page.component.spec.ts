4fc76139b9dad9d178226251f1c849eebce9d815
Giuseppe Digilio <giuseppe.digilio@4science.it>
2018-05-15 18:12:35 +0200

    Added tests

---
+++ src/app/+login-page/login-page.component.spec.ts
@@ -0,0 +1,47 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Store } from '@ngrx/store';
+import { TranslateModule } from '@ngx-translate/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/Observable/of';
+
+import { LoginPageComponent } from './login-page.component';
+
+fdescribe('LoginPageComponent', () => {
+  let comp: LoginPageComponent;
+  let fixture: ComponentFixture<LoginPageComponent>;
+
+  const store: Store<LoginPageComponent> = jasmine.createSpyObj('store', {
+    /* tslint:disable:no-empty */
+    dispatch: {},
+    /* tslint:enable:no-empty */
+    select: Observable.of(true)
+  });
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        TranslateModule.forRoot()
+      ],
+      declarations: [LoginPageComponent],
+      providers: [
+        {
+          provide: Store, useValue: store
+        }
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoginPageComponent);
+    comp = fixture.componentInstance; // SearchPageComponent test instance
+    fixture.detectChanges();
+  });
+
+  it('should create instance', () => {
+    expect(comp).toBeDefined()
+  });
+
+});
