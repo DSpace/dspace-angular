7d8324d7015afe535d276313466f4f1747daaec3
Giuseppe Digilio <giuseppe.digilio@4science.it>
2018-05-24 11:34:28 +0200

    Test to fix travis build issue

--- src/app/+search-page/search-options.model.ts
+++ src/app/+search-page/search-options.model.ts
@@ -1,5 +1,6 @@
 import { isNotEmpty } from '../shared/empty.util';
 import { URLCombiner } from '../core/url-combiner/url-combiner';
+import 'core-js/library/fn/object/entries';
 
 export enum ViewMode {
   List = 'list',
