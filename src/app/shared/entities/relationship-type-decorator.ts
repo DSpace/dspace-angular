26098c6bd07bf816b5f6ad74809c269ea5afd003
Art Lowel <art@atmire.com>
2018-05-29 10:45:30 +0200

    Throw an error when there are multiple components to render the same type/viewmode combination

--- src/app/shared/entities/relationship-type-decorator.ts
+++ src/app/shared/entities/relationship-type-decorator.ts
@@ -1,4 +1,4 @@
-import { hasNoValue } from '../empty.util';
+import { hasNoValue, hasValue } from '../empty.util';
 import { ElementViewMode } from '../view-mode';
 
 export const DEFAULT_RELATIONSHIP_TYPE = 'Default';
@@ -8,6 +8,9 @@ export function rendersRelationshipType(type: string, viewMode: ElementViewMode)
   return function decorator(component: any) {
     if (hasNoValue(map.get(viewMode))) {
       map.set(viewMode, new Map());
+    }
+    if (hasValue(map.get(viewMode).get(type))) {
+      throw new Error(`There can't be more than one component to render Items of type "${type}" in view mode "${viewMode}"`);
     }
     map.get(viewMode).set(type, component);
   };
