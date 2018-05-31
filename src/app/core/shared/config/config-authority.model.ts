dc7cc9cdb8c9ba193b8a0c0f388046ac4ebf531b
Giuseppe Digilio <giuseppe.digilio@4science.it>
2018-05-31 17:59:30 +0200

    Fixes

--- src/app/core/shared/config/config-authority.model.ts
+++ src/app/core/shared/config/config-authority.model.ts
@@ -1,6 +1,7 @@
 import { autoserialize, autoserializeAs, inheritSerialization } from 'cerialize';
 import { ConfigObject } from './config.model';
 import { SubmissionSectionModel } from './config-submission-section.model';
+import { isNotEmpty } from '../../../shared/empty.util';
 
 @inheritSerialization(ConfigObject)
 export class ConfigAuthorityModel extends ConfigObject {
@@ -20,4 +21,7 @@ export class ConfigAuthorityModel extends ConfigObject {
   @autoserialize
   language: string;
 
+  hasValue(): boolean {
+    return isNotEmpty(this.value);
+  }
 }
