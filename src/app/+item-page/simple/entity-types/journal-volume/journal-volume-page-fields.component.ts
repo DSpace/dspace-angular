c99d05c5f98d3d8b6b17459fb31e98f2a71201a3
Kristof De Langhe <kristof.delanghe@atmire.com>
2018-05-31 14:02:04 +0200

    52211: Item display regular metadata journal-types

---
+++ src/app/+item-page/simple/entity-types/journal-volume/journal-volume-page-fields.component.ts
@@ -0,0 +1,30 @@
+import { Component, Inject } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { ItemDataService } from '../../../../core/data/item-data.service';
+import { Item } from '../../../../core/shared/item.model';
+import { rendersEntityType } from '../../../../shared/entities/entity-type-decorator';
+import { ITEM } from '../../../../shared/entities/switcher/entity-type-switcher.component';
+import { ElementViewMode } from '../../../../shared/view-mode';
+import {
+  EntityPageFieldsComponent, filterRelationsByTypeLabel,
+  relationsToItems
+} from '../shared/entity-page-fields.component';
+
+@rendersEntityType('JournalVolume', ElementViewMode.Full)
+@Component({
+  selector: 'ds-journal-volume-page-fields',
+  styleUrls: ['./journal-volume-page-fields.component.scss'],
+  templateUrl: './journal-volume-page-fields.component.html'
+})
+export class JournalVolumePageFieldsComponent extends EntityPageFieldsComponent {
+
+  constructor(
+    @Inject(ITEM) public item: Item,
+    private ids: ItemDataService
+  ) {
+    super(item);
+  }
+  ngOnInit(): void {
+    super.ngOnInit();
+  }
+}
