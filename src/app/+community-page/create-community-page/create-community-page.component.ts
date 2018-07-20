819610476ef91fcee5748d3f49788ce79e2c86c6
Kristof De Langhe <kristof.delanghe@atmire.com>
2018-07-20 16:04:02 +0200

    54472: Intermediate commit

---
+++ src/app/+community-page/create-community-page/create-community-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ds-create-community',
+  templateUrl: './create-community-page.component.html'
+})
+export class CreateCommunityPageComponent {
+
+}
