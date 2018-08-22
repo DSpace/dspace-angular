d9cd292ec179c4320702640b673271e454d6a22f
lotte <lotte_hofstede@hotmail.com>
2018-08-22 09:26:31 +0200

    Revert "added preloading guard for search results"
    This reverts commit d9e70bd4daf0afcf8f59d5f9961b61d9948ccb55.

--- src/app/+search-page/search-page-routing.module.ts
+++ src/app/+search-page/search-page-routing.module.ts
@@ -2,22 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { SearchPageComponent } from './search-page.component';
-import { SearchPagePreloader } from './search-page-preloader.guard';
 
 @NgModule({
   imports: [
     RouterModule.forChild([
-      {
-        path: '',
-        component: SearchPageComponent,
-        data: { title: 'search.title' },
-        canActivate: [SearchPagePreloader]
-      }
+      { path: '', component: SearchPageComponent, data: { title: 'search.title' } }
     ])
-  ],
-  providers: [
-    SearchPagePreloader
   ]
 })
-export class SearchPageRoutingModule {
-}
+export class SearchPageRoutingModule { }
