363e1459cc8894be9c21614ae6ea4f60b80905d9
Kristof De Langhe <kristof.delanghe@atmire.com>
2018-09-18 11:22:41 +0200

    55558: RouteService tests fix

--- src/app/shared/testing/router-stub.ts
+++ src/app/shared/testing/router-stub.ts
@@ -1,9 +1,11 @@
+import { Observable } from 'rxjs/Observable';
 
 export class RouterStub {
   url: string;
   //noinspection TypeScriptUnresolvedFunction
   navigate = jasmine.createSpy('navigate');
   parseUrl = jasmine.createSpy('parseUrl');
+  events = Observable.of({});
   navigateByUrl(url): void {
     this.url = url;
   }
