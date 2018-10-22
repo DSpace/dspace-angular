221e20f495d12da4ecfaa09dbaf305a6eb7ec378
Kristof De Langhe <kristof.delanghe@atmire.com>
2018-10-22 17:51:51 +0200

    56434: ItemPageFieldComponent tests

---
+++ src/app/+item-page/simple/field-components/specific-field/author/item-page-author-field.component.spec.ts
@@ -0,0 +1,45 @@
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { ChangeDetectionStrategy, NO_ERRORS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MockTranslateLoader } from '../../../../../shared/testing/mock-translate-loader';
+import { MetadataValuesComponent } from '../../../../field-components/metadata-values/metadata-values.component';
+import { mockItemWithMetadataFieldAndValue } from '../item-page-field.component.spec';
+import { ItemPageAuthorFieldComponent } from './item-page-author-field.component';
+
+let comp: ItemPageAuthorFieldComponent;
+let fixture: ComponentFixture<ItemPageAuthorFieldComponent>;
+
+const mockFields = ['dc.contributor.author', 'dc.creator', 'dc.contributor'];
+const mockValue = 'test value';
+
+describe('ItemPageAuthorFieldComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useClass: MockTranslateLoader
+        }
+      })],
+      declarations: [ItemPageAuthorFieldComponent, MetadataValuesComponent],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).overrideComponent(ItemPageAuthorFieldComponent, {
+      set: {changeDetection: ChangeDetectionStrategy.Default}
+    }).compileComponents();
+  }));
+
+  for (const field of mockFields) {
+    beforeEach(async(() => {
+      fixture = TestBed.createComponent(ItemPageAuthorFieldComponent);
+      comp = fixture.componentInstance;
+      comp.item = mockItemWithMetadataFieldAndValue(field, mockValue);
+      fixture.detectChanges();
+    }));
+
+    describe(`when the item contains metadata for ${field}`, () => {
+      it('should display display the correct metadata value', () => {
+        expect(fixture.nativeElement.innerHTML).toContain(mockValue);
+      });
+    });
+  }
+});
