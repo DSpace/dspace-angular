f75f5e9be7e4ccd5bb53898233b5481b46ce3aba
Yana De Pauw <yana@atmire.com>
2018-12-18 10:06:01 +0100

    57498: Delete item cleanup

--- src/app/+item-page/edit-item-page/item-delete/item-delete.component.ts
+++ src/app/+item-page/edit-item-page/item-delete/item-delete.component.ts
@@ -2,11 +2,7 @@ import {Component} from '@angular/core';
 import {first} from 'rxjs/operators';
 import {RestResponse} from '../../../core/cache/response-cache.models';
 import {AbstractSimpleItemActionComponent} from '../simple-item-action/abstract-simple-item-action.component';
-import {RemoteData} from '../../../core/data/remote-data';
-import {Item} from '../../../core/shared/item.model';
-import {findSuccessfulAccordingTo} from '../edit-item-operators';
 import {getItemEditPath} from '../../item-page-routing.module';
-import {getCommunityModulePath} from '../../../app-routing.module';
 
 @Component({
   selector: 'ds-item-delete',
@@ -37,10 +33,11 @@ export class ItemDeleteComponent extends AbstractSimpleItemActionComponent {
    */
   processRestResponse(response: RestResponse) {
     if (response.isSuccessful) {
-        this.notificationsService.success(this.translateService.get('item.edit.' + this.messageKey + '.success'));
-        this.router.navigate(['']);
+      this.notificationsService.success(this.translateService.get('item.edit.' + this.messageKey + '.success'));
+      this.router.navigate(['']);
     } else {
       this.notificationsService.error(this.translateService.get('item.edit.' + this.messageKey + '.error'));
       this.router.navigate([getItemEditPath(this.item.id)]);
-    }  }
+    }
+  }
 }
