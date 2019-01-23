4e0678a0bdec1a2ef04fe4d1afdb9ce74994236f
Kristof De Langhe <kristof.delanghe@atmire.com>
2019-01-23 14:42:23 +0100

    58789: MetadataRegistryComponent tests

--- src/app/+admin/admin-registries/metadata-registry/metadata-registry.component.spec.ts
+++ src/app/+admin/admin-registries/metadata-registry/metadata-registry.component.spec.ts
@@ -1,5 +1,5 @@
 import { MetadataRegistryComponent } from './metadata-registry.component';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, fakeAsync, inject, TestBed, tick } from '@angular/core/testing';
 import { of as observableOf } from 'rxjs';
 import { RemoteData } from '../../../core/data/remote-data';
 import { PaginatedList } from '../../../core/data/paginated-list';
@@ -13,8 +13,11 @@ import { EnumKeysPipe } from '../../../shared/utils/enum-keys-pipe';
 import { PaginationComponent } from '../../../shared/pagination/pagination.component';
 import { HostWindowServiceStub } from '../../../shared/testing/host-window-service-stub';
 import { HostWindowService } from '../../../shared/host-window.service';
+import { ChangeDetectionStrategy, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NotificationsService } from '../../../shared/notifications/notifications.service';
+import { NotificationsServiceStub } from '../../../shared/testing/notifications-service-stub';
 
-describe('MetadataRegistryComponent', () => {
+fdescribe('MetadataRegistryComponent', () => {
   let comp: MetadataRegistryComponent;
   let fixture: ComponentFixture<MetadataRegistryComponent>;
   let registryService: RegistryService;
@@ -33,9 +36,15 @@ describe('MetadataRegistryComponent', () => {
     }
   ];
   const mockSchemas = observableOf(new RemoteData(false, false, true, undefined, new PaginatedList(null, mockSchemasList)));
+  /* tslint:disable:no-empty */
   const registryServiceStub = {
-    getMetadataSchemas: () => mockSchemas
+    getMetadataSchemas: () => mockSchemas,
+    getActiveMetadataSchema: () => observableOf(undefined),
+    getSelectedMetadataSchemas: () => observableOf([]),
+    editMetadataSchema: (schema) => {},
+    cancelEditMetadataSchema: () => {}
   };
+  /* tslint:enable:no-empty */
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -43,8 +52,12 @@ describe('MetadataRegistryComponent', () => {
       declarations: [MetadataRegistryComponent, PaginationComponent, EnumKeysPipe],
       providers: [
         { provide: RegistryService, useValue: registryServiceStub },
-        { provide: HostWindowService, useValue: new HostWindowServiceStub(0) }
-      ]
+        { provide: HostWindowService, useValue: new HostWindowServiceStub(0) },
+        { provide: NotificationsService, useValue: NotificationsServiceStub }
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).overrideComponent(MetadataRegistryComponent, {
+      set: { changeDetection: ChangeDetectionStrategy.Default }
     }).compileComponents();
   }));
 
@@ -52,8 +65,11 @@ describe('MetadataRegistryComponent', () => {
     fixture = TestBed.createComponent(MetadataRegistryComponent);
     comp = fixture.componentInstance;
     fixture.detectChanges();
-    registryService = (comp as any).service;
   });
+
+  beforeEach(inject([RegistryService], (s) => {
+    registryService = s;
+  }));
 
   it('should contain two schemas', () => {
     const tbody: HTMLElement = fixture.debugElement.query(By.css('#metadata-schemas>tbody')).nativeElement;
@@ -61,10 +77,37 @@ describe('MetadataRegistryComponent', () => {
   });
 
   it('should contain the correct schemas', () => {
-    const dcName: HTMLElement = fixture.debugElement.query(By.css('#metadata-schemas tr:nth-child(1) td:nth-child(3)')).nativeElement;
+    const dcName: HTMLElement = fixture.debugElement.query(By.css('#metadata-schemas tr:nth-child(1) td:nth-child(4)')).nativeElement;
     expect(dcName.textContent).toBe('dc');
 
-    const mockName: HTMLElement = fixture.debugElement.query(By.css('#metadata-schemas tr:nth-child(2) td:nth-child(3)')).nativeElement;
+    const mockName: HTMLElement = fixture.debugElement.query(By.css('#metadata-schemas tr:nth-child(2) td:nth-child(4)')).nativeElement;
     expect(mockName.textContent).toBe('mock');
   });
+
+  describe('when clicking a metadata schema row', () => {
+    let row: HTMLElement;
+
+    beforeEach(() => {
+      spyOn(registryService, 'editMetadataSchema');
+      row = fixture.debugElement.query(By.css('.selectable-row')).nativeElement;
+      row.click();
+      fixture.detectChanges();
+    });
+
+    it('should start editing the selected schema', async(() => {
+      fixture.whenStable().then(() => {
+        expect(registryService.editMetadataSchema).toHaveBeenCalledWith(mockSchemasList[0]);
+      });
+    }));
+
+    it('should cancel editing the selected schema when clicked again', async(() => {
+      spyOn(registryService, 'getActiveMetadataSchema').and.returnValue(observableOf(mockSchemasList[0]));
+      spyOn(registryService, 'cancelEditMetadataSchema');
+      row.click();
+      fixture.detectChanges();
+      fixture.whenStable().then(() => {
+        expect(registryService.cancelEditMetadataSchema).toHaveBeenCalled();
+      });
+    }));
+  })
 });
