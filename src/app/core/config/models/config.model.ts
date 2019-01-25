e90190cd1f425a1169b318df42ac12a0c92d4742
Giuseppe Digilio <giuseppe.digilio@4science.it>
2019-01-25 09:43:01 +0100

    Changed ConfigObject model

--- src/app/core/config/models/config.model.ts
+++ src/app/core/config/models/config.model.ts
@@ -1,22 +1,10 @@
-import { autoserialize } from 'cerialize';
+import { autoserialize, inheritSerialization } from 'cerialize';
 import { NormalizedObject } from '../../cache/models/normalized-object.model';
 
+@inheritSerialization(NormalizedObject)
 export abstract class ConfigObject extends  NormalizedObject {
 
   @autoserialize
   public name: string;
 
-  @autoserialize
-  public type: any;
-
-  @autoserialize
-  public _links: {
-    [name: string]: string
-  };
-
-  /**
-   * The link to the rest endpoint where this config object can be found
-   */
-  @autoserialize
-  self: string;
 }
