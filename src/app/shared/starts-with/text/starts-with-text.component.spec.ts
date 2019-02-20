eee7e210b05092370bcd6b9f5bb6e0b2f3e492da
Kristof De Langhe <kristof.delanghe@atmire.com>
2019-02-20 11:17:54 +0100

    60168: StartsWithText component tests

--- src/app/shared/starts-with/text/starts-with-text.component.spec.ts
+++ src/app/shared/starts-with/text/starts-with-text.component.spec.ts
@@ -1,49 +1,40 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { RouterTestingModule } from '@angular/router/testing';
 import { TranslateModule } from '@ngx-translate/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { ActivatedRoute, Router } from '@angular/router';
-import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
-import { of as observableOf } from 'rxjs/internal/observable/of';
-import { By } from '@angular/platform-browser';
-import { StartsWithDateComponent } from './starts-with-date.component';
-import { ActivatedRouteStub } from '../../testing/active-router-stub';
 import { EnumKeysPipe } from '../../utils/enum-keys-pipe';
-import { RouterStub } from '../../testing/router-stub';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { StartsWithTextComponent } from './starts-with-text.component';
 
-describe('StartsWithDateComponent', () => {
-  let comp: StartsWithDateComponent;
-  let fixture: ComponentFixture<StartsWithDateComponent>;
+describe('StartsWithTextComponent', () => {
+  let comp: StartsWithTextComponent;
+  let fixture: ComponentFixture<StartsWithTextComponent>;
   let route: ActivatedRoute;
   let router: Router;
 
-  const options = [2019, 2018, 2017, 2016, 2015];
-
-  const activatedRouteStub = Object.assign(new ActivatedRouteStub(), {
-    params: observableOf({}),
-    queryParams: observableOf({})
-  });
+  const options = ['0-9', 'A', 'B', 'C'];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [CommonModule, RouterTestingModule.withRoutes([]), TranslateModule.forRoot(), NgbModule.forRoot()],
-      declarations: [StartsWithDateComponent, EnumKeysPipe],
+      declarations: [StartsWithTextComponent, EnumKeysPipe],
       providers: [
-        { provide: 'startsWithOptions', useValue: options },
-        { provide: ActivatedRoute, useValue: activatedRouteStub },
-        { provide: Router, useValue: new RouterStub() }
+        { provide: 'startsWithOptions', useValue: options }
       ],
       schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(StartsWithDateComponent);
+    fixture = TestBed.createComponent(StartsWithTextComponent);
     comp = fixture.componentInstance;
     fixture.detectChanges();
     route = (comp as any).route;
     router = (comp as any).router;
+    spyOn(router, 'navigate');
   });
 
   it('should create a FormGroup containing a startsWith FormControl', () => {
@@ -71,10 +62,10 @@ describe('StartsWithDateComponent', () => {
     });
   });
 
-  describe('when selecting the second option in the dropdown', () => {
+  describe('when selecting "0-9" in the dropdown', () => {
     let select;
     let input;
-    const expectedValue = '' + options[0];
+    const expectedValue = '0';
     const extras = {
       queryParams: Object.assign({ startsWith: expectedValue }),
       queryParamsHandling: 'merge'
@@ -88,7 +79,66 @@ describe('StartsWithDateComponent', () => {
       fixture.detectChanges();
     });
 
-    it('should set startsWith to the correct value', () => {
+    it('should set startsWith to "0"', () => {
+      expect(comp.startsWith).toEqual(expectedValue);
+    });
+
+    it('should add a startsWith query parameter', () => {
+      expect(router.navigate).toHaveBeenCalledWith([], extras);
+    });
+
+    it('should automatically fill in the input field', () => {
+      expect(input.value).toEqual(expectedValue);
+    });
+  });
+
+  describe('when selecting an option in the dropdown', () => {
+    let select;
+    let input;
+    const expectedValue = options[1];
+    const extras = {
+      queryParams: Object.assign({ startsWith: expectedValue }),
+      queryParamsHandling: 'merge'
+    };
+
+    beforeEach(() => {
+      select = fixture.debugElement.query(By.css('select')).nativeElement;
+      input = fixture.debugElement.query(By.css('input')).nativeElement;
+      select.value = select.options[2].value;
+      select.dispatchEvent(new Event('change'));
+      fixture.detectChanges();
+    });
+
+    it('should set startsWith to the expected value', () => {
+      expect(comp.startsWith).toEqual(expectedValue);
+    });
+
+    it('should add a startsWith query parameter', () => {
+      expect(router.navigate).toHaveBeenCalledWith([], extras);
+    });
+
+    it('should automatically fill in the input field', () => {
+      expect(input.value).toEqual(expectedValue);
+    });
+  });
+
+  describe('when clicking an option in the list', () => {
+    let optionLink;
+    let input;
+    const expectedValue = options[1];
+    const extras = {
+      queryParams: Object.assign({ startsWith: expectedValue }),
+      queryParamsHandling: 'merge'
+    };
+
+    beforeEach(() => {
+      optionLink = fixture.debugElement.query(By.css('.list-inline-item:nth-child(2) > a')).nativeElement;
+      input = fixture.debugElement.query(By.css('input')).nativeElement;
+      optionLink.click();
+      fixture.detectChanges();
+    });
+
+    it('should set startsWith to the expected value', () => {
       expect(comp.startsWith).toEqual(expectedValue);
     });
 
@@ -103,7 +153,7 @@ describe('StartsWithDateComponent', () => {
 
   describe('when filling in the input form', () => {
     let form;
-    const expectedValue = '2015';
+    const expectedValue = 'A';
     const extras = {
       queryParams: Object.assign({ startsWith: expectedValue }),
       queryParamsHandling: 'merge'
