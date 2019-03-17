fa218abada576a032fea095cc89f7313ef424d1b
Giuseppe Digilio <giuseppe.digilio@4science.it>
2019-03-17 16:52:07 +0100

    Fixed property groups form Group and EPerson models and added comments

--- src/app/core/eperson/models/group.model.ts
+++ src/app/core/eperson/models/group.model.ts
@@ -1,12 +1,28 @@
+import { Observable } from 'rxjs';
+
 import { DSpaceObject } from '../../shared/dspace-object.model';
+import { PaginatedList } from '../../data/paginated-list';
+import { RemoteData } from '../../data/remote-data';
 
 export class Group extends DSpaceObject {
 
-  public groups: Group[];
+  /**
+   * List of Groups that this Group belong to
+   */
+  public groups: Observable<RemoteData<PaginatedList<Group>>>;
 
+  /**
+   * A string representing the unique handle of this Group
+   */
   public handle: string;
 
+  /**
+   * A string representing the name of this Group
+   */
   public name: string;
 
+  /**
+   * A string representing the name of this Group is permanent
+   */
   public permanent: boolean;
 }
--- src/app/core/eperson/models/normalized-eperson.model.ts
+++ src/app/core/eperson/models/normalized-eperson.model.ts
@@ -1,37 +1,62 @@
-import { autoserialize, autoserializeAs, inheritSerialization } from 'cerialize';
+import { autoserialize, deserialize, inheritSerialization } from 'cerialize';
+
 import { CacheableObject } from '../../cache/object-cache.reducer';
 import { ListableObject } from '../../../shared/object-collection/shared/listable-object.model';
 import { NormalizedDSpaceObject } from '../../cache/models/normalized-dspace-object.model';
 import { EPerson } from './eperson.model';
-import { mapsTo } from '../../cache/builders/build-decorators';
-import { Group } from './group.model';
-import { NormalizedGroup } from './normalized-group.model';
+import { mapsTo, relationship } from '../../cache/builders/build-decorators';
+import { ResourceType } from '../../shared/resource-type';
 
 @mapsTo(EPerson)
 @inheritSerialization(NormalizedDSpaceObject)
 export class NormalizedEPerson extends NormalizedDSpaceObject<EPerson> implements CacheableObject, ListableObject {
 
+  /**
+   * A string representing the unique handle of this EPerson
+   */
   @autoserialize
   public handle: string;
 
-  @autoserializeAs(NormalizedGroup)
-  groups: Group[];
+  /**
+   * List of Groups that this EPerson belong to
+   */
+  @deserialize
+  @relationship(ResourceType.Group, true)
+  groups: string[];
 
+  /**
+   * A string representing the netid of this EPerson
+   */
   @autoserialize
   public netid: string;
 
+  /**
+   * A string representing the last active date for this EPerson
+   */
   @autoserialize
   public lastActive: string;
 
+  /**
+   * A boolean representing if this EPerson can log in
+   */
   @autoserialize
   public canLogIn: boolean;
 
+  /**
+   * The EPerson email address
+   */
   @autoserialize
   public email: string;
 
+  /**
+   * A boolean representing if this EPerson require certificate
+   */
   @autoserialize
   public requireCertificate: boolean;
 
+  /**
+   * A boolean representing if this EPerson registered itself
+   */
   @autoserialize
   public selfRegistered: boolean;
 }
