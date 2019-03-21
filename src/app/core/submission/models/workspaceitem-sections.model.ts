1804a467f32b3d60755b0104cacff0cfe6c64770
Giuseppe Digilio <giuseppe.digilio@4science.it>
2019-03-21 10:00:21 +0100

    Removed WorkspaceitemSectionDetectDuplicateObject

--- src/app/core/submission/models/workspaceitem-sections.model.ts
+++ src/app/core/submission/models/workspaceitem-sections.model.ts
@@ -2,7 +2,6 @@ import { WorkspaceitemSectionFormObject } from './workspaceitem-section-form.mod
 import { WorkspaceitemSectionLicenseObject } from './workspaceitem-section-license.model';
 import { WorkspaceitemSectionUploadObject } from './workspaceitem-section-upload.model';
 import { WorkspaceitemSectionRecycleObject } from './workspaceitem-section-recycle.model';
-import { WorkspaceitemSectionDetectDuplicateObject } from './workspaceitem-section-deduplication.model';
 
 export class WorkspaceitemSectionsObject {
   [name: string]: WorkspaceitemSectionDataType;
@@ -13,5 +12,4 @@ export type WorkspaceitemSectionDataType
   | WorkspaceitemSectionFormObject
   | WorkspaceitemSectionLicenseObject
   | WorkspaceitemSectionRecycleObject
-  | WorkspaceitemSectionDetectDuplicateObject
   | string;
