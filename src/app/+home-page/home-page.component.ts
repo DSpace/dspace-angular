ed42513c55413939a23e068d3d81375e83d07b1f
lotte <lotte_hofstede@hotmail.com>
2019-03-27 16:46:19 +0100

    added banner and changed fonts

--- src/app/+home-page/home-page.component.ts
+++ src/app/+home-page/home-page.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ds-home-page',
   styleUrls: ['./home-page.component.scss'],
-  templateUrl: './home-page.component.html'
+  // templateUrl: './home-page.component.html'
+  templateUrl: './themes/home-page.component.preview-release.html'
 })
 export class HomePageComponent {
 }
