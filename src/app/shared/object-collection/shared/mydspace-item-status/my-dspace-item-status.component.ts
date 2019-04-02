25d955c8046d80b59fb617944961545143936649
Giuseppe Digilio <giuseppe.digilio@4science.it>
2019-04-02 19:21:00 +0200

    Added tests and comments

--- src/app/shared/object-collection/shared/mydspace-item-status/my-dspace-item-status.component.ts
+++ src/app/shared/object-collection/shared/mydspace-item-status/my-dspace-item-status.component.ts
@@ -1,18 +1,34 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { MyDspaceItemStatusType } from './my-dspace-item-status-type';
 
+/**
+ * This component represents a badge with mydspace item status
+ */
 @Component({
   selector: 'ds-mydspace-item-status',
   styleUrls: ['./my-dspace-item-status.component.scss'],
   templateUrl: './my-dspace-item-status.component.html'
 })
-
 export class MyDSpaceItemStatusComponent implements OnInit {
 
+  /**
+   * This mydspace item status
+   */
   @Input() status: MyDspaceItemStatusType;
+
+  /**
+   * This badge class
+   */
   public badgeClass: string;
+
+  /**
+   * This badge content
+   */
   public badgeContent: string;
 
+  /**
+   * Initialize badge content and class
+   */
   ngOnInit() {
     this.badgeContent = this.status;
     this.badgeClass = 'text-light badge ';
--- src/app/shared/object-collection/shared/mydspace-item-submitter/item-submitter.component.ts
+++ src/app/shared/object-collection/shared/mydspace-item-submitter/item-submitter.component.ts
@@ -8,19 +8,31 @@ import { RemoteData } from '../../../../core/data/remote-data';
 import { isNotEmpty, isNotUndefined } from '../../../empty.util';
 import { Workflowitem } from '../../../../core/submission/models/workflowitem.model';
 
+/**
+ * This component represents a badge with submitter information.
+ */
 @Component({
   selector: 'ds-item-submitter',
   styleUrls: ['./item-submitter.component.scss'],
   templateUrl: './item-submitter.component.html'
 })
-
 export class ItemSubmitterComponent implements OnInit {
+
+  /**
+   * The target object
+   */
   @Input() object: any;
 
-  submitter: Observable<EPerson>;
+  /**
+   * The Eperson object
+   */
+  submitter$: Observable<EPerson>;
 
+  /**
+   * Initialize submitter object
+   */
   ngOnInit() {
-    this.submitter = (this.object.workflowitem as Observable<RemoteData<Workflowitem>>).pipe(
+    this.submitter$ = (this.object.workflowitem as Observable<RemoteData<Workflowitem>>).pipe(
       filter((rd: RemoteData<Workflowitem>) => (rd.hasSucceeded && isNotUndefined(rd.payload))),
       flatMap((rd: RemoteData<Workflowitem>) => rd.payload.submitter as Observable<RemoteData<EPerson>>),
       find((rd: RemoteData<EPerson>) => rd.hasSucceeded && isNotEmpty(rd.payload)),
