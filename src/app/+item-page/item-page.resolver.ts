22bd69ba13ad70d8fc8c8b1c2ba61745f7d0ec42
lotte <lotte_hofstede@hotmail.com>
2019-05-08 16:32:32 +0200

    misc fixes

--- src/app/+item-page/item-page.resolver.ts
+++ src/app/+item-page/item-page.resolver.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot } from '@angular/router';
+import { EMPTY, Observable } from 'rxjs';
 import { RemoteData } from '../core/data/remote-data';
 import { getSucceededRemoteData } from '../core/shared/operators';
 import { ItemDataService } from '../core/data/item-data.service';
 import { Item } from '../core/shared/item.model';
-import { tap } from 'rxjs/operators';
+import { hasValue } from '../shared/empty.util';
+import { find, map } from 'rxjs/operators';
 
 /**
  * This class represents a resolver that requests a specific item before the route is activated
  */
 @Injectable()
 export class ItemPageResolver implements Resolve<RemoteData<Item>> {
-  constructor(private itemService: ItemDataService) {
+  constructor(private itemService: ItemDataService, private router: Router) {
   }
 
   /**
@@ -22,8 +23,16 @@ export class ItemPageResolver implements Resolve<RemoteData<Item>> {
    * @returns Observable<<RemoteData<Item>> Emits the found item based on the parameters in the current route
    */
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<RemoteData<Item>> {
-    return this.itemService.findById(route.params.id).pipe(
-      getSucceededRemoteData()
-    );
+    return this.itemService.findById(route.params.id)
+      .pipe(
+        find((RD) => hasValue(RD.error) || RD.hasSucceeded),
+        map((RD) => {
+          if (hasValue(RD.error)) {
+            this.router.navigateByUrl('/404', { skipLocationChange: true });
+            return null;
+          }
+          return RD;
+        })
+      );
   }
 }
