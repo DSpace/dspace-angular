bd8177c17d88e55f7f120855e67f3cfbd7f6f4a7
Kristof De Langhe <kristof.delanghe@atmire.com>
2019-06-07 17:46:45 +0200

    62741: AoT build fix

--- src/app/shared/object-list/item-type-badge/item-type-badge.component.ts
+++ src/app/shared/object-list/item-type-badge/item-type-badge.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { ListableObject } from '../../object-collection/shared/listable-object.model';
+import { SearchResult } from '../../../+search-page/search-result.model';
+import { DSpaceObject } from '../../../core/shared/dspace-object.model';
 
 @Component({
   selector: 'ds-item-type-badge',
   templateUrl: './item-type-badge.component.html'
 })
 export class ItemTypeBadgeComponent {
-  @Input() object: ListableObject;
+  @Input() object: SearchResult<DSpaceObject>;
 }
