8188da0c7587ad15b59488cd86395c77d2b0d0cd
Kristof De Langhe <kristof.delanghe@atmire.com>
2019-07-11 13:24:24 +0200

    63669: Intermediate commit

--- src/app/+collection-page/edit-collection-page/collection-source/collection-source.component.ts
+++ src/app/+collection-page/edit-collection-page/collection-source/collection-source.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AbstractTrackableComponent } from '../../../shared/trackable/abstract-trackable.component';
+import { DynamicFormControlModel, DynamicInputModel, DynamicTextAreaModel } from '@ng-dynamic-forms/core';
 
 /**
  * Component for managing the content source of the collection
@@ -7,6 +9,28 @@ import { Component } from '@angular/core';
   selector: 'ds-collection-source',
   templateUrl: './collection-source.component.html',
 })
-export class CollectionSourceComponent {
-  /* TODO: Implement Collection Edit - Content Source */
+export class CollectionSourceComponent extends AbstractTrackableComponent {
+  externalSource = false;
+
+  /**
+   * The dynamic form fields used for creating/editing a collection
+   * @type {(DynamicInputModel | DynamicTextAreaModel)[]}
+   */
+  formModel: DynamicFormControlModel[] = [
+    new DynamicInputModel({
+      id: 'title',
+      name: 'dc.title',
+      required: true,
+      validators: {
+        required: null
+      },
+      errorMessages: {
+        required: 'Please enter a name for this title'
+      },
+    }),
+    new DynamicTextAreaModel({
+      id: 'provider',
+      name: 'provider',
+    })
+  ];
 }
