50024c8c559216053fc8323c91a4a2b8a816ae4e
Giuseppe Digilio <giuseppe.digilio@4science.it>
2019-07-25 19:16:08 +0200

    During submission retrieve collection list only at first load

--- src/app/submission/form/collection/submission-form-collection.component.ts
+++ src/app/submission/form/collection/submission-form-collection.component.ts
@@ -209,43 +209,46 @@ export class SubmissionFormCollectionComponent implements OnChanges, OnInit {
         elementsPerPage: 100
       };
 
-      // @TODO replace with search/top browse endpoint
-      // @TODO implement community/subcommunity hierarchy
-      const communities$ = this.communityDataService.findAll(findOptions).pipe(
-        find((communities: RemoteData<PaginatedList<Community>>) => isNotEmpty(communities.payload)),
-        mergeMap((communities: RemoteData<PaginatedList<Community>>) => communities.payload.page));
+      // Retrieve collection list only when is the first change
+      if (changes.currentCollectionId.isFirstChange()) {
+        // @TODO replace with search/top browse endpoint
+        // @TODO implement community/subcommunity hierarchy
+        const communities$ = this.communityDataService.findAll(findOptions).pipe(
+          find((communities: RemoteData<PaginatedList<Community>>) => isNotEmpty(communities.payload)),
+          mergeMap((communities: RemoteData<PaginatedList<Community>>) => communities.payload.page));
 
-      const listCollection$ = communities$.pipe(
-        flatMap((communityData: Community) => {
-          return this.collectionDataService.getAuthorizedCollectionByCommunity(communityData.uuid).pipe(
-            find((collections: RemoteData<PaginatedList<Collection>>) => !collections.isResponsePending && collections.hasSucceeded),
-            mergeMap((collections: RemoteData<PaginatedList<Collection>>) => collections.payload.page),
-            filter((collectionData: Collection) => isNotEmpty(collectionData)),
-            map((collectionData: Collection) => ({
-              communities: [{ id: communityData.id, name: communityData.name }],
-              collection: { id: collectionData.id, name: collectionData.name }
-            }))
-          );
-        }),
-        reduce((acc: any, value: any) => [...acc, ...value], []),
-        startWith([])
-      );
+        const listCollection$ = communities$.pipe(
+          flatMap((communityData: Community) => {
+            return this.collectionDataService.getAuthorizedCollectionByCommunity(communityData.uuid).pipe(
+              find((collections: RemoteData<PaginatedList<Collection>>) => !collections.isResponsePending && collections.hasSucceeded),
+              mergeMap((collections: RemoteData<PaginatedList<Collection>>) => collections.payload.page),
+              filter((collectionData: Collection) => isNotEmpty(collectionData)),
+              map((collectionData: Collection) => ({
+                communities: [{ id: communityData.id, name: communityData.name }],
+                collection: { id: collectionData.id, name: collectionData.name }
+              }))
+            );
+          }),
+          reduce((acc: any, value: any) => [...acc, ...value], []),
+          startWith([])
+        );
 
-      const searchTerm$ = this.searchField.valueChanges.pipe(
-        debounceTime(200),
-        distinctUntilChanged(),
-        startWith('')
-      );
+        const searchTerm$ = this.searchField.valueChanges.pipe(
+          debounceTime(200),
+          distinctUntilChanged(),
+          startWith('')
+        );
 
-      this.searchListCollection$ = combineLatest(searchTerm$, listCollection$).pipe(
-        map(([searchTerm, listCollection]) => {
-          this.disabled$.next(isEmpty(listCollection));
-          if (isEmpty(searchTerm)) {
-            return listCollection;
-          } else {
-            return listCollection.filter((v) => v.collection.name.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1).slice(0, 5);
-          }
-        }));
+        this.searchListCollection$ = combineLatest(searchTerm$, listCollection$).pipe(
+          map(([searchTerm, listCollection]) => {
+            this.disabled$.next(isEmpty(listCollection));
+            if (isEmpty(searchTerm)) {
+              return listCollection;
+            } else {
+              return listCollection.filter((v) => v.collection.name.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1).slice(0, 5);
+            }
+          }));
+      }
     }
   }
 
