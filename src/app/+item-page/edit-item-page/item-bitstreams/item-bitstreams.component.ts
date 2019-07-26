277aad26c887d6e20cddfdd25abf989803775743
Kristof De Langhe <kristof.delanghe@atmire.com>
2019-07-26 10:32:51 +0200

    63945: BitstreamDataService + bitstream remove

--- src/app/+item-page/edit-item-page/item-bitstreams/item-bitstreams.component.ts
+++ src/app/+item-page/edit-item-page/item-bitstreams/item-bitstreams.component.ts
@@ -1,11 +1,22 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, Inject, OnDestroy } from '@angular/core';
 import { AbstractItemUpdateComponent } from '../abstract-item-update/abstract-item-update.component';
-import { switchMap, take } from 'rxjs/operators';
+import { map, switchMap, take } from 'rxjs/operators';
 import { Bitstream } from '../../../core/shared/bitstream.model';
 import { getBundleNames, toBitstreamsArray, toBundleMap } from '../../../core/shared/item-bitstreams-utils';
 import { Observable } from 'rxjs/internal/Observable';
-import { FieldUpdates } from '../../../core/data/object-updates/object-updates.reducer';
+import { FieldUpdate, FieldUpdates } from '../../../core/data/object-updates/object-updates.reducer';
 import { Subscription } from 'rxjs/internal/Subscription';
+import { ItemDataService } from '../../../core/data/item-data.service';
+import { ObjectUpdatesService } from '../../../core/data/object-updates/object-updates.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NotificationsService } from '../../../shared/notifications/notifications.service';
+import { TranslateService } from '@ngx-translate/core';
+import { GLOBAL_CONFIG, GlobalConfig } from '../../../../config';
+import { BitstreamDataService } from '../../../core/data/bitstream-data.service';
+import { FieldChangeType } from '../../../core/data/object-updates/object-updates.actions';
+import { isNotEmptyOperator } from '../../../shared/empty.util';
+import { zip as observableZip } from 'rxjs';
+import { RestResponse } from '../../../core/cache/response.models';
 
 @Component({
   selector: 'ds-item-bitstreams',
@@ -22,6 +33,19 @@ export class ItemBitstreamsComponent extends AbstractItemUpdateComponent impleme
   updatesMap: Map<string, Observable<FieldUpdates>>;
 
   updatesMapSub: Subscription;
+
+  constructor(
+    public itemService: ItemDataService,
+    public objectUpdatesService: ObjectUpdatesService,
+    public router: Router,
+    public notificationsService: NotificationsService,
+    public translateService: TranslateService,
+    @Inject(GLOBAL_CONFIG) public EnvConfig: GlobalConfig,
+    public route: ActivatedRoute,
+    public bitstreamService: BitstreamDataService
+  ) {
+    super(itemService, objectUpdatesService, router, notificationsService, translateService, EnvConfig, route);
+  }
 
   /**
    * Set up and initialize all fields
@@ -61,7 +85,17 @@ export class ItemBitstreamsComponent extends AbstractItemUpdateComponent impleme
   }
 
   submit() {
-    // TODO: submit changes
+    const removedBitstreams$ = this.item.bitstreams.pipe(
+      toBitstreamsArray(),
+      switchMap((bitstreams: Bitstream[]) => this.objectUpdatesService.getFieldUpdatesExclusive(this.url, bitstreams) as Observable<FieldUpdates>),
+      map((fieldUpdates: FieldUpdates) => Object.values(fieldUpdates).filter((fieldUpdate: FieldUpdate) => fieldUpdate.changeType === FieldChangeType.REMOVE)),
+      map((fieldUpdates: FieldUpdate[]) => fieldUpdates.map((fieldUpdate: FieldUpdate) => fieldUpdate.field)),
+      isNotEmptyOperator()
+    );
+    removedBitstreams$.pipe(
+      take(1),
+      switchMap((removedBistreams: Bitstream[]) => observableZip(...removedBistreams.map((bitstream: Bitstream) => this.bitstreamService.delete(bitstream))))
+    ).subscribe();
   }
 
   ngOnDestroy(): void {
---
+++ src/app/core/data/bitstream-data.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { DataService } from './data.service';
+import { Bitstream } from '../shared/bitstream.model';
+import { RequestService } from './request.service';
+import { RemoteDataBuildService } from '../cache/builders/remote-data-build.service';
+import { NormalizedObjectBuildService } from '../cache/builders/normalized-object-build.service';
+import { Store } from '@ngrx/store';
+import { CoreState } from '../core.reducers';
+import { BrowseService } from '../browse/browse.service';
+import { ObjectCacheService } from '../cache/object-cache.service';
+import { HALEndpointService } from '../shared/hal-endpoint.service';
+import { NotificationsService } from '../../shared/notifications/notifications.service';
+import { HttpClient } from '@angular/common/http';
+import { DSOChangeAnalyzer } from './dso-change-analyzer.service';
+import { FindAllOptions } from './request.models';
+import { Observable } from 'rxjs/internal/Observable';
+
+@Injectable()
+export class BitstreamDataService extends DataService<Bitstream> {
+  protected linkPath = 'bitstreams';
+  protected forceBypassCache = false;
+
+  constructor(
+    protected requestService: RequestService,
+    protected rdbService: RemoteDataBuildService,
+    protected dataBuildService: NormalizedObjectBuildService,
+    protected store: Store<CoreState>,
+    protected bs: BrowseService,
+    protected objectCache: ObjectCacheService,
+    protected halService: HALEndpointService,
+    protected notificationsService: NotificationsService,
+    protected http: HttpClient,
+    protected comparator: DSOChangeAnalyzer<Bitstream>) {
+    super();
+  }
+
+  getBrowseEndpoint(options: FindAllOptions = {}, linkPath: string = this.linkPath): Observable<string> {
+    return this.halService.getEndpoint(linkPath);
+  }
+}
