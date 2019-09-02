7e292cdde496d1411b53dca3956e6ed421ca6a06
Julius Gruber <julius.gruber@tuwien.ac.at>
2019-09-02 16:23:30 +0200

    Branch change commit

--- src/app/shared/log-in/methods/shibboleth/dynamic-shibboleth.component.ts
+++ src/app/shared/log-in/methods/shibboleth/dynamic-shibboleth.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject, Input, OnInit} from '@angular/core';
+import { Component, Inject, Input, OnInit } from '@angular/core';
 import { renderAuthMethodFor } from '../../authMethods-decorator';
 import { AuthMethodType } from '../../authMethods-type';
 import { AuthMethodModel } from '../../../../core/auth/models/auth-method.model';
@@ -12,17 +12,20 @@ import { AuthMethodModel } from '../../../../core/auth/models/auth-method.model'
 @renderAuthMethodFor(AuthMethodType.Shibboleth)
 export class DynamicShibbolethComponent implements OnInit {
 
-  @Input()authMethodModel: AuthMethodModel;
+  @Input() authMethodModel: AuthMethodModel;
+
+  buttonHref: string;
 
   /**
    * @constructor
    */
   constructor(@Inject('authMethodModelProvider') public injectedAuthMethodModel: AuthMethodModel) {
     this.authMethodModel = injectedAuthMethodModel;
+    this.buttonHref = ('https://fis.tiss.tuwien.ac.at' + this.authMethodModel.location + '/shibboleth')
   }
 
   ngOnInit(): void {
     console.log('injectedAuthMethodModel', this.injectedAuthMethodModel);
-    }
+  }
 
 }
