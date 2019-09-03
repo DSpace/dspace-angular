526fab8008dcbef31db906c2d257b041530882f7
Julius Gruber <julius.gruber@tuwien.ac.at>
2019-09-03 14:07:59 +0200

    Branch change

--- src/app/shared/log-in/methods/shibboleth/dynamic-shibboleth.component.ts
+++ src/app/shared/log-in/methods/shibboleth/dynamic-shibboleth.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, Input, OnInit } from '@angular/core';
 import { renderAuthMethodFor } from '../../authMethods-decorator';
 import { AuthMethodType } from '../../authMethods-type';
 import { AuthMethodModel } from '../../../../core/auth/models/auth-method.model';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'ds-dynamic-shibboleth',
@@ -17,15 +18,29 @@ export class DynamicShibbolethComponent implements OnInit {
   buttonHref: string;
 
   /**
+   * The authentication form.
+   * @type {FormGroup}
+   */
+  public shibbForm: FormGroup;
+
+  /**
    * @constructor
    */
-  constructor(@Inject('authMethodModelProvider') public injectedAuthMethodModel: AuthMethodModel) {
+  constructor(@Inject('authMethodModelProvider') public injectedAuthMethodModel: AuthMethodModel,
+              private formBuilder: FormBuilder) {
     this.authMethodModel = injectedAuthMethodModel;
     this.buttonHref = ('https://fis.tiss.tuwien.ac.at' + this.authMethodModel.location + '/shibboleth')
   }
 
   ngOnInit(): void {
     console.log('injectedAuthMethodModel', this.injectedAuthMethodModel);
+    // set formGroup
+    this.shibbForm = this.formBuilder.group({
+      shibbButton: [''],
+    });
   }
 
+  submit() {
+    console.log('submit() was callled');
+  }
 }
