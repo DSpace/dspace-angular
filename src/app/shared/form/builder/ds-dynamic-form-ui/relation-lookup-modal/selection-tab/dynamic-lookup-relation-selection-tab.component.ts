77e2fde15a8ab15de6772f4370806b482a1beaf8
lotte <lotte_hofstede@hotmail.com>
2019-09-04 08:47:32 +0200

    added selected results to lookup tab

--- src/app/shared/form/builder/ds-dynamic-form-ui/relation-lookup-modal/selection-tab/dynamic-lookup-relation-selection-tab.component.ts
+++ src/app/shared/form/builder/ds-dynamic-form-ui/relation-lookup-modal/selection-tab/dynamic-lookup-relation-selection-tab.component.ts
@@ -1,9 +1,14 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { SEARCH_CONFIG_SERVICE } from '../../../../../../+my-dspace-page/my-dspace-page.component';
 import { SearchConfigurationService } from '../../../../../../core/shared/search/search-configuration.service';
 import { Observable } from 'rxjs';
 import { RelationshipOptions } from '../../../models/relationship-options.model';
 import { ListableObject } from '../../../../../object-collection/shared/listable-object.model';
+import { RemoteData } from '../../../../../../core/data/remote-data';
+import { map, take } from 'rxjs/operators';
+import { createSuccessfulRemoteDataObject } from '../../../../../testing/utils';
+import { PaginationComponentOptions } from '../../../../../pagination/pagination-component-options.model';
+import { PaginatedList } from '../../../../../../core/data/paginated-list';
 
 @Component({
   selector: 'ds-dynamic-lookup-relation-selection-tab',
@@ -24,10 +29,19 @@ export class DsDynamicLookupRelationSelectionTabComponent {
   @Input() itemRD$;
   @Input() repeatable: boolean;
   @Input() selection$: Observable<ListableObject[]>;
+  @Input() selectionRD$: Observable<RemoteData<PaginatedList<ListableObject>>>;
+  initialPagination = Object.assign(new PaginationComponentOptions(), {
+    id: 'submission-relation-list',
+    pageSize: 5
+  });
 
-  constructor(
-
-  ) {
+  constructor() {
   }
 
+  ngOnInit() {
+    this.selectionRD$ = this.selection$.pipe(
+      take(1),
+      map((selection) => createSuccessfulRemoteDataObject(new PaginatedList({} as any, selection)))
+    );
+  }
 }
