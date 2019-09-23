2aaa0cf726447cabc9d460c009868f72d3a0881c
L. Henze <lh435@nyu.edu>
2019-09-23 12:43:00 -0400

    Comcol Handle component

--- src/app/shared/comcol-page-handle/comcol-page-handle.component.ts
+++ src/app/shared/comcol-page-handle/comcol-page-handle.component.ts
@@ -1,13 +1,10 @@
-
-import { Observable, of as observableOf, combineLatest as observableCombineLatest } from 'rxjs';
-import { Component, Input } from '@angular/core';
+import { Component, Input, Inject, Injectable } from '@angular/core';
 import { GlobalConfig } from '../../../config/global-config.interface';
-import { Inject, Injectable } from '@angular/core';
 import { GLOBAL_CONFIG } from '../../../config';
 import { RESTURLCombiner } from '../../core/url-combiner/rest-url-combiner';
 import { URLCombiner } from '../../core/url-combiner/url-combiner';
 /**
- * This component renders the value of "handle"
+ * This component builds a URL from the value of "handle"
  */
 
 @Component({
@@ -22,7 +19,7 @@ export class ComcolPageHandleComponent {
   // Optional title
   @Input() title: string;
 
-  // The content to render.
+  // The value of "handle"
   @Input() content: string;
 
   constructor(@Inject(GLOBAL_CONFIG) private EnvConfig: GlobalConfig) {
@@ -31,8 +28,7 @@ export class ComcolPageHandleComponent {
     return new RESTURLCombiner(this.EnvConfig, '/').toString();
   }
   public getHandle(content: string): string {
-    const href = new URLCombiner(this.getRootHref(), '/handle/', this.content).toString();
-    return href;
+    return new URLCombiner(this.getRootHref(), '/handle/', this.content).toString();
   }
 
 }
