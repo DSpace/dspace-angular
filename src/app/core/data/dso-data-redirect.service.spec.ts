5ec9b7c29f4c90f1604613e123d140e14b4150a9
Michael W Spalti <mspalti@gmail.com>
2019-09-30 16:57:06 -0700

    Added new unit tests.

--- src/app/core/data/dso-data-redirect.service.spec.ts
+++ src/app/core/data/dso-data-redirect.service.spec.ts
@@ -1,7 +1,6 @@
 import { cold, getTestScheduler } from 'jasmine-marbles';
 import { TestScheduler } from 'rxjs/testing';
 import { RemoteDataBuildService } from '../cache/builders/remote-data-build.service';
-import { DSpaceObject } from '../shared/dspace-object.model';
 import { HALEndpointService } from '../shared/hal-endpoint.service';
 import { FindByIDRequest } from './request.models';
 import { RequestService } from './request.service';
@@ -11,7 +10,6 @@ import { HttpClient } from '@angular/common/http';
 import { NormalizedObjectBuildService } from '../cache/builders/normalized-object-build.service';
 import { IdentifierType } from '../index/index.reducer';
 import { DsoDataRedirectService } from './dso-data-redirect.service';
-import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { CoreState } from '../core.reducers';
 
@@ -22,7 +20,7 @@ describe('DsoDataRedirectService', () => {
   let requestService: RequestService;
   let rdbService: RemoteDataBuildService;
   let objectCache: ObjectCacheService;
-  let router: Router;
+  let router;
   let remoteData;
   const dsoUUID = '9b4f22f4-164a-49db-8817-3316b6ee5746';
   const dsoHandle = '1234567789/22';
@@ -31,15 +29,12 @@ describe('DsoDataRedirectService', () => {
   const requestHandleURL = `https://rest.api/rest/api/pid/find?id=${encodedHandle}`;
   const requestUUIDURL = `https://rest.api/rest/api/pid/find?id=${dsoUUID}`;
   const requestUUID = '34cfed7c-f597-49ef-9cbe-ea351f0023c2';
-  const testObject = {
-    uuid: dsoUUID
-  } as DSpaceObject;
 
   beforeEach(() => {
     scheduler = getTestScheduler();
 
     halService = jasmine.createSpyObj('halService', {
-      getEndpoint: cold('a', { a: pidLink })
+      getEndpoint: cold('a', {a: pidLink})
     });
     requestService = jasmine.createSpyObj('requestService', {
       generateRequestId: requestUUID,
@@ -47,21 +42,20 @@ describe('DsoDataRedirectService', () => {
     });
     rdbService = jasmine.createSpyObj('rdbService', {
       buildSingle: cold('a', {
-        a: {
-          payload: testObject
-        }
+        a: remoteData
       })
     });
-    router = jasmine.createSpyObj('router', {
-      navigate: () => true
-    });
+    router = {
+      navigate: jasmine.createSpy('navigate')
+    };
     remoteData = {
       isSuccessful: true,
       error: undefined,
       hasSucceeded: true,
+      isLoading: false,
       payload: {
         type: 'item',
-        id: '123456789'
+        uuid: '123456789'
       }
     };
     objectCache = {} as ObjectCacheService;
@@ -86,7 +80,7 @@ describe('DsoDataRedirectService', () => {
   });
 
   describe('findById', () => {
-    it('should call HALEndpointService with the path to the dso endpoint', () => {
+    it('should call HALEndpointService with the path to the pid endpoint', () => {
       scheduler.schedule(() => service.findById(dsoUUID));
       scheduler.flush();
 
@@ -107,6 +101,13 @@ describe('DsoDataRedirectService', () => {
       expect(requestService.configure).toHaveBeenCalledWith(new FindByIDRequest(requestUUID, requestHandleURL, dsoHandle, IdentifierType.HANDLE), false);
     });
 
-    // TODO: test for router.navigate
-  });
+    it('should navigate to dso route', () => {
+      const redir = service.findById(dsoHandle, IdentifierType.HANDLE);
+      // The framework would normally subscribe but do it here so we can test navigation.
+      redir.subscribe();
+      scheduler.schedule(() => redir);
+      scheduler.flush();
+      expect(router.navigate).toHaveBeenCalledWith([remoteData.payload.type + 's/' + remoteData.payload.uuid]);
+    });
+  })
 });
