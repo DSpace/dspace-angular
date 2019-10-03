cddaf539aad4e0f7c2dba4bf4133d80368dea4ce
Kristof De Langhe <kristof.delanghe@atmire.com>
2019-10-03 16:36:07 +0200

    65272: add, delete and edit button for collection item template

--- src/app/+collection-page/edit-collection-page/collection-metadata/collection-metadata.component.ts
+++ src/app/+collection-page/edit-collection-page/collection-metadata/collection-metadata.component.ts
@@ -8,7 +8,9 @@ import { Observable } from 'rxjs/internal/Observable';
 import { RemoteData } from '../../../core/data/remote-data';
 import { Item } from '../../../core/shared/item.model';
 import { getRemoteDataPayload, getSucceededRemoteData } from '../../../core/shared/operators';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, take } from 'rxjs/operators';
+import { combineLatest as combineLatestObservable } from 'rxjs';
+import { NotificationsService } from '../../../shared/notifications/notifications.service';
 
 /**
  * Component for editing a collection's metadata
@@ -23,24 +25,71 @@ export class CollectionMetadataComponent extends ComcolMetadataComponent<Collect
   /**
    * The collection's template item
    */
-  templateItemRD$: Observable<RemoteData<Item>>;
+  itemTemplateRD$: Observable<RemoteData<Item>>;
 
   public constructor(
     protected collectionDataService: CollectionDataService,
     protected itemTemplateService: ItemTemplateDataService,
     protected router: Router,
-    protected route: ActivatedRoute
+    protected route: ActivatedRoute,
+    protected notificationsService: NotificationsService
   ) {
     super(collectionDataService, router, route);
   }
 
   ngOnInit(): void {
     super.ngOnInit();
+    this.initTemplateItem();
+  }
 
-    this.templateItemRD$ = this.dsoRD$.pipe(
+  initTemplateItem() {
+    this.itemTemplateRD$ = this.dsoRD$.pipe(
       getSucceededRemoteData(),
       getRemoteDataPayload(),
       switchMap((collection: Collection) => this.itemTemplateService.findByCollectionID(collection.uuid))
     );
+  }
+
+  addItemTemplate() {
+    const collection$ = this.dsoRD$.pipe(
+      getSucceededRemoteData(),
+      getRemoteDataPayload(),
+      take(1)
+    );
+    const template$ = collection$.pipe(
+      switchMap((collection: Collection) => this.itemTemplateService.create(new Item(), collection.uuid)),
+      getSucceededRemoteData(),
+      getRemoteDataPayload(),
+      take(1)
+    );
+
+    combineLatestObservable(collection$, template$).subscribe(([collection, template]) => {
+      this.router.navigate(['collections', collection.uuid, 'itemtemplate']);
+    });
+  }
+
+  deleteItemTemplate() {
+    const collection$ = this.dsoRD$.pipe(
+      getSucceededRemoteData(),
+      getRemoteDataPayload(),
+      take(1)
+    );
+    const template$ = collection$.pipe(
+      switchMap((collection: Collection) => this.itemTemplateService.findByCollectionID(collection.uuid)),
+      getSucceededRemoteData(),
+      getRemoteDataPayload(),
+      take(1)
+    );
+
+    combineLatestObservable(collection$, template$).pipe(
+      switchMap(([collection, template]) => this.itemTemplateService.deleteByCollectionID(template, collection.uuid))
+    ).subscribe((success: boolean) => {
+      if (success) {
+        this.notificationsService.success(null, 'collections.edit.template.notifications.delete.success');
+        this.initTemplateItem();
+      } else {
+        this.notificationsService.error(null, 'collections.edit.template.notifications.delete.error');
+      }
+    });
   }
 }
