143604fa4dded5d2caa50ad6ece962eafc8ef0a2
Kristof De Langhe <kristof.delanghe@atmire.com>
2019-10-03 15:07:05 +0200

    65272: Intermediate commit

--- src/app/+collection-page/edit-collection-page/collection-metadata/collection-metadata.component.ts
+++ src/app/+collection-page/edit-collection-page/collection-metadata/collection-metadata.component.ts
@@ -3,6 +3,12 @@ import { ComcolMetadataComponent } from '../../../shared/comcol-forms/edit-comco
 import { Collection } from '../../../core/shared/collection.model';
 import { CollectionDataService } from '../../../core/data/collection-data.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ItemTemplateDataService } from '../../../core/data/item-template-data.service';
+import { Observable } from 'rxjs/internal/Observable';
+import { RemoteData } from '../../../core/data/remote-data';
+import { Item } from '../../../core/shared/item.model';
+import { getRemoteDataPayload, getSucceededRemoteData } from '../../../core/shared/operators';
+import { switchMap } from 'rxjs/operators';
 
 /**
  * Component for editing a collection's metadata
@@ -14,11 +20,27 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class CollectionMetadataComponent extends ComcolMetadataComponent<Collection> {
   protected frontendURL = '/collections/';
 
+  /**
+   * The collection's template item
+   */
+  templateItemRD$: Observable<RemoteData<Item>>;
+
   public constructor(
     protected collectionDataService: CollectionDataService,
+    protected itemTemplateService: ItemTemplateDataService,
     protected router: Router,
     protected route: ActivatedRoute
   ) {
     super(collectionDataService, router, route);
+  }
+
+  ngOnInit(): void {
+    super.ngOnInit();
+
+    this.templateItemRD$ = this.dsoRD$.pipe(
+      getSucceededRemoteData(),
+      getRemoteDataPayload(),
+      switchMap((collection: Collection) => this.itemTemplateService.findByCollectionID(collection.uuid))
+    );
   }
 }
