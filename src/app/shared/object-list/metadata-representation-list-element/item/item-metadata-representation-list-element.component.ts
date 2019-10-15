f1b4b57cdbdce54d62e9bb017c7a08761bfcbca4
Kristof De Langhe <kristof.delanghe@atmire.com>
2019-10-15 13:58:03 +0200

    65529: AoT build error fixes

--- src/app/shared/object-list/metadata-representation-list-element/item/item-metadata-representation-list-element.component.ts
+++ src/app/shared/object-list/metadata-representation-list-element/item/item-metadata-representation-list-element.component.ts
@@ -1,7 +1,7 @@
 import { MetadataRepresentationListElementComponent } from '../metadata-representation-list-element.component';
 import { Component, Inject } from '@angular/core';
 import { ITEM } from '../../../items/switcher/item-type-switcher.component';
-import { MetadataRepresentation } from '../../../../core/shared/metadata-representation/metadata-representation.model';
+import { ItemMetadataRepresentation } from '../../../../core/shared/metadata-representation/item/item-metadata-representation.model';
 
 @Component({
   selector: 'ds-item-metadata-representation-list-element',
@@ -11,7 +11,7 @@ import { MetadataRepresentation } from '../../../../core/shared/metadata-represe
  * An abstract class for displaying a single ItemMetadataRepresentation
  */
 export class ItemMetadataRepresentationListElementComponent extends MetadataRepresentationListElementComponent {
-  constructor(@Inject(ITEM) public metadataRepresentation: MetadataRepresentation) {
+  constructor(@Inject(ITEM) public metadataRepresentation: ItemMetadataRepresentation) {
     super(metadataRepresentation);
   }
 }
