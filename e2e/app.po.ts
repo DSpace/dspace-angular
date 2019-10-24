6f8219373fed64632d1ba1cdddb04a5213a336f0
Antoine Snyers <antoine@atmire.com>
2019-10-24 11:22:52 +0200

    Try waitForAngular()

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -2,10 +2,8 @@ import { browser, by, element } from 'protractor';
 
 export class ProtractorPage {
   navigateTo() {
-    // return browser.get('/');
     return browser.get('/')
-      .then(() => browser.getPageSource())
-      .then((txt) => {console.log(txt.substring(txt.indexOf('<body>')));});
+      .then(() => browser.waitForAngular());
   }
 
   getPageTitleText() {
