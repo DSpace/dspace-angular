5ff634a26facf8362f36e1afbde3f93f5180d627
Marie Verdonck <maria.verdonck@atmire.com>
2019-11-26 18:06:07 +0100

    Last bit of doc

--- src/app/community-list-page/community-list-page.component.ts
+++ src/app/community-list-page/community-list-page.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+/**
+ * Page with title and the community list tree, as described in community-list.component;
+ * navigated to with community-list.page.routing.module
+ */
 @Component({
   selector: 'ds-community-list-page',
   templateUrl: './community-list-page.component.html',
