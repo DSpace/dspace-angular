907358edc593a89dcfffdabf0ce6bdbba9b9bbfc
Art Lowel <art.lowel@atmire.com>
2019-12-17 09:58:05 +0100

    add test to prove the response body isn't part of the output

--- src/app/core/data/status-code-only-response-parsing.service.spec.ts
+++ src/app/core/data/status-code-only-response-parsing.service.spec.ts
@@ -11,6 +11,17 @@ describe('StatusCodeOnlyResponseParsingService', () => {
 
   describe('parse', () => {
 
+    it('should return a RestResponse that doesn\'t contain the response body', () => {
+      const payload = 'd9128e44-183b-479d-aa2e-d39435838bf6';
+      const result = service.parse(undefined, {
+        payload,
+        statusCode: 201,
+        statusText: '201'
+      });
+
+      expect(JSON.stringify(result).indexOf(payload)).toBe(-1);
+    });
+
     describe('when the response is successful', () => {
       beforeEach(() => {
         statusCode = 201;
