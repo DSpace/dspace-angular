9e31b61d424bd19c95473650a72d155b5dc349ab
Kristof De Langhe <kristof.delanghe@atmire.com>
2020-02-26 16:32:12 +0100

    69115: post master merge changes to workflow-action

--- src/app/core/data/workflow-action-data.service.ts
+++ src/app/core/data/workflow-action-data.service.ts
@@ -2,7 +2,6 @@ import { DataService } from './data.service';
 import { WorkflowAction } from '../tasks/models/workflow-action-object.model';
 import { RequestService } from './request.service';
 import { RemoteDataBuildService } from '../cache/builders/remote-data-build.service';
-import { NormalizedObjectBuildService } from '../cache/builders/normalized-object-build.service';
 import { Store } from '@ngrx/store';
 import { CoreState } from '../core.reducers';
 import { ObjectCacheService } from '../cache/object-cache.service';
@@ -13,18 +12,20 @@ import { DefaultChangeAnalyzer } from './default-change-analyzer.service';
 import { FindListOptions } from './request.models';
 import { Observable } from 'rxjs/internal/Observable';
 import { Injectable } from '@angular/core';
+import { dataService } from '../cache/builders/build-decorators';
+import { WORKFLOW_ACTION } from '../tasks/models/workflow-action-object.resource-type';
 
 /**
  * A service responsible for fetching/sending data from/to the REST API on the workflowactions endpoint
  */
 @Injectable()
+@dataService(WORKFLOW_ACTION)
 export class WorkflowActionDataService extends DataService<WorkflowAction> {
   protected linkPath = 'workflowactions';
 
   constructor(
     protected requestService: RequestService,
     protected rdbService: RemoteDataBuildService,
-    protected dataBuildService: NormalizedObjectBuildService,
     protected store: Store<CoreState>,
     protected objectCache: ObjectCacheService,
     protected halService: HALEndpointService,
