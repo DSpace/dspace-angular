0b65bdac46b487bffc7130ca658cf3c0ec7c42a4
Giuseppe Digilio <giuseppe.digilio@4science.it>
2020-04-06 20:47:52 +0200

    fixed lint error

--- src/app/+item-page/edit-item-page/item-authorizations/item-authorizations.component.spec.ts
+++ src/app/+item-page/edit-item-page/item-authorizations/item-authorizations.component.spec.ts
@@ -3,7 +3,7 @@ import { ComponentFixture } from '@angular/core/testing';
 import { ItemAuthorizationsComponent } from './item-authorizations.component';
 
 describe('ItemAuthorizationsComponent', () => {
-  let comp: ItemAuthorizationsComponent;
-  let fixture: ComponentFixture<ItemAuthorizationsComponent>;
+  // let comp: ItemAuthorizationsComponent;
+  // let fixture: ComponentFixture<ItemAuthorizationsComponent>;
 
 });
