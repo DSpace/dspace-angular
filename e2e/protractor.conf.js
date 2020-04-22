2e367cb87c26c14f54439927a70d1e32d071e15d
lotte <lotte_hofstede@hotmail.com>
2020-04-22 15:29:52 +0200

    testing e2e on travis

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -33,7 +33,7 @@ exports.config = {
     'version': '',
     'platform': 'ANY',
     'chromeOptions': {
-      'args': [ '--headless', '--disable-gpu' ]
+      args: ['--headless', '--no-sandbox', '--disable-gpu']
     }
   },
   // -----------------------------------------------------------------
