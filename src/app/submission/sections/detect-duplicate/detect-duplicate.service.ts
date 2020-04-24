ef6726d549529589243ad72865af3dbba443b351
Matteo Perelli <matteo.perelli@4science.it>
2020-04-24 16:42:15 +0200

    Comments added for the detect-duplicate.service

--- src/app/submission/sections/detect-duplicate/detect-duplicate.service.ts
+++ src/app/submission/sections/detect-duplicate/detect-duplicate.service.ts
@@ -9,12 +9,28 @@ import { submissionSectionDataFromIdSelector } from '../../selectors';
 import { WorkspaceitemSectionDetectDuplicateObject } from '../../../core/submission/models/workspaceitem-section-deduplication.model';
 import { isEmpty, isNotEmpty } from '../../../shared/empty.util';
 
+/**
+ * A service that provides methods used in the deduplication process.
+ */
 @Injectable()
 export class DetectDuplicateService {
 
+  /**
+   * Initialize service variables.
+   * @param {Store<SubmissionState>} store
+   */
   constructor(private store: Store<SubmissionState>) {
   }
 
+  /**
+   * Get the list of the possible duplications.
+   * @param {string} submissionId
+   *    The submission id
+   * @param {string} sectionId
+   *    The section id
+   * @return Observable<Object>
+   *    Returns the list of the possible duplications
+   */
   getDuplicateMatches(submissionId: string, sectionId: string) {
     return this.store.pipe(
       select(submissionSectionDataFromIdSelector(submissionId, sectionId)),
@@ -29,6 +45,17 @@ export class DetectDuplicateService {
       distinctUntilChanged());
   }
 
+  /**
+   * Get the list of the possible duplications for a specific submission scope.
+   * @param {string} submissionId
+   *    The submission id
+   * @param {string} sectionId
+   *    The section id
+   * @param {boolean} isWorkFlow
+   *    If TRUE the submission scope is the 'workflow'; 'workspace' otherwise.
+   * @return Observable<Object>
+   *    Returns the list of the possible duplications
+   */
   getDuplicateMatchesByScope(submissionId: string, sectionId: string, isWorkFlow: boolean) {
     return this.getDuplicateMatches(submissionId, sectionId).pipe(
       map((item: WorkspaceitemSectionDetectDuplicateObject) => {
@@ -52,12 +79,28 @@ export class DetectDuplicateService {
     );
   }
 
+  /**
+   * Get the count of the possible duplications.
+   * @param {string} submissionId
+   *    The submission id
+   * @param {string} sectionId
+   *    The section id
+   * @return Observable<number>
+   *    Returns the number of the possible duplications
+   */
   getDuplicateTotalMatches(submissionId: string, sectionId: string) {
     return this.getDuplicateMatches(submissionId, sectionId).pipe(
       map((sectionData: WorkspaceitemSectionDetectDuplicateObject) => Object.keys(sectionData.matches).length),
       distinctUntilChanged());
   }
 
+  /**
+   * Save the decision into the store.
+   * @param {string} submissionId
+   *    The submission id
+   * @param {string} sectionId
+   *    The section id
+   */
   saveDuplicateDecision(submissionId: string, sectionId: string): void {
     this.store.dispatch(new SetDuplicateDecisionAction(submissionId, sectionId));
   }
