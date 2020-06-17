752cf9778789d587b550b17f7cbd1641bbc3d6b9
Kristof De Langhe <kristof.delanghe@atmire.com>
2020-06-17 13:12:49 +0200

    71380: Fix tests

--- src/app/shared/pagination-drag-and-drop/abstract-paginated-drag-and-drop-list.component.spec.ts
+++ src/app/shared/pagination-drag-and-drop/abstract-paginated-drag-and-drop-list.component.spec.ts
@@ -52,10 +52,8 @@ describe('AbstractPaginatedDragAndDropListComponent', () => {
 
   beforeEach(() => {
     objectUpdatesService = jasmine.createSpyObj('objectUpdatesService', {
-      initializeWithCustomOrder: {},
-      addPageToCustomOrder: {},
-      getFieldUpdatesByCustomOrder: observableOf(updates),
-      saveMoveFieldUpdate: {}
+      initialize: {},
+      getFieldUpdatesExclusive: observableOf(updates)
     });
     elRef = {
       nativeElement: jasmine.createSpyObj('nativeElement', {
@@ -71,56 +69,14 @@ describe('AbstractPaginatedDragAndDropListComponent', () => {
     component.ngOnInit();
   });
 
-  it('should call initializeWithCustomOrder to initialize the first page and add it to initializedPages', (done) => {
-    expect(component.initializedPages.indexOf(0)).toBeLessThan(0);
-    component.updates$.pipe(take(1)).subscribe(() => {
-      expect(objectUpdatesService.initializeWithCustomOrder).toHaveBeenCalled();
-      expect(component.initializedPages.indexOf(0)).toBeGreaterThanOrEqual(0);
-      done();
-    });
+  it('should call initialize to initialize the objects in the store', () => {
+    expect(objectUpdatesService.initialize).toHaveBeenCalled();
   });
 
   it('should initialize the updates correctly', (done) => {
     component.updates$.pipe(take(1)).subscribe((fieldUpdates) => {
       expect(fieldUpdates).toEqual(updates);
       done();
-    });
-  });
-
-  describe('when a new page is loaded', () => {
-    const page = 5;
-
-    beforeEach((done) => {
-      component.updates$.pipe(take(1)).subscribe(() => {
-        component.currentPage$.next(page);
-        objectsRD$.next(objectsRD);
-        done();
-      });
-    });
-
-    it('should call addPageToCustomOrder to initialize the new page and add it to initializedPages', (done) => {
-      component.updates$.pipe(take(1)).subscribe(() => {
-        expect(objectUpdatesService.addPageToCustomOrder).toHaveBeenCalled();
-        expect(component.initializedPages.indexOf(page - 1)).toBeGreaterThanOrEqual(0);
-        done();
-      });
-    });
-
-    describe('twice', () => {
-      beforeEach((done) => {
-        component.updates$.pipe(take(1)).subscribe(() => {
-          component.currentPage$.next(page);
-          objectsRD$.next(objectsRD);
-          done();
-        });
-      });
-
-      it('shouldn\'t call addPageToCustomOrder again, as the page has already been initialized', (done) => {
-        component.updates$.pipe(take(1)).subscribe(() => {
-          expect(objectUpdatesService.addPageToCustomOrder).toHaveBeenCalledTimes(1);
-          done();
-        });
-      });
     });
   });
 
@@ -149,30 +105,31 @@ describe('AbstractPaginatedDragAndDropListComponent', () => {
 
       beforeEach(() => {
         elRef.nativeElement.querySelector.and.returnValue(hoverElement);
-        component.initializedPages.push(hoverPage - 1);
+        spyOn(component.dropObject, 'emit');
         component.drop(event);
       });
 
-      it('should detect the page and set currentPage$ to its value', () => {
-        expect(component.currentPage$.value).toEqual(hoverPage);
-      });
-
-      it('should detect the page and update the pagination component with its value', () => {
-        expect(paginationComponent.doPageChange).toHaveBeenCalledWith(hoverPage);
-      });
-
-      it('should send out a saveMoveFieldUpdate with the correct values', () => {
-        expect(objectUpdatesService.saveMoveFieldUpdate).toHaveBeenCalledWith(url, event.previousIndex, 0, 0, hoverPage - 1, object1);
+      it('should send out a dropObject event with the expected processed paginated indexes', () => {
+        expect(component.dropObject.emit).toHaveBeenCalledWith(Object.assign({
+          fromIndex: ((component.currentPage$.value - 1) * component.pageSize) + event.previousIndex,
+          toIndex: ((hoverPage - 1) * component.pageSize),
+          finish: jasmine.anything()
+        }));
       });
     });
 
     describe('when the user is not hovering over a new page', () => {
       beforeEach(() => {
+        spyOn(component.dropObject, 'emit');
         component.drop(event);
       });
 
-      it('should send out a saveMoveFieldUpdate with the correct values', () => {
-        expect(objectUpdatesService.saveMoveFieldUpdate).toHaveBeenCalledWith(url, event.previousIndex, event.currentIndex, 0, 0);
+      it('should send out a dropObject event with the expected properties', () => {
+        expect(component.dropObject.emit).toHaveBeenCalledWith(Object.assign({
+          fromIndex: event.previousIndex,
+          toIndex: event.currentIndex,
+          finish: jasmine.anything()
+        }));
       });
     });
   });
