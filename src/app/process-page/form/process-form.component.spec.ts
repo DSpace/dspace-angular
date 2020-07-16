98eb0aac000ede72944ba96428e940c8b43092f2
Giuseppe Digilio <giuseppe.digilio@4science.it>
2020-07-16 13:08:17 +0200

    [CST-3088] Fixed travis failure

--- src/app/process-page/form/process-form.component.spec.ts
+++ src/app/process-page/form/process-form.component.spec.ts
@@ -76,8 +76,9 @@ describe('ProcessFormComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create', (done) => {
     expect(component).toBeTruthy();
+    done();
   });
 
   it('should call invoke on the scriptService on submit', () => {
