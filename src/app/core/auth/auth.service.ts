6fecdfadb2521a1d6269170579ec3025eeff4090
Giuseppe Digilio <giuseppe.digilio@4science.it>
2020-07-21 18:53:09 +0200

    Fixed issue that redirect always to home page after checking authentication on CSR

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -423,6 +423,7 @@ export class AuthService {
         } else {
           // If redirectUrl is empty use history.
           this.routeService.getHistory().pipe(
+            filter((history) => history.length > 0),
             take(1)
           ).subscribe((history) => {
             let redirUrl;
