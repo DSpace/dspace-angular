bbc7844b5dbbc74306bea027647e41c98d03fbdb
Art Lowel <art@atmire.com>
2020-08-28 11:58:43 +0200

    fix lgtm issues

--- src/app/+admin/admin-search-page/admin-search-results/admin-search-result-grid-element/item-search-result/item-admin-search-result-grid-element.component.ts
+++ src/app/+admin/admin-search-page/admin-search-results/admin-search-result-grid-element/item-search-result/item-admin-search-result-grid-element.component.ts
@@ -4,21 +4,11 @@ import { ViewMode } from '../../../../../core/shared/view-mode.model';
 import { getListableObjectComponent, listableObjectComponent } from '../../../../../shared/object-collection/shared/listable-object/listable-object.decorator';
 import { Context } from '../../../../../core/shared/context.model';
 import { ItemSearchResult } from '../../../../../shared/object-collection/shared/item-search-result.model';
-import { URLCombiner } from '../../../../../core/url-combiner/url-combiner';
 import { SearchResultGridElementComponent } from '../../../../../shared/object-grid/search-result-grid-element/search-result-grid-element.component';
 import { TruncatableService } from '../../../../../shared/truncatable/truncatable.service';
 import { BitstreamDataService } from '../../../../../core/data/bitstream-data.service';
 import { GenericConstructor } from '../../../../../core/shared/generic-constructor';
 import { ListableObjectDirective } from '../../../../../shared/object-collection/shared/listable-object/listable-object.directive';
-import { getItemEditRoute } from '../../../../../+item-page/item-page-routing-paths';
-import {
-  ITEM_EDIT_MOVE_PATH,
-  ITEM_EDIT_DELETE_PATH,
-  ITEM_EDIT_PUBLIC_PATH,
-  ITEM_EDIT_PRIVATE_PATH,
-  ITEM_EDIT_REINSTATE_PATH,
-  ITEM_EDIT_WITHDRAW_PATH
-} from '../../../../../+item-page/edit-item-page/edit-item-page.routing-paths';
 
 @listableObjectComponent(ItemSearchResult, ViewMode.GridElement, Context.AdminSearch)
 @Component({
