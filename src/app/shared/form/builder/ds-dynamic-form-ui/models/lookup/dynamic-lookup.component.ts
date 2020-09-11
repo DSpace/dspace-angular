1e8669a73812e807e6656e474be8395ab337f2ce
Giuseppe Digilio <giuseppe.digilio@4science.it>
2020-09-11 15:30:38 +0200

    Fixed issue with change not detected

--- src/app/shared/form/builder/ds-dynamic-form-ui/models/lookup/dynamic-lookup.component.ts
+++ src/app/shared/form/builder/ds-dynamic-form-ui/models/lookup/dynamic-lookup.component.ts
@@ -290,6 +290,7 @@ export class DsDynamicLookupComponent extends DsDynamicVocabularyComponent imple
       } else {
         this.firstInputValue = displayValue || '';
       }
+      this.cdr.detectChanges();
     }
   }
 
