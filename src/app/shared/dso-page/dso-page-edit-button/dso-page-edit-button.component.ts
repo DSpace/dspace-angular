18577f016c9098529ba28beefbcb63c2eaaa9f23
Kristof De Langhe <kristof.delanghe@atmire.com>
2020-10-27 10:46:33 +0100

    74200: DsoPageEditButtonComponent test cases

--- src/app/shared/dso-page/dso-page-edit-button/dso-page-edit-button.component.ts
+++ src/app/shared/dso-page/dso-page-edit-button/dso-page-edit-button.component.ts
@@ -9,14 +9,29 @@ import { FeatureID } from '../../../core/data/feature-authorization/feature-id';
   templateUrl: './dso-page-edit-button.component.html',
   styleUrls: ['./dso-page-edit-button.component.scss']
 })
+/**
+ * Display a button linking to the edit page of a DSpaceObject
+ */
 export class DsoPageEditButtonComponent implements OnInit {
-
+  /**
+   * The DSpaceObject to display a button to the edit page for
+   */
   @Input() dso: DSpaceObject;
 
+  /**
+   * The prefix of the route to the edit page (before the object's UUID, e.g. "items")
+   */
   @Input() pageRoutePrefix: string;
 
+  /**
+   * A message for the tooltip on the button
+   * Supports i18n keys
+   */
   @Input() tooltipMsg: string;
 
+  /**
+   * Whether or not the current user is authorized to edit the DSpaceObject
+   */
   isAuthorized$: Observable<boolean>;
 
   constructor(protected authorizationService: AuthorizationDataService) { }
