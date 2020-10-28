371b8e72f79480ff6ced42c357cdc35cbfb5143a
Dániel Péter Sipos <dsipos@monguz.hu>
2020-10-28 16:13:41 +0100

    #674 remove async

--- src/app/shared/object-grid/grid-thumbnail/grid-thumbnail.component.ts
+++ src/app/shared/object-grid/grid-thumbnail/grid-thumbnail.component.ts
@@ -33,7 +33,6 @@ export class GridThumbnailComponent implements OnInit {
 
   ngOnInit(): void {
     this.src$ = new BehaviorSubject<string>(this.defaultImage);
-    // console.log('this.thumbnail', this.thumbnail);
     if (isObservable(this.thumbnail)) {
       this.thumbnail.subscribe((thumbnailRD) => {
         this.checkThumbnail(thumbnailRD.payload);
