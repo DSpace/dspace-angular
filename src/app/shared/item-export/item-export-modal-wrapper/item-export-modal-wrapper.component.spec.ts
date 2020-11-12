58cc40a903d15c16ee923ee951cce02a33f40550
Giuseppe Digilio <giuseppe.digilio@4science.it>
2020-11-12 14:21:52 +0100

    Fixed errors with test

--- src/app/shared/item-export/item-export-modal-wrapper/item-export-modal-wrapper.component.spec.ts
+++ src/app/shared/item-export/item-export-modal-wrapper/item-export-modal-wrapper.component.spec.ts
@@ -1,16 +1,34 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ItemExportModalWrapperComponent } from './item-export-modal-wrapper.component';
+import { AuthService } from '../../../core/auth/auth.service';
+import { AuthServiceMock } from '../../mocks/auth.service.mock';
+import { ViewContainerRef } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 describe('ItemExportModalWrapperComponent', () => {
   let component: ItemExportModalWrapperComponent;
   let fixture: ComponentFixture<ItemExportModalWrapperComponent>;
 
+  const modalService = {
+    open: () => {
+      return {
+        result: new Promise((res, rej) => {/****/
+        })
+      };
+    }
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ItemExportModalWrapperComponent ]
+      declarations: [ItemExportModalWrapperComponent],
+      providers: [
+        { provide: AuthService, useClass: AuthServiceMock },
+        { provide: NgbModal, useValue: modalService },
+        ViewContainerRef
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
