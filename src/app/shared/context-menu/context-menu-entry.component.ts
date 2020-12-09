e6dcdf8c02519772b586c8367b5c0cc85951cafd
Giuseppe Digilio <giuseppe.digilio@4science.it>
2020-12-09 19:18:10 +0100

    [CST-3617] create context-menu component

---
+++ src/app/shared/context-menu/context-menu-entry.component.ts
@@ -0,0 +1,33 @@
+import { Component, Inject } from '@angular/core';
+
+import { DSpaceObject } from '../../core/shared/dspace-object.model';
+import { DSpaceObjectType } from '../../core/shared/dspace-object-type.model';
+
+/**
+ * This component renders a context menu option that provides the links to edit item page.
+ */
+@Component({
+  selector: 'ds-context-menu-entry',
+  template: ''
+})
+export abstract class ContextMenuEntryComponent {
+
+  /**
+   * The related dso
+   */
+  contextMenuObject: DSpaceObject;
+
+  /**
+   * The related dso type
+   */
+  contextMenuObjectType: DSpaceObjectType;
+
+  constructor(
+    @Inject('contextMenuObjectProvider') protected injectedContextMenuObject: DSpaceObject,
+    @Inject('contextMenuObjectTypeProvider') protected injectedContextMenuObjectType: DSpaceObjectType
+  ) {
+    this.contextMenuObject = injectedContextMenuObject
+    this.contextMenuObjectType = injectedContextMenuObjectType
+  }
+
+}
