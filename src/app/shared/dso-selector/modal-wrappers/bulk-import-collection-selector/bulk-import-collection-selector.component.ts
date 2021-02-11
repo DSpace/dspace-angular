b99f1539b501a435d684e49c7f2f34ffe03063c5
Corrado Lombardi <corrado.lombardi@4science.it>
2021-02-11 11:35:26 +0100

    updated imports

--- src/app/shared/dso-selector/modal-wrappers/bulk-import-collection-selector/bulk-import-collection-selector.component.ts
+++ src/app/shared/dso-selector/modal-wrappers/bulk-import-collection-selector/bulk-import-collection-selector.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { getBulkImportRoute } from 'src/app/app-routing-paths';
-import { Collection } from 'src/app/core/shared/collection.model';
-import { DSpaceObjectType } from 'src/app/core/shared/dspace-object-type.model';
-import { DSpaceObject } from 'src/app/core/shared/dspace-object.model';
+import { getBulkImportRoute } from '../../../../app-routing-paths';
+import { Collection } from '../../../../core/shared/collection.model';
+import { DSpaceObjectType } from '../../../../core/shared/dspace-object-type.model';
+import { DSpaceObject } from '../../../../core/shared/dspace-object.model';
 import { DSOSelectorModalWrapperComponent, SelectorActionType } from '../dso-selector-modal-wrapper.component';
 
 @Component({
