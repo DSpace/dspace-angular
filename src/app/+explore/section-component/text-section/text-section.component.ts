171256da430792e34ad561f925295f88b2414715
Corrado Lombardi <corrado.lombardi@4science.it>
2021-02-15 14:55:20 +0100

    [CSTPER-421] removed container from text section as we can use directly its content

--- src/app/+explore/section-component/text-section/text-section.component.ts
+++ src/app/+explore/section-component/text-section/text-section.component.ts
@@ -1,23 +1,17 @@
 import { Observable, of } from 'rxjs';
-import { TextBoxSection, TextRowSection } from './../../../core/layout/models/section.model';
+import { TextRowSection } from './../../../core/layout/models/section.model';
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'ds-text-section',
   templateUrl: './text-section.component.html'
 })
-export class TextSectionComponent implements OnInit {
+export class TextSectionComponent {
 
   @Input()
   sectionId: string;
 
   @Input()
-  textBoxSection: TextBoxSection;
-
-  textRows$: Observable<TextRowSection[]>;
-
-  ngOnInit() {
-    this.textRows$ = of(this.textBoxSection.textRows);
-  }
+  textRowSection: TextRowSection;
 
 }
