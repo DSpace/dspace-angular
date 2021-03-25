6cc56176b2b721ae1259be57effc0fbd0f208fbb
lotte <lotte_hofstede@hotmail.com>
2021-03-25 17:27:34 +0100

    fixed path for custom theme item-page

--- src/themes/custom/app/+item-page/simple/item-page.component.ts
+++ src/themes/custom/app/+item-page/simple/item-page.component.ts
@@ -9,7 +9,7 @@ import { fadeInOut } from '../../../../../app/shared/animations/fade';
  */
 @Component({
   selector: 'ds-item-page',
-  // styleUrls: ['../item-page.component.scss'],
+  // styleUrls: ['./item-page.component.scss'],
   styleUrls: ['../../../../../app/+item-page/simple/item-page.component.scss'],
   // templateUrl: './item-page.component.html',
   templateUrl: '../../../../../app/+item-page/simple/item-page.component.html',
