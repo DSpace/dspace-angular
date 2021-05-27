bcfb890e1aa1dae27e87fd57afc1cd6232532f0e
Yura Bondarenko <yura.bondarenko@atmire.com>
2021-05-27 16:34:52 +0200

    79597: Update unit tests

--- src/app/thumbnail/thumbnail.component.spec.ts
+++ src/app/thumbnail/thumbnail.component.spec.ts
@@ -5,6 +5,10 @@ import { Bitstream } from '../core/shared/bitstream.model';
 import { SafeUrlPipe } from '../shared/utils/safe-url-pipe';
 
 import { ThumbnailComponent } from './thumbnail.component';
+import { RemoteData } from '../core/data/remote-data';
+import {
+  createFailedRemoteDataObject, createPendingRemoteDataObject, createSuccessfulRemoteDataObject,
+} from '../shared/remote-data.utils';
 
 // tslint:disable-next-line:pipe-prefix
 @Pipe({ name: 'translate' })
@@ -28,40 +32,12 @@ describe('ThumbnailComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ThumbnailComponent);
-    comp = fixture.componentInstance; // BannerComponent test instance
+    comp = fixture.componentInstance; // ThumbnailComponent test instance
     de = fixture.debugElement.query(By.css('div.thumbnail'));
     el = de.nativeElement;
   });
 
-  describe('when the thumbnail exists', () => {
-    it('should display an image', () => {
-      const thumbnail = new Bitstream();
-      thumbnail._links = {
-        self: { href: 'self.url' },
-        bundle: { href: 'bundle.url' },
-        format: { href: 'format.url' },
-        content: { href: 'content.url' },
-      };
-      comp.thumbnail = thumbnail;
-      fixture.detectChanges();
-      const image: HTMLElement = de.query(By.css('img')).nativeElement;
-      expect(image.getAttribute('src')).toBe(comp.thumbnail._links.content.href);
-    });
-    it('should include the alt text', () => {
-      const thumbnail = new Bitstream();
-      thumbnail._links = {
-        self: { href: 'self.url' },
-        bundle: { href: 'bundle.url' },
-        format: { href: 'format.url' },
-        content: { href: 'content.url' },
-      };
-      comp.thumbnail = thumbnail;
-      fixture.detectChanges();
-      const image: HTMLElement = de.query(By.css('img')).nativeElement;
-      expect(image.getAttribute('alt')).toBe('TRANSLATED ' + comp.alt);
-    });
-  });
-  describe(`when the thumbnail doesn't exist`, () => {
+  const withoutThumbnail = () => {
     describe('and there is a default image', () => {
       it('should display the default image', () => {
         comp.src = 'http://bit.stream';
@@ -73,6 +49,7 @@ describe('ThumbnailComponent', () => {
         comp.src = 'http://bit.stream';
         comp.defaultImage = 'http://default.img';
         comp.errorHandler();
+        comp.ngOnChanges();
         fixture.detectChanges();
         const image: HTMLElement = de.query(By.css('img')).nativeElement;
         expect(image.getAttribute('alt')).toBe('TRANSLATED ' + comp.alt);
@@ -84,10 +61,98 @@ describe('ThumbnailComponent', () => {
         comp.errorHandler();
         expect(comp.src).toBe(null);
 
+        comp.ngOnChanges();
         fixture.detectChanges();
         const placeholder = fixture.debugElement.query(By.css('div.thumbnail-placeholder')).nativeElement;
         expect(placeholder.innerHTML).toBe('TRANSLATED ' + comp.placeholder);
       });
+    });
+  };
+
+  describe('with thumbnail as Bitstream', () => {
+    let thumbnail: Bitstream;
+    beforeEach(() => {
+      thumbnail = new Bitstream();
+      thumbnail._links = {
+        self: { href: 'self.url' },
+        bundle: { href: 'bundle.url' },
+        format: { href: 'format.url' },
+        content: { href: 'content.url' },
+      };
+    });
+
+    it('should display an image', () => {
+      comp.thumbnail = thumbnail;
+      comp.ngOnChanges();
+      fixture.detectChanges();
+      const image: HTMLElement = de.query(By.css('img')).nativeElement;
+      expect(image.getAttribute('src')).toBe(comp.thumbnail._links.content.href);
+    });
+
+    it('should include the alt text', () => {
+      comp.thumbnail = thumbnail;
+      comp.ngOnChanges();
+      fixture.detectChanges();
+      const image: HTMLElement = de.query(By.css('img')).nativeElement;
+      expect(image.getAttribute('alt')).toBe('TRANSLATED ' + comp.alt);
+    });
+
+    describe('when there is no thumbnail', () => {
+      withoutThumbnail();
+    });
+  });
+
+  describe('with thumbnail as RemoteData<Bitstream>', () => {
+    let thumbnail: RemoteData<Bitstream>;
+
+    describe('while loading', () => {
+      beforeEach(() => {
+        thumbnail = createPendingRemoteDataObject();
+      });
+
+      it('should show a loading animation', () => {
+        comp.thumbnail = thumbnail;
+        comp.ngOnChanges();
+        fixture.detectChanges();
+        expect(de.query(By.css('ds-loading'))).toBeTruthy();
+      });
+    });
+
+    describe('when there is a thumbnail', () => {
+      beforeEach(() => {
+        const bitstream = new Bitstream();
+        bitstream._links = {
+          self: { href: 'self.url' },
+          bundle: { href: 'bundle.url' },
+          format: { href: 'format.url' },
+          content: { href: 'content.url' },
+        };
+        thumbnail = createSuccessfulRemoteDataObject(bitstream);
+      });
+
+      it('should display an image', () => {
+        comp.thumbnail = thumbnail;
+        comp.ngOnChanges();
+        fixture.detectChanges();
+        const image: HTMLElement = de.query(By.css('img')).nativeElement;
+        expect(image.getAttribute('src')).toBe(comp.thumbnail.payload._links.content.href);
+      });
+
+      it('should display the alt text', () => {
+        comp.thumbnail = thumbnail;
+        comp.ngOnChanges();
+        fixture.detectChanges();
+        const image: HTMLElement = de.query(By.css('img')).nativeElement;
+        expect(image.getAttribute('alt')).toBe('TRANSLATED ' + comp.alt);
+      });
+    });
+
+    describe('when there is no thumbnail', () => {
+      beforeEach(() => {
+        thumbnail = createFailedRemoteDataObject();
+      });
+
+      withoutThumbnail();
     });
   });
 });
