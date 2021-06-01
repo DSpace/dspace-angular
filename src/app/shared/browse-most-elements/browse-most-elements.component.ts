f04338b60670df7a59acf86da5161e0c3cde5c8a
Alessandro Martelli <alessandro.martelli@4science.it>
2021-06-01 12:19:16 +0200

    [DSC-30] fixed change detection error

--- src/app/shared/browse-most-elements/browse-most-elements.component.ts
+++ src/app/shared/browse-most-elements/browse-most-elements.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 import { SearchService } from '../../core/shared/search/search.service';
 import { PaginatedSearchOptions } from '../search/paginated-search-options.model';
 import { DSpaceObject } from '../../core/shared/dspace-object.model';
@@ -6,6 +6,7 @@ import { SearchResult } from '../search/search-result.model';
 import { Context } from '../../core/shared/context.model';
 import { RemoteData } from '../../core/data/remote-data';
 import { PaginatedList } from '../../core/data/paginated-list.model';
+import { getFirstCompletedRemoteData } from '../../core/shared/operators';
 
 @Component({
   selector: 'ds-browse-most-elements',
@@ -21,13 +22,15 @@ export class BrowseMostElementsComponent implements OnInit {
 
   searchResults: RemoteData<PaginatedList<SearchResult<DSpaceObject>>>;
 
-  constructor(private searchService: SearchService) { /* */ }
+  constructor(private searchService: SearchService, private cdr: ChangeDetectorRef) { /* */ }
 
   ngOnInit() {
 
-    this.searchService.search(this.paginatedSearchOptions)
-      .subscribe((response: RemoteData<PaginatedList<SearchResult<DSpaceObject>>>) => {
+    this.searchService.search(this.paginatedSearchOptions).pipe(
+      getFirstCompletedRemoteData(),
+    ).subscribe((response: RemoteData<PaginatedList<SearchResult<DSpaceObject>>>) => {
           this.searchResults = response as any;
+          this.cdr.detectChanges();
       });
   }
 
