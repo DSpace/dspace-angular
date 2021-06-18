c696b783931ccd0cbf35fc019a2a33338c9ad6f7
Marie Verdonck <maria.verdonck@atmire.com>
2021-06-18 13:24:34 +0200

    79700: idle time and grace period testing times removed

--- src/environments/environment.common.ts
+++ src/environments/environment.common.ts
@@ -47,11 +47,9 @@ export const environment: GlobalConfig = {
     // Authority UI settings
     ui: {
       // the amount of time before the idle warning is shown
-      // timeUntilIdle: 15 * 60 * 1000, // 15 minutes
-      timeUntilIdle: 30 * 1000, // 30 seconds
+      timeUntilIdle: 15 * 60 * 1000, // 15 minutes
       // the amount of time the user has to react after the idle warning is shown before they are logged out.
-      // idleGracePeriod: 5 * 60 * 1000, // 5 minutes
-      idleGracePeriod: 1 * 60 * 1000, // 1 minute
+      idleGracePeriod: 5 * 60 * 1000, // 5 minutes
     },
     // Authority REST settings
     rest: {
