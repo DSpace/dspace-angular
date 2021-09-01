4642935e4a87497a6c988383a23e6661feb01186
Davide Negretti <davide.negretti@4science.it>
2021-09-01 11:36:12 +0200

    [DSC-99] Added VALUEPAIR rendering (Not working if user is not authenticated - Tests TBD)

--- src/app/layout/default-layout/boxes/components/valuepair/valuepair.component.ts
+++ src/app/layout/default-layout/boxes/components/valuepair/valuepair.component.ts
@@ -6,14 +6,14 @@ import {FieldRenderingType, MetadataBoxFieldRendering} from '../metadata-box.dec
 import {RenderingTypeModelComponent} from '../rendering-type.model';
 import {VocabularyService} from '../../../../../core/submission/vocabularies/vocabulary.service';
 import {VocabularyOptions} from '../../../../../core/submission/vocabularies/models/vocabulary-options.model';
-import {getFirstSucceededRemoteDataPayload} from '../../../../../core/shared/operators';
+import {getFirstCompletedRemoteData, getFirstSucceededRemoteDataPayload} from '../../../../../core/shared/operators';
 import {catchError} from 'rxjs/operators';
 import {of} from 'rxjs';
 import {hasNoValue} from '../../../../../shared/empty.util';
+import {HttpErrorResponse} from '@angular/common/http';
 
 /**
- * This component renders the links metadata fields.
- * The metadata value is used for href and text
+ * This component renders the valuepair (value + display) metadata fields.
  */
 @Component({
   // tslint:disable-next-line: component-selector
@@ -42,29 +42,23 @@ export class ValuepairComponent extends RenderingTypeModelComponent implements O
       itemsToBeRendered = [...this.metadataValues];
     }
 
-    let vocabularyOptions;
+    // let vocabularyOptions;
 
     this.item.owningCollection.pipe(
       getFirstSucceededRemoteDataPayload(),
     ).subscribe((collection) => {
 
-      vocabularyOptions = new VocabularyOptions(null, this.field.metadata, collection.uuid);
+      const vocabularyOptions = new VocabularyOptions(null, this.field.metadata, collection.uuid);
       this.vocabularyService.searchVocabularyByMetadataAndCollection(vocabularyOptions).pipe(
-        getFirstSucceededRemoteDataPayload(),
-        catchError(() => of(null))
+        getFirstCompletedRemoteData(),
       ).subscribe((vocabulary) => {
 
-        // TODO: fix catch error
-
-        console.log('Vocabulary = ' + JSON.stringify(vocabulary));
-
-        if (hasNoValue(vocabulary)) {
-          console.log('Items: ' + JSON.stringify(itemsToBeRendered));
+        if (vocabulary.hasFailed) {
           this.values = itemsToBeRendered;
           return;
         }
 
-        vocabularyOptions.name = vocabulary.name;
+        vocabularyOptions.name = vocabulary.payload.name;
 
         itemsToBeRendered.forEach((metadataValue) => {
           this.vocabularyService.getVocabularyEntryByValue(metadataValue, vocabularyOptions).subscribe(
