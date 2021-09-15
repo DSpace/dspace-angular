051682f0b489cd74ac937768759b86ab054690d4
Giuseppe Digilio <giuseppe.digilio@4science.it>
2021-09-15 16:25:16 +0200

    [CST-4551] Refactoring valuepair.component in  order to remove nested subscriptions

--- src/app/layout/default-layout/boxes/components/valuepair/valuepair.component.ts
+++ src/app/layout/default-layout/boxes/components/valuepair/valuepair.component.ts
@@ -1,14 +1,17 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import {TranslateService} from '@ngx-translate/core';
+import { TranslateService } from '@ngx-translate/core';
+import { BehaviorSubject, from, interval, race } from 'rxjs';
+import { filter, map, mapTo, mergeMap, reduce, switchMap, take } from 'rxjs/operators';
 
-import {FieldRenderingType, MetadataBoxFieldRendering} from '../metadata-box.decorator';
-import {RenderingTypeModelComponent} from '../rendering-type.model';
-import {VocabularyService} from '../../../../../core/submission/vocabularies/vocabulary.service';
-import {VocabularyOptions} from '../../../../../core/submission/vocabularies/models/vocabulary-options.model';
-import {getFirstCompletedRemoteData, getFirstSucceededRemoteDataPayload} from '../../../../../core/shared/operators';
-import {AuthService} from '../../../../../core/auth/auth.service';
-import {take} from 'rxjs/operators';
+import { FieldRenderingType, MetadataBoxFieldRendering } from '../metadata-box.decorator';
+import { RenderingTypeModelComponent } from '../rendering-type.model';
+import { VocabularyService } from '../../../../../core/submission/vocabularies/vocabulary.service';
+import { VocabularyOptions } from '../../../../../core/submission/vocabularies/models/vocabulary-options.model';
+import { getFirstSucceededRemoteDataPayload } from '../../../../../core/shared/operators';
+import { AuthService } from '../../../../../core/auth/auth.service';
+import { Collection } from '../../../../../core/shared/collection.model';
+import { Vocabulary } from '../../../../../core/submission/vocabularies/models/vocabulary.model';
 
 /**
  * This component renders the valuepair (value + display) metadata fields.
@@ -25,7 +28,7 @@ export class ValuepairComponent extends RenderingTypeModelComponent implements O
   /**
    * list of values
    */
-  values: string[] = [];
+  values: BehaviorSubject<string[]> = new BehaviorSubject<string[]>([]);
 
   constructor(
     protected translateService: TranslateService,
@@ -35,7 +38,6 @@ export class ValuepairComponent extends RenderingTypeModelComponent implements O
   }
 
   ngOnInit(): void {
-    const values = [];
     let itemsToBeRendered = [];
     if (this.indexToBeRendered >= 0) {
       itemsToBeRendered.push(this.metadataValues[this.indexToBeRendered]);
@@ -43,45 +45,36 @@ export class ValuepairComponent extends RenderingTypeModelComponent implements O
       itemsToBeRendered = [...this.metadataValues];
     }
 
-    this.authService.isAuthenticated().pipe(take(1)).subscribe((isAuth) => {
-        if (isAuth) {
-          this.item.owningCollection.pipe(
-            getFirstSucceededRemoteDataPayload(),
-          ).subscribe((collection) => {
-
-            const vocabularyOptions = new VocabularyOptions(null, this.field.metadata, collection.uuid);
-            this.vocabularyService.searchVocabularyByMetadataAndCollection(vocabularyOptions).pipe(
-              getFirstCompletedRemoteData(),
-            ).subscribe((vocabulary) => {
-
-              if (vocabulary.hasFailed) {
-                // In case of error, show fallback values
-                console.warn('Retrieving vocabulary has failed');
-                this.values = itemsToBeRendered;
-                return;
-              }
-
-              vocabularyOptions.name = vocabulary.payload.name;
-
-              itemsToBeRendered.forEach((metadataValue) => {
-                this.vocabularyService.getVocabularyEntryByValue(metadataValue, vocabularyOptions).subscribe(
-                  (entry) => {
-                    this.values.push(entry.display);
-                  }
+    const entries$ = this.authService.isAuthenticated().pipe(
+      take(1),
+      filter((isAuth) => isAuth),
+      switchMap(() => this.item.owningCollection.pipe(
+        getFirstSucceededRemoteDataPayload(),
+      )),
+      switchMap((collection: Collection) => {
+        const vocabularyOptions = new VocabularyOptions(null, this.field.metadata, collection.uuid);
+        return this.vocabularyService.searchVocabularyByMetadataAndCollection(vocabularyOptions).pipe(
+          getFirstSucceededRemoteDataPayload(),
+          mergeMap((vocabulary: Vocabulary) => {
+            vocabularyOptions.name = vocabulary.name;
+            return from(itemsToBeRendered).pipe(
+              mergeMap((metadataValue) => {
+                return this.vocabularyService.getVocabularyEntryByValue(metadataValue, vocabularyOptions).pipe(
+                  map((entry) => entry.display)
                 );
-              });
-
-              this.values = values;
-
-            });
-          });
-        } else {
-          // If user is not authenticated, show fallback values
-          console.warn('User not authorized');
-          this.values = itemsToBeRendered;
-        }
-      }
+              }),
+              reduce((acc: any, value: any) => [...acc, value], []),
+            );
+          })
+        );
+      })
     );
+    const initValues$ = interval(5000).pipe(mapTo(itemsToBeRendered));
+
+    race([entries$, initValues$]).pipe(take(1))
+      .subscribe((values: string[]) => {
+        this.values.next(values);
+      });
 
   }
 
