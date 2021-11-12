8cea23cf5e05e27e01430df49d63083ecca428a3
Davide Negretti <davide.negretti@4science.it>
2021-11-12 12:22:47 +0100

    [DSC-314] pending-changes.guard and submission-edit-can-deactivate.service test (WIP)

--- src/app/submission/edit/submission-edit-can-deactivate.service.spec.ts
+++ src/app/submission/edit/submission-edit-can-deactivate.service.spec.ts
@@ -1,16 +1,42 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SubmissionEditCanDeactivateService } from './submission-edit-can-deactivate.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SubmissionService } from '../submission.service';
+import { of } from 'rxjs';
 
-describe('SubmissionEditCanDeactivateService', () => {
+fdescribe('SubmissionEditCanDeactivateService', () => {
   let service: SubmissionEditCanDeactivateService;
+  let submissionService: SubmissionService;
+
+  const submissionServiceSpy = jasmine.createSpyObj('submissionService', ['hasUnsavedModification']);
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      declarations: [],
+      imports: [],
+      providers: [
+        { provide: ActivatedRoute, useValue: {} },
+        { provide: Router, useValue: {} },
+        { provide: SubmissionService, useValue: {} },
+      ]
+    });
     service = TestBed.inject(SubmissionEditCanDeactivateService);
+    submissionService = TestBed.inject(SubmissionService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('when there are unsaved changes', () => {
+    beforeEach(() => {
+      submissionServiceSpy.hasUnsavedModification.and.returnValue(of(false));
+    });
+    it('should ...', () => {
+      // TODO
+      expect(false).toBeTrue();
+    });
+  });
+
 });
