25a51c97644a81cef0504faf11859074711a477b
Giuseppe Digilio <giuseppe.digilio@4science.it>
2021-11-15 18:52:33 +0100

    [DSC-287] Add test to guard

--- src/app/core/server-check/server-check.guard.spec.ts
+++ src/app/core/server-check/server-check.guard.spec.ts
@@ -1,16 +1,55 @@
-import { TestBed } from '@angular/core/testing';
-
 import { ServerCheckGuard } from './server-check.guard';
+import { createFailedRemoteDataObject$, createSuccessfulRemoteDataObject$ } from '../../shared/remote-data.utils';
+import { first } from 'rxjs/operators';
+import { getPageInternalServerErrorRoute } from '../../app-routing-paths';
 
 describe('ServerCheckGuard', () => {
   let guard: ServerCheckGuard;
+  let router;
+  let rootDataServiceStub: any;
+
+  rootDataServiceStub = jasmine.createSpyObj('RootDataService', {
+    findRoot: jasmine.createSpy('findRoot')
+  });
+  router = jasmine.createSpyObj('Router', {
+    navigateByUrl: jasmine.createSpy('navigateByUrl')
+  });
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    guard = TestBed.inject(ServerCheckGuard);
+    guard = new ServerCheckGuard(router, rootDataServiceStub);
   });
 
   it('should be created', () => {
     expect(guard).toBeTruthy();
+  });
+
+  describe('when root endpoint has succeeded', () => {
+    beforeEach(() => {
+      rootDataServiceStub.findRoot.and.returnValue(createSuccessfulRemoteDataObject$({}));
+    });
+
+    it('should not redirect to error page', () => {
+      guard.canActivate({} as any, {} as any).pipe(
+        first()
+      ).subscribe((canActivate: boolean) => {
+        expect(canActivate).toEqual(true);
+        expect(router.navigateByUrl).not.toHaveBeenCalled();
+      });
+    });
+  });
+
+  describe('when root endpoint has not succeeded', () => {
+    beforeEach(() => {
+      rootDataServiceStub.findRoot.and.returnValue(createFailedRemoteDataObject$());
+    });
+
+    it('should redirect to error page', () => {
+      guard.canActivate({} as any, {} as any).pipe(
+        first()
+      ).subscribe((canActivate: boolean) => {
+        expect(canActivate).toEqual(false);
+        expect(router.navigateByUrl).toHaveBeenCalledWith(getPageInternalServerErrorRoute());
+      });
+    });
   });
 });
