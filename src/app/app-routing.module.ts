f672e12433dd3118331ab5ed197a3b281c61d8e0
Tim Donohue <tim.donohue@lyrasis.org>
2021-12-14 09:40:00 -0600

    Remove unnecessary relativeLinkResolution: 'legacy'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -203,7 +203,6 @@ import { GroupAdministratorGuard } from './core/data/feature-authorization/featu
       ]}
     ],{
     onSameUrlNavigation: 'reload',
-    relativeLinkResolution: 'legacy'
 })
   ],
   exports: [RouterModule],
