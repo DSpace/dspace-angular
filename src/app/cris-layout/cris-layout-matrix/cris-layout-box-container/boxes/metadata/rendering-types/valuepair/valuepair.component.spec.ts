76cc75dcc466b095b4f03711c50f6dccb7d3c9de
Alessandro Martelli <alessandro.martelli@4science.it>
2021-12-28 14:57:11 +0100

    [DSC-327] fix test

--- src/app/cris-layout/cris-layout-matrix/cris-layout-box-container/boxes/metadata/rendering-types/valuepair/valuepair.component.spec.ts
+++ src/app/cris-layout/cris-layout-matrix/cris-layout-box-container/boxes/metadata/rendering-types/valuepair/valuepair.component.spec.ts
@@ -8,7 +8,8 @@ import { Item } from '../../../../../../../core/shared/item.model';
 import { EMPTY, of } from 'rxjs';
 import {AuthService} from '../../../../../../../core/auth/auth.service';
 import {AuthServiceStub} from '../../../../../../../shared/testing/auth-service.stub';
-import {LayoutField} from '../../../../../../../core/layout/models/box.model';
+import { LayoutField } from '../../../../../../../core/layout/models/box.model';
+import { DsDatePipe } from '../../../../../../pipes/ds-date.pipe';
 
 const METADATA_KEY_1 = 'person.knowsLanguage';
 const METADATA_KEY_2 = 'dc.type';
@@ -16,7 +17,7 @@ const METADATA_KEY_2 = 'dc.type';
 const VOCABULARY_NAME_1 = 'common_iso_languages';
 const VOCABULARY_NAME_2 = 'types';
 
-xdescribe('ValuepairComponent', () => {
+describe('ValuepairComponent', () => {
 
   let component: ValuepairComponent;
   let fixture: ComponentFixture<ValuepairComponent>;
@@ -28,25 +29,13 @@ xdescribe('ValuepairComponent', () => {
     switch (key) {
       case METADATA_KEY_1:
         return [
-          {
-            value: 'it',
-            authority: null,
-          },
-          {
-            value: 'en',
-            authority: null,
-          }
+          { value: 'it', authority: null },
+          { value: 'en', authority: null }
         ];
       case METADATA_KEY_2:
         return [
-          {
-            value: undefined,
-            authority: VOCABULARY_NAME_2 + ':asd',
-          },
-          {
-            value: undefined,
-            authority: VOCABULARY_NAME_2 + ':fgh',
-          },
+          { value: undefined, authority: VOCABULARY_NAME_2 + ':asd' },
+          { value: undefined, authority: VOCABULARY_NAME_2 + ':fgh' },
         ];
       default:
         return [];
@@ -96,41 +85,37 @@ xdescribe('ValuepairComponent', () => {
 
   const vocabularyServiceSpy = jasmine.createSpyObj('vocabularyService', { getPublicVocabularyEntryByValue: EMPTY });
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        TranslateModule.forRoot({
-          loader: {
-            provide: TranslateLoader,
-            useClass: TranslateLoaderMock
-          }
-        }),
-      ],
-      declarations: [ValuepairComponent],
-      providers: [
-        {provide: VocabularyService, useValue: vocabularyServiceSpy}
-      ],
-    }).compileComponents();
-
-    vocabularyService = TestBed.inject(VocabularyService);
-
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ValuepairComponent);
-    component = fixture.componentInstance;
-  });
-
-
   describe('when the vocabulary has no authority', () => {
 
-    // test item 1
+    beforeEach(waitForAsync(() => {
+      TestBed.configureTestingModule({
+        imports: [
+          TranslateModule.forRoot({
+            loader: {
+              provide: TranslateLoader,
+              useClass: TranslateLoaderMock
+            }
+          }),
+        ],
+        declarations: [ValuepairComponent, DsDatePipe],
+        providers: [
+          { provide: VocabularyService, useValue: vocabularyServiceSpy },
+          { provide: AuthService, useValue: authService },
+          { provide: 'fieldProvider', useValue: testField1 },
+          { provide: 'itemProvider', useValue: testItem1 },
+          { provide: 'metadataValueProvider', useValue: { value: 'it', authority: null } },
+          { provide: 'renderingSubTypeProvider', useValue: VOCABULARY_NAME_1 },
+        ],
+      }).compileComponents();
+
+      vocabularyService = TestBed.inject(VocabularyService);
+
+    }));
 
     beforeEach(() => {
-      component.item = testItem1;
-      component.field = testField1;
-      component.renderingSubType = VOCABULARY_NAME_1;
-      // vocabularyServiceSpy.getPublicVocabularyEntryByValue.and.callFake(vocabularyEntriesMock);
+      fixture = TestBed.createComponent(ValuepairComponent);
+      component = fixture.componentInstance;
+      vocabularyServiceSpy.getPublicVocabularyEntryByValue.and.callFake(vocabularyEntriesMock);
       fixture.detectChanges();
     });
 
@@ -141,19 +126,43 @@ xdescribe('ValuepairComponent', () => {
 
     it('should ...', () => {
       expect(vocabularyService.getPublicVocabularyEntryByValue).toHaveBeenCalledWith(VOCABULARY_NAME_1, 'it');
-      expect(vocabularyService.getPublicVocabularyEntryByValue).toHaveBeenCalledWith(VOCABULARY_NAME_1, 'en');
     });
+
 
   });
 
+
   describe('when the vocabulary has an authority', () => {
 
-    // test item 2
+    beforeEach(waitForAsync(() => {
+      TestBed.configureTestingModule({
+        imports: [
+          TranslateModule.forRoot({
+            loader: {
+              provide: TranslateLoader,
+              useClass: TranslateLoaderMock
+            }
+          }),
+        ],
+        declarations: [ValuepairComponent, DsDatePipe],
+        providers: [
+          { provide: VocabularyService, useValue: vocabularyServiceSpy },
+          { provide: AuthService, useValue: authService },
+          { provide: 'fieldProvider', useValue: testField2 },
+          { provide: 'itemProvider', useValue: testItem2 },
+          { provide: 'metadataValueProvider', useValue: {value: undefined, authority: VOCABULARY_NAME_2 + ':asd' } },
+          { provide: 'renderingSubTypeProvider', useValue: VOCABULARY_NAME_2 },
+        ],
+      }).compileComponents();
+
+      vocabularyService = TestBed.inject(VocabularyService);
+
+    }));
 
     beforeEach(() => {
-      component.item = testItem2;
-      component.field = testField2;
-      component.renderingSubType = VOCABULARY_NAME_2;
+      fixture = TestBed.createComponent(ValuepairComponent);
+      component = fixture.componentInstance;
+      vocabularyServiceSpy.getPublicVocabularyEntryByValue.and.callFake(vocabularyEntriesMock);
       fixture.detectChanges();
     });
 
@@ -161,10 +170,12 @@ xdescribe('ValuepairComponent', () => {
       expect(component).toBeTruthy();
     });
 
+
     it('should ...', () => {
       expect(vocabularyService.getPublicVocabularyEntryByValue).toHaveBeenCalledWith(VOCABULARY_NAME_2, 'asd');
-      expect(vocabularyService.getPublicVocabularyEntryByValue).toHaveBeenCalledWith(VOCABULARY_NAME_2, 'fgh');
     });
 
+
   });
+
 });
