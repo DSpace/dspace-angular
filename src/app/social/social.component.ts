fef6c2e15f0d34e1674858323a066a3def2bb273
Alessandro Martelli <alessandro.martelli@4science.it>
2021-12-30 16:09:23 +0100

    [DSC-254] implemented social.service.ts to evaluate whether display social buttons or not

--- src/app/social/social.component.ts
+++ src/app/social/social.component.ts
@@ -1,13 +1,11 @@
 import {
   Component,
-  Inject,
+  Inject, OnDestroy,
   OnInit,
 } from '@angular/core';
-import {environment} from '../../environments/environment.common';
-import {DOCUMENT} from '@angular/common';
-import {CookieService} from '../core/services/cookie.service';
-import {filter, map, mergeMap} from 'rxjs/operators';
-import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
+import { DOCUMENT } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { SocialService } from './social.service';
 
 @Component({
   selector: 'ds-social',
@@ -17,92 +15,45 @@ import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
 /**
  * Component to render dinamically the social2 buttons using addThis plugin
  */
-export class SocialComponent implements OnInit {
-  /**
-   * The official site ID of addthis plugin
-   */
-  idSocial: string = environment.addThisPlugin.siteId;
-  /**
-   * The official site url of script
-   */
-  urlSocialScript: string = environment.addThisPlugin.scriptUrl;
+export class SocialComponent implements OnInit, OnDestroy {
+
   /**
    * The script containing the profile ID
    */
   script: HTMLScriptElement;
-  /**
-   * Boolean to show or not the social buttons
-   */
-  showSocialButtons = false;
-  /**
-   * The social2 buttons shown in ui
-   */
-  socialButtons: HTMLElement;
-  /**
-   * Boolean that represents if the script is uploaded in dom
-   */
-  appended = false;
-  shareAccepted = environment.addThisPlugin.socialNetworksEnabled;
+
+  subscription;
+
   constructor(@Inject(DOCUMENT) private _document: Document,
-              protected cookie: CookieService,
+              private socialService: SocialService,
               private activatedRoute: ActivatedRoute,
-              private router: Router,
   ) {
-    const klaroCookie = this.cookie.get('klaro-anonymous');
-    if (klaroCookie && klaroCookie['add-this']) {
-      this.shareAccepted = true;
-    }
-    environment.addThisPlugin.socialNetworksEnabled = this.shareAccepted;
   }
 
   ngOnInit() {
-    if (!this.shareAccepted) {
-      return;
-    }
-    if (!this.script) {
-      this.initializeScript();
-    }
-    this.router.events.pipe(
-      filter(events => events instanceof NavigationEnd),
-      map(evt => this.activatedRoute),
-      map(route => {
-        while (route.firstChild) {
-          route = route.firstChild;
-        }
-        return route;
-      }))
-      .pipe(
-        filter(route => route.outlet === 'primary'),
-        mergeMap(route => route.data)
-      ).subscribe(route => {
-      if (route.showSocialButtons !== undefined) {
-        if (route.showSocialButtons) {
-          if (this.appended) {
-            this.showOrHideSocialButtons('block');
-          } else {
-            this._document.body.appendChild(this.script);
-            this.appended = true;
-          }
-        } else {
-          this.showOrHideSocialButtons('none');
-        }
+    this.subscription = this.socialService.showSocialButtons(this.activatedRoute).subscribe((show) => {
+      if (show) {
+        this.showSocialButtons();
       } else {
-        this.socialButtons = this._document.querySelector('#at-expanding-share-button');
-        if (this.socialButtons) {
-          this.socialButtons.style.display = 'none';
-        }
+        this.hideSocialButtons();
       }
     });
   }
-  public initializeScript() {
-    this.script = this._document.createElement('script');
-    this.script.type = 'text/javascript';
-    this.script.src = this.urlSocialScript + this.idSocial;
-  }
-  showOrHideSocialButtons(style) {
-    this.socialButtons = this._document.querySelector('#at-expanding-share-button');
-    if (this.socialButtons) {
-      this.socialButtons.style.display = style;
+
+  showSocialButtons() {
+    // console.log('Showing social buttons');
+    if (!this.script) {
+      this.script = this.socialService.initializeAddThisScript(this._document);
     }
+    this.socialService.show(this._document);
   }
+
+  hideSocialButtons() {
+    this.socialService.hide(this._document);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe()
+  }
+
 }
