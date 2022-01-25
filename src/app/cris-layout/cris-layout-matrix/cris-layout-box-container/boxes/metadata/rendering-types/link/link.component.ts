2366df5a9817073bee5e0328af1481bccd723f38
Davide Negretti <davide.negretti@4science.com>
2022-01-25 18:17:47 +0100

    [CST-4936] Render links without 'http://' (cherry-pickable)

--- src/app/cris-layout/cris-layout-matrix/cris-layout-box-container/boxes/metadata/rendering-types/link/link.component.ts
+++ src/app/cris-layout/cris-layout-matrix/cris-layout-box-container/boxes/metadata/rendering-types/link/link.component.ts
@@ -66,7 +66,7 @@ export class LinkComponent extends RenderingTypeValueModelComponent implements O
         linkText = (hasValue(this.renderingSubType) &&
         this.renderingSubType.toUpperCase() === TYPES.EMAIL) ? this.metadataValue.value : this.translateService.instant(this.field.label);
     } else {
-        metadataValue = this.metadataValue.value;
+        metadataValue = this.metadataValue.value.search('://') > 0 ? this.metadataValue.value : 'http://' + this.metadataValue.value;
         linkText = (hasValue(this.renderingSubType) &&
         this.renderingSubType.toUpperCase() === TYPES.LABEL) ? this.translateService.instant(this.field.label) : this.metadataValue.value;
     }
