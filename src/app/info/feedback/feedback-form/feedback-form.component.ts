6bbc7f96c27d5c1014cb8c8ca53910027a3e2f11
Giuseppe Digilio <giuseppe.digilio@4science.it>
2022-01-27 16:35:32 +0100

    [CST-4875] remove unused imports

--- src/app/info/feedback/feedback-form/feedback-form.component.ts
+++ src/app/info/feedback/feedback-form/feedback-form.component.ts
@@ -9,7 +9,7 @@ import { TranslateService } from '@ngx-translate/core';
 import { AuthService } from '../../../core/auth/auth.service';
 import { EPerson } from '../../../core/eperson/models/eperson.model';
 import { getFirstCompletedRemoteData } from '../../../core/shared/operators';
-import { Router, UrlSegmentGroup } from '@angular/router';
+import { Router } from '@angular/router';
 import { getHomePageRoute } from '../../../app-routing-paths';
 import { take } from 'rxjs/operators';
 import { NativeWindowRef, NativeWindowService } from '../../../core/services/window.service';
