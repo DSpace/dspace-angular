ac8e25e2d4907a0252ad671543ca7716b176138f
Davide Negretti <davide.negretti@4science.it>
2022-01-31 17:59:22 +0100

    [DSC-448] Statistics labels fixed

--- src/app/core/statistics/models/usage-report.model.ts
+++ src/app/core/statistics/models/usage-report.model.ts
@@ -6,6 +6,7 @@ import { HALResource } from '../../shared/hal-resource.model';
 import { USAGE_REPORT } from './usage-report.resource-type';
 import { HALLink } from '../../shared/hal-link.model';
 import { deserialize, autoserializeAs } from 'cerialize';
+import { StatisticsType } from '../../../statistics-page/cris-statistics-page/statistics-type.model';
 
 /**
  * A usage report.
@@ -27,7 +28,7 @@ export class UsageReport extends HALResource {
   id: string;
 
   @autoserializeAs('view-mode')
-  viewMode: string;
+  viewMode: StatisticsType;
 
   @autoserializeAs('report-type')
   reportType: string;
