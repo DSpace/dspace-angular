8a19fbe26a0d0aac917ce30b2774b5a954275deb
Alisa Ismailati <alisa.ismailati@atis.al>
2022-02-03 10:26:55 +0100

    DSC-451 convert to link labels in statistics page

--- src/app/statistics-page/cris-statistics-page/statistics-chart/statistics-table/statistics-table.component.ts
+++ src/app/statistics-page/cris-statistics-page/statistics-chart/statistics-table/statistics-table.component.ts
@@ -1,3 +1,4 @@
+import { EntityTypeEnum } from './../../../../cris-layout/enums/entity-type.enum';
 import { Component, OnInit } from '@angular/core';
 
 import { StatisticsType } from '../../statistics-type.model';
@@ -5,39 +6,48 @@ import { renderChartFor } from '../../cris-statistics-element-decorator';
 import { StatisticsChartDataComponent } from '../statistics-chart-data/statistics-chart-data.component';
 
 @Component({
-  selector: 'ds-statistics-table',
-  templateUrl: './statistics-table.component.html',
-  styleUrls: ['./statistics-table.component.scss']
+	selector: 'ds-statistics-table',
+	templateUrl: './statistics-table.component.html',
+	styleUrls: ['./statistics-table.component.scss'],
 })
-
 /**
  * Component that represents a table for report
  */
 @renderChartFor(StatisticsType.table)
 export class StatisticsTableComponent extends StatisticsChartDataComponent implements OnInit {
+	/**
+	 * Boolean indicating whether the usage report has data
+	 */
+	hasData: boolean;
 
+	/**
+	 * The table headers
+	 */
+	headers: string[];
 
-  /**
-   * Boolean indicating whether the usage report has data
-   */
-  hasData: boolean;
+	/**
+	 *Array to store entity types that need to be converted to link,
+	 *in order to check from the template if point's label it should be a simple label or a link
+	 * @memberof StatisticsTableComponent
+	 */
+	entityTypesToConvertToLink = [
+		EntityTypeEnum.Item,
+		EntityTypeEnum.Bitstream,
+		EntityTypeEnum.Collection,
+		EntityTypeEnum.Community,
+	];
 
-  /**
-   * The table headers
-   */
-  headers: string[];
-
-
-  /**
-   * Check if report has information and if data is present to show in the view
-   * Insert table headers
-   */
-  ngOnInit() {
-    this.hasData = !!this.report && this.report.points.length > 0;
-    if (this.hasData) {
-      this.headers = [this.report.points[0].type, Object.keys(this.report.points[0].values)[0]];
-    }
-  }
-
-
+	/**
+	 * Check if report has information and if data is present to show in the view
+	 * Insert table headers
+	 */
+	ngOnInit() {
+		this.hasData = !!this.report && this.report.points.length > 0;
+		if (this.hasData) {
+			this.headers = [
+				this.report.points[0].type,
+				Object.keys(this.report.points[0].values)[0],
+			];
+		}
+	}
 }
---
+++ src/app/statistics-page/cris-statistics-page/statistics-pipes/create-link.pipe.ts
@@ -0,0 +1,38 @@
+import { EntityTypeEnum } from './../../../cris-layout/enums/entity-type.enum';
+import { Point } from './../../../core/statistics/models/usage-report.model';
+import { Pipe, PipeTransform } from '@angular/core';
+import { getItemPageRoute } from 'src/app/item-page/item-page-routing-paths';
+import { Item } from 'src/app/core/shared/item.model';
+import { getBitstreamDownloadRoute } from 'src/app/app-routing-paths';
+import { getCommunityPageRoute } from 'src/app/community-page/community-page-routing-paths';
+import { getCollectionPageRoute } from 'src/app/collection-page/collection-page-routing-paths';
+
+@Pipe({
+	name: 'createLink',
+})
+export class CreateLinkPipe implements PipeTransform {
+	transform(value: Point): string {
+		if (value) {
+			let link = '';
+			switch (value.type) {
+				case EntityTypeEnum.Community:
+					link = getCommunityPageRoute(value.id);
+					break;
+				case EntityTypeEnum.Collection:
+					link = getCollectionPageRoute(value.id);
+					break;
+				case EntityTypeEnum.Item:
+					const item = new Item();
+					item.id = value.id;
+					link = getItemPageRoute(item);
+					break;
+				case EntityTypeEnum.Bitstream:
+					link = getBitstreamDownloadRoute(value);
+					break;
+				default:
+					return link;
+			}
+			return link;
+		}
+	}
+}
