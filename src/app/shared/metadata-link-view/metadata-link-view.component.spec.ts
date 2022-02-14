99333223fac528b0cc8c667b13f07890f7e85d4c
Giuseppe Digilio <giuseppe.digilio@4science.it>
2022-02-14 10:21:23 +0100

    [DSC-456] Fix test

--- src/app/shared/metadata-link-view/metadata-link-view.component.spec.ts
+++ src/app/shared/metadata-link-view/metadata-link-view.component.spec.ts
@@ -1,18 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { of } from 'rxjs';
+import { v4 as uuidv4 } from 'uuid';
 
 import { MetadataLinkViewComponent } from './metadata-link-view.component';
 import { ItemDataService } from '../../core/data/item-data.service';
-import { of } from 'rxjs';
 import { Item } from '../../core/shared/item.model';
 import { createSuccessfulRemoteDataObject$ } from '../remote-data.utils';
-import { By } from '@angular/platform-browser';
 import { SharedModule } from '../shared.module';
-import { RouterTestingModule } from '@angular/router/testing';
+import { MetadataValue } from '../../core/shared/metadata.models';
+import SpyObj = jasmine.SpyObj;
 
 describe('MetadataLinkViewComponent', () => {
   let component: MetadataLinkViewComponent;
   let fixture: ComponentFixture<MetadataLinkViewComponent>;
-  let itemService: ItemDataService;
+  let itemService: SpyObj<ItemDataService>;
+  const validAuthority = uuidv4();
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -30,27 +35,27 @@ describe('MetadataLinkViewComponent', () => {
     bundles: of({}),
     metadata: {
       'dspace.entity.type': [
-        {
+        Object.assign(new MetadataValue(), {
           value: 'Person'
-        }
+        })
       ],
       'person.orgunit.id': [
-        {
+        Object.assign(new MetadataValue(), {
           value: 'OrgUnit',
           authority: '2'
-        }
+        })
       ],
       'person.identifier.orcid': [
-        {
+        Object.assign(new MetadataValue(), {
           language: 'en_US',
           value: '0000-0001-8918-3592'
-        }
+        })
       ],
       'cris.orcid.authenticated': [
-        {
+        Object.assign(new MetadataValue(), {
           language: null,
           value: 'authenticated'
-        }
+        })
       ]
     }
   });
@@ -60,20 +65,20 @@ describe('MetadataLinkViewComponent', () => {
     bundles: of({}),
     metadata: {
       'dspace.entity.type': [
-        {
+        Object.assign(new MetadataValue(), {
           value: 'OrgUnit'
-        }
+        })
       ],
       'orgunit.person.id': [
-        {
+        Object.assign(new MetadataValue(), {
           value: 'Person',
           authority: '1'
-        }
+        })
       ],
     }
   });
 
-  const testMetadataValueWithoutAuthority = Object.assign(new Item(), {
+  const testMetadataValueWithoutAuthority = Object.assign(new MetadataValue(), {
     authority:null,
     confidence:-1,
     language:null,
@@ -82,28 +87,23 @@ describe('MetadataLinkViewComponent', () => {
     value:'Università degli Studi di Milano Bicocca',
   });
 
-  const testMetadataValueWithAuthority = Object.assign(new Item(), {
-    authority:'1',
-    confidence:-1,
+  const testMetadataValueWithAuthority = Object.assign(new MetadataValue(), {
+    authority: validAuthority,
+    confidence: 600,
     language:null,
     place:0,
     uuid:'56e99d82-2cae-4cce-8d12-39899dea7c72',
     value:'Università degli Studi di Milano Bicocca',
   });
 
-  itemService = Object.assign( {
-    findById: (id: string) => {
-      if (id === '1') {
-        return createSuccessfulRemoteDataObject$(testPerson);
-      } else {
-        return createSuccessfulRemoteDataObject$(testOrgunit);
-      }
-    }
+  itemService = jasmine.createSpyObj('ItemDataService', {
+    findById: jasmine.createSpy('findById')
   });
 
   describe('Check metadata without authority', () => {
     beforeEach(() => {
       fixture = TestBed.createComponent(MetadataLinkViewComponent);
+      itemService.findById.and.returnValue(createSuccessfulRemoteDataObject$(testOrgunit));
       component = fixture.componentInstance;
       component.item = testPerson;
       component.metadata = testMetadataValueWithoutAuthority;
@@ -131,6 +131,7 @@ describe('MetadataLinkViewComponent', () => {
   describe('Check metadata with authority', () => {
     beforeEach(() => {
       fixture = TestBed.createComponent(MetadataLinkViewComponent);
+      itemService.findById.and.returnValue(createSuccessfulRemoteDataObject$(testPerson));
       component = fixture.componentInstance;
       component.item = testPerson;
       component.metadata = testMetadataValueWithAuthority;
