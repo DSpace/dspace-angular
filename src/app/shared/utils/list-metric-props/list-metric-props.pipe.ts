f30e34d3f7a2556fb3ca6620dff7eec1a38d5602
Alisa Ismailati <alisa.ismailati@atis.al>
2022-02-15 16:03:58 +0100

    [DSC-463] metric-dimension

--- src/app/shared/utils/list-metric-props/list-metric-props.pipe.ts
+++ src/app/shared/utils/list-metric-props/list-metric-props.pipe.ts
@@ -6,9 +6,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ListMetricPropsPipe implements PipeTransform {
   transform(remark: any, property: string, isListElement: boolean): string {
     if (isListElement) {
-      return remark[`list-${property}`];
+      return remark[`list-${property}`] ? remark[`list-${property}`] : null;
     } else {
-      return remark[property];
+      return remark[property] ? remark[property] : null;
     }
   }
 }
