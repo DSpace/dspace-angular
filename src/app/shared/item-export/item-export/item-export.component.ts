6e85df2d16b34b4ca7f0a8b27170379b873bf065
Davide Negretti <davide.negretti@4science.it>
2022-02-17 12:35:40 +0100

    [DSC-92] Bulk item export

--- src/app/shared/item-export/item-export/item-export.component.ts
+++ src/app/shared/item-export/item-export/item-export.component.ts
@@ -9,6 +9,9 @@ import { ItemExportFormConfiguration, ItemExportService } from '../item-export.s
 import { ItemExportFormatMolteplicity } from '../../../core/itemexportformat/item-export-format.service';
 import { NotificationsService } from '../../notifications/notifications.service';
 import { TranslateService } from '@ngx-translate/core';
+import { ItemExportFormat } from '../../../core/itemexportformat/model/item-export-format.model';
+import { DSpaceObjectType } from '../../../core/shared/dspace-object-type.model';
+import { isNotEmpty } from '../../empty.util';
 
 @Component({
   selector: 'ds-item-export',
@@ -16,12 +19,38 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class ItemExportComponent implements OnInit {
 
+  /**
+   * Export format suitable for bulk import
+   */
+  public static BULK_IMPORT_READY_XLS_SUFFIX = '-bulkimportready-xls';
+
   @Input() molteplicity: ItemExportFormatMolteplicity;
   @Input() item: Item;
   @Input() searchOptions: SearchOptions;
 
   public configuration: ItemExportFormConfiguration;
   public exportForm: FormGroup;
+
+  /**
+   * When true, show collection selector
+   */
+  selectCollection = false;
+
+  /**
+   * The selected entity type.
+   * This is used by ds-administered-collection-selector when the "bulk import ready" format has been chosen.
+   */
+  selectedEntityType: string;
+
+  /**
+   * The UUID of the selected collection. This is needed by the "bulk import ready" format.
+   */
+  bulkImportXlsEntityTypeCollectionUUID: string;
+
+  /**
+   * This is used by ds-administered-collection-selector when the "bulk import ready" format has been chosen.
+   */
+  bulkImportXlsCollectionSelector = [DSpaceObjectType.COLLECTION];
 
   constructor(protected itemExportService: ItemExportService,
               protected router: Router,
@@ -46,6 +75,18 @@ export class ItemExportComponent implements OnInit {
   onEntityTypeChange(entityType: string) {
     this.itemExportService.onSelectEntityType(this.configuration.entityTypes, entityType).pipe(take(1)).subscribe((configuration) => {
       this.configuration = configuration;
+
+      // Add a new Excel export format suitable for bulk import
+      this.selectedEntityType = entityType;
+      const xlsConfigurationFormat: ItemExportFormat = Object.assign(new ItemExportFormat(), {
+        type: 'itemexportformat',
+        id: entityType.toLowerCase() + ItemExportComponent.BULK_IMPORT_READY_XLS_SUFFIX,
+        mimeType: 'application/vnd.ms-excel',
+        entityType: entityType,
+        molteplicity: 'MULTIPLE',
+      });
+      this.configuration.formats.push(xlsConfigurationFormat);
+
       this.exportForm.controls.format.patchValue(this.configuration.format);
     });
   }
@@ -57,20 +98,41 @@ export class ItemExportComponent implements OnInit {
     });
   }
 
+  onCollectionSelect(collection) {
+    this.bulkImportXlsEntityTypeCollectionUUID = collection.uuid;
+    this.selectCollection = true;
+  }
+
   onSubmit() {
     if (this.exportForm.valid) {
-      this.itemExportService.submitForm(
-        this.molteplicity,
-        this.item,
-        this.searchOptions,
-        this.exportForm.value.entityType,
-        this.exportForm.value.format).pipe(take(1)).subscribe((processId) => {
+      if (this.exportForm.value.format.id.indexOf(ItemExportComponent.BULK_IMPORT_READY_XLS_SUFFIX) > 0 && !this.selectCollection) {
+        // if the "bulk import" format has been chosen, show the collection selection form
+        this.selectCollection = true;
+      } else {
+        // select the collection and submit
 
-        const title = this.translate.get('item-export.process.title');
-        this.notificationsService.process(processId.toString(),5000, title);
+        if (isNotEmpty(this.bulkImportXlsEntityTypeCollectionUUID)) {
+          this.searchOptions.query = `location.coll:${this.bulkImportXlsEntityTypeCollectionUUID}`;
+          console.log(JSON.stringify(this.searchOptions));
+        }
 
-        this.activeModal.close();
-      });
+        this.itemExportService.submitForm(
+          this.molteplicity,
+          this.item,
+          this.searchOptions,
+          this.exportForm.value.entityType,
+          this.exportForm.value.format
+        ).pipe(take(1)).subscribe((processId) => {
+          const title = this.translate.get('item-export.process.title');
+          this.notificationsService.process(processId.toString(),5000, title);
+
+          this.selectCollection = false;
+          this.selectedEntityType = undefined;
+          this.bulkImportXlsEntityTypeCollectionUUID = undefined;
+
+          this.activeModal.close();
+        });
+      }
     }
   }
 
