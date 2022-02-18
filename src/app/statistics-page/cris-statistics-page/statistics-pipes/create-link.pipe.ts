58d93c3bbb5e8057496d224e4c695ce82d9f31ab
Alisa Ismailati <alisa.ismailati@4science.com>
2022-02-18 16:46:20 +0100

    lint fix

--- src/app/statistics-page/cris-statistics-page/statistics-pipes/create-link.pipe.ts
+++ src/app/statistics-page/cris-statistics-page/statistics-pipes/create-link.pipe.ts
@@ -53,7 +53,7 @@ export class CreateLinkPipe implements PipeTransform {
       }
       return link$.pipe(
         map((link: string) => {
-          return link? `${environment.ui.baseUrl}${link}` : '';
+          return link ? `${environment.ui.baseUrl}${link}` : '';
         })
       );
     }
