dc00fd4c759898431686c65fa30cd56fc5012749
Art Lowel <art@atmire.com>
2022-02-22 12:20:19 +0100

    add themed community list component

--- src/themes/custom/theme.module.ts
+++ src/themes/custom/theme.module.ts
@@ -79,6 +79,7 @@ import { HeaderComponent } from './app/header/header.component';
 import { FooterComponent } from './app/footer/footer.component';
 import { BreadcrumbsComponent } from './app/breadcrumbs/breadcrumbs.component';
 import { HeaderNavbarWrapperComponent } from './app/header-nav-wrapper/header-navbar-wrapper.component';
+import { CommunityListComponent } from './app/community-list-page/community-list/community-list.component';
 
 const DECLARATIONS = [
   HomePageComponent,
@@ -119,7 +120,8 @@ const DECLARATIONS = [
   HeaderComponent,
   NavbarComponent,
   HeaderNavbarWrapperComponent,
-  BreadcrumbsComponent
+  BreadcrumbsComponent,
+  CommunityListComponent
 ];
 
 @NgModule({
