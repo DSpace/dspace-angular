d8b520749ff5896a58a52c0337d4881e4b09c01a
emilialobo <Dspace@portatilbiblio.bibliounp.unp.edu.ar>
2022-03-08 17:58:51 -0300

    Archivos de configuración nuevos

--- src/themes/custom-unpsjb/app/header/header.component.ts
+++ src/themes/custom-unpsjb/app/header/header.component.ts
@@ -8,12 +8,8 @@ import { HeaderComponent as BaseComponent } from '../../../../app/header/header.
   selector: 'ds-header',
   // styleUrls: ['header.component.scss'],
   styleUrls: ['../../../../app/header/header.component.scss'],
-  
-  // Descomento the templateUrl which references the "header.component.html" file in your theme directory
-  templateUrl: 'header.component.html',
-  // Comento out the templateUrl which references the default "src/app/header/header.component.html" file.
-  //templateUrl: '../../../../app/header/header.component.html',
-
+  // templateUrl: 'header.component.html',
+  templateUrl: '../../../../app/header/header.component.html',
 })
 export class HeaderComponent extends BaseComponent {
 }
