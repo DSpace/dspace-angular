bfdda43a48acc411ef5e093c96d68ab3e69397c5
Yura Bondarenko <yura.bondarenko@atmire.com>
2022-03-16 18:38:14 +0100

    87968: Update devServer configuration

--- webpack/webpack.browser.ts
+++ webpack/webpack.browser.ts
@@ -6,8 +6,9 @@ import { commonExports } from './webpack.common';
 module.exports = Object.assign({}, commonExports, {
   target: 'web',
   devServer: {
-    before(app, server) {
+    setupMiddlewares(middlewares, server) {
       buildAppConfig(join(process.cwd(), 'src/assets/config.json'));
+      return middlewares;
     }
  }
 });
