feb56f7b36e8a2266c2bb8ab2b34d2ccae1ebc62
emilialobo <Dspace@portatilbiblio.bibliounp.unp.edu.ar>
2022-03-17 20:46:06 -0300

    se corrige archivo es.json5 - se actualiza home-news.component - se agregan imagenes

--- src/themes/custom-unpsjb/app/home-page/home-news/home-news.component.ts
+++ src/themes/custom-unpsjb/app/home-page/home-news/home-news.component.ts
@@ -3,10 +3,10 @@ import { HomeNewsComponent as BaseComponent } from '../../../../../app/home-page
 
 @Component({
   selector: 'ds-home-news',
-  // styleUrls: ['./home-news.component.scss'],
-  styleUrls: ['../../../../../app/home-page/home-news/home-news.component.scss'],
-  // templateUrl: './home-news.component.html'
-  templateUrl: '../../../../../app/home-page/home-news/home-news.component.html'
+  styleUrls: ['./home-news.component.scss'],
+  //styleUrls: ['../../../../../app/home-page/home-news/home-news.component.scss'],
+  templateUrl: './home-news.component.html'
+  //templateUrl: '../../../../../app/home-page/home-news/home-news.component.html'
 })
 
 /**
