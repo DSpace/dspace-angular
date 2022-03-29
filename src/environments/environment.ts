565beb540289d2e345aa1ac517a9cda4b7ff9d8e
MilanMajchrák <milan.majchak@dataquest.sk>
2022-03-29 15:27:58 +0200

    Added Integration test to check footer color. Changed BE API to localhost.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,7 +13,16 @@ export const environment: Partial<AppConfig> = {
     preboot: true,
     async: true,
     time: false
-  }
+  },
+
+  // The REST API server settings.
+  rest: {
+    ssl: false,
+    host: 'localhost',
+    port: 8080,
+    // NOTE: Space is capitalized because 'namespace' is a reserved string in TypeScript
+    nameSpace: '/server',
+  },
 };
 
 /*
