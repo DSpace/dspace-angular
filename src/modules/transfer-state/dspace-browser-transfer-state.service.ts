cc745b422565b9fea1ede6817757bc35c2ac8db0
Yura Bondarenko <yura.bondarenko@atmire.com>
2022-04-27 15:05:19 +0200

    90978: Minor fixes

--- src/modules/transfer-state/dspace-browser-transfer-state.service.ts
+++ src/modules/transfer-state/dspace-browser-transfer-state.service.ts
@@ -7,7 +7,7 @@ import { isNotEmpty } from '../../app/shared/empty.util';
 
 @Injectable()
 export class DSpaceBrowserTransferState extends DSpaceTransferState {
-  async transfer(): Promise<boolean> {
+  transfer(): Promise<boolean> {
     const state = this.transferState.get<any>(DSpaceTransferState.NGRX_STATE, null);
     this.transferState.remove(DSpaceTransferState.NGRX_STATE);
     this.store.dispatch(new StoreAction(StoreActionTypes.REHYDRATE, state));
