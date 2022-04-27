aa78a2991cf7185f6776b2096e81ff62311ec62d
Rezart Vata <rezart.vata@4science.com>
2022-04-27 20:18:33 +0200

    CST-5253] Finished functionalities

--- src/app/workspaceitems-edit-page/workspaceitems-edit-page-routing.module.ts
+++ src/app/workspaceitems-edit-page/workspaceitems-edit-page-routing.module.ts
@@ -4,6 +4,8 @@ import { RouterModule } from '@angular/router';
 import { AuthenticatedGuard } from '../core/auth/authenticated.guard';
 import { ThemedSubmissionEditComponent } from '../submission/edit/themed-submission-edit.component';
 import { I18nBreadcrumbResolver } from '../core/breadcrumbs/i18n-breadcrumb.resolver';
+import { ThemedFullItemPageComponent } from '../item-page/full/themed-full-item-page.component';
+import { ItemFromWorkspaceResolver } from './item-from-workspace.resolver';
 
 @NgModule({
   imports: [
@@ -17,7 +19,17 @@ import { I18nBreadcrumbResolver } from '../core/breadcrumbs/i18n-breadcrumb.reso
           breadcrumb: I18nBreadcrumbResolver
         },
         data: { title: 'submission.edit.title', breadcrumbKey: 'submission.edit' }
-      }
+      },
+      {
+        canActivate: [AuthenticatedGuard],
+        path: ':id/view',
+        component: ThemedFullItemPageComponent,
+        resolve: {
+          dso: ItemFromWorkspaceResolver,
+          breadcrumb: I18nBreadcrumbResolver
+        },
+        data: { title: 'workspace-item.view.title', breadcrumbKey: 'workspace-item.view' }
+      },
     ])
   ]
 })
--- src/app/workspaceitems-edit-page/workspaceitems-edit-page.module.ts
+++ src/app/workspaceitems-edit-page/workspaceitems-edit-page.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { WorkspaceitemsEditPageRoutingModule } from './workspaceitems-edit-page-routing.module';
 import { SubmissionModule } from '../submission/submission.module';
+import { ItemFromWorkspaceResolver } from './item-from-workspace.resolver';
 
 @NgModule({
   imports: [
@@ -11,7 +12,8 @@ import { SubmissionModule } from '../submission/submission.module';
     SharedModule,
     SubmissionModule,
   ],
-  declarations: []
+  declarations: [],
+  providers: [ItemFromWorkspaceResolver]
 })
 /**
  * This module handles all modules that need to access the workspaceitems edit page.
