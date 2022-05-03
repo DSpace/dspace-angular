804930fbe2b5cca46343d24bf7486034cf5cc4f8
Luca Giamminonni <luca.giamminonni@4science.it>
2022-05-03 15:35:51 +0200

    [CST-5307] Improved researcher profile component

--- src/app/entity-groups/research-entities/item-list-elements/search-result-list-elements/person/person-search-result-list-element.component.ts
+++ src/app/entity-groups/research-entities/item-list-elements/search-result-list-elements/person/person-search-result-list-element.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { listableObjectComponent } from '../../../../../shared/object-collection/shared/listable-object/listable-object.decorator';
 import { ViewMode } from '../../../../../core/shared/view-mode.model';
 import { ItemSearchResultListElementComponent } from '../../../../../shared/object-list/search-result-list-element/item-search-result/item-types/item/item-search-result-list-element.component';
+import {TruncatableService} from '../../../../../shared/truncatable/truncatable.service';
+import {DSONameService} from '../../../../../core/breadcrumbs/dso-name.service';
+import {isNotEmpty} from '../../../../../shared/empty.util';
 
 @listableObjectComponent('PersonSearchResult', ViewMode.ListElement)
 @Component({
@@ -14,9 +17,15 @@ import { ItemSearchResultListElementComponent } from '../../../../../shared/obje
  */
 export class PersonSearchResultListElementComponent extends ItemSearchResultListElementComponent {
 
+  public constructor(protected truncatableService: TruncatableService, protected dsoNameService: DSONameService) {
+    super(truncatableService, dsoNameService);
+  }
+
   get name() {
-    return this.value ?
-      this.value :
-      this.firstMetadataValue('person.familyName') + ', ' + this.firstMetadataValue('person.givenName');
+    let personName = this.dsoNameService.getName(this.dso);
+    if (isNotEmpty(this.firstMetadataValue('person.familyName')) && isNotEmpty(this.firstMetadataValue('person.givenName'))) {
+      personName = this.firstMetadataValue('person.familyName') + ', ' + this.firstMetadataValue('person.givenName');
+    }
+    return personName;
   }
 }
