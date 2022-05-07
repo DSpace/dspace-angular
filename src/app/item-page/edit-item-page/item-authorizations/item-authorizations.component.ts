d15277849b7ead1acb703c2da146363022e096dc
Alisa Ismailati <alisa.ismailati@4science.com>
2022-05-07 16:21:08 +0200

    [CST-5677] Item authorization page

--- src/app/item-page/edit-item-page/item-authorizations/item-authorizations.component.ts
+++ src/app/item-page/edit-item-page/item-authorizations/item-authorizations.component.ts
@@ -1,3 +1,5 @@
+import { isEqual } from 'lodash';
+import { DSONameService } from './../../../core/breadcrumbs/dso-name.service';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
@@ -25,7 +27,8 @@ interface BundleBitstreamsMapEntry {
 
 @Component({
   selector: 'ds-item-authorizations',
-  templateUrl: './item-authorizations.component.html'
+  templateUrl: './item-authorizations.component.html',
+  styleUrls:['./item-authorizations.component.scss']
 })
 /**
  * Component that handles the item Authorizations
@@ -36,13 +39,19 @@ export class ItemAuthorizationsComponent implements OnInit, OnDestroy {
    * A map that contains all bitstream of the item's bundles
    * @type {Observable<Map<string, Observable<PaginatedList<Bitstream>>>>}
    */
-  public bundleBitstreamsMap: Map<string, Observable<PaginatedList<Bitstream>>> = new Map<string, Observable<PaginatedList<Bitstream>>>();
+  public bundleBitstreamsMap: Map<string, BitstreamMapValue> = new Map<string, BitstreamMapValue>();
 
   /**
-   * The list of bundle for the item
+   * The list of all bundles for the item
    * @type {Observable<PaginatedList<Bundle>>}
    */
   private bundles$: BehaviorSubject<Bundle[]> = new BehaviorSubject<Bundle[]>([]);
+
+  /**
+   * The list of bundles to be displayed in the template
+   * @type {BehaviorSubject<Bundle[]>}
+   */
+  bundlesToShow$: BehaviorSubject<Bundle[]> = new BehaviorSubject<Bundle[]>([]);
 
   /**
    * The target editing item
@@ -57,6 +66,37 @@ export class ItemAuthorizationsComponent implements OnInit, OnDestroy {
   private subs: Subscription[] = [];
 
   /**
+   * The size of the bundles to be loaded on demand
+   * @type {number}
+   */
+  bunblesPerPage = 6;
+
+  /**
+   * The number of current page
+   * @type {number}
+   */
+  bunblesPageSize = 1;
+
+  /**
+   * The flag to show or not the 'Load more' button
+   * based on the condition if all the bundles are loaded or not
+   * @type {boolean}
+   */
+  allBundlesLoaded = false;
+
+  /**
+   * Initial size of loaded bitstreams
+   * The size of incrementation used in bitstream pagination
+   */
+  bitstreamSize = 4;
+
+  /**
+   * The size of the loaded bitstremas at a certain moment
+   * @private
+   */
+  private bitstreamPageSize = 4;
+
+  /**
    * Initialize instance variables
    *
    * @param {LinkService} linkService
@@ -64,7 +104,8 @@ export class ItemAuthorizationsComponent implements OnInit, OnDestroy {
    */
   constructor(
     private linkService: LinkService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private nameService: DSONameService
   ) {
   }
 
@@ -72,16 +113,53 @@ export class ItemAuthorizationsComponent implements OnInit, OnDestroy {
    * Initialize the component, setting up the bundle and bitstream within the item
    */
   ngOnInit(): void {
+   this.getBundlesPerItem();
+  }
+
+  /**
+   * Return the item's UUID
+   */
+  getItemUUID(): Observable<string> {
+    return this.item$.pipe(
+      map((item: Item) => item.id),
+      first((UUID: string) => isNotEmpty(UUID))
+    );
+  }
+
+  /**
+ * Return the item's name
+ */
+  getItemName(): Observable<string> {
+    return this.item$.pipe(
+      map((item: Item) => this.nameService.getName(item))
+    );
+  }
+
+  /**
+   * Return all item's bundles
+   *
+   * @return an observable that emits all item's bundles
+   */
+  getItemBundles(): Observable<Bundle[]> {
+    return this.bundles$.asObservable();
+  }
+
+  /**
+   * Get all bundles per item
+   * and all the bitstreams per bundle
+   * @param page number of current page
+   */
+  getBundlesPerItem(page: number = 1) {
     this.item$ = this.route.data.pipe(
       map((data) => data.dso),
       getFirstSucceededRemoteDataWithNotEmptyPayload(),
       map((item: Item) => this.linkService.resolveLink(
         item,
-        followLink('bundles', {}, followLink('bitstreams'))
+        followLink('bundles', {findListOptions: {currentPage : page, elementsPerPage: this.bunblesPerPage}}, followLink('bitstreams'))
       ))
     ) as Observable<Item>;
 
-    const bundles$: Observable<PaginatedList<Bundle>> = this.item$.pipe(
+    const bundles$: Observable<PaginatedList<Bundle>> =  this.item$.pipe(
       filter((item: Item) => isNotEmpty(item.bundles)),
       mergeMap((item: Item) => item.bundles),
       getFirstSucceededRemoteDataWithNotEmptyPayload(),
@@ -96,35 +174,38 @@ export class ItemAuthorizationsComponent implements OnInit, OnDestroy {
         take(1),
         map((list: PaginatedList<Bundle>) => list.page)
       ).subscribe((bundles: Bundle[]) => {
-        this.bundles$.next(bundles);
+        if (isEqual(bundles.length,0) || bundles.length < this.bunblesPerPage) {
+          this.allBundlesLoaded = true;
+        }
+        if (isEqual(page, 1)) {
+          this.bundles$.next(bundles);
+          this.bundlesToShow$.next(bundles);
+        } else {
+          this.bundlesToShow$.next(this.bundles$.getValue().concat(bundles));
+          this.bundles$.next(this.bundles$.getValue().concat(bundles));
+        }
       }),
       bundles$.pipe(
         take(1),
         mergeMap((list: PaginatedList<Bundle>) => list.page),
         map((bundle: Bundle) => ({ id: bundle.id, bitstreams: this.getBundleBitstreams(bundle) }))
       ).subscribe((entry: BundleBitstreamsMapEntry) => {
-        this.bundleBitstreamsMap.set(entry.id, entry.bitstreams);
+        let bitstreamMapValues: BitstreamMapValue = {
+          isCollapsed: true,
+          allBitstreamsLoaded: false,
+          bitstreams: null
+        };
+        let bits = entry.bitstreams.pipe(
+          map((b: PaginatedList<Bitstream>) => {
+            bitstreamMapValues.allBitstreamsLoaded = b?.page.length < this.bitstreamSize ;
+            let firstLoaded = [...b.page.slice(0, this.bitstreamSize)];
+            return firstLoaded;
+          })
+        );
+        bitstreamMapValues.bitstreams = bits;
+        this.bundleBitstreamsMap.set(entry.id, bitstreamMapValues);
       })
     );
-  }
-
-  /**
-   * Return the item's UUID
-   */
-  getItemUUID(): Observable<string> {
-    return this.item$.pipe(
-      map((item: Item) => item.id),
-      first((UUID: string) => isNotEmpty(UUID))
-    );
-  }
-
-  /**
-   * Return all item's bundles
-   *
-   * @return an observable that emits all item's bundles
-   */
-  getItemBundles(): Observable<Bundle[]> {
-    return this.bundles$.asObservable();
   }
 
   /**
@@ -143,6 +224,48 @@ export class ItemAuthorizationsComponent implements OnInit, OnDestroy {
   }
 
   /**
+   * Changes the collapsible state of the area that contains the bitstream list
+   * @param bundleId Id of bundle responsible for the requested bitstreams
+   */
+  collapseArea(bundleId: string) {
+    this.bundleBitstreamsMap.get(bundleId).isCollapsed = !this.bundleBitstreamsMap.get(bundleId).isCollapsed;
+  }
+
+  /**
+   * Loads as much bundles as initial value of bundleSize to be displayed
+   */
+  onBunbleLoad(){
+    this.bunblesPageSize ++;
+    this.getBundlesPerItem(this.bunblesPageSize);
+  }
+
+  /**
+   * Calculates the bitstreams that are going to be loaded on demand,
+   * based on the number configured on this.bitstreamSize.
+   * @param bundle parent of bitstreams that are requested to be shown
+   * @returns Subscription
+   */
+  onBitstreamsLoad(bundle: Bundle) {
+   return this.getBundleBitstreams(bundle).pipe(
+      map((res: PaginatedList<Bitstream>) => {
+        let nextBitstreams = res?.page.slice(this.bitstreamPageSize, this.bitstreamPageSize + this.bitstreamSize);
+        let bitstreamsToShow = this.bundleBitstreamsMap.get(bundle.id).bitstreams.pipe(
+          map((existingBits: Bitstream[])=> {
+            return [... existingBits, ...nextBitstreams];
+          })
+        );
+        this.bitstreamPageSize = this.bitstreamPageSize + this.bitstreamSize;
+        let bitstreamMapValues: BitstreamMapValue = {
+          bitstreams: bitstreamsToShow ,
+          isCollapsed: this.bundleBitstreamsMap.get(bundle.id).isCollapsed,
+          allBitstreamsLoaded: res?.page.length <= this.bitstreamPageSize
+        };
+        this.bundleBitstreamsMap.set(bundle.id, bitstreamMapValues);
+      })
+    ).subscribe();
+  }
+
+  /**
    * Unsubscribe from all subscriptions
    */
   ngOnDestroy(): void {
@@ -150,4 +273,10 @@ export class ItemAuthorizationsComponent implements OnInit, OnDestroy {
       .filter((subscription) => hasValue(subscription))
       .forEach((subscription) => subscription.unsubscribe());
   }
+}
+
+export interface BitstreamMapValue {
+  bitstreams: Observable<Bitstream[]>;
+  isCollapsed: boolean;
+  allBitstreamsLoaded: boolean;
 }
