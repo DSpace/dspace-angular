f2e4a005a235f1fe895e2c29f39d2e8138e49826
Kristof De Langhe <kristof.delanghe@atmire.com>
2022-05-10 17:31:43 +0200

    89676: themed custom search-results

---
+++ src/themes/custom/app/shared/search/search-results/search-results.component.ts
@@ -0,0 +1,16 @@
+import { SearchResultsComponent as BaseComponent } from '../../../../../../app/shared/search/search-results/search-results.component';
+import { Component } from '@angular/core';
+import { fadeIn, fadeInOut } from '../../../../../../app/shared/animations/fade';
+
+@Component({
+  selector: 'ds-search-results',
+  // templateUrl: './search-results.component.html',
+  templateUrl: '../../../../../../app/shared/search/search-results/search-results.component.html',
+  animations: [
+    fadeIn,
+    fadeInOut
+  ]
+})
+export class SearchResultsComponent extends BaseComponent {
+
+}
--- src/themes/custom/theme.module.ts
+++ src/themes/custom/theme.module.ts
@@ -84,6 +84,7 @@ import { SearchModule } from '../../app/shared/search/search.module';
 import { ResourcePoliciesModule } from '../../app/shared/resource-policies/resource-policies.module';
 import { ComcolModule } from '../../app/shared/comcol/comcol.module';
 import { FeedbackComponent } from './app/info/feedback/feedback.component';
+import { SearchResultsComponent } from './app/shared/search/search-results/search-results.component';
 
 const DECLARATIONS = [
   FileSectionComponent,
@@ -126,7 +127,8 @@ const DECLARATIONS = [
   NavbarComponent,
   HeaderNavbarWrapperComponent,
   BreadcrumbsComponent,
-  FeedbackComponent
+  FeedbackComponent,
+  SearchResultsComponent,
 ];
 
 @NgModule({
