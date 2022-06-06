39cba0414a69908063aad98c8abd0a318d0eaa41
Davide Negretti <davide.negretti@4science.it>
2022-06-06 16:18:22 +0200

    Revert "[CST-5535] hide empty panels"
    This reverts commit 692ab040dc7ff471e1839673b04121780f787224.

--- src/app/health-page/health-panel/health-panel.component.ts
+++ src/app/health-page/health-panel/health-panel.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { HealthResponse } from '../models/health-component.model';
 import { TranslateService } from '@ngx-translate/core';
-import {isNotEmpty} from '../../shared/empty.util';
 
 @Component({
   selector: 'ds-health-panel',
@@ -30,20 +29,12 @@ export class HealthPanelComponent implements OnInit {
   /**
    * Return translated label if exist for the given property
    *
-   * @param panelKey
+   * @param property
    */
   public getPanelLabel(panelKey: string): string {
     const translationKey = `health-page.section.${panelKey}.title`;
     const translation = this.translate.instant(translationKey);
 
     return (translation === translationKey) ? panelKey : translation;
-  }
-
-  /**
-   * Check if the current entry has data to be shown
-   * @param entryValue
-   */
-  showEntryData(entryValue) {
-    return isNotEmpty(entryValue?.components) || isNotEmpty(entryValue?.details);
   }
 }
