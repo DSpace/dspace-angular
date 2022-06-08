94d2ee7413cdd85d9161fcb29e843269d39ce448
MilanMajchrák <milan.majchak@dataquest.sk>
2022-06-08 13:19:15 +0200

    added scripts for harvesting

---
+++ build-scripts/run/harvest.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ "x$ENVFILE" == "x" ]]; then
+    export ENVFILE=$(pwd)/envs/.default
+fi
+
+source $ENVFILE
+
+# wiki: https://wiki.lyrasis.org/display/DSDOC7x/OAI#OAI-HarvestingfromanotherDSpace
+pushd ../..
+# test connection
+docker-compose --env-file $ENVFILE -p dq-d7 -f docker/cli.yml run --rm dspace-cli harvest -g -a http://lindat.mff.cuni.cz/repository/oai/request -i hdl_11234_3430
+# set up collection for harvesting
+docker-compose --env-file $ENVFILE -p dq-d7 -f docker/cli.yml run --rm dspace-cli harvest -s -c 123456789/1123 -a http://lindat.mff.cuni.cz/repository/oai/request -i hdl_11234_3430 -m dc -t 1
+# start harvesting
+docker-compose --env-file $ENVFILE -p dq-d7 -f docker/cli.yml run --rm dspace-cli harvest -r -c 123456789/1123 -a http://lindat.mff.cuni.cz/repository/oai/request -i hdl_11234_3430 -m dc -t 1 -e test@test.edu
+popd
