a89b5b3498f31fa88fabcf935fcc40ab987de4be
Michal Rovnanik on WS <michal@rovnanik.sk>
2022-06-08 14:15:24 +0200

    invalid variables interpolation

--- build-scripts/run/harvest.bat
+++ build-scripts/run/harvest.bat
@@ -5,7 +5,7 @@ pushd ..\..
 :: test connection
 docker-compose --env-file %ENVFILE% -p dq-d7 -f docker/cli.yml run --rm dspace-cli harvest -g -a http://lindat.mff.cuni.cz/repository/oai/request -i hdl_11234_3430
 :: set up collection for harvesting
-docker-compose --env-file $ENVFILE -p dq-d7 -f docker/cli.yml run --rm dspace-cli harvest -s -c 123456789/1123 -a http://lindat.mff.cuni.cz/repository/oai/request -i hdl_11234_3430 -m dc -t 1
+docker-compose --env-file %ENVFILE% -p dq-d7 -f docker/cli.yml run --rm dspace-cli harvest -s -c 123456789/2 -a http://lindat.mff.cuni.cz/repository/oai/request -i hdl_11234_3430 -m dc -t 1 -e test@test.edu
 :: start harvesting
-docker-compose --env-file $ENVFILE -p dq-d7 -f docker/cli.yml run --rm dspace-cli harvest -r -c 123456789/1123 -a http://lindat.mff.cuni.cz/repository/oai/request -i hdl_11234_3430 -m dc -t 1 -e test@test.edu
+docker-compose --env-file %ENVFILE% -p dq-d7 -f docker/cli.yml run --rm dspace-cli harvest -r -c 123456789/2 -a http://lindat.mff.cuni.cz/repository/oai/request -i hdl_11234_3430 -m dc -t 1 -e test@test.edu
 popd
