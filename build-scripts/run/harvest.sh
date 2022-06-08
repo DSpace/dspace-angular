2b5d7238d92bb9c264b4ef7e4224744f592b9b81
MilanMajchrák <milan.majchak@dataquest.sk>
2022-06-08 16:17:16 +0200

    Import collection with community

--- build-scripts/run/harvest.sh
+++ build-scripts/run/harvest.sh
@@ -8,10 +8,12 @@ source $ENVFILE
 
 # wiki: https://wiki.lyrasis.org/display/DSDOC7x/OAI#OAI-HarvestingfromanotherDSpace
 pushd ../..
+:: import community with collection
+docker-compose --env-file $ENVFILE -p dq-d7 -f docker/cli.yml -v build-scripts/run/dump:/dump run --rm dspace-cli structure-builder -f /dump/communityCollection.xml -o output.xml -e test@test.edu
 # test connection
 docker-compose --env-file $ENVFILE -p dq-d7 -f docker/cli.yml run --rm dspace-cli harvest -g -a http://lindat.mff.cuni.cz/repository/oai/request -i hdl_11234_3430
 # set up collection for harvesting
-docker-compose --env-file $ENVFILE -p dq-d7 -f docker/cli.yml run --rm dspace-cli harvest -s -c 123456789/1123 -a http://lindat.mff.cuni.cz/repository/oai/request -i hdl_11234_3430 -m dc -t 1
+docker-compose --env-file $ENVFILE -p dq-d7 -f docker/cli.yml run --rm dspace-cli harvest -s -c 123456789/2 -a http://lindat.mff.cuni.cz/repository/oai/request -i hdl_11234_3430 -m dc -t 1 -e test@test.edu
 # start harvesting
-docker-compose --env-file $ENVFILE -p dq-d7 -f docker/cli.yml run --rm dspace-cli harvest -r -c 123456789/1123 -a http://lindat.mff.cuni.cz/repository/oai/request -i hdl_11234_3430 -m dc -t 1 -e test@test.edu
+docker-compose --env-file $ENVFILE -p dq-d7 -f docker/cli.yml run --rm dspace-cli harvest -r -c 123456789/2 -a http://lindat.mff.cuni.cz/repository/oai/request -i hdl_11234_3430 -m dc -t 1 -e test@test.edu
 popd
