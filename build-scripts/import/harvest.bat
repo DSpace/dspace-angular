ea28f2ea5dae0d27106e140ded0cd0a0182b4fe7
MilanMajchrák <milan.majchak@dataquest.sk>
2022-06-09 08:19:14 +0200

    fixed required changes

--- build-scripts/import/harvest.bat
+++ build-scripts/import/harvest.bat
@@ -3,7 +3,7 @@ IF "%ENVFILE%"=="" set ENVFILE=%cd%/envs/.default
 :: wiki: https://wiki.lyrasis.org/display/DSDOC7x/OAI#OAI-HarvestingfromanotherDSpace
 pushd ..\..
 :: import community with collection
-docker-compose --env-file %ENVFILE% -p dq-d7 -f docker/cli.yml -v build-scripts/run/dump:/dump run --rm dspace-cli structure-builder -f /dump/communityCollection.xml -o output.xml -e test@test.edu
+docker-compose --env-file %ENVFILE% -p dq-d7 -f docker/cli.yml -v build-scripts/import/assets:/assets run --rm dspace-cli structure-builder -f /assets/test_community_collection.xml -o /assets/test_import_output.xml -e test@test.edu
 :: test connection
 docker-compose --env-file %ENVFILE% -p dq-d7 -f docker/cli.yml run --rm dspace-cli harvest -g -a http://lindat.mff.cuni.cz/repository/oai/request -i hdl_11234_3430
 :: set up collection for harvesting
