3cb5e0c22662656eae4a1e1460c97cf7414bcce9
Giuseppe Digilio <giuseppe.digilio@4science.it>
2022-06-10 11:32:48 +0200

    [CST-5668] Fix button style and tooltip messages

--- src/app/shared/dso-page/dso-page-orcid-button/dso-page-orcid-button.component.ts
+++ src/app/shared/dso-page/dso-page-orcid-button/dso-page-orcid-button.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
+
 import { BehaviorSubject } from 'rxjs';
 import { take } from 'rxjs/operators';
-import { AuthorizationDataService } from 'src/app/core/data/feature-authorization/authorization-data.service';
-import { FeatureID } from 'src/app/core/data/feature-authorization/feature-id';
-import { DSpaceObject } from 'src/app/core/shared/dspace-object.model';
+
+import { AuthorizationDataService } from '../../../core/data/feature-authorization/authorization-data.service';
+import { FeatureID } from '../../../core/data/feature-authorization/feature-id';
+import { DSpaceObject } from '../../../core/shared/dspace-object.model';
 
 @Component({
   selector: 'ds-dso-page-orcid-button',
@@ -20,12 +22,6 @@ export class DsoPageOrcidButtonComponent implements OnInit {
   * The prefix of the route to the edit page (before the object's UUID, e.g. "items")
   */
  @Input() pageRoute: string;
-
- /**
-  * A message for the tooltip on the button
-  * Supports i18n keys
-  */
- @Input() tooltipMsg: string;
 
  /**
   * Whether or not the current user is authorized to edit the DSpaceObject
