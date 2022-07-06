530db26587b4faada51a961980f1beea06fdc681
Luca Giamminonni <luca.giamminonni@4science.it>
2022-07-06 18:12:15 +0200

    [CST-5337] Fixed lint error

--- src/app/core/suggestion-notifications/qa/models/quality-assurance-event.model.ts
+++ src/app/core/suggestion-notifications/qa/models/quality-assurance-event.model.ts
@@ -14,6 +14,7 @@ import {CacheableObject} from '../../../cache/cacheable-object.model';
 /**
  * The interface representing the Quality Assurance event message
  */
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface QualityAssuranceEventMessageObject {
 
 }
