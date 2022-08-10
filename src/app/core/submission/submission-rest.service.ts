9af298f7979168f7a7a51ef8e17be275ecd38b55
Rezart Vata <rezart.vata@atis.al>
2022-08-10 14:50:57 +0200

    [CST-6399] Adding projections to submission getById projection parmeter

--- src/app/core/submission/submission-rest.service.ts
+++ src/app/core/submission/submission-rest.service.ts
@@ -68,9 +68,14 @@ export class SubmissionRestService {
    * @param collectionId
    *    The owning collection for the object
    */
-  protected getEndpointByIDHref(endpoint, resourceID, collectionId?: string): string {
+  protected getEndpointByIDHref(endpoint, resourceID, collectionId?: string, projections: string[] = []): string {
     let url = isNotEmpty(resourceID) ? `${endpoint}/${resourceID}` : `${endpoint}`;
     url = new URLCombiner(url, '?projection=full').toString();
+
+    projections.forEach((projection) => {
+      url = new URLCombiner(url, '&projection=' + projection).toString();
+    });
+
     if (collectionId) {
       url = new URLCombiner(url, `&owningCollection=${collectionId}`).toString();
     }
@@ -109,10 +114,10 @@ export class SubmissionRestService {
    * @return Observable<SubmitDataResponseDefinitionObject>
    *     server response
    */
-  public getDataById(linkName: string, id: string): Observable<SubmitDataResponseDefinitionObject> {
+  public getDataById(linkName: string, id: string, projections: string[] = []): Observable<SubmitDataResponseDefinitionObject> {
     const requestId = this.requestService.generateRequestId();
     return this.halService.getEndpoint(linkName).pipe(
-      map((endpointURL: string) => this.getEndpointByIDHref(endpointURL, id)),
+      map((endpointURL: string) => this.getEndpointByIDHref(endpointURL, id, null, projections)),
       filter((href: string) => isNotEmpty(href)),
       distinctUntilChanged(),
       map((endpointURL: string) => new SubmissionRequest(requestId, endpointURL)),
