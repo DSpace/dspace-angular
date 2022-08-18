424c1305687ba3cd27089bcfd970b11c12f4bb57
Davide Negretti <davide.negretti@4science.it>
2022-08-18 16:43:50 +0200

    [DSC-628] comments

--- src/config/advanced-attachment-rendering.config.ts
+++ src/config/advanced-attachment-rendering.config.ts
@@ -1,22 +1,31 @@
 /**
- * Interface configuration to show/hide advnaced attachment informations
+ * Interface configuration to define the advanced attachment rendering settings
  */
 export interface AdvancedAttachmentRenderingConfig {
   metadata: AttachmentMetadataConfig[];
   pagination: AdvancedAttachmentRenderingPaginationConfig;
 }
 
+/**
+ * Interface configuration to select which are the advanced attachment information to show
+ */
 export interface AttachmentMetadataConfig {
   name: string;
   type: AdvancedAttachmentElementType;
   truncatable?: boolean;
 }
 
+/**
+ * Interface configuration to define the type for each element shown in the advanced attachment feature
+ */
 export enum AdvancedAttachmentElementType {
   Metadata = 'metadata',
   Attribute = 'attribute'
 }
 
+/**
+ * Interface configuration to define the pagination of the advanced attachment rendering
+ */
 export interface AdvancedAttachmentRenderingPaginationConfig {
   enabled: boolean;
   elementsPerPage: number;
