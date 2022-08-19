606995881fa0a5e1040f74cb4a7053bdf838f826
lotte <lotte_hofstede@hotmail.com>
2022-08-19 11:05:08 +0200

    93665: fixes for post 7.2 and updated back button for browse by pages

--- src/app/shared/object-list/browse-entry-list-element/browse-entry-list-element.component.ts
+++ src/app/shared/object-list/browse-entry-list-element/browse-entry-list-element.component.ts
@@ -7,6 +7,9 @@ import { listableObjectComponent } from '../../object-collection/shared/listable
 import { PaginationService } from '../../../core/pagination/pagination.service';
 import { Params } from '@angular/router';
 import { BBM_PAGINATION_ID } from '../../../browse-by/browse-by-metadata-page/browse-by-metadata-page.component';
+import { RouteService } from 'src/app/core/services/route.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'ds-browse-entry-list-element',
@@ -19,26 +22,34 @@ import { BBM_PAGINATION_ID } from '../../../browse-by/browse-by-metadata-page/br
  */
 @listableObjectComponent(BrowseEntry, ViewMode.ListElement)
 export class BrowseEntryListElementComponent extends AbstractListableElementComponent<BrowseEntry> implements OnInit {
-  queryParams: Params;
+  /**
+   * Emits the query parameters for the link of this browse entry list element
+   */
+  queryParams$: Observable<Params>;
 
-  constructor(private paginationService: PaginationService) {
+  constructor(private paginationService: PaginationService, private routeService: RouteService) {
     super();
   }
 
   ngOnInit() {
-    this.queryParams = this.getQueryParams();
+    this.queryParams$ = this.getQueryParams();
   }
 
   /**
    * Get the query params to access the item page of this browse entry.
    */
-  private getQueryParams(): Params {
+  private getQueryParams(): Observable<Params> {
     const pageParamName = this.paginationService.getPageParam(BBM_PAGINATION_ID);
-    return {
-      value: this.object.value,
-      authority: !!this.object.authority ? this.object.authority : undefined,
-      startsWith: undefined,
-      [pageParamName]: null,
-    };
+    return this.routeService.getQueryParameterValue(pageParamName).pipe(
+      map((currentPage) => {
+        return {
+          value: this.object.value,
+          authority: !!this.object.authority ? this.object.authority : undefined,
+          startsWith: undefined,
+          [pageParamName]: null,
+          [BBM_PAGINATION_ID + '.return']: currentPage
+        };
+      })
+    );
   }
 }
