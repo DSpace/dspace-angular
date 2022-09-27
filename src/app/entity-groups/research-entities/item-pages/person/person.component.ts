6a932ec6ac65ef6794b3945fc6be0dbd0fd30af1
Yana De Pauw <yana@atmire.com>
2022-09-27 16:21:56 +0200

    94390: Remove unused import

--- src/app/entity-groups/research-entities/item-pages/person/person.component.ts
+++ src/app/entity-groups/research-entities/item-pages/person/person.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { ViewMode } from '../../../../core/shared/view-mode.model';
 import { listableObjectComponent } from '../../../../shared/object-collection/shared/listable-object/listable-object.decorator';
-import { MetadataValue } from '../../../../core/shared/metadata.models';
 import { ItemComponent } from '../../../../item-page/simple/item-types/shared/item.component';
 
 @listableObjectComponent('Person', ViewMode.StandalonePage)
