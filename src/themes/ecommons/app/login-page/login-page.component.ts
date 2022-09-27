2d02296568da86ecac90223ca151883e171a1356
Melissa Wallace <mhk33@cornell.edu>
2022-09-27 14:16:20 -0400

    ECOMMONS-1458: remove dspace logo from login and logout templates

--- src/themes/ecommons/app/login-page/login-page.component.ts
+++ src/themes/ecommons/app/login-page/login-page.component.ts
@@ -8,8 +8,8 @@ import { LoginPageComponent as BaseComponent } from '../../../../app/login-page/
   selector: 'ds-login-page',
   // styleUrls: ['./login-page.component.scss'],
   styleUrls: ['../../../../app/login-page/login-page.component.scss'],
-  // templateUrl: './login-page.component.html'
-  templateUrl: '../../../../app/login-page/login-page.component.html'
+  templateUrl: './login-page.component.html'
+  // templateUrl: '../../../../app/login-page/login-page.component.html'
 })
 export class LoginPageComponent extends BaseComponent {
 }
--- src/themes/ecommons/app/logout-page/logout-page.component.ts
+++ src/themes/ecommons/app/logout-page/logout-page.component.ts
@@ -5,8 +5,8 @@ import { LogoutPageComponent as BaseComponent} from '../../../../app/logout-page
   selector: 'ds-logout-page',
   // styleUrls: ['./logout-page.component.scss'],
   styleUrls: ['../../../../app/logout-page/logout-page.component.scss'],
-  // templateUrl: './logout-page.component.html'
-  templateUrl: '../../../../app/logout-page/logout-page.component.html'
+  templateUrl: './logout-page.component.html'
+  // templateUrl: '../../../../app/logout-page/logout-page.component.html'
 })
 export class LogoutPageComponent extends BaseComponent {
 }
