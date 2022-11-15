b22dfb0d13adae9e313405ae01772349c318ea91
Giuseppe Digilio <giuseppe.digilio@4science.it>
2022-11-15 16:23:42 +0100

    [DSC-831] Don't show admin sidebar when no visible top sections are found

--- src/app/admin/admin-sidebar/admin-sidebar.component.ts
+++ src/app/admin/admin-sidebar/admin-sidebar.component.ts
@@ -1,6 +1,6 @@
 import { Component, HostListener, Injector, OnInit } from '@angular/core';
 import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
-import { debounceTime, distinctUntilChanged, first, map, withLatestFrom } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, first, map, take, withLatestFrom } from 'rxjs/operators';
 import { AuthService } from '../../core/auth/auth.service';
 import { slideHorizontal, slideSidebar } from '../../shared/animations/slide';
 import { MenuComponent } from '../../shared/menu/menu.component';
@@ -9,6 +9,7 @@ import { CSSVariableService } from '../../shared/sass-helper/sass-helper.service
 import { AuthorizationDataService } from '../../core/data/feature-authorization/authorization-data.service';
 import { MenuID } from '../../shared/menu/menu-id.model';
 import { ActivatedRoute } from '@angular/router';
+import { MenuSection } from '../../shared/menu/menu-section.model';
 
 /**
  * Component representing the admin sidebar
@@ -68,12 +69,21 @@ export class AdminSidebarComponent extends MenuComponent implements OnInit {
   ngOnInit(): void {
     super.ngOnInit();
     this.sidebarWidth = this.variableService.getVariable('sidebarItemsWidth');
-    this.authService.isAuthenticated()
-      .subscribe((loggedIn: boolean) => {
-        if (loggedIn) {
-          this.menuService.showMenu(this.menuID);
-        }
-      });
+    combineLatest([
+      this.authService.isAuthenticated(),
+      this.menuService.getMenuTopSections(this.menuID).pipe(
+        map((topSections: MenuSection[]) => topSections
+          .filter((topSection: MenuSection) => topSection.visible).length > 0
+        )
+      )
+    ]).pipe(take(1))
+      .subscribe(([loggedIn, hasTopSections]: [boolean, boolean]) => {
+      // admin sidebar menu hidden by default when no visible top sections are found
+      if (loggedIn && hasTopSections) {
+        this.menuService.showMenu(this.menuID);
+      }
+    });
+
     this.menuCollapsed.pipe(first())
       .subscribe((collapsed: boolean) => {
         this.sidebarOpen = !collapsed;
