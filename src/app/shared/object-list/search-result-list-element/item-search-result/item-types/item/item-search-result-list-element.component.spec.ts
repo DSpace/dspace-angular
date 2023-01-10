5660e7dc701a575cb0f4df5c732d6ce1ee92bb06
Nikunj Sharma <nikunj.sharma@4science.com>
2023-01-11 02:46:20 +0530

    [CST-7604] test cases update

--- src/app/shared/object-list/search-result-list-element/item-search-result/item-types/item/item-search-result-list-element.component.spec.ts
+++ src/app/shared/object-list/search-result-list-element/item-search-result/item-types/item/item-search-result-list-element.component.spec.ts
@@ -16,7 +16,9 @@ let fixture: ComponentFixture<ItemSearchResultListElementComponent>;
 const dcTitle = 'This is just another <em>title</em>';
 const mockItemWithMetadata: ItemSearchResult = Object.assign(new ItemSearchResult(), {
   hitHighlights: {
-    'dc.title': [dcTitle],
+    'dc.title': [{
+      value: dcTitle
+    }],
   },
   indexableObject:
     Object.assign(new Item(), {
@@ -25,7 +27,7 @@ const mockItemWithMetadata: ItemSearchResult = Object.assign(new ItemSearchResul
         'dc.title': [
           {
             language: 'en_US',
-            value: 'This is just another title'
+            value: dcTitle
           }
         ],
         'dc.contributor.author': [
@@ -62,7 +64,114 @@ const mockItemWithoutMetadata: ItemSearchResult = Object.assign(new ItemSearchRe
       metadata: {}
     })
 });
-
+const mockPerson: ItemSearchResult = Object.assign(new ItemSearchResult(), {
+  hitHighlights: {
+    'person.familyName': [{
+      value: '<em>Michel</em>'
+    }],
+  },
+  indexableObject:
+    Object.assign(new Item(), {
+      bundles: observableOf({}),
+      entityType: 'Person',
+      metadata: {
+        'dc.title': [
+          {
+            language: 'en_US',
+            value: 'This is just another title'
+          }
+        ],
+        'dc.contributor.author': [
+          {
+            language: 'en_US',
+            value: 'Smith, Donald'
+          }
+        ],
+        'dc.publisher': [
+          {
+            language: 'en_US',
+            value: 'a publisher'
+          }
+        ],
+        'dc.date.issued': [
+          {
+            language: 'en_US',
+            value: '2015-06-26'
+          }
+        ],
+        'dc.description.abstract': [
+          {
+            language: 'en_US',
+            value: 'This is the abstract'
+          }
+        ],
+        'person.familyName': [
+          {
+            value: 'Michel'
+          }
+        ],
+        'dspace.entity.type': [
+          {
+            value: 'Person'
+          }
+        ]
+      }
+    })
+});
+const mockOrgUnit: ItemSearchResult = Object.assign(new ItemSearchResult(), {
+  hitHighlights: {
+    'organization.legalName': [{
+      value: '<em>Science</em>'
+    }],
+  },
+  indexableObject:
+    Object.assign(new Item(), {
+      bundles: observableOf({}),
+      entityType: 'OrgUnit',
+      metadata: {
+        'dc.title': [
+          {
+            language: 'en_US',
+            value: 'This is just another title'
+          }
+        ],
+        'dc.contributor.author': [
+          {
+            language: 'en_US',
+            value: 'Smith, Donald'
+          }
+        ],
+        'dc.publisher': [
+          {
+            language: 'en_US',
+            value: 'a publisher'
+          }
+        ],
+        'dc.date.issued': [
+          {
+            language: 'en_US',
+            value: '2015-06-26'
+          }
+        ],
+        'dc.description.abstract': [
+          {
+            language: 'en_US',
+            value: 'This is the abstract'
+          }
+        ],
+        'organization.legalName': [
+          {
+            value: 'Science'
+          }
+        ],
+        'dspace.entity.type': [
+          {
+            value: 'OrgUnit'
+          }
+        ]
+      }
+    })
+});
 const environmentUseThumbs = {
   browseBy: {
     showThumbnails: true
@@ -217,6 +326,30 @@ describe('ItemSearchResultListElementComponent', () => {
     it('should show highlighted title', () => {
       const titleField = fixture.debugElement.query(By.css('.item-list-title'));
       expect(titleField.nativeNode.innerHTML).toEqual(dcTitle);
+    });
+  });
+
+  describe('When the item is Person and has title', () => {
+    beforeEach(() => {
+      publicationListElementComponent.object = mockPerson;
+      fixture.detectChanges();
+    });
+
+    it('should show highlighted title', () => {
+      const titleField = fixture.debugElement.query(By.css('.item-list-title'));
+      expect(titleField.nativeNode.innerHTML).toEqual('<em>Michel</em>');
+    });
+  });
+
+  describe('When the item is orgUnit and has title', () => {
+    beforeEach(() => {
+      publicationListElementComponent.object = mockOrgUnit;
+      fixture.detectChanges();
+    });
+
+    it('should show highlighted title', () => {
+      const titleField = fixture.debugElement.query(By.css('.item-list-title'));
+      expect(titleField.nativeNode.innerHTML).toEqual('<em>Science</em>');
     });
   });
 
