f9f3ad32c78040521b950752eccfbd9dc9840305
Nikunj Sharma <nikunj.sharma@4science.com>
2023-01-10 19:37:06 +0530

    [CST-7604] added hithighlight for person and orgunit

--- src/app/shared/mocks/dso-name.service.mock.ts
+++ src/app/shared/mocks/dso-name.service.mock.ts
@@ -6,4 +6,15 @@ export class DSONameServiceMock {
   public getName(dso: DSpaceObject) {
     return UNDEFINED_NAME;
   }
+
+  public getHitHighlights(object: any, dso: DSpaceObject) {
+    if (object.hitHighlights && object.hitHighlights['dc.title']) {
+      return object.hitHighlights['dc.title'][0];
+    } else if (object.hitHighlights && object.hitHighlights['organization.legalName']) {
+      return object.hitHighlights['organization.legalName'][0];
+    } else if (object.hitHighlights && (object.hitHighlights['person.familyName'] || object.hitHighlights['person.givenName'])) {
+      return `${object.hitHighlights['person.familyName'][0] || ''}, ${object.hitHighlights['person.givenName'][0] || ''}`;
+    }
+    return UNDEFINED_NAME;
+  }
 }
