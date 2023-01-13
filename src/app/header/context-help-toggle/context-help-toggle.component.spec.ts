84c8b7690ac1d95bdbae1a032b161af97719b0cf
Koen Pauwels <koen.pauwels@atmire.com>
2023-01-13 14:05:33 +0100

    97732 ContextHelpToggleComponent tests

--- src/app/header/context-help-toggle/context-help-toggle.component.spec.ts
+++ src/app/header/context-help-toggle/context-help-toggle.component.spec.ts
@@ -1,14 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ContextHelpToggleComponent } from './context-help-toggle.component';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
+import { ContextHelpService } from '../../shared/context-help.service';
+import { of as observableOf } from 'rxjs';
+import { By } from '@angular/platform-browser';
 
 describe('ContextHelpToggleComponent', () => {
   let component: ContextHelpToggleComponent;
   let fixture: ComponentFixture<ContextHelpToggleComponent>;
+  let contextHelpService;
 
   beforeEach(async () => {
+    contextHelpService = jasmine.createSpyObj('contextHelpService',
+      ['toggleIcons']);
     await TestBed.configureTestingModule({
-      declarations: [ ContextHelpToggleComponent ]
+      declarations: [ ContextHelpToggleComponent ],
+      providers: [
+        { provide: ContextHelpService, useValue: contextHelpService },
+      ],
+      imports: [ TranslateModule.forRoot() ]
     })
     .compileComponents();
   });
@@ -21,5 +32,10 @@ describe('ContextHelpToggleComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('clicking the button should toggle context help icon visibility', () => {
+    fixture.debugElement.query(By.css('a')).nativeElement.click();
+    expect(contextHelpService.toggleIcons).toHaveBeenCalled();
   });
 });
