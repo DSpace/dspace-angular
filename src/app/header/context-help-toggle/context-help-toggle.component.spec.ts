bcbfafcb324a5e6e201467ff6c383aec5ad02f74
Koen Pauwels <koen.pauwels@atmire.com>
2023-01-16 09:36:57 +0100

    97732 Context help button now disabled when there are no tooltips available on the page

--- src/app/header/context-help-toggle/context-help-toggle.component.spec.ts
+++ src/app/header/context-help-toggle/context-help-toggle.component.spec.ts
@@ -3,17 +3,19 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ContextHelpToggleComponent } from './context-help-toggle.component';
 import { TranslateService, TranslateModule } from '@ngx-translate/core';
 import { ContextHelpService } from '../../shared/context-help.service';
-import { of as observableOf } from 'rxjs';
+import { of as observableOf, BehaviorSubject } from 'rxjs';
 import { By } from '@angular/platform-browser';
 
 describe('ContextHelpToggleComponent', () => {
   let component: ContextHelpToggleComponent;
   let fixture: ComponentFixture<ContextHelpToggleComponent>;
   let contextHelpService;
+  let contextHelpEmpty$ = new BehaviorSubject(true);
 
   beforeEach(async () => {
     contextHelpService = jasmine.createSpyObj('contextHelpService',
-      ['toggleIcons']);
+      ['toggleIcons', 'contextHelpEmpty$']);
+    contextHelpService.contextHelpEmpty$.and.returnValue(contextHelpEmpty$);
     await TestBed.configureTestingModule({
       declarations: [ ContextHelpToggleComponent ],
       providers: [
@@ -34,8 +36,28 @@ describe('ContextHelpToggleComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('clicking the button should toggle context help icon visibility', () => {
-    fixture.debugElement.query(By.css('a')).nativeElement.click();
-    expect(contextHelpService.toggleIcons).toHaveBeenCalled();
+  describe('if there are elements on the page with a tooltip', () => {
+    beforeEach(() => {
+      contextHelpEmpty$.next(false);
+      fixture.detectChanges();
+    });
+
+    it('clicking the button should toggle context help icon visibility', () => {
+      fixture.whenStable().then((done) => {
+        fixture.debugElement.query(By.css('a')).nativeElement.click();
+        expect(contextHelpService.toggleIcons).toHaveBeenCalled();
+        done();
+      });
+    });
+  });
+
+  describe('if there are no elements on the page with a tooltip', () => {
+    it('clicking the button does not toggle context help icon visibility', () => {
+      fixture.whenStable().then((done) => {
+        fixture.debugElement.query(By.css('a')).nativeElement.click();
+        expect(contextHelpService.toggleIcons).toHaveBeenCalledTimes(0);
+        done();
+      });
+    });
   });
 });
