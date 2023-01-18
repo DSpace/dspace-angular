572f5ac4c577057d6df03de894e5d1489b43514a
Alexandre Vryghem <alexandre@atmire.com>
2023-01-18 13:45:04 +0100

    98535: Added missing tests to the new advanced workflow components

--- src/app/workflowitems-edit-page/advanced-workflow-action/advanced-workflow-action-select-reviewer/advanced-workflow-action-select-reviewer.component.spec.ts
+++ src/app/workflowitems-edit-page/advanced-workflow-action/advanced-workflow-action-select-reviewer/advanced-workflow-action-select-reviewer.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { AdvancedWorkflowActionSelectReviewerComponent } from './advanced-workflow-action-select-reviewer.component';
-import { ActivatedRoute, convertToParamMap } from '@angular/router';
+import {
+  AdvancedWorkflowActionSelectReviewerComponent,
+  ADVANCED_WORKFLOW_TASK_OPTION_SELECT_REVIEWER
+} from './advanced-workflow-action-select-reviewer.component';
+import { ActivatedRoute } from '@angular/router';
 import { WorkflowItemDataService } from '../../../core/submission/workflowitem-data.service';
 import { WorkflowItemDataServiceStub } from '../../../shared/testing/workflow-item-data-service.stub';
 import { RouterTestingModule } from '@angular/router/testing';
@@ -14,10 +17,19 @@ import { TranslateModule } from '@ngx-translate/core';
 import { ClaimedTaskDataService } from '../../../core/tasks/claimed-task-data.service';
 import { ClaimedTaskDataServiceStub } from '../../../shared/testing/claimed-task-data-service.stub';
 import { of as observableOf } from 'rxjs';
+import { WorkflowItem } from '../../../core/submission/models/workflowitem.model';
+import { createSuccessfulRemoteDataObject$, createSuccessfulRemoteDataObject } from '../../../shared/remote-data.utils';
+import { Item } from '../../../core/shared/item.model';
+import { EPersonMock, EPersonMock2 } from '../../../shared/testing/eperson.mock';
+import { ProcessTaskResponse } from '../../../core/tasks/models/process-task-response';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
+const claimedTaskId = '2';
 const workflowId = '1';
 
 describe('AdvancedWorkflowActionSelectReviewerComponent', () => {
+  const workflowItem: WorkflowItem = new WorkflowItem();
+  workflowItem.item = createSuccessfulRemoteDataObject$(new Item());
   let component: AdvancedWorkflowActionSelectReviewerComponent;
   let fixture: ComponentFixture<AdvancedWorkflowActionSelectReviewerComponent>;
 
@@ -46,11 +58,13 @@ describe('AdvancedWorkflowActionSelectReviewerComponent', () => {
           useValue: {
             data: observableOf({
               id: workflowId,
+              wfi: createSuccessfulRemoteDataObject(workflowItem),
             }),
             snapshot: {
-              queryParams: convertToParamMap({
+              queryParams: {
+                claimedTask: claimedTaskId,
                 workflow: 'testaction',
-              }),
+              },
             },
           },
         },
@@ -60,6 +74,7 @@ describe('AdvancedWorkflowActionSelectReviewerComponent', () => {
         { provide: WorkflowActionDataService, useValue: workflowActionDataService },
         { provide: WorkflowItemDataService, useValue: workflowItemDataService },
       ],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
   });
 
@@ -67,9 +82,49 @@ describe('AdvancedWorkflowActionSelectReviewerComponent', () => {
     fixture = TestBed.createComponent(AdvancedWorkflowActionSelectReviewerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    spyOn(component, 'previousPage');
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  afterEach(() => {
+    fixture.debugElement.nativeElement.remove();
+  });
+
+  describe('performAction', () => {
+    it('should call the claimedTaskDataService with the list of selected ePersons', () => {
+      spyOn(claimedTaskDataService, 'submitTask').and.returnValue(observableOf(new ProcessTaskResponse(true)));
+      component.selectedReviewers = [EPersonMock, EPersonMock2];
+
+      component.performAction();
+
+      expect(claimedTaskDataService.submitTask).toHaveBeenCalledWith(claimedTaskId, {
+        [ADVANCED_WORKFLOW_TASK_OPTION_SELECT_REVIEWER]: true,
+        eperson: [EPersonMock.id, EPersonMock2.id],
+      });
+      expect(notificationService.success).toHaveBeenCalled();
+      expect(component.previousPage).toHaveBeenCalled();
+    });
+
+    it('should not call the claimedTaskDataService with the list of selected ePersons when it\'s empty', () => {
+      spyOn(claimedTaskDataService, 'submitTask').and.returnValue(observableOf(new ProcessTaskResponse(true)));
+      component.selectedReviewers = [];
+
+      component.performAction();
+
+      expect(claimedTaskDataService.submitTask).not.toHaveBeenCalled();
+    });
+
+    it('should not call the return to mydspace page when the request failed', () => {
+      spyOn(claimedTaskDataService, 'submitTask').and.returnValue(observableOf(new ProcessTaskResponse(false)));
+      component.selectedReviewers = [EPersonMock, EPersonMock2];
+
+      component.performAction();
+
+      expect(claimedTaskDataService.submitTask).toHaveBeenCalledWith(claimedTaskId, {
+        [ADVANCED_WORKFLOW_TASK_OPTION_SELECT_REVIEWER]: true,
+        eperson: [EPersonMock.id, EPersonMock2.id],
+      });
+      expect(notificationService.error).toHaveBeenCalled();
+      expect(component.previousPage).not.toHaveBeenCalled();
+    });
   });
 });
