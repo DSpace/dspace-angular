4f476adbc102cdce10205bf9928668c51a774318
Michael Spalti <mspalti@gmail.com>
2023-01-21 04:00:46 -0800

    Updated themed component.

--- src/app/shared/results-back-button/themed-results-back-button.component.ts
+++ src/app/shared/results-back-button/themed-results-back-button.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ThemedComponent } from '../theme-support/themed.component';
 import { ResultsBackButtonComponent } from './results-back-button.component';
+import { BehaviorSubject } from 'rxjs';
+import { PaginationComponentOptions } from '../pagination/pagination-component-options.model';
 
 @Component({
   selector: 'ds-themed-results-back-button',
@@ -8,6 +10,12 @@ import { ResultsBackButtonComponent } from './results-back-button.component';
   templateUrl: '../theme-support/themed.component.html',
 })
 export class ThemedResultsBackButtonComponent extends ThemedComponent<ResultsBackButtonComponent> {
+
+  @Input() previousPage$?: BehaviorSubject<string>;
+
+  @Input() paginationConfig?: PaginationComponentOptions;
+
+  protected inAndOutputNames: (keyof ResultsBackButtonComponent & keyof this)[] = ['previousPage$', 'paginationConfig'];
 
   protected getComponentName(): string {
     return 'ResultsBackButtonComponent';
