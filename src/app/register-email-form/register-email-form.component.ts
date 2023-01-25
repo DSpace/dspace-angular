0d7a030960116e6635f5dacd9c007c992dbd9d9d
Alexandre Vryghem <alexandre@atmire.com>
2023-01-25 17:46:21 +0100

    98863: Fixed typo and added domain name validation on input field

--- src/app/register-email-form/register-email-form.component.ts
+++ src/app/register-email-form/register-email-form.component.ts
@@ -3,11 +3,12 @@ import {EpersonRegistrationService} from '../core/data/eperson-registration.serv
 import {NotificationsService} from '../shared/notifications/notifications.service';
 import {TranslateService} from '@ngx-translate/core';
 import {Router} from '@angular/router';
-import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators, ValidatorFn } from '@angular/forms';
 import {Registration} from '../core/shared/registration.model';
 import {RemoteData} from '../core/data/remote-data';
 import {ConfigurationDataService} from '../core/data/configuration-data.service';
 import {getAllCompletedRemoteData} from '../core/shared/operators';
+import { ConfigurationProperty } from '../core/shared/configuration-property.model';
 
 @Component({
   selector: 'ds-register-email-form',
@@ -48,22 +49,32 @@ export class RegisterEmailFormComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    const validators: ValidatorFn[] = [
+      Validators.required,
+      Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')
+    ];
     this.form = this.formBuilder.group({
       email: new FormControl('', {
-        validators: [Validators.required,
-          Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')
-        ],
+        validators: validators,
       })
     });
     this.validMailDomains = [];
     this.configurationService.findByPropertyName('authentication-password.domain.valid')
       .pipe(getAllCompletedRemoteData())
-      .subscribe((remoteData) => {
+      .subscribe((remoteData: RemoteData<ConfigurationProperty>) => {
+        if (remoteData.payload) {
           for (const remoteValue of remoteData.payload.values) {
             this.validMailDomains.push(remoteValue);
+            if (this.validMailDomains.length !== 0 && this.MESSAGE_PREFIX === 'register-page.registration') {
+              this.form.get('email').setValidators([
+                ...validators,
+                Validators.pattern(this.validMailDomains.map((domain: string) => '(^.*@' + domain.replace(new RegExp('\\.', 'g'), '\\.') + '$)').join('|')),
+              ]);
+              this.form.updateValueAndValidity();
+            }
           }
         }
-      );
+      });
   }
 
   /**
