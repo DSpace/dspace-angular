d6f458863f40f6ac0c102f9a0b888e58cae857f2
Kim Shepherd <kim@shepherd.nz>
2023-01-25 13:01:57 +1300

    [TLC-249] Improve model of identifier data in workspace section

--- src/app/core/submission/models/workspaceitem-section-identifiers.model.ts
+++ src/app/core/submission/models/workspaceitem-section-identifiers.model.ts
@@ -1,9 +1,9 @@
 /*
  * Object model for the data returned by the REST API to present minted identifiers in a submission section
  */
+import { Identifier } from '../../../shared/object-list/identifier-data/identifier.model';
+
 export interface WorkspaceitemSectionIdentifiersObject {
-  doi?: string
-  handle?: string
-  otherIdentifiers?: string[]
+  identifiers?: Identifier[]
   displayTypes?: string[]
 }
