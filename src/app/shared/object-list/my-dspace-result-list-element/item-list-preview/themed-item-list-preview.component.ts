8eab0f743f3769c97e9aebaefe78f5df94cefa90
cris <cristian.guzman.contacto@gmail.com>
2023-01-28 17:48:39 +0000

    The collection is displayed in the workflow task

--- src/app/shared/object-list/my-dspace-result-list-element/item-list-preview/themed-item-list-preview.component.ts
+++ src/app/shared/object-list/my-dspace-result-list-element/item-list-preview/themed-item-list-preview.component.ts
@@ -1,9 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectorRef, Component, ComponentFactoryResolver, Input } from '@angular/core';
 import { ThemedComponent } from '../../../theme-support/themed.component';
 import { ItemListPreviewComponent } from './item-list-preview.component';
 import { Item } from '../../../../core/shared/item.model';
 import { MyDspaceItemStatusType } from '../../../object-collection/shared/mydspace-item-status/my-dspace-item-status-type';
 import { SearchResult } from '../../../search/models/search-result.model';
+import { WorkflowItem } from 'src/app/core/submission/models/workflowitem.model';
+import { ThemeService } from 'src/app/shared/theme-support/theme.service';
+import { CollectionDataService } from 'src/app/core/data/collection-data.service';
+import { environment } from '../../../../../../src/environments/environment';
 
 /**
  * Themed wrapper for ItemListPreviewComponent
@@ -11,7 +15,7 @@ import { SearchResult } from '../../../search/models/search-result.model';
 @Component({
   selector: 'ds-themed-item-list-preview',
   styleUrls: [],
-  templateUrl: '../../../theme-support/themed.component.html',
+  templateUrl: 'themed-item-list-preview.component.html'
 })
 export class ThemedItemListPreviewComponent extends ThemedComponent<ItemListPreviewComponent> {
   protected inAndOutputNames: (keyof ItemListPreviewComponent & keyof this)[] = ['item', 'object', 'status', 'showSubmitter'];
@@ -24,6 +28,43 @@ export class ThemedItemListPreviewComponent extends ThemedComponent<ItemListPrev
 
   @Input() showSubmitter = false;
 
+  @Input() workflowItem: WorkflowItem;
+
+  collection: any = null;
+
+  uiBaseUrl: string = environment.ui.baseUrl;
+
+  constructor(
+    protected resolver: ComponentFactoryResolver,
+    protected cdr: ChangeDetectorRef,
+    protected themeService: ThemeService,
+    protected collectionDataService: CollectionDataService
+  ){
+    super(resolver, cdr, themeService)
+  }
+
+  ngOnInit() {
+    super.ngOnInit()
+    this.getCollectionName()
+  }
+
+  protected getCollectionName(): void {
+
+    if (!this.item) {
+      return;
+    }
+
+    const collectionId = this.workflowItem?.sections.collection;
+
+    if (!collectionId) {
+      return;
+    }
+
+    this.collectionDataService.findByHref(`${environment.rest.baseUrl}/api/core/collections/${collectionId}`).subscribe(collection => {
+      this.collection = collection?.payload;
+      console.log("collection", this.collection)
+    });
+  }
 
   protected getComponentName(): string {
     return 'ItemListPreviewComponent';
