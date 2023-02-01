80670855ffa81b0d6f723430bb24420d375fe373
Alexandre Vryghem <alexandre@atmire.com>
2023-02-01 14:53:13 +0100

    98855: Added themeable FileDownloadLinkComponent to custom theme

--- src/themes/custom/eager-theme.module.ts
+++ src/themes/custom/eager-theme.module.ts
@@ -43,6 +43,7 @@ import {
 
 import { CommunityListElementComponent } from './app/shared/object-list/community-list-element/community-list-element.component';
 import { CollectionListElementComponent} from './app/shared/object-list/collection-list-element/collection-list-element.component';
+import { FileDownloadLinkComponent } from './app/shared/file-download-link/file-download-link.component';
 
 
 /**
@@ -58,6 +59,7 @@ const ENTRY_COMPONENTS = [
 
   CommunityListElementComponent,
   CollectionListElementComponent,
+  FileDownloadLinkComponent,
 ];
 
 const DECLARATIONS = [
