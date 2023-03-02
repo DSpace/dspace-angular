62e122ab9e93d1cc21cbcb6b33b8dd77a846816f
Michael Spalti <mspalti@gmail.com>
2023-03-02 11:09:11 -0800

    reverted unintended change in bitstream resolver
    Removed comment from template

--- src/app/bitstream-page/bitstream-page.resolver.ts
+++ src/app/bitstream-page/bitstream-page.resolver.ts
@@ -32,7 +32,7 @@ export class BitstreamPageResolver implements Resolve<RemoteData<Bitstream>> {
    * or an error if something went wrong
    */
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<RemoteData<Bitstream>> {
-    return this.bitstreamService.findById(route.params.id, true, true, ...this.followLinks)
+    return this.bitstreamService.findById(route.params.id, true, false, ...this.followLinks)
       .pipe(
         getFirstCompletedRemoteData(),
       );
