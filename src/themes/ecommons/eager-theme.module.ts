bc3edfd74fd70445cda3dac39fc168644f1c5e75
Brandon T. Kowalski <kowalski@cornell.edu>
2023-03-13 11:57:42 -0400

    Remove unused theme files

--- src/themes/ecommons/eager-theme.module.ts
+++ src/themes/ecommons/eager-theme.module.ts
@@ -1,49 +1,40 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { SharedModule } from '../../app/shared/shared.module';
-import { HomeNewsComponent } from './app/home-page/home-news/home-news.component';
-import { NavbarComponent } from './app/navbar/navbar.component';
-import { HeaderComponent } from './app/header/header.component';
-import { HeaderNavbarWrapperComponent } from './app/header-nav-wrapper/header-navbar-wrapper.component';
-import { SearchModule } from '../../app/shared/search/search.module';
-import { RootModule } from '../../app/root.module';
-import { NavbarModule } from '../../app/navbar/navbar.module';
-import { PublicationComponent } from './app/item-page/simple/item-types/publication/publication.component';
-import { ItemPageModule } from '../../app/item-page/item-page.module';
-import { FooterComponent } from './app/footer/footer.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {SharedModule} from '../../app/shared/shared.module';
+import {HomeNewsComponent} from './app/home-page/home-news/home-news.component';
+import {SearchModule} from '../../app/shared/search/search.module';
+import {RootModule} from '../../app/root.module';
+import {NavbarModule} from '../../app/navbar/navbar.module';
+import {ItemPageModule} from '../../app/item-page/item-page.module';
+import {FooterComponent} from './app/footer/footer.component';
 
 /**
  * Add components that use a custom decorator to ENTRY_COMPONENTS as well as DECLARATIONS.
  * This will ensure that decorator gets picked up when the app loads
  */
-const ENTRY_COMPONENTS = [
-  PublicationComponent,
-];
+const ENTRY_COMPONENTS = [];
 
 const DECLARATIONS = [
-  ...ENTRY_COMPONENTS,
-  HomeNewsComponent,
-  HeaderComponent,
-  HeaderNavbarWrapperComponent,
-  NavbarComponent,
-  FooterComponent,
+    ...ENTRY_COMPONENTS,
+    HomeNewsComponent,
+    FooterComponent,
 ];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    SharedModule,
-    SearchModule,
-    FormsModule,
-    RootModule,
-    NavbarModule,
-    ItemPageModule,
-  ],
-  declarations: DECLARATIONS,
-  providers: [
-    ...ENTRY_COMPONENTS.map((component) => ({ provide: component }))
-  ],
+    imports: [
+        CommonModule,
+        SharedModule,
+        SearchModule,
+        FormsModule,
+        RootModule,
+        NavbarModule,
+        ItemPageModule,
+    ],
+    declarations: DECLARATIONS,
+    providers: [
+        ...ENTRY_COMPONENTS.map((component) => ({provide: component}))
+    ],
 })
 /**
  * This module is included in the main bundle that gets downloaded at first page load. So it should
