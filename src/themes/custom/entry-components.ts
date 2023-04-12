6487b5eef30ba186df630badfcac4b75e0188d9b
Alexandre Vryghem <alexandre@atmire.com>
2023-04-12 13:41:43 +0200

    100839: Added themed BrowseEntryListElementComponent to custom theme

--- src/themes/custom/entry-components.ts
+++ src/themes/custom/entry-components.ts
@@ -1,5 +1,9 @@
 import { PublicationComponent } from './app/item-page/simple/item-types/publication/publication.component';
+import {
+  BrowseEntryListElementComponent
+} from './app/shared/object-list/browse-entry-list-element/browse-entry-list-element.component';
 
 export const ENTRY_COMPONENTS = [
-  PublicationComponent
+  PublicationComponent,
+  BrowseEntryListElementComponent,
 ];
