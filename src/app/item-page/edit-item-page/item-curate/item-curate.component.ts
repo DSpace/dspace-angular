57eb6da97a8fc4c6edfe1b061c638084c0212ee9
Enea Jahollari <enea.jahollari@4science.com>
2023-05-08 14:57:20 +0200

    fixup! Fix change detection issue

--- src/app/item-page/edit-item-page/item-curate/item-curate.component.ts
+++ src/app/item-page/edit-item-page/item-curate/item-curate.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
-import { filter, map, shareReplay } from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
+import { filter, map, take } from 'rxjs/operators';
 import { RemoteData } from '../../../core/data/remote-data';
 import { Observable } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { DSONameService } from '../../../core/breadcrumbs/dso-name.service';
-import { Collection } from '../../../core/shared/collection.model';
 import { hasValue } from '../../../shared/empty.util';
+import { Item } from '../../../core/shared/item.model';
 
 /**
  * Component for managing a collection's curation tasks
@@ -14,22 +14,26 @@ import { hasValue } from '../../../shared/empty.util';
   selector: 'ds-item-curate',
   templateUrl: './item-curate.component.html',
 })
-export class ItemCurateComponent {
-  dsoRD$: Observable<RemoteData<Collection>> = this.route.parent.data.pipe(
-    map((data) => data.dso),
-    shareReplay(1)
-  );
-
-  itemName$: Observable<string> = this.dsoRD$.pipe(
-    filter((rd: RemoteData<Collection>) => hasValue(rd)),
-    map((rd: RemoteData<Collection>) => {
-      console.log(rd);
-      return this.dsoNameService.getName(rd.payload);
-    })
-  );
+export class ItemCurateComponent implements OnInit {
+  dsoRD$: Observable<RemoteData<Item>>;
+  itemName$: Observable<string>;
 
   constructor(
     private route: ActivatedRoute,
     private dsoNameService: DSONameService,
   ) {}
+
+  ngOnInit(): void {
+    this.dsoRD$ = this.route.parent.data.pipe(
+      take(1),
+      map((data) => data.dso),
+    );
+
+    this.itemName$ = this.dsoRD$.pipe(
+      filter((rd: RemoteData<Item>) => hasValue(rd)),
+      map((rd: RemoteData<Item>) => {
+        return this.dsoNameService.getName(rd.payload);
+      })
+    );
+  }
 }
