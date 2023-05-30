d9ed91df05be56b32c693a793e675e0a19afd32d
Paulo Graça <paulo1978@gmail.com>
2023-05-30 11:46:59 +0100

    new project metadata representation component

---
+++ src/app/entity-groups/research-entities/metadata-representations/project/project-item-metadata-list-element.component.spec.ts
@@ -0,0 +1,46 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ChangeDetectionStrategy, NO_ERRORS_SCHEMA } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { ItemMetadataRepresentation } from '../../../../core/shared/metadata-representation/item/item-metadata-representation.model';
+import { Item } from '../../../../core/shared/item.model';
+import { ProjectItemMetadataListElementComponent } from './project-item-metadata-list-element.component';
+import { MetadataValue } from '../../../../core/shared/metadata.models';
+
+const projectTitle = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.';
+const mockItem = Object.assign(new Item(), { metadata: { 'dc.title': [{ value: projectTitle }] } });
+const virtMD = Object.assign(new MetadataValue(), { value: projectTitle });
+
+const mockItemMetadataRepresentation = Object.assign(new ItemMetadataRepresentation(virtMD), mockItem);
+
+describe('ProjectItemMetadataListElementComponent', () => {
+  let comp: ProjectItemMetadataListElementComponent;
+  let fixture: ComponentFixture<ProjectItemMetadataListElementComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports:[
+        NgbModule
+      ],
+      declarations: [ProjectItemMetadataListElementComponent],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).overrideComponent(ProjectItemMetadataListElementComponent, {
+      set: { changeDetection: ChangeDetectionStrategy.Default }
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProjectItemMetadataListElementComponent);
+    comp = fixture.componentInstance;
+    comp.metadataRepresentation = mockItemMetadataRepresentation;
+    fixture.detectChanges();
+  });
+
+  it('should show the project\'s name as a link', () => {
+    const linkText = fixture.debugElement.query(By.css('a')).nativeElement.textContent;
+    expect(linkText).toBe(projectTitle);
+  });
+
+});
