58d6417549e7e7d7b2f7807571c15370b760dcba
Alisa Ismailati <alisa.ismailati@4science.com>
2023-05-31 16:02:13 +0200

    [DSC-381] Fixed failing unit tests

--- src/app/breadcrumbs/breadcrumbs.component.spec.ts
+++ src/app/breadcrumbs/breadcrumbs.component.spec.ts
@@ -10,22 +10,29 @@ import { TranslateLoaderMock } from '../shared/testing/translate-loader.mock';
 import { RouterTestingModule } from '@angular/router/testing';
 import { of as observableOf } from 'rxjs';
 import { DebugElement } from '@angular/core';
+import { IsTextTruncatedPipe } from './breadcrumb/is-text-truncated.pipe';
+import { TruncateBreadcrumbItemCharactersPipe } from './breadcrumb/truncate-breadcrumb-item-characters.pipe';
 
 describe('BreadcrumbsComponent', () => {
   let component: BreadcrumbsComponent;
   let fixture: ComponentFixture<BreadcrumbsComponent>;
   let breadcrumbsServiceMock: BreadcrumbsService;
+  let truncateTextPipe: TruncateBreadcrumbItemCharactersPipe;
 
   const expectBreadcrumb = (listItem: DebugElement, text: string, url: string) => {
     const anchor = listItem.query(By.css('a'));
-
+    const truncatedText = truncateTextPipe.transform(text);
     if (url == null) {
       expect(anchor).toBeNull();
-      expect(listItem.nativeElement.innerHTML).toEqual(text);
+       // remove leading whitespace characters
+      const textWithoutSpaces = listItem.nativeElement.innerHTML.trimStart().replace(/^\s+/, '');
+      expect(textWithoutSpaces).toEqual(truncatedText);
     } else {
       expect(anchor).toBeInstanceOf(DebugElement);
       expect(anchor.attributes.href).toEqual(url);
-      expect(anchor.nativeElement.innerHTML).toEqual(text);
+       // remove leading whitespace characters
+      const textWithoutSpaces = anchor.nativeElement.innerHTML.trimStart().replace(/^\s+/, '');
+      expect(textWithoutSpaces).toEqual(truncatedText);
     }
   };
 
@@ -43,6 +50,8 @@ describe('BreadcrumbsComponent', () => {
       declarations: [
         BreadcrumbsComponent,
         VarDirective,
+        IsTextTruncatedPipe,
+        TruncateBreadcrumbItemCharactersPipe,
       ],
       imports: [
         RouterTestingModule.withRoutes([]),
@@ -55,10 +64,12 @@ describe('BreadcrumbsComponent', () => {
       ],
       providers: [
         { provide: BreadcrumbsService, useValue: breadcrumbsServiceMock },
+        { provide: TruncateBreadcrumbItemCharactersPipe, useClass: TruncateBreadcrumbItemCharactersPipe },
       ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(BreadcrumbsComponent);
+    truncateTextPipe = TestBed.inject(TruncateBreadcrumbItemCharactersPipe);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
