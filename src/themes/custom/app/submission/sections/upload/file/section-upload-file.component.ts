97673471f7aadee954bf4a06cd9038c40d1f8a53
Mark H. Wood <mwood@iu.edu>
2023-06-01 12:20:59 -0400

    Remove inherited input declarations.

--- src/themes/custom/app/submission/sections/upload/file/section-upload-file.component.ts
+++ src/themes/custom/app/submission/sections/upload/file/section-upload-file.component.ts
@@ -1,13 +1,4 @@
-import {
-    Component, Input, ViewChild
-} from '@angular/core';
-
-import {
-    SubmissionFormsModel
-} from 'src/app/core/config/models/config-submission-forms.model';
-import {
-    SubmissionSectionUploadFileEditComponent
-} from 'src/app/submission/sections/upload/file/edit/section-upload-file-edit.component';
+import { Component } from '@angular/core';
 import {
     SubmissionSectionUploadFileComponent as BaseComponent
 } from 'src/app/submission/sections/upload/file/section-upload-file.component';
@@ -24,66 +15,4 @@ import {
 })
 export class SubmissionSectionUploadFileComponent
     extends BaseComponent {
-
-  /**
-   * The list of available access condition
-   * @type {Array}
-   */
-  @Input() availableAccessConditionOptions: any[];
-
-  /**
-   * The submission id
-   * @type {string}
-   */
-  @Input() collectionId: string;
-
-  /**
-   * Define if collection access conditions policy type :
-   * POLICY_DEFAULT_NO_LIST : is not possible to define additional access group/s for the single file
-   * POLICY_DEFAULT_WITH_LIST : is possible to define additional access group/s for the single file
-   * @type {number}
-   */
-  @Input() collectionPolicyType: number;
-
-  /**
-   * The configuration for the bitstream's metadata form
-   * @type {SubmissionFormsModel}
-   */
-  @Input() configMetadataForm: SubmissionFormsModel;
-
-  /**
-   * The bitstream id
-   * @type {string}
-   */
-  @Input() fileId: string;
-
-  /**
-   * The bitstream array key
-   * @type {string}
-   */
-  @Input() fileIndex: string;
-
-  /**
-   * The bitstream id
-   * @type {string}
-   */
-  @Input() fileName: string;
-
-  /**
-   * The section id
-   * @type {string}
-   */
-  @Input() sectionId: string;
-
-  /**
-   * The submission id
-   * @type {string}
-   */
-  @Input() submissionId: string;
-
-  /**
-   * The [[SubmissionSectionUploadFileEditComponent]] reference
-   * @type {SubmissionSectionUploadFileEditComponent}
-   */
-  @ViewChild(SubmissionSectionUploadFileEditComponent) fileEditComp: SubmissionSectionUploadFileEditComponent;
 }
