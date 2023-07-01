7ef3a914f9b1e85ddef29bbe9ff50af610c397e1
Alexandre Vryghem <alexandre@atmire.com>
2023-07-01 13:23:02 +0200

    Fix ItemComponent no expectation tests
    - renamed `showBackButton` to `showBackButton$` because it's an observable
    - `showBackButton$` only emits `true` when the back button should be shown, it does not emit `false` when it should be hidden, it just did nothing

--- src/app/item-page/simple/item-types/shared/item.component.ts
+++ src/app/item-page/simple/item-types/shared/item.component.ts
@@ -5,7 +5,7 @@ import { getItemPageRoute } from '../../../item-page-routing-paths';
 import { RouteService } from '../../../../core/services/route.service';
 import { Observable } from 'rxjs';
 import { getDSpaceQuery, isIiifEnabled, isIiifSearchEnabled } from './item-iiif-utils';
-import { filter, map, take } from 'rxjs/operators';
+import { map, take } from 'rxjs/operators';
 import { Router } from '@angular/router';
 
 @Component({
@@ -27,7 +27,7 @@ export class ItemComponent implements OnInit {
   /**
    * Used to show or hide the back to results button in the view.
    */
-  showBackButton: Observable<boolean>;
+  showBackButton$: Observable<boolean>;
 
   /**
    * Route to the item page
@@ -73,10 +73,9 @@ export class ItemComponent implements OnInit {
 
     this.itemPageRoute = getItemPageRoute(this.object);
     // hide/show the back button
-    this.showBackButton = this.routeService.getPreviousUrl().pipe(
-      filter(url => this.previousRoute.test(url)),
+    this.showBackButton$ = this.routeService.getPreviousUrl().pipe(
+      map((url: string) => this.previousRoute.test(url)),
       take(1),
-      map(() => true)
     );
     // check to see if iiif viewer is required.
     this.iiifEnabled = isIiifEnabled(this.object);
