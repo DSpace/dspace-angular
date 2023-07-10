23aefb7385a987c5cb3f3eb169b37037f032a126
Eike Martin Löhden <eike.loehden@ub.uni-marburg.de>
2023-07-10 15:29:14 +0200

    Added themed-user-menu component.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -284,6 +284,7 @@ import {
 } from '../item-page/simple/field-components/specific-field/title/themed-item-page-field.component';
 import { BitstreamListItemComponent } from './object-list/bitstream-list-item/bitstream-list-item.component';
 import { NgxPaginationModule } from 'ngx-pagination';
+import {ThemedUserMenuComponent} from './auth-nav-menu/user-menu/themed-user-menu.component';
 
 const MODULES = [
   CommonModule,
@@ -332,6 +333,7 @@ const COMPONENTS = [
   AuthNavMenuComponent,
   ThemedAuthNavMenuComponent,
   UserMenuComponent,
+  ThemedUserMenuComponent,
   DsSelectComponent,
   ErrorComponent,
   LangSwitchComponent,
