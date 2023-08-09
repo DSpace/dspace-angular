36dfb69670aad003e35fd1aec889064f0234aca9
Sondissimo <vianellimattia@icloud.com>
2023-08-09 10:10:00 +0200

    CST-11012 notify info component updated

--- src/app/core/coar-notify/notify-info/notify-info.component.ts
+++ src/app/core/coar-notify/notify-info/notify-info.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthorizationDataService } from '../../data/feature-authorization/authorization-data.service';
+import { FeatureID } from '../../data/feature-authorization/feature-id';
 
 @Component({
-  selector: 'ds-notify-info',
-  templateUrl: './notify-info.component.html',
-  styleUrls: ['./notify-info.component.scss']
+    selector: 'ds-notify-info',
+    templateUrl: './notify-info.component.html',
+    styleUrls: ['./notify-info.component.scss']
 })
 export class NotifyInfoComponent implements OnInit {
 
-  constructor() {
-    //
-  }
+    coarLdnEnabled: boolean;
 
-  ngOnInit(): void {
-    //
-  }
+    constructor(
+        private authorizationService: AuthorizationDataService,
+    ) {
+    }
 
+    ngOnInit() {
+        this.authorizationService.isAuthorized(FeatureID.CoarLdnEnabled).subscribe(enabled => {
+            this.coarLdnEnabled = enabled;
+        });
+    }
 }
