537926334c6c99f3329f4f7255f639e0361c1e72
Enea Jahollari <enea.jahollari@4science.com>
2023-09-26 12:17:18 +0200

    feat: fix SubmissionEditComponent test

--- src/app/submission/edit/submission-edit.component.spec.ts
+++ src/app/submission/edit/submission-edit.component.spec.ts
@@ -3,15 +3,14 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 
-import { of as observableOf } from 'rxjs';
-import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { of, of as observableOf } from 'rxjs';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { SubmissionEditComponent } from './submission-edit.component';
 import { NotificationsService } from '../../shared/notifications/notifications.service';
 import { NotificationsServiceStub } from '../../shared/testing/notifications-service.stub';
 import { SubmissionService } from '../submission.service';
 import { SubmissionServiceStub } from '../../shared/testing/submission-service.stub';
-import { getMockTranslateService } from '../../shared/mocks/translate.service.mock';
 
 import { RouterStub } from '../../shared/testing/router.stub';
 import { ActivatedRouteStub } from '../../shared/testing/active-router.stub';
@@ -20,6 +19,14 @@ import { createSuccessfulRemoteDataObject$ } from '../../shared/remote-data.util
 import { ItemDataService } from '../../core/data/item-data.service';
 import { SubmissionJsonPatchOperationsServiceStub } from '../../shared/testing/submission-json-patch-operations-service.stub';
 import { SubmissionJsonPatchOperationsService } from '../../core/submission/submission-json-patch-operations.service';
+import { AuthServiceStub } from '../../shared/testing/auth-service.stub';
+import { AuthService } from '../../core/auth/auth.service';
+import { HALEndpointService } from '../../core/shared/hal-endpoint.service';
+import { SectionsService } from '../sections/sections.service';
+import { SectionsServiceStub } from '../../shared/testing/sections-service.stub';
+import { ThemeService } from '../../shared/theme-support/theme.service';
+import { getMockThemeService } from '../../shared/mocks/theme-service.mock';
+import { provideMockStore } from '@ngrx/store/testing';
 
 describe('SubmissionEditComponent Component', () => {
 
@@ -29,6 +36,9 @@ describe('SubmissionEditComponent Component', () => {
   let itemDataService: ItemDataService;
   let submissionJsonPatchOperationsServiceStub: SubmissionJsonPatchOperationsServiceStub;
   let router: RouterStub;
+  let halService: jasmine.SpyObj<HALEndpointService>;
+
+  let themeService = getMockThemeService();
 
   const submissionId = '826';
   const route: ActivatedRouteStub = new ActivatedRouteStub();
@@ -38,6 +48,11 @@ describe('SubmissionEditComponent Component', () => {
     itemDataService = jasmine.createSpyObj('itemDataService', {
       findByHref: createSuccessfulRemoteDataObject$(submissionObject.item),
     });
+
+    halService = jasmine.createSpyObj('halService', {
+      getEndpoint: of('fake-url')
+    });
+
     TestBed.configureTestingModule({
     imports: [
         TranslateModule.forRoot(),
@@ -51,9 +66,14 @@ describe('SubmissionEditComponent Component', () => {
         { provide: SubmissionService, useClass: SubmissionServiceStub },
         { provide: SubmissionJsonPatchOperationsService, useClass: SubmissionJsonPatchOperationsServiceStub },
         { provide: ItemDataService, useValue: itemDataService },
-        { provide: TranslateService, useValue: getMockTranslateService() },
+        // { provide: TranslateService, useValue: getMockTranslateService() },
         { provide: Router, useValue: new RouterStub() },
         { provide: ActivatedRoute, useValue: route },
+        { provide: AuthService, useValue: new AuthServiceStub() },
+        { provide: HALEndpointService, useValue: halService },
+        { provide: SectionsService, useValue: new SectionsServiceStub() },
+        { provide: ThemeService, useValue: themeService },
+        provideMockStore()
     ],
     schemas: [NO_ERRORS_SCHEMA]
 }).compileComponents();
@@ -79,6 +99,9 @@ describe('SubmissionEditComponent Component', () => {
     submissionServiceStub.retrieveSubmission.and.returnValue(
       createSuccessfulRemoteDataObject$(submissionObject)
     );
+    submissionServiceStub.getSubmissionObject.and.returnValue(observableOf(submissionObject));
+    submissionServiceStub.getSubmissionStatus.and.returnValue(observableOf(true));
+
 
     fixture.detectChanges();
 
