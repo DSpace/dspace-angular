be86fea039d43d4cfbe3622a84643a0726d8640d
Enea Jahollari <enea.jahollari@4science.com>
2023-09-27 15:29:00 +0200

    fix FileDownloadLinkComponent tests

--- src/app/shared/file-download-link/file-download-link.component.spec.ts
+++ src/app/shared/file-download-link/file-download-link.component.spec.ts
@@ -10,6 +10,8 @@ import { FeatureID } from '../../core/data/feature-authorization/feature-id';
 import { Item } from '../../core/shared/item.model';
 import { getItemModuleRoute } from '../../item-page/item-page-routing-paths';
 import { RouterLinkDirectiveStub } from '../testing/router-link-directive.stub';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRouteStub } from '../testing/active-router.stub';
 
 describe('FileDownloadLinkComponent', () => {
   let component: FileDownloadLinkComponent;
@@ -41,12 +43,17 @@ describe('FileDownloadLinkComponent', () => {
 
   function initTestbed() {
     TestBed.configureTestingModule({
-    imports: [FileDownloadLinkComponent],
-    declarations: [RouterLinkDirectiveStub],
+    imports: [FileDownloadLinkComponent, RouterLinkDirectiveStub],
     providers: [
+      RouterLinkDirectiveStub,
         { provide: AuthorizationDataService, useValue: authorizationService },
+        { provide: ActivatedRoute, useValue: new ActivatedRouteStub()}
     ]
 })
+      .overrideComponent(FileDownloadLinkComponent, {
+        remove: {imports: [RouterLink]},
+        add: {imports: [RouterLinkDirectiveStub]}
+      })
       .compileComponents();
   }
 
