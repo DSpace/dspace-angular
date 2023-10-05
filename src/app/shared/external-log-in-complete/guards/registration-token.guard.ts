742a078306ff5453a9402ab956d7d10f47176c0f
Alisa Ismailati <alisa.ismailati@4science.com>
2023-10-05 11:22:56 +0200

    [CST-10703] RegistrationTokenGuard fix

--- src/app/shared/external-log-in-complete/guards/registration-token.guard.ts
+++ src/app/shared/external-log-in-complete/guards/registration-token.guard.ts
@@ -19,7 +19,7 @@ export class RegistrationTokenGuard implements CanActivate {
   constructor(
     private router: Router,
     private epersonRegistrationService: EpersonRegistrationService
-  ) {}
+  ) { }
 
   /**
    * Determines if a user can activate a route based on the registration token.
@@ -31,20 +31,22 @@ export class RegistrationTokenGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Promise<boolean> | boolean | Observable<boolean> {
-    if (route.params.token) {
+    if (route.queryParams.token) {
       return this.epersonRegistrationService
         .searchRegistrationByToken(route.params.token)
         .pipe(
           getFirstCompletedRemoteData(),
           map(
-            (data: RemoteData<Registration>) =>
-              data.hasSucceeded && hasValue(data)
-          ),
-          tap((isValid: boolean) => {
-            if (!isValid) {
-              this.router.navigate(['/404']);
+            (data: RemoteData<Registration>) => {
+              // TODO: remove console.log
+              console.log(data, 'RegistrationTokenGuard');
+              if (data.hasSucceeded && hasValue(data)) {
+                return true;
+              } else {
+                this.router.navigate(['/404']);
+              }
             }
-          })
+          )
         );
     } else {
       this.router.navigate(['/404']);
