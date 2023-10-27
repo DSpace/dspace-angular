6e9a89d3d9b2e5d6b18950a8517da205041fc5c4
Vincenzo Mecca <vincenzo.mecca@4science.com>
2023-10-27 14:55:33 +0200

    [DSC-1326] Enables info alert for hidden publications

--- src/app/cris-layout/cris-layout-matrix/cris-layout-box-container/boxes/relation/cris-layout-relation-box.component.ts
+++ src/app/cris-layout/cris-layout-matrix/cris-layout-box-container/boxes/relation/cris-layout-relation-box.component.ts
@@ -5,6 +5,11 @@ import { CrisLayoutBoxModelComponent } from '../../../../models/cris-layout-box-
 import { TranslateService } from '@ngx-translate/core';
 import { CrisLayoutBox, RelationBoxConfiguration } from '../../../../../core/layout/models/box.model';
 import { Item } from '../../../../../core/shared/item.model';
+import { AuthService } from '../../../../../core/auth/auth.service';
+import { Observable, shareReplay, switchMap } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+import { hasValue } from '../../../../../shared/empty.util';
+import { EPerson } from '../../../../../core/eperson/models/eperson.model';
 
 @Component({
   selector: 'ds-cris-layout-search-box',
@@ -31,10 +36,18 @@ export class CrisLayoutRelationBoxComponent extends CrisLayoutBoxModelComponent 
    */
   // sideBarWidth = 3;
 
+  protected isResearcherProfileOwner$: Observable<boolean>;
+  protected isResearchoutputs: boolean;
+
+  showSearchResultNotice$: Observable<boolean>;
+  searchResultNotice$: Observable<string>;
+
   constructor(public cd: ChangeDetectorRef,
               protected translateService: TranslateService,
               @Inject('boxProvider') public boxProvider: CrisLayoutBox,
-              @Inject('itemProvider') public itemProvider: Item) {
+              @Inject('itemProvider') public itemProvider: Item,
+              protected authService: AuthService,
+  ) {
     super(translateService, boxProvider, itemProvider);
   }
 
@@ -43,6 +56,39 @@ export class CrisLayoutRelationBoxComponent extends CrisLayoutBoxModelComponent 
 
     this.searchFilter = `scope=${this.item.id}`;
     this.configuration = (this.box.configuration as RelationBoxConfiguration)['discovery-configuration'];
+    this.isResearchoutputs =
+      this.configuration.endsWith('researchoutputs') || this.configuration.endsWith('Publication');
+    this.isResearcherProfileOwner$ = this.isResearcherProfileOwner();
+    this.showSearchResultNotice$ = this.getShowSearchResultNotice();
+    this.searchResultNotice$ = this.getSearchResultNotice();
   }
 
+  protected getShowSearchResultNotice(): Observable<boolean> {
+    return this.isResearcherProfileOwner$.pipe(filter(Boolean), shareReplay(1));
+  }
+
+  protected getSearchResultNotice(researcherProfileOwner$: Observable<boolean> = this.showSearchResultNotice$): Observable<string> {
+    return researcherProfileOwner$
+      .pipe(
+        switchMap(() => this.translateService.get('manage.relationships.hidden-related-items-alert')),
+        shareReplay(1)
+      );
+  }
+
+  protected isResearcherProfileOwner(isResearchoutputs: boolean = this.isResearchoutputs): Observable<boolean> {
+    return this.authService.getAuthenticatedUserFromStore()
+      .pipe(
+        filter(hasValue),
+        map(user => isResearchoutputs && hasValue(this.getPersonEntity()) && hasValue(this.getOwner(user))),
+        shareReplay(1)
+      );
+  }
+
+  protected getOwner(user: EPerson) {
+    return this.item.firstMetadataValue('dspace.object.owner', { authority: user.id });
+  }
+
+  protected getPersonEntity() {
+    return this.item.firstMetadataValue('dspace.entity.type', { value: 'Person' });
+  }
 }
