066211d3fc119d02b31e6f1afcbd7f9138d491b5
milanmajchrak <90026355+milanmajchrak@users.noreply.github.com>
2023-10-27 15:09:35 +0200

    ufal/fe-update-vanilla-table-item-versions
    * Updated item versions table and defined translations.
    
    * Removed test of removed columns
    
    * Versions temp commit
    
    * All versions are shown
    
    * Show all versions from metadata
    
    * Added docs and comments.

--- src/app/item-page/versions/item-versions.component.ts
+++ src/app/item-page/versions/item-versions.component.ts
@@ -5,10 +5,10 @@ import {
   getAllSucceededRemoteDataPayload,
   getFirstCompletedRemoteData,
   getFirstSucceededRemoteData,
-  getFirstSucceededRemoteDataPayload,
+  getFirstSucceededRemoteDataPayload, getFirstSucceededRemoteListPayload,
   getRemoteDataPayload
 } from '../../core/shared/operators';
-import { map, mergeMap, startWith, switchMap, take, tap } from 'rxjs/operators';
+import { map, mergeMap, startWith, switchMap, take, tap} from 'rxjs/operators';
 import {
   getItemEditVersionhistoryRoute,
   getItemPageRoute,
@@ -41,7 +41,11 @@ import { PaginationService } from 'src/app/core/pagination/pagination.service';
 import { Version } from '../../core/shared/version.model';
 import { PaginatedSearchOptions } from '../../shared/search/models/paginated-search-options.model';
 import { followLink } from '../../shared/utils/follow-link-config.model';
-import { hasValue, hasValueOperator, isNotNull } from '../../shared/empty.util';
+import { hasValue, hasValueOperator, isNotEmpty, isNotNull } from '../../shared/empty.util';
+import { DSONameService } from '../../core/breadcrumbs/dso-name.service';
+import isEqual from 'lodash/isEqual';
+import { RequestParam } from '../../core/cache/models/request-param.model';
+import { FindListOptions } from '../../core/data/find-list-options.model';
 
 @Component({
   selector: 'ds-item-versions',
@@ -166,6 +170,20 @@ export class ItemVersionsComponent implements OnDestroy, OnInit {
    */
   showSubmitter$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(null);
 
+  /**
+   * If the version table is empty load the versions from the metadata `dc.relation.replaces` and
+   * `dc.relation.isreplacedby`
+   */
+  versionsFromMetadata: BehaviorSubject<RelationNameHandle[]> = new BehaviorSubject<RelationNameHandle[]>([]);
+
+  /**
+   * Loading of the name from the items handles for the items which are stored in the metadata dc.relation.replaces` and
+   * `dc.relation.isreplacedby`
+   * Names are stored in this dict to avoid endless calling rest API to get the name of the Item.
+   * @private
+   */
+  private nameCache: { [handle: string]: string } = {};
+
   constructor(private versionHistoryService: VersionHistoryDataService,
               private versionService: VersionDataService,
               private itemService: ItemDataService,
@@ -180,6 +198,7 @@ export class ItemVersionsComponent implements OnDestroy, OnInit {
               private workspaceItemDataService: WorkspaceitemDataService,
               private workflowItemDataService: WorkflowItemDataService,
               private configurationService: ConfigurationDataService,
+              private dsoNameService: DSONameService
   ) {
   }
 
@@ -535,9 +554,18 @@ export class ItemVersionsComponent implements OnDestroy, OnInit {
     }
   }
 
-  ngOnDestroy(): void {
-    this.cleanupSubscribes();
-    this.paginationService.clearPagination(this.options.id);
+  getItemNameFromVersion(version: Version) {
+    return version.item
+      .pipe(
+        getFirstSucceededRemoteDataPayload(),
+        map((item: Item) => this.dsoNameService.getName(item)));
+  }
+
+  getItemHandleFromVersion(version: Version) {
+    return version.item
+      .pipe(
+        getFirstSucceededRemoteDataPayload(),
+        map((item: Item) => item.firstMetadataValue('dc.identifier.uri')));
   }
 
   /**
@@ -547,4 +575,178 @@ export class ItemVersionsComponent implements OnDestroy, OnInit {
     this.subs.filter((sub) => hasValue(sub)).forEach((sub) => sub.unsubscribe());
   }
 
+  /**
+   * The Item from the current version could have linked another versions in the metadata `dc.relation.replaces` and
+   * `dc.relation.isreplacedby`. Get all this linked versions and show `name` and `handle` of that Items.
+   * @param versions
+   */
+  gelAllVersions(versions: Version[]) {
+    let allVersions: BehaviorSubject<VersionWithRelations[]> = new BehaviorSubject<VersionWithRelations[]>([]);
+    // Get item from current version and get all linked versions from the metadata `dc.relation.replaces`
+    // and `dc.relation.isreplacedby`
+    versions.forEach((version: Version) => {
+      // Send request to DSpace API to get item from the version.
+      version.item
+        .pipe(getFirstSucceededRemoteDataPayload())
+        .subscribe((item: Item) => {
+          let relationReplaces: RelationNameHandle[] = [];
+          // Get all metadatavalues from `dc.relation.replaces`
+          for (const metadataValue of item.allMetadataValues('dc.relation.replaces')) {
+            const newRelationReplaces: RelationNameHandle = {
+              name: '',
+              handle: metadataValue
+            };
+            relationReplaces.push(newRelationReplaces);
+          }
+          // Store isreplacedby
+          let relationIsReplacedBy: RelationNameHandle[] = [];
+          const allReplacedBy = [];
+          // Get all metadatavalues from `dc.relation.isreplacedby`
+          item.allMetadataValues('dc.relation.isreplacedby').forEach((metadataValue: string) => {
+            allReplacedBy.push(metadataValue);
+            const newRelationIsReplacedBy: RelationNameHandle = {
+              name: '',
+              handle: metadataValue
+            };
+            relationIsReplacedBy.push(newRelationIsReplacedBy);
+          });
+          // Add all linked versions to the VersionWithRelations object.
+          const newVersionWithRelations: VersionWithRelations = {
+            version: version,
+            replaces: relationReplaces,
+            isreplacedby: relationIsReplacedBy,
+            handle: item.firstMetadataValue('dc.identifier.uri')
+          };
+          // Ignore handle from the metadata if it is handle of current Item which is fetched from the version.
+          if (allReplacedBy.includes(this.item.firstMetadataValue('dc.identifier.uri'))) {
+            newVersionWithRelations.isreplacedby = [];
+          }
+          let updatedArray = allVersions.value;
+          updatedArray.push(newVersionWithRelations);
+          updatedArray = this.filterVersions(updatedArray);
+          allVersions.next(updatedArray);
+        });
+    });
+
+    return allVersions;
+  }
+
+  /**
+   * If the VersionsRD$ object is empty - there is not data in the database, but the Item has metadata
+   * `dc.relation.replaces` and `dc.relation.isreplacedby` show Item's info from that metadata fields.
+   */
+  getVersionsFromMetadata(item: Item) {
+    let relations = item.allMetadataValues('dc.relation.replaces');
+    // Merge two arrays into one
+    relations.push(...item.allMetadataValues('dc.relation.isreplacedby'));
+
+    relations.forEach((value: string, index) => {
+      const newRelationNameHandle: RelationNameHandle = {
+        name: this.getNameFromHandle(value),
+        handle: value
+      };
+      const currentVersionsFromMetadata = this.versionsFromMetadata.value;
+      currentVersionsFromMetadata[index] = newRelationNameHandle;
+      this.versionsFromMetadata.next(currentVersionsFromMetadata);
+    });
+    return this.versionsFromMetadata;
+  }
+
+  /**
+   * Remove duplicities from the replaces and isreplacedby arrays.
+   */
+  filterVersions(allVersions: VersionWithRelations[]) {
+    const filteredVersions: VersionWithRelations[] = [];
+    // Filter all versions table: Remove record from the table where the item:
+    // 1. has previous version handle the same as in the `dc.relation.replaces`
+    // 2. has new version handle the same the as in the `dc.relation.isreplacedby`
+    for (const currentVersion of allVersions) {
+      const index = allVersions.indexOf(currentVersion);
+      let previousVersion: VersionWithRelations;
+      let newestVersion: VersionWithRelations;
+
+      if (isNotNull(allVersions?.[index - 1])) {
+        newestVersion = allVersions?.[index - 1];
+      }
+      if (isNotNull(allVersions?.[index + 1])) {
+        previousVersion = allVersions?.[index + 1];
+      }
+
+      // Process previous versions
+      if (isNotEmpty(previousVersion)) {
+        const newReplaces = [];
+        currentVersion.replaces.forEach((relationNameHandle: RelationNameHandle) => {
+          if (isEqual(previousVersion.handle, relationNameHandle.handle)) {
+            return;
+          }
+          newReplaces.push(relationNameHandle);
+        });
+        currentVersion.replaces = newReplaces;
+      }
+
+      // Process newest versions
+      if (isNotEmpty(newestVersion)) {
+        const newIsReplacedBy = [];
+        currentVersion.isreplacedby.forEach((relationNameHandle: RelationNameHandle) => {
+          if (isEqual(newestVersion.handle, relationNameHandle.handle)) {
+            return;
+          }
+          newIsReplacedBy.push(relationNameHandle);
+        });
+        currentVersion.isreplacedby = newIsReplacedBy;
+      }
+      filteredVersions.push(currentVersion);
+    }
+    return filteredVersions;
+  }
+
+  /**
+   * Call rest API to get Item's name following its handle.
+   * The names are stored in the dictionary to avoid endless API calling.
+   */
+  getNameFromHandle(handle) {
+    if (!this.nameCache[handle]) {
+      // Create params for the request
+      const params = [new RequestParam('handle', handle)];
+      const paramOptions = Object.assign(new FindListOptions(), {
+        searchParams: [...params]
+      });
+
+      // Send request and process the async response
+      this.itemService
+        .searchBy('byHandle', paramOptions, false, true)
+        .pipe(
+          getFirstSucceededRemoteListPayload())
+        .subscribe((itemList: Item[]) => {
+          this.nameCache[handle] = this.dsoNameService.getName(itemList?.[0]);
+        });
+    }
+    return this.nameCache[handle];
+  }
+
+  ngOnDestroy(): void {
+    this.cleanupSubscribes();
+    this.paginationService.clearPagination(this.options.id);
+  }
+
+}
+
+/**
+ * It is override of the `Version` class. We need to show all replaces and isreplacedby metadata values
+ * for the current Version.
+ */
+export interface VersionWithRelations {
+  version: Version;
+  handle: string,
+  replaces: RelationNameHandle[];
+  isreplacedby: RelationNameHandle[];
+}
+
+/**
+ * Show handle and name of the Item which is fetched from the `dc.relation.replaces` and `dc.relation.isreplacedby`
+ * metadata of the current version.
+ */
+export interface RelationNameHandle {
+  name: string,
+  handle: string
 }
