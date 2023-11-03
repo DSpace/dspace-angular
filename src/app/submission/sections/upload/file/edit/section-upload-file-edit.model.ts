9ce9606c38eaf3a03c9ea6e49838ef6dbcc1d739
Giuseppe Digilio <giuseppe.digilio@4science.com>
2023-11-03 22:12:00 +0100

    [CST-12043] Fix layout

--- src/app/submission/sections/upload/file/edit/section-upload-file-edit.model.ts
+++ src/app/submission/sections/upload/file/edit/section-upload-file-edit.model.ts
@@ -59,16 +59,15 @@ export const BITSTREAM_FORM_ACCESS_CONDITION_TYPE_LAYOUT: DynamicFormControlLayo
 };
 export const BITSTREAM_FORM_PRIMARY_LAYOUT: DynamicFormControlLayout = {
   element: {
-    host: 'col-6',
-    textAlign: 'end'
+    host: 'col-12',
+    container: 'text-right'
   },
 };
 
 export const BITSTREAM_FORM_PRIMARY: DynamicSwitchModelConfig = {
   id: 'primary',
   name: 'primary',
-  label: 'bitstream.edit.form.primaryBitstream.label',
-  labelPosition: 'start',
+  label: 'bitstream.edit.form.primaryBitstream.label'
 };
 
 
