19eb92c803c3e8856924a8acfb195e570b118c97
Vincenzo Mecca <vincenzo.mecca@4science.com>
2023-11-10 09:22:17 +0100

    [DSC-1328] Fixes communities & collections admin menu

--- src/app/menu.resolver.ts
+++ src/app/menu.resolver.ts
@@ -663,7 +663,7 @@ export class MenuResolver implements Resolve<boolean> {
         {
           id: 'browse_global_communities_and_collections',
           active: false,
-          visible: !environment.layout.navbar.showCommunityCollection,
+          visible: Boolean(environment.layout.navbar.showCommunityCollection),
           model: {
             type: MenuItemType.LINK,
             text: `menu.section.communities_and_collections`,
