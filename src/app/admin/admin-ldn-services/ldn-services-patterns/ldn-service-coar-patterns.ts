f4b8c7f1f1deb128c9671e9fc054b1e28e6555a5
Mattia Vianelli <mattia.vianelli@4science.com>
2023-11-10 10:44:39 +0100

    CST-12179 Removed comments

--- src/app/admin/admin-ldn-services/ldn-services-patterns/ldn-service-coar-patterns.ts
+++ src/app/admin/admin-ldn-services/ldn-services-patterns/ldn-service-coar-patterns.ts
@@ -1,82 +1,31 @@
 export const notifyPatterns = [
-  //{
-     'ack-accept',
-    //name: 'ldn-service.form.pattern.acknowledge-and-accept.label',
-    //description: 'ldn-service.form.pattern.acknowledge-and-accept.description',
-    //category: 'ldn-service.form.pattern.acknowledge-and-accept.category'
-  //},
-  //{
-     'ack-reject',
-    //name: 'ldn-service.form.pattern.acknowledge-and-reject.label',
-    //description: 'ldn-service.form.pattern.acknowledge-and-reject.description',
-    //category: 'ldn-service.form.pattern.acknowledge-and-reject.category'
-  //},
-  //{
-    'ack-tentative-accept',
-    //name: 'ldn-service.form.pattern.acknowledge-and-tentatively-accept.label',
-    //description: 'ldn-service.form.pattern.acknowledge-and-tentatively-accept.description',
-    //category: 'ldn-service.form.pattern.acknowledge-and-tentatively-accept.category'
-  //},
-  //{
-     'ack-tentative-reject',
-    //name: 'ldn-service.form.pattern.acknowledge-and-tentatively-reject.label',
-    //description: 'ldn-service.form.pattern.acknowledge-and-tentatively-reject.description',
-    //category: 'ldn-service.form.pattern.acknowledge-and-tentatively-reject.category'
-  //},
-  //{
-     'announce-endorsement',
-    //name: 'ldn-service.form.pattern.announce-endorsement.label',
-    //description: 'ldn-service.form.pattern.announce-endorsement.description',
-    //category: 'ldn-service.form.pattern.announce-endorsement.category'
-  //},
-  //{
-    'announce-ingest',
-    //name: 'ldn-service.form.pattern.announce-ingest.label',
-    //description: 'ldn-service.form.pattern.announce-ingest.description',
-    //category: 'ldn-service.form.pattern.announce-ingest.category'
-  //},
-  //{
-    'announce-relationship',
-    //name: 'ldn-service.form.pattern.announce-relationship.label',
-    //description: 'ldn-service.form.pattern.announce-relationship.description',
-    //category: 'ldn-service.form.pattern.announce-relationship.category'
-  //},
-  //{
-   'announce-review',
-    //name: 'ldn-service.form.pattern.announce-review.label',
-    //description: 'ldn-service.form.pattern.announce-review.description',
-    //category: 'ldn-service.form.pattern.announce-review.category'
-  //},
-  //{
-    'announce-service-result',
-    //name: 'ldn-service.form.pattern.announce-service-result.label',
-    //description: 'ldn-service.form.pattern.announce-service-result.description',
-    //category: 'ldn-service.form.pattern.announce-service-result.category'
-  //},
-  //{
-     'request-endorsement',
-    //name: 'ldn-service.form.pattern.request-endorsement.label',
-    //description: 'ldn-service.form.pattern.request-endorsement.description',
-    //category: 'ldn-service.form.pattern.request-endorsement.category'
-  //},
-  //{
-    'request-ingest',
-    //name: 'ldn-service.form.pattern.request-ingest.label',
-    //description: 'ldn-service.form.pattern.request-ingest.description',
-    //category: 'ldn-service.form.pattern.request-ingest.category'
-  //},
-  //{
-    'request-review',
-    //name: 'ldn-service.form.pattern.request-review.label',
-    //description: 'ldn-service.form.pattern.request-review.description',
-    //category: 'ldn-service.form.pattern.request-review.category'
-  //},
-  //{
-     'undo-offer',
-    //name: 'ldn-service.form.pattern.undo-offer.label',
-    //description: 'ldn-service.form.pattern.undo-offer.description',
-    //category: 'ldn-service.form.pattern.undo-offer.category'
-  //}
+
+  'ack-accept',
+
+  'ack-reject',
+
+  'ack-tentative-accept',
+
+  'ack-tentative-reject',
+
+  'announce-endorsement',
+
+  'announce-ingest',
+
+  'announce-relationship',
+
+  'announce-review',
+
+  'announce-service-result',
+
+  'request-endorsement',
+
+  'request-ingest',
+
+  'request-review',
+
+  'undo-offer',
+
 ];
 
 
