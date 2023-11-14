ba6be749fe30610880bb4f100a085a5cb7b8d0ea
Giuseppe Digilio <giuseppe.digilio@4science.com>
2023-11-14 21:19:21 +0100

    [DSC-1328] Fix show community and collection menu that was visible for not admin users

--- src/app/menu.resolver.ts
+++ src/app/menu.resolver.ts
@@ -663,7 +663,7 @@ export class MenuResolver implements Resolve<boolean> {
         {
           id: 'browse_global_communities_and_collections',
           active: false,
-          visible: Boolean(environment.layout.navbar.showCommunityCollection),
+          visible: authorized && !environment.layout.navbar.showCommunityCollection,
           model: {
             type: MenuItemType.LINK,
             text: `menu.section.communities_and_collections`,
