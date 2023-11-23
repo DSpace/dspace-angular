7d2fdb759888ef73a4cc8e901fbb97b2a3b895a0
Davide Negretti <davide.negretti@4science.com>
2023-11-23 14:07:23 +0100

    [DURACOM-195] Base components (simplify header-navbar-wrapper)

--- src/app/header-nav-wrapper/header-navbar-wrapper.component.ts
+++ src/app/header-nav-wrapper/header-navbar-wrapper.component.ts
@@ -1,8 +1,5 @@
-import { Component, HostBinding, OnDestroy, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { AppState } from '../app.reducer';
-import { hasValue } from '../shared/empty.util';
-import { Observable, Subscription } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { MenuService } from '../shared/menu/menu.service';
 import { MenuID } from '../shared/menu/menu-id.model';
 import { HostWindowService, WidthCategory } from '../shared/host-window.service';
@@ -15,17 +12,14 @@ import { HostWindowService, WidthCategory } from '../shared/host-window.service'
   styleUrls: ['header-navbar-wrapper.component.scss'],
   templateUrl: 'header-navbar-wrapper.component.html',
 })
-export class HeaderNavbarWrapperComponent implements OnInit, OnDestroy {
-  @HostBinding('class.open') isOpen = false;
-  private sub: Subscription;
-  public isNavBarCollapsed: Observable<boolean>;
+export class HeaderNavbarWrapperComponent implements OnInit {
+  public isNavBarCollapsed$: Observable<boolean>;
   public isMobile$: Observable<boolean>;
 
   menuID = MenuID.PUBLIC;
   maxMobileWidth = WidthCategory.SM;
 
   constructor(
-    private store: Store<AppState>,
     private menuService: MenuService,
     protected windowService: HostWindowService,
   ) {
@@ -33,13 +27,7 @@ export class HeaderNavbarWrapperComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.isMobile$ = this.windowService.isUpTo(this.maxMobileWidth);
-    this.isNavBarCollapsed = this.menuService.isMenuCollapsed(this.menuID);
-    this.sub = this.isNavBarCollapsed.subscribe((isCollapsed) => this.isOpen = !isCollapsed);
+    this.isNavBarCollapsed$ = this.menuService.isMenuCollapsed(this.menuID);
   }
 
-  ngOnDestroy() {
-    if (hasValue(this.sub)) {
-      this.sub.unsubscribe();
-    }
-  }
 }
