486d2ad8c11e7914a5c7639ac7617f0453ad67fe
Stefano Maffei <stefano.maffei@4science.it>
2023-12-05 15:29:59 +0100

    [CST-12881] fix typo

--- src/app/item-page/simple/qa-event-notification/qa-event-notification.component.ts
+++ src/app/item-page/simple/qa-event-notification/qa-event-notification.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Item } from '../../../core/shared/item.model';
 import { getFirstCompletedRemoteData, getPaginatedListPayload, getRemoteDataPayload } from '../../../core/shared/operators';
 import { Observable, filter } from 'rxjs';
-import { AlertType } from '../../../shared/alert/aletr-type';
+import { AlertType } from '../../../shared/alert/alert-type';
 import { FindListOptions } from '../../../core/data/find-list-options.model';
 import { RequestParam } from '../../../core/cache/models/request-param.model';
 import { QualityAssuranceSourceDataService } from '../../../core/suggestion-notifications/qa/source/quality-assurance-source-data.service';
