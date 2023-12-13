763311ed29a3146a08bd56fefa7e6e75d5ae80f9
FrancescoMolinaro <francesco.molinaro@4science.com>
2023-12-13 18:13:00 +0100

    add navigation resolver config

--- src/app/admin/admin-ldn-services/admin-ldn-services-routing.module.ts
+++ src/app/admin/admin-ldn-services/admin-ldn-services-routing.module.ts
@@ -1,33 +1,43 @@
 import {NgModule} from '@angular/core';
-import {RouterModule} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 import {LdnServicesOverviewComponent} from './ldn-services-directory/ldn-services-directory.component';
 import {LdnServiceNewComponent} from './ldn-service-new/ldn-service-new.component';
 import {LdnServiceFormEditComponent} from './ldn-service-form-edit/ldn-service-form-edit.component';
 import {NavigationBreadcrumbResolver} from "../../core/breadcrumbs/navigation-breadcrumb.resolver";
+import {I18nBreadcrumbResolver} from "../../core/breadcrumbs/i18n-breadcrumb.resolver";
+
+
+const moduleRoutes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: LdnServicesOverviewComponent,
+    resolve: {breadcrumb: I18nBreadcrumbResolver},
+    data: {title: 'ldn-registered-services.title', breadcrumbKey: 'ldn-registered-services.new'},
+  },
+  {
+    path: 'new',
+    resolve: {breadcrumb: NavigationBreadcrumbResolver},
+    component: LdnServiceNewComponent,
+    data: {title: 'ldn-register-new-service.title', breadcrumbKey: 'ldn-register-new-service'}
+  },
+  {
+    path: 'edit/:serviceId',
+    resolve: {breadcrumb: NavigationBreadcrumbResolver},
+    component: LdnServiceFormEditComponent,
+    data: {title: 'ldn-edit-service.title', breadcrumbKey: 'ldn-edit-service'}
+  },
+];
+
+const relatedRoutes = moduleRoutes.map(route => {
+  return {...route, data: {...route.data, parentRoute: moduleRoutes[0]}}
+})
 
 @NgModule({
   imports: [
-    RouterModule.forChild([
-      {
-        path: '',
-        pathMatch: 'full',
-        component: LdnServicesOverviewComponent,
-        resolve: {breadcrumb: NavigationBreadcrumbResolver},
-        data: {title: 'ldn-registered-services.title', breadcrumbKey: 'ldn-registered-services.new'},
-      },
-      {
-        path: 'new',
-        resolve: {breadcrumb: NavigationBreadcrumbResolver},
-        component: LdnServiceNewComponent,
-        data: {title: 'ldn-register-new-service.title', breadcrumbKey: 'ldn-register-new-service'}
-      },
-      {
-        path: 'edit/:serviceId',
-        resolve: {breadcrumb: NavigationBreadcrumbResolver},
-        component: LdnServiceFormEditComponent,
-        data: {title: 'ldn-edit-service.title', breadcrumbKey: 'ldn-edit-service'}
-      },
-    ]),
+    RouterModule.forChild(moduleRoutes.map(route => {
+      return {...route, data: {...route.data, relatedRoutes }}
+    }))
   ]
 })
 export class AdminLdnServicesRoutingModule {
--- src/app/core/breadcrumbs/navigation-breadcrumb.resolver.ts
+++ src/app/core/breadcrumbs/navigation-breadcrumb.resolver.ts
@@ -1,8 +1,9 @@
 import { BreadcrumbConfig } from '../../breadcrumbs/breadcrumb/breadcrumb-config.model';
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import {ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot, Routes} from '@angular/router';
 import { hasNoValue } from '../../shared/empty.util';
 import { currentPathFromSnapshot } from '../../shared/utils/route.utils';
+import {NavigationBreadcrumbsService} from "./navigation-breadcrumb.service";
 
 /**
  * The class that resolves a BreadcrumbConfig object with an i18n key string for a route
@@ -11,9 +12,13 @@ import { currentPathFromSnapshot } from '../../shared/utils/route.utils';
   providedIn: 'root'
 })
 export class NavigationBreadcrumbResolver implements Resolve<BreadcrumbConfig<string>> {
-  constructor() {
+  constructor(protected breadcrumbService: NavigationBreadcrumbsService) {
   }
 
+
+  private getUniqueParentRoutes(routes: Routes) : Routes {
+    return [...new Set(routes.map(route => route.data.parentRoute))];
+  }
   /**
    * Method for resolving an I18n breadcrumb configuration object
    * @param {ActivatedRouteSnapshot} route The current ActivatedRouteSnapshot
@@ -21,7 +26,11 @@ export class NavigationBreadcrumbResolver implements Resolve<BreadcrumbConfig<st
    * @returns BreadcrumbConfig object
    */
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): BreadcrumbConfig<string> {
-   console.log(route, state);
-   return null
+    const path = route.routeConfig.path;
+    const relatedRoutes = route.data.relatedRoutes.filter(relatedRoute => relatedRoute.path !== path);
+    const uniqueParentRoutes = this.getUniqueParentRoutes(route.data.relatedRoutes);
+
+   console.log(path, relatedRoutes, uniqueParentRoutes);
+   return {provider: this.breadcrumbService, key: "combinedBredcrumbKeys", url: ""}
   }
 }
