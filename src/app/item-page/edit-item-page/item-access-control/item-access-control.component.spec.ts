ca5cca384d5be26355c695dc9fa92c54acf947da
Andrea Barbasso <andrea.barbasso@4science.com>
2023-12-14 17:53:56 +0100

    [DURACOM-191] fix item-access-control component test

--- src/app/item-page/edit-item-page/item-access-control/item-access-control.component.spec.ts
+++ src/app/item-page/edit-item-page/item-access-control/item-access-control.component.spec.ts
@@ -1,16 +1,43 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ItemAccessControlComponent } from './item-access-control.component';
+import { of } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import {
+  AccessControlFormContainerComponent
+} from '../../../shared/access-control-form-container/access-control-form-container.component';
 
-// TODO: enable this test suite and fix it
-xdescribe('ItemAccessControlComponent', () => {
+describe('ItemAccessControlComponent', () => {
   let component: ItemAccessControlComponent;
   let fixture: ComponentFixture<ItemAccessControlComponent>;
+  let routeStub = {
+    parent: {
+      parent: {
+        data: {
+          pipe: () => {
+            return {
+              pipe: () => {
+                return of({});
+              }
+            };
+          }
+        }
+      }
+    }
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-    imports: [ItemAccessControlComponent]
+    imports: [ItemAccessControlComponent],
+    providers: [{
+      provide: ActivatedRoute, useValue: routeStub
+    }]
 })
+    .overrideComponent(ItemAccessControlComponent, {
+      remove: {
+        imports: [AccessControlFormContainerComponent]
+      }
+    })
     .compileComponents();
   });
 
