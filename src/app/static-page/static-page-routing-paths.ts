eb5db6fb30955d9ca8b2a71ca903e8882b9daf0c
milanmajchrak <90026355+milanmajchrak@users.noreply.github.com>
2023-12-14 16:56:28 +0100

    ufal/fe-fix-static-page-redirect (#421)
    * Fixed static page redirects.
    
    * Refactored processing links.

--- src/app/static-page/static-page-routing-paths.ts
+++ src/app/static-page/static-page-routing-paths.ts
@@ -3,4 +3,5 @@
  */
 export const STATIC_PAGE_PATH = 'static';
 export const STATIC_FILES_PROJECT_PATH = 'static-files';
+export const HTML_SUFFIX = '.html';
 export const STATIC_FILES_DEFAULT_ERROR_PAGE_PATH = STATIC_FILES_PROJECT_PATH + '/' + 'error.html';
