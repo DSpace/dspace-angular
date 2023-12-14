ae452b33885a658594e2ceb3c4d9aeeef6912c94
Andrea Barbasso <andrea.barbasso@4science.com>
2023-12-14 17:48:26 +0100

    [DURACOM-191] fix community-access-control component test

--- src/app/community-page/edit-community-page/community-access-control/community-access-control.component.spec.ts
+++ src/app/community-page/edit-community-page/community-access-control/community-access-control.component.spec.ts
@@ -1,16 +1,43 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CommunityAccessControlComponent } from './community-access-control.component';
+import {
+  AccessControlFormContainerComponent
+} from '../../../shared/access-control-form-container/access-control-form-container.component';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
-// TODO: enable this test suite and fix it
-xdescribe('CommunityAccessControlComponent', () => {
+describe('CommunityAccessControlComponent', () => {
   let component: CommunityAccessControlComponent;
   let fixture: ComponentFixture<CommunityAccessControlComponent>;
+  let routeStub = {
+    parent: {
+      parent: {
+        data: {
+          pipe: () => {
+            return {
+              pipe: () => {
+                return of({});
+              }
+            };
+          }
+        }
+      }
+    }
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-    imports: [CommunityAccessControlComponent]
+    imports: [CommunityAccessControlComponent],
+    providers: [{
+      provide: ActivatedRoute, useValue: routeStub
+    }]
 })
+    .overrideComponent(CommunityAccessControlComponent, {
+      remove: {
+        imports: [AccessControlFormContainerComponent]
+      }
+    })
     .compileComponents();
   });
 
