20780fae20b0c366465d7a5801778caef92c33b2
Andrea Barbasso <andrea.barbasso@4science.com>
2023-12-20 11:56:06 +0100

    [DURACOM-191] fix import

--- src/app/process-page/form/process-parameters/parameter-select/parameter-select.component.spec.ts
+++ src/app/process-page/form/process-parameters/parameter-select/parameter-select.component.spec.ts
@@ -1,5 +1,4 @@
-import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
-import { TranslateModule } from '@ngx-translate/core';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { ParameterSelectComponent } from './parameter-select.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
