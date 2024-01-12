61a05dff13a80c75aeac0efe041e89b4dd44e3f3
FrancescoMolinaro <francesco.molinaro@4science.com>
2024-01-12 18:06:09 +0100

    add navigation with new config, add custom filter

--- src/app/admin/admin-notify-dashboard/admin-notify-logs/admin-notify-outgoing/admin-notify-outgoing.component.ts
+++ src/app/admin/admin-notify-dashboard/admin-notify-logs/admin-notify-outgoing/admin-notify-outgoing.component.ts
@@ -2,6 +2,10 @@ import { Component, Inject } from '@angular/core';
 import { SEARCH_CONFIG_SERVICE } from '../../../../my-dspace-page/my-dspace-page.component';
 import { Context } from '../../../../core/shared/context.model';
 import { SearchConfigurationService } from '../../../../core/shared/search/search-configuration.service';
+import { Observable } from "rxjs";
+import { ActivatedRoute, ActivatedRouteSnapshot, Router } from "@angular/router";
+import { ViewMode } from "../../../../core/shared/view-mode.model";
+import { tap } from "rxjs/operators";
 
 
 @Component({
@@ -15,8 +19,38 @@ import { SearchConfigurationService } from '../../../../core/shared/search/searc
   ]
 })
 export class AdminNotifyOutgoingComponent {
-  protected readonly context = Context.CoarNotify;
+  public selectedSearchConfig$: Observable<string>;
+  public defaultConfiguration = 'NOTIFY.outgoing';
+  public isLoading = true;
 
-  constructor(@Inject(SEARCH_CONFIG_SERVICE) public searchConfigService: SearchConfigurationService) {
+
+  protected readonly context = Context.CoarNotify;
+  constructor(@Inject(SEARCH_CONFIG_SERVICE) public searchConfigService: SearchConfigurationService,
+              private router: Router,
+              private route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.selectedSearchConfig$ = this.searchConfigService.getCurrentConfiguration(this.defaultConfiguration).pipe(
+        tap(() => this.isLoading = false)
+    )
+  }
+
+  public resetDefaultConfiguration() {
+    this.router.navigate([this.getResolvedUrl(this.route.snapshot)], {
+      queryParams: {
+        configuration: this.defaultConfiguration,
+        view: ViewMode.Table
+      },
+    })
+  }
+
+  /**
+   *
+   * @param route url path
+   * @returns url path
+   */
+  private getResolvedUrl(route: ActivatedRouteSnapshot): string {
+    return route.pathFromRoot.map(v => v.url.map(segment => segment.toString()).join('/')).join('/');
   }
 }
--- src/app/admin/admin-notify-dashboard/admin-notify-metrics/admin-notify-metrics.component.ts
+++ src/app/admin/admin-notify-dashboard/admin-notify-metrics/admin-notify-metrics.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { AdminNotifyMetricsRow } from './admin-notify-metrics.model';
+import { ActivatedRoute, Router } from "@angular/router";
+import { ViewMode } from "../../../core/shared/view-mode.model";
 
 @Component({
   selector: 'ds-admin-notify-metrics',
@@ -10,7 +12,23 @@ export class AdminNotifyMetricsComponent {
   @Input()
   boxesConfig: AdminNotifyMetricsRow[];
 
-  public navigateToSelectedSearchConfig($event: string) {
-    console.log($event);
+  private incomingConfiguration = 'NOTIFY.incoming';
+  private inboundPath = '/inbound';
+  private outboundPath = '/outbound';
+
+  constructor(private router: Router) {
+  }
+
+
+  public navigateToSelectedSearchConfig(searchConfig: string) {
+    const isIncomingConfig = searchConfig.startsWith(this.incomingConfiguration);
+    const selectedPath = isIncomingConfig ? this.inboundPath : this.outboundPath;
+
+    this.router.navigate([`${this.router.url}${selectedPath}`], {
+      queryParams: {
+        configuration: searchConfig,
+        view: ViewMode.Table
+      },
+    })
   }
 }
