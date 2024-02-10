7a10834f82c9e646cd7e53dd3050bcb187fe01ae
Kim Shepherd <kim@shepherd.nz>
2024-02-11 12:21:56 +1300

    [TLC-674] Dupe layout changes as per review
    i18n fix
    alignment fix
    comment fix

--- src/app/shared/object-list/duplicate-data/duplicate.resource-type.ts
+++ src/app/shared/object-list/duplicate-data/duplicate.resource-type.ts
@@ -1,7 +1,7 @@
 import { ResourceType } from 'src/app/core/shared/resource-type';
 
 /**
- * The resource type for Access Status
+ * The resource type for Duplicate preview stubs
  *
  * Needs to be in a separate file to prevent circular
  * dependencies in webpack.
