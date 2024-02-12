0501730fca548379ed0628c7192e4deecaf0d4df
milanmajchrak <90026355+milanmajchrak@users.noreply.github.com>
2024-02-12 09:17:47 +0100

    ufal/cannot-login-after-upgrade
    Merge pull request #2731 from DSpace/backport-2670-to-main (#512)
    
    [Port main] Ensure HALEndpointService doesn't use stale responses
    
    Co-authored-by: Tim Donohue <tim.donohue@lyrasis.org>

--- src/app/core/server-check/server-check.guard.ts
+++ src/app/core/server-check/server-check.guard.ts
@@ -53,10 +53,8 @@ export class ServerCheckGuard implements CanActivateChild {
    */
   listenForRouteChanges(): void {
     // we'll always be too late for the first NavigationStart event with the router subscribe below,
-    // so this statement is for the very first route operation. A `find` without using the cache,
-    // rather than an invalidateRootCache, because invalidating as the app is bootstrapping can
-    // break other features
-    this.rootDataService.findRoot(false);
+    // so this statement is for the very first route operation.
+    this.rootDataService.invalidateRootCache();
 
     this.router.events.pipe(
       filter(event => event instanceof NavigationStart),
