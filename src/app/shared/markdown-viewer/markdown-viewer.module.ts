d614db187e4ee6f4b80c747aa4c57397c2625c2a
Giuseppe Digilio <giuseppe.digilio@4science.com>
2024-02-20 20:00:32 +0100

    [UXP-114] Remove unused import

--- src/app/shared/markdown-viewer/markdown-viewer.module.ts
+++ src/app/shared/markdown-viewer/markdown-viewer.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MarkdownViewerComponent } from './markdown-viewer.component';
-import { NuMarkdownModule } from '@ng-util/markdown';
 import { SharedModule } from '../shared.module';
 
 @NgModule({
   declarations: [ MarkdownViewerComponent ],
   exports: [ MarkdownViewerComponent ],
-    imports: [CommonModule, NuMarkdownModule, SharedModule]
+  imports: [ CommonModule, SharedModule ]
 })
 export class MarkdownViewerModule { }
