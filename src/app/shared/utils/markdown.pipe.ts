81b0d76f1ba4923ed763c05eae4f128bec115a3b
Giuseppe Digilio <giuseppe.digilio@4science.com>
2024-02-21 16:29:36 +0100

    [DURACOM-240] Improve markdown pipe in order to check for empty values

--- src/app/shared/utils/markdown.pipe.ts
+++ src/app/shared/utils/markdown.pipe.ts
@@ -11,6 +11,7 @@ import {
 } from '@angular/platform-browser';
 
 import { environment } from '../../../environments/environment';
+import { isEmpty } from '../empty.util';
 
 const markdownItLoader = async () => (await import('markdown-it')).default;
 type LazyMarkdownIt = ReturnType<typeof markdownItLoader>;
@@ -60,8 +61,8 @@ export class MarkdownPipe implements PipeTransform {
   ) {
   }
 
-  async transform(value: string): Promise<SafeHtml> {
-    if (!environment.markdown.enabled) {
+  async transform(value: string, forcePreview = false): Promise<SafeHtml> {
+    if (isEmpty(value) || (!environment.markdown.enabled && !forcePreview)) {
       return value;
     }
     const MarkdownIt = await this.markdownIt;
