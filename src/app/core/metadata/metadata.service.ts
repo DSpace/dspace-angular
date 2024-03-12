d7c331a06499605c322ee56f71ec9b4764d1d91f
Giuseppe Digilio <giuseppe.digilio@4science.com>
2024-03-12 19:34:11 +0100

    [DSC-1454] limit author to first 20 entries to avoid issue with item page rendering

--- src/app/core/metadata/metadata.service.ts
+++ src/app/core/metadata/metadata.service.ts
@@ -8,12 +8,12 @@ import { TranslateService } from '@ngx-translate/core';
 import {
   BehaviorSubject,
   combineLatest,
-  Observable,
-  of as observableOf,
   concat as observableConcat,
-  EMPTY
+  EMPTY,
+  Observable,
+  of as observableOf
 } from 'rxjs';
-import { filter, map, switchMap, take, mergeMap } from 'rxjs/operators';
+import { filter, map, mergeMap, switchMap, take } from 'rxjs/operators';
 
 import { hasNoValue, hasValue, isNotEmpty } from '../../shared/empty.util';
 import { DSONameService } from '../breadcrumbs/dso-name.service';
@@ -25,10 +25,7 @@ import { BitstreamFormat } from '../shared/bitstream-format.model';
 import { Bitstream } from '../shared/bitstream.model';
 import { DSpaceObject } from '../shared/dspace-object.model';
 import { Item } from '../shared/item.model';
-import {
-  getFirstCompletedRemoteData,
-  getFirstSucceededRemoteDataPayload
-} from '../shared/operators';
+import { getFirstCompletedRemoteData, getFirstSucceededRemoteDataPayload } from '../shared/operators';
 import { RootDataService } from '../data/root-data.service';
 import { getBitstreamDownloadRoute } from '../../app-routing-paths';
 import { BundleDataService } from '../data/bundle-data.service';
@@ -245,7 +242,8 @@ export class MetadataService {
    * Add <meta name="citation_author" ... >  to the <head>
    */
   private setCitationAuthorTags(): void {
-    const values: string[] = this.getMetaTagValues(['dc.author', 'dc.contributor.author', 'dc.creator']);
+    // limit author to first 20 entries to avoid issue with item page rendering
+    const values: string[] = this.getMetaTagValues(['dc.author', 'dc.contributor.author', 'dc.creator']).slice(0, 20);
     this.addMetaTags('citation_author', values);
   }
 
