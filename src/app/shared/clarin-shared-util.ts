8df82616604d3f7973792b33bd92bd121e7417dc
milanmajchrak <90026355+milanmajchrak@users.noreply.github.com>
2024-03-18 14:34:40 +0100

    Show authors from the `dc.contributor.other` metadata field (#544)
    * Show authors from the `dc.contributor.other` metadata field

--- src/app/shared/clarin-shared-util.ts
+++ src/app/shared/clarin-shared-util.ts
@@ -50,18 +50,14 @@ export function convertMetadataFieldIntoSearchType(field: string[]) {
  * @param item current Item
  * @param itemAuthors BehaviourSubject (async) of Authors with search links
  * @param baseUrl e.g. localhost:8080
+ * @param fields metadata fields where authors are stored
  */
-export function loadItemAuthors(item, itemAuthors, baseUrl) {
+export function loadItemAuthors(item, itemAuthors, baseUrl, fields) {
   if (isNull(item) || isNull(itemAuthors) || isNull(baseUrl)) {
     return;
   }
 
-  let authorsMV: MetadataValue[] = item?.metadata?.['dc.contributor.author'];
-  // Harvested Items has authors in the metadata field `dc.creator`.
-  if (isUndefined(authorsMV)) {
-    authorsMV = item?.metadata?.['dc.creator'];
-  }
-
+  let authorsMV: MetadataValue[] = item?.allMetadata(fields);
   if (isUndefined(authorsMV)) {
     return null;
   }
