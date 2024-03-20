a2f795d934f14bfa2fbdcd35464bae77ff93d1e2
Sascha Szott <szott@gmx.de>
2024-03-20 17:26:18 +0100

    fixed lint warnings

--- src/app/item-page/edit-item-page/item-version-history/item-version-history.component.ts
+++ src/app/item-page/edit-item-page/item-version-history/item-version-history.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
