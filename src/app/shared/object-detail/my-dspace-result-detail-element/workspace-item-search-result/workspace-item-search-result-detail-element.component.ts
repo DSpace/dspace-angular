b290dac32fb5e856850f1ff5c2a51d2e0f1af39b
Sascha Szott <szott@gmx.de>
2024-03-20 17:26:45 +0100

    fixed lint warnings

--- src/app/shared/object-detail/my-dspace-result-detail-element/workspace-item-search-result/workspace-item-search-result-detail-element.component.ts
+++ src/app/shared/object-detail/my-dspace-result-detail-element/workspace-item-search-result/workspace-item-search-result-detail-element.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+} from '@angular/core';
 import { Observable } from 'rxjs';
 import { find } from 'rxjs/operators';
 
