fb8733ca0bbf7d937a66c780803e5b61f7efdbc1
Alexandre Vryghem <alexandre@atmire.com>
2024-04-02 16:27:54 +0200

    113901: Store the retrieved Relationships in the store
    This needs to be done here too, since tilted relationships won't be returned by the api/core/items/{uuid}/relationships endpoint

--- src/app/item-page/edit-item-page/item-relationships/edit-relationship-list/edit-relationship-list.component.ts
+++ src/app/item-page/edit-item-page/item-relationships/edit-relationship-list/edit-relationship-list.component.ts
@@ -512,10 +512,15 @@ export class EditRelationshipListComponent implements OnInit, OnDestroy {
             false,
             true,
             ...linksToFollow
-          )),
-      ).subscribe((rd: RemoteData<PaginatedList<Relationship>>) => {
-        this.relationshipsRd$.next(rd);
-      })
+          ),
+        ),
+        tap((rd: RemoteData<PaginatedList<Relationship>>) => {
+          this.relationshipsRd$.next(rd);
+        }),
+        getFirstSucceededRemoteDataPayload(),
+      ).subscribe((relationshipPaginatedList: PaginatedList<Relationship>) => {
+        this.objectUpdatesService.initialize(this.url, relationshipPaginatedList.page, new Date());
+      }),
     );
 
     // keep isLastPage$ up to date based on relationshipsRd$
