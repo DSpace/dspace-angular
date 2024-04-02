19548fc6d4abd588fba21d0a30f496553b639f43
Giuseppe Digilio <giuseppe.digilio@4science.com>
2024-04-02 12:31:32 +0200

    [DURACOM-234] Move resolver dependency to relate functional one

--- src/app/community-page/community-page-administrator.guard.ts
+++ src/app/community-page/community-page-administrator.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
+  ResolveFn,
   Router,
   RouterStateSnapshot,
 } from '@angular/router';
@@ -13,6 +14,7 @@ import { AuthService } from '../core/auth/auth.service';
 import { AuthorizationDataService } from '../core/data/feature-authorization/authorization-data.service';
 import { DsoPageSingleFeatureGuard } from '../core/data/feature-authorization/feature-authorization-guard/dso-page-single-feature.guard';
 import { FeatureID } from '../core/data/feature-authorization/feature-id';
+import { RemoteData } from '../core/data/remote-data';
 import { Community } from '../core/shared/community.model';
 import { CommunityPageResolver } from './community-page.resolver';
 
@@ -23,11 +25,13 @@ import { CommunityPageResolver } from './community-page.resolver';
  * Guard for preventing unauthorized access to certain {@link Community} pages requiring administrator rights
  */
 export class CommunityPageAdministratorGuard extends DsoPageSingleFeatureGuard<Community> {
-  constructor(protected resolver: CommunityPageResolver,
-              protected authorizationService: AuthorizationDataService,
+
+  protected resolver: ResolveFn<RemoteData<Community>> = CommunityPageResolver;
+
+  constructor(protected authorizationService: AuthorizationDataService,
               protected router: Router,
               protected authService: AuthService) {
-    super(resolver, authorizationService, router, authService);
+    super(authorizationService, router, authService);
   }
 
   /**
