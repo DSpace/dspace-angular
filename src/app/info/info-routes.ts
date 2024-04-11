e10630f131e71a61e1eec617fb0eed7ba7572e68
Alexandre Vryghem <alexandre@atmire.com>
2024-04-12 01:38:34 +0200

    Added functionality to disable COAR notify support link in footer
    Also fixed error that showed a blank page instead of a 404 when disabling EULA/Privacy Policy pages

--- src/app/info/info-routes.ts
+++ src/app/info/info-routes.ts
@@ -1,17 +1,26 @@
+import {
+  Route,
+  Routes,
+} from '@angular/router';
+
 import { environment } from '../../environments/environment';
 import { i18nBreadcrumbResolver } from '../core/breadcrumbs/i18n-breadcrumb.resolver';
+import { notifyInfoGuard } from '../core/coar-notify/notify-info/notify-info.guard';
 import { feedbackGuard } from '../core/feedback/feedback.guard';
+import { hasValue } from '../shared/empty.util';
 import { ThemedEndUserAgreementComponent } from './end-user-agreement/themed-end-user-agreement.component';
 import { ThemedFeedbackComponent } from './feedback/themed-feedback.component';
 import {
+  COAR_NOTIFY_SUPPORT,
   END_USER_AGREEMENT_PATH,
   FEEDBACK_PATH,
   PRIVACY_PATH,
 } from './info-routing-paths';
+import { NotifyInfoComponent } from './notify-info/notify-info.component';
 import { ThemedPrivacyComponent } from './privacy/themed-privacy.component';
 
 
-export const ROUTES = [
+export const ROUTES: Routes = [
   {
     path: FEEDBACK_PATH,
     component: ThemedFeedbackComponent,
@@ -31,4 +40,16 @@ export const ROUTES = [
     resolve: { breadcrumb: i18nBreadcrumbResolver },
     data: { title: 'info.privacy.title', breadcrumbKey: 'info.privacy' },
   } : undefined,
-];
+  environment.info.enableCOARNotifySupport ? {
+    path: COAR_NOTIFY_SUPPORT,
+    component: NotifyInfoComponent,
+    canActivate: [notifyInfoGuard],
+    resolve: {
+      breadcrumb: i18nBreadcrumbResolver,
+    },
+    data: {
+      title: 'info.coar-notify-support.title',
+      breadcrumbKey: 'info.coar-notify-support',
+    },
+  } : undefined,
+].filter((route: Route) => hasValue(route));
