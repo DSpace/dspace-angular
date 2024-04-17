b1824da72a7f1b4c3d13e3db778f57a51eddad3f
William Welling <wwelling@tamu.edu>
2024-04-17 09:30:10 -0500

    Return constant type value for explicitly named component

--- src/app/core/shared/value-list-browse-definition.model.ts
+++ src/app/core/shared/value-list-browse-definition.model.ts
@@ -31,6 +31,32 @@ export class ValueListBrowseDefinition extends NonHierarchicalBrowseDefinition {
   };
 
   getRenderType(): string {
-    return this.dataType;
+    // FIXME
+    // 4/17/2024 9:03 AM CDT https://github.com/DSpace/dspace-angular/blob/dspace-7.6.1/src/app/core/shared/non-hierarchical-browse-definition.ts
+    // dataType: BrowseByDataType;
+
+    // 4/17/2024 9:03 AM CDT https://github.com/DSpace/dspace-angular/blob/dspace-7.6.1/src/app/browse-by/browse-by-switcher/browse-by-decorator.ts#L9
+    // export enum BrowseByDataType {
+    //   Title = 'title',
+    //   Metadata = 'text',
+    //   Date = 'date'
+    // }
+
+    // 4/17/2024 9:03 AM CDT https://github.com/DSpace/dspace-angular/blob/dspace-7.6.1/src/app/core/shared/value-list-browse-definition.resource-type.ts
+    // ...
+    // 4/17/2024 9:05 AM CDT https://github.com/DSpace/dspace-angular/blob/dspace-7.6.1/src/app/shared/object-list/metadata-representation-list-element/browse-link/browse-link-metadata-list-element.component.ts
+    // 4/17/2024 9:06 AM CDT https://github.com/DSpace/dspace-angular/blob/dspace-7.6.1/src/app/shared/object-list/metadata-representation-list-element/browse-link/browse-link-metadata-list-element.component.html#L5
+    // ...
+    // 4/17/2024 9:10 AM CDT https://github.com/DSpace/dspace-angular/blob/dspace-7.6.1/src/app/item-page/simple/metadata-representation-list/themed-metadata-representation-list.component.ts
+    // 4/17/2024 9:10 AM CDT https://github.com/DSpace/dspace-angular/blob/dspace-7.6.1/src/app/item-page/simple/metadata-representation-list/metadata-representation-list.component.html
+    // ...
+    // 4/17/2024 9:11 AM CDT https://github.com/DSpace/dspace-angular/blob/dspace-7.6.1/src/app/shared/metadata-representation/metadata-representation-loader.component.ts
+    // 4/17/2024 9:11 AM CDT https://github.com/DSpace/dspace-angular/blob/dspace-7.6.1/src/app/shared/metadata-representation/metadata-representation-loader.component.html
+
+    // Return `VALUE_LIST_BROWSE_DEFINITION.value` for simple item view to render a link for 'dc.contributor.author' or 'dc.creator' of Person item type to the correct browse by URL with queryParam `value` and not `startsWith`.
+    // https://github.com/DSpace/dspace-angular/blob/dspace-7.6.1/src/app/item-page/simple/item-types/untyped-item/untyped-item.component.html#L29
+
+    // return this.dataType;
+    return this.type.value;
   }
 }
