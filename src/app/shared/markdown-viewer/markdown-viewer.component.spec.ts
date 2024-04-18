272e0f2de5afd1a65a7635a3acf2458d649d00ad
Simone Ramundi <simone.ramundi@4science.com>
2024-04-18 15:46:42 +0200

    [DSC-1524] Adjusted test for End User Agreement not shown

--- src/app/shared/markdown-viewer/markdown-viewer.component.spec.ts
+++ src/app/shared/markdown-viewer/markdown-viewer.component.spec.ts
@@ -2,13 +2,16 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MarkdownViewerComponent } from './markdown-viewer.component';
 
+import { SharedModule } from '../shared.module';
+
 describe('DsMarkdownViewerComponent', () => {
   let component: MarkdownViewerComponent;
   let fixture: ComponentFixture<MarkdownViewerComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MarkdownViewerComponent ]
+      declarations: [ MarkdownViewerComponent ],
+      imports: [ SharedModule ]
     })
     .compileComponents();
   });
