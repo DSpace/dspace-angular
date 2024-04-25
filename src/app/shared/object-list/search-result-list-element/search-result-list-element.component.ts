f055ff6fbd91b804bddc6530dae69e07a63ae099
Oscar Chacón <oscar@escire.lat>
2024-04-25 16:19:21 -0600

    Fix: author search result without co-author

--- src/app/shared/object-list/search-result-list-element/search-result-list-element.component.ts
+++ src/app/shared/object-list/search-result-list-element/search-result-list-element.component.ts
@@ -48,6 +48,28 @@ export class SearchResultListElementComponent<T extends SearchResult<K>, K exten
   }
 
   /**
+   * Gets all matching metadata string values from hitHighlights or dso metadata.
+   *
+   * @param {string|string[]} keyOrKeys The metadata key(s) in scope. Wildcards are supported; see [[Metadata]].
+   * @returns {string[]} the matching string values or an empty array.
+   */
+  allMetadataNoExcludingValues(keyOrKeys: string | string[]): string[] {
+    let dsoMetadata: string[] = Metadata.allValues([this.dso.metadata], keyOrKeys);
+    let highlights: string[] = Metadata.allValues([this.object.hitHighlights], keyOrKeys);
+    let removedHighlights: string[] = highlights.map(str => str.replace(/<\/?em>/g, ''));
+    for (let i = 0; i < removedHighlights.length; i++) {
+        let index = dsoMetadata.indexOf(removedHighlights[i]);        
+        if (index !== -1) {
+          dsoMetadata[index] = highlights[i];
+        }
+    }
+    console.log(dsoMetadata);
+    console.log(highlights);
+    console.log(removedHighlights);
+    return dsoMetadata;
+  }
+
+  /**
    * Gets the first matching metadata string value from hitHighlights or dso metadata, preferring hitHighlights.
    *
    * @param {string|string[]} keyOrKeys The metadata key(s) in scope. Wildcards are supported; see [[Metadata]].
