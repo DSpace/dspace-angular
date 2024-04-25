814c81b7b7b82b1890f4b006e58150bf723a5acd
Art Lowel <art.lowel@gmail.com>
2024-04-25 10:56:54 +0200

    update test
    (cherry picked from commit e4b098e64dd98af3d87c8f9f2f28f9c3ea3c0336)

--- src/app/item-page/edit-item-page/item-relationships/item-relationships.component.spec.ts
+++ src/app/item-page/edit-item-page/item-relationships/item-relationships.component.spec.ts
@@ -285,7 +285,7 @@ describe('ItemRelationshipsComponent', () => {
     });
 
     it('it should delete the correct relationship', () => {
-      expect(relationshipService.deleteRelationship).toHaveBeenCalledWith(relationships[1].uuid, 'left');
+      expect(relationshipService.deleteRelationship).toHaveBeenCalledWith(relationships[1].uuid, 'left', false);
     });
   });
 
