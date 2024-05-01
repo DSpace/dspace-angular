0feb57fa1bb07b63b9cca80b027453d039fc6792
David Tolmie <dtolmie@uw.edu>
2024-05-01 14:22:28 -0700

    Update untyped-item.component.ts
    Activating the UW theme for simple untyped item

--- src/themes/uwlib/app/item-page/simple/item-types/untyped-item/untyped-item.component.ts
+++ src/themes/uwlib/app/item-page/simple/item-types/untyped-item/untyped-item.component.ts
@@ -17,8 +17,8 @@ import {
   selector: 'ds-untyped-item',
   // styleUrls: ['./untyped-item.component.scss'],
   styleUrls: ['../../../../../../../app/item-page/simple/item-types/untyped-item/untyped-item.component.scss'],
-  // templateUrl: './untyped-item.component.html',
-  templateUrl: '../../../../../../../app/item-page/simple/item-types/untyped-item/untyped-item.component.html',
+  templateUrl: './untyped-item.component.html',
+  // templateUrl: '../../../../../../../app/item-page/simple/item-types/untyped-item/untyped-item.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UntypedItemComponent extends BaseComponent {
