07a03bfa2d01b8c87deace41ca5e605ebd87684a
Giuseppe Digilio <giuseppe.digilio@4science.com>
2024-05-16 15:59:48 +0200

    fix after merge

--- src/app/item-page/simple/field-components/specific-field/item-page-field.component.ts
+++ src/app/item-page/simple/field-components/specific-field/item-page-field.component.ts
@@ -12,25 +12,6 @@ import { Item } from '../../../../core/shared/item.model';
 import { getFirstCompletedRemoteData } from '../../../../core/shared/operators';
 import { MetadataValuesComponent } from '../../../field-components/metadata-values/metadata-values.component';
 import { ImageField } from './image-field';
-import { getFirstCompletedRemoteData } from '../../../../core/shared/operators';
-
-/**
- * Interface that encapsulate Image configuration for this component.
- */
-export interface ImageField {
-  /**
-   * URI that is used to retrieve the image.
-   */
-  URI: string;
-  /**
-   * i18n Key that represents the alt text to display
-   */
-  alt: string;
-  /**
-   * CSS variable that contains the height of the inline image.
-   */
-  heightVar: string;
-}
 
 
 /**
