99384a70605b2eccaaae249103673c3735da8c85
Alexandre Vryghem <alexandre@atmire.com>
2024-05-21 12:02:15 +0200

    115051: Created ThemedAdminSearchPageComponent

--- src/themes/custom/lazy-theme.module.ts
+++ src/themes/custom/lazy-theme.module.ts
@@ -114,6 +114,7 @@ import { ObjectListComponent } from './app/shared/object-list/object-list.compon
 import { BrowseByMetadataPageComponent } from './app/browse-by/browse-by-metadata-page/browse-by-metadata-page.component';
 import { BrowseByDatePageComponent } from './app/browse-by/browse-by-date-page/browse-by-date-page.component';
 import { BrowseByTitlePageComponent } from './app/browse-by/browse-by-title-page/browse-by-title-page.component';
+import { AdminSearchPageComponent } from './app/admin/admin-search-page/admin-search-page.component';
 
 const DECLARATIONS = [
   FileSectionComponent,
@@ -168,8 +169,7 @@ const DECLARATIONS = [
   BrowseByMetadataPageComponent,
   BrowseByDatePageComponent,
   BrowseByTitlePageComponent,
-
-
+  AdminSearchPageComponent,
 ];
 
 @NgModule({
