19a85ddb6035fd249ea32a6eb78fc0df479d5784
Victor Hugo Duran Santiago <victorhugodurans@gmail.com>
2024-05-24 10:05:45 -0600

    Fix - Change the breadcrumbs to list on platform mobile, update test component

--- src/app/breadcrumbs/breadcrumbs.component.ts
+++ src/app/breadcrumbs/breadcrumbs.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Breadcrumb } from './breadcrumb/breadcrumb.model';
 import { BreadcrumbsService } from './breadcrumbs.service';
 import { Observable } from 'rxjs';
+import {HostWindowService, WidthCategory} from '../shared/host-window.service';
 
 /**
  * Component representing the breadcrumbs of a page
@@ -9,10 +10,16 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'ds-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
-  styleUrls: ['./breadcrumbs.component.scss']
+  styleUrls: ['./breadcrumbs.component.scss'],
 })
 export class BreadcrumbsComponent {
 
+  public isMobile$: Observable<boolean>;
+
+  /**
+   * Observable of max mobile width
+   */
+  maxMobileWidth = WidthCategory.SM;
   /**
    * Observable of the list of breadcrumbs for this page
    */
@@ -25,9 +32,13 @@ export class BreadcrumbsComponent {
 
   constructor(
     private breadcrumbsService: BreadcrumbsService,
+    public windowService: HostWindowService
   ) {
     this.breadcrumbs$ = breadcrumbsService.breadcrumbs$;
     this.showBreadcrumbs$ = breadcrumbsService.showBreadcrumbs$;
+  }
+  ngOnInit(): void {
+    this.isMobile$ = this.windowService.isUpTo(this.maxMobileWidth);
   }
 
 }
