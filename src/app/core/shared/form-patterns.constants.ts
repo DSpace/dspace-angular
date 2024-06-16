4b49c7c8fad603d75682349303739d65afb3c4e7
Victor Hugo Duran Santiago <victorhugodurans@gmail.com>
2024-06-15 21:45:45 -0600

    Change patterns for form with email inputs

--- src/app/core/shared/form-patterns.constants.ts
+++ src/app/core/shared/form-patterns.constants.ts
@@ -1,2 +1,2 @@
 //Patterns for form validations
-export const VALID_EMAIL_PATTERN = "^[a-zA-Z0-9.!#$%&\\' * +\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$";
+export const VALID_EMAIL_PATTERN = "^[a-zA-Z0-9.!#$%&\'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$";
