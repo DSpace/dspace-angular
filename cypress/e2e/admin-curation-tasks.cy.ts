2ad9a44b99cbaedf22a6133c816b4cb39fe7d67f
FrancescoMolinaro <francesco.molinaro@4science.com>
2024-07-22 17:53:03 +0200

    [CST-15592] add accessibility tests for:
    Admin Curation Task
    Bitstream Format
    Health Page
    Metadata Schema

--- cypress/e2e/admin-curation-tasks.cy.ts
+++ cypress/e2e/admin-curation-tasks.cy.ts
@@ -1,16 +1,16 @@
 import { testA11y } from 'cypress/support/utils';
 
-describe('Metadata Registry', () => {
+describe('Admin Curation Tasks', () => {
   beforeEach(() => {
     // Must login as an Admin to see the page
-    cy.visit('/admin/registries/metadata');
+    cy.visit('/admin/curation-tasks');
     cy.loginViaForm(Cypress.env('DSPACE_TEST_ADMIN_USER'), Cypress.env('DSPACE_TEST_ADMIN_PASSWORD'));
   });
 
   it('should pass accessibility tests', () => {
     // Page must first be visible
-    cy.get('ds-metadata-registry').should('be.visible');
-    // Analyze <ds-metadata-registry> for accessibility issues
-    testA11y('ds-metadata-registry');
+    cy.get('ds-admin-curation-task').should('be.visible');
+    // Analyze <ds-admin-curation-task> for accessibility issues
+    testA11y('ds-admin-curation-task');
   });
 });
