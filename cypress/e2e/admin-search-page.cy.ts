69f618e8566445b800b83bcc4604bd2e59a98b28
FrancescoMolinaro <francesco.molinaro@4science.com>
2024-07-23 17:13:20 +0200

    [CST-15592] improve tests, add attributes for testing, fix wrong references

--- cypress/e2e/admin-search-page.cy.ts
+++ cypress/e2e/admin-search-page.cy.ts
@@ -12,6 +12,9 @@ describe('Admin Search Page', () => {
     cy.get('ds-admin-search-page').should('be.visible');
     // At least one search result should be displayed
     cy.get('[data-test="list-object"]').should('be.visible');
+    // Click each filter toggle to open *every* filter
+    // (As we want to scan filter section for accessibility issues as well)
+    cy.get('[data-test="filter-toggle"]').click({ multiple: true });
     // Analyze <ds-admin-search-page> for accessibility issues
     testA11y('ds-admin-search-page');
   });
--- cypress/e2e/health-page.cy.ts
+++ cypress/e2e/health-page.cy.ts
@@ -1,16 +1,35 @@
 import { testA11y } from 'cypress/support/utils';
 import { Options } from 'cypress-axe';
 
-describe('Health Page', () => {
-  beforeEach(() => {
-    // Must login as an Admin to see the page
-    cy.visit('/health');
-    cy.loginViaForm(Cypress.env('DSPACE_TEST_ADMIN_USER'), Cypress.env('DSPACE_TEST_ADMIN_PASSWORD'));
-  });
 
+beforeEach(() => {
+  // Must login as an Admin to see the page
+  cy.visit('/health');
+  cy.loginViaForm(Cypress.env('DSPACE_TEST_ADMIN_USER'), Cypress.env('DSPACE_TEST_ADMIN_PASSWORD'));
+});
+
+describe('Health Page > Status Tab', () => {
   it('should pass accessibility tests', () => {
     // Page must first be visible
     cy.get('ds-health-page').should('be.visible');
+    // Analyze <ds-health-page> for accessibility issues
+    testA11y('ds-health-page', {
+      rules: {
+        // All panels are accordians & fail "aria-required-children" and "nested-interactive".
+        // Seem to require updating ng-bootstrap and https://github.com/DSpace/dspace-angular/issues/2216
+        'aria-required-children': { enabled: false },
+        'nested-interactive': { enabled: false },
+      },
+    } as Options);
+  });
+});
+
+describe('Health Page > Info Tab', () => {
+  it('should pass accessibility tests', () => {
+    // Page must first be visible
+    cy.get('ds-health-page').should('be.visible');
+    cy.get('a[data-test="health-page.info-tab"]').click();
+
     // Analyze <ds-health-page> for accessibility issues
     testA11y('ds-health-page', {
       rules: {
