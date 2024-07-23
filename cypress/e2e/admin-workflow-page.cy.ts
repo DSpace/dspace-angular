db891f2f16103203a8b1cdab4bf363028bd4a820
FrancescoMolinaro <francesco.molinaro@4science.com>
2024-07-23 17:13:20 +0200

    [CST-15592] improve tests, add attributes for testing, fix wrong references

--- cypress/e2e/admin-workflow-page.cy.ts
+++ cypress/e2e/admin-workflow-page.cy.ts
@@ -12,6 +12,9 @@ describe('Admin Workflow Page', () => {
     cy.get('ds-admin-workflow-page').should('be.visible');
     // At least one search result should be displayed
     cy.get('[data-test="list-object"]').should('be.visible');
+    // Click each filter toggle to open *every* filter
+    // (As we want to scan filter section for accessibility issues as well)
+    cy.get('[data-test="filter-toggle"]').click({ multiple: true });
     // Analyze <ds-admin-workflow-page> for accessibility issues
     testA11y('ds-admin-workflow-page');
   });
