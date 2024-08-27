e258e657f29614d272f5b32eb85431275559e912
Andrea Barbasso <andrea.barbasso@4science.com>
2024-08-27 16:54:48 +0200

    [DSC-1847] create map for renderExternalLoginConfirmationFor decorator

--- src/app/external-log-in/decorators/external-log-in.methods-decorator.ts
+++ src/app/external-log-in/decorators/external-log-in.methods-decorator.ts
@@ -1,12 +1,17 @@
 import { AuthRegistrationType } from '../../core/auth/models/auth.registration-type';
+import { OrcidConfirmationComponent } from '../registration-types/orcid-confirmation/orcid-confirmation.component';
 
 /**
  * Map to store the external login confirmation component for the given auth method type
  */
 const authMethodsMap = new Map();
+
+authMethodsMap.set(AuthRegistrationType.Orcid, OrcidConfirmationComponent);
+
 /**
  * Decorator to register the external login confirmation component for the given auth method type
  * @param authMethodType the type of the external login method
+ * @deprecated
  */
 export function renderExternalLoginConfirmationFor(
   authMethodType: AuthRegistrationType,
