3f4bf7ce0fdf73494bcb28045ef6d7cb8a4634dd
Andreas Awouters <andreas.awouters@atmire.com>
2024-09-04 16:32:30 +0200

    117803: Fix existing tests

--- src/app/item-page/edit-item-page/item-bitstreams/item-edit-bitstream-bundle/item-edit-bitstream-bundle.component.spec.ts
+++ src/app/item-page/edit-item-page/item-bitstreams/item-edit-bitstream-bundle/item-edit-bitstream-bundle.component.spec.ts
@@ -6,6 +6,15 @@ import { Item } from '../../../../core/shared/item.model';
 import { Bundle } from '../../../../core/shared/bundle.model';
 import { ResponsiveTableSizes } from '../../../../shared/responsive-table-sizes/responsive-table-sizes';
 import { ResponsiveColumnSizes } from '../../../../shared/responsive-table-sizes/responsive-column-sizes';
+import { BundleDataService } from '../../../../core/data/bundle-data.service';
+import { of as observableOf } from 'rxjs';
+import { ObjectUpdatesService } from '../../../../core/data/object-updates/object-updates.service';
+import { PaginationServiceStub } from '../../../../shared/testing/pagination-service.stub';
+import { RequestService } from '../../../../core/data/request.service';
+import { getMockRequestService } from '../../../../shared/mocks/request.service.mock';
+import { ItemBitstreamsService } from '../item-bitstreams.service';
+import { PaginationService } from '../../../../core/pagination/pagination.service';
+import { PaginationComponentOptions } from '../../../../shared/pagination/pagination-component-options.model';
 
 describe('ItemEditBitstreamBundleComponent', () => {
   let comp: ItemEditBitstreamBundleComponent;
@@ -31,10 +40,37 @@ describe('ItemEditBitstreamBundleComponent', () => {
     }
   });
 
+  const restEndpoint = 'fake-rest-endpoint';
+  const bundleService = jasmine.createSpyObj('bundleService', {
+    getBitstreamsEndpoint: observableOf(restEndpoint),
+    getBitstreams: null,
+  });
+
+  const objectUpdatesService = {
+    initialize: () => {
+      // do nothing
+    },
+  };
+
+  const itemBitstreamsService = jasmine.createSpyObj('itemBitstreamsService', {
+    getInitialBitstreamsPaginationOptions: Object.assign(new PaginationComponentOptions(), {
+      id: 'bundles-pagination-options',
+      currentPage: 1,
+      pageSize: 9999
+    }),
+  });
+
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [TranslateModule.forRoot()],
       declarations: [ItemEditBitstreamBundleComponent],
+      providers: [
+        { provide: BundleDataService, useValue: bundleService },
+        { provide: ObjectUpdatesService, useValue: objectUpdatesService },
+        { provide: PaginationService, useValue: new PaginationServiceStub() },
+        { provide: RequestService, useValue: getMockRequestService() },
+        { provide: ItemBitstreamsService, useValue: itemBitstreamsService },
+      ],
       schemas: [
         NO_ERRORS_SCHEMA
       ]
