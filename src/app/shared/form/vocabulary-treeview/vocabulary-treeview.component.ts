5d410b7c533324ef2fe1e33e31a173ee0bc17613
andreaNeki <andrea.montoya@neki-it.com.br>
2024-09-27 16:18:56 -0300

    Trying to correct an error in the focus implementation
    (cherry picked from commit 73d39abfe562a3ecd8b6e4ed20e8744a177ea1de)

--- src/app/shared/form/vocabulary-treeview/vocabulary-treeview.component.ts
+++ src/app/shared/form/vocabulary-treeview/vocabulary-treeview.component.ts
@@ -75,7 +75,7 @@ export class VocabularyTreeviewComponent implements OnDestroy, OnInit, OnChanges
   /**
    * Implemented to manage focus on input
    */
-  @ViewChild('searchInput') searchInput: ElementRef;
+  @ViewChild('searchInput') searchInput!: ElementRef;
 
   /**
    * The {@link VocabularyOptions} object
@@ -339,7 +339,9 @@ export class VocabularyTreeviewComponent implements OnDestroy, OnInit, OnChanges
       this.storedNodeMap = new Map<string, TreeviewFlatNode>();
       this.vocabularyTreeviewService.restoreNodes();
     }
-    this.searchInput.nativeElement.focus();
+    if (this.searchInput) {
+      this.searchInput.nativeElement.focus();
+    }
   }
 
   add() {
