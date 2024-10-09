2b466bb382553907d743a2cdbed2df3f90782814
FrancescoMolinaro <francesco.molinaro@4science.com>
2024-10-09 14:16:47 +0200

    [CST-15592] add interceptor for edit group, fix landmark-unique accessibility issue in pagination component

--- cypress/e2e/edit-group.cy.ts
+++ cypress/e2e/edit-group.cy.ts
@@ -5,6 +5,9 @@ describe('Edit Group', () => {
     // Must login as an Admin to see the page
     cy.visit('/access-control/groups/'.concat(Cypress.env('DSPACE_ADMINISTRATOR_GROUP')).concat('/edit'));
     cy.loginViaForm(Cypress.env('DSPACE_TEST_ADMIN_USER'), Cypress.env('DSPACE_TEST_ADMIN_PASSWORD'));
+    // Wait for all search components to be loaded
+    cy.intercept('GET', '/server/api/eperson/groups/'.concat(Cypress.env('DSPACE_ADMINISTRATOR_GROUP').concat('/subgroups*'))).as('subGroups');
+    cy.wait('@subGroups');
   });
 
   it('should pass accessibility tests', () => {
