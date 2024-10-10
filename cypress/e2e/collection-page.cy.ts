ba2570b83bb1631cd7799b6a04c57bb974ce0bc4
FrancescoMolinaro <francesco.molinaro@4science.com>
2024-10-10 16:18:01 +0200

    Add interceptor for collection page to avoid timeout

--- cypress/e2e/collection-page.cy.ts
+++ cypress/e2e/collection-page.cy.ts
@@ -4,8 +4,9 @@ describe('Collection Page', () => {
 
     it('should pass accessibility tests', () => {
         cy.visit('/collections/'.concat(Cypress.env('DSPACE_TEST_COLLECTION')));
-
-        // <ds-collection-page> tag must be loaded
+        cy.intercept('GET','server/api/core/collections/'.concat(Cypress.env('DSPACE_TEST_COLLECTION'))).as('collection');
+        cy.wait('@collection');
+      // <ds-collection-page> tag must be loaded
         cy.get('ds-collection-page').should('be.visible');
 
         // Analyze <ds-collection-page> for accessibility issues
