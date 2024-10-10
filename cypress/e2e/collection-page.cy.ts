6388613948cb6d090ff459b0124541ac876707ae
FrancescoMolinaro <francesco.molinaro@4science.com>
2024-10-10 18:00:35 +0200

    revert adding interceptor as not reliable on remote test

--- cypress/e2e/collection-page.cy.ts
+++ cypress/e2e/collection-page.cy.ts
@@ -2,14 +2,13 @@ import { testA11y } from 'cypress/support/utils';
 
 describe('Collection Page', () => {
 
-    it('should pass accessibility tests', () => {
-        cy.visit('/collections/'.concat(Cypress.env('DSPACE_TEST_COLLECTION')));
-        cy.intercept('GET','/server/api/dso/find?uuid='.concat(Cypress.env('DSPACE_TEST_COLLECTION'))).as('dso');
-        cy.wait('@dso');
-      // <ds-collection-page> tag must be loaded
-        cy.get('ds-collection-page').should('be.visible');
+  it('should pass accessibility tests', () => {
+    cy.visit('/collections/'.concat(Cypress.env('DSPACE_TEST_COLLECTION')));
 
-        // Analyze <ds-collection-page> for accessibility issues
-        testA11y('ds-collection-page');
-    });
+    // <ds-collection-page> tag must be loaded
+    cy.get('ds-collection-page').should('be.visible');
+
+    // Analyze <ds-collection-page> for accessibility issues
+    testA11y('ds-collection-page');
+  });
 });
