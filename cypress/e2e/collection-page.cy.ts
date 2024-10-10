7cf7749367ce46fe9c9c7e3959f60e0f5aeec5ba
FrancescoMolinaro <francesco.molinaro@4science.com>
2024-10-10 17:32:46 +0200

    correct intercept to dso resolved for route

--- cypress/e2e/collection-page.cy.ts
+++ cypress/e2e/collection-page.cy.ts
@@ -4,8 +4,8 @@ describe('Collection Page', () => {
 
     it('should pass accessibility tests', () => {
         cy.visit('/collections/'.concat(Cypress.env('DSPACE_TEST_COLLECTION')));
-        cy.intercept('GET','server/api/core/collections/'.concat(Cypress.env('DSPACE_TEST_COLLECTION'))).as('collection');
-        cy.wait('@collection');
+        cy.intercept('GET','/server/api/dso/find?uuid='.concat(Cypress.env('DSPACE_TEST_COLLECTION'))).as('dso');
+        cy.wait('@dso');
       // <ds-collection-page> tag must be loaded
         cy.get('ds-collection-page').should('be.visible');
 
