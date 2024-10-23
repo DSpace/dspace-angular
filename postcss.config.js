e7ff5646084696d96d33c56bd0ea04568c375d85
Tim Donohue <tim.donohue@lyrasis.org>
2024-10-23 13:56:46 -0500

    Remove unused postcss-responsive-type

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   plugins: [
     require('postcss-import')(),
-    require('postcss-preset-env')(),
-    require('postcss-responsive-type')()
+    require('postcss-preset-env')()
   ]
 };
