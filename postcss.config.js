04410485a5e08f125b6a595115921a45d6982860
Tim Donohue <tim.donohue@lyrasis.org>
2024-10-23 13:55:36 -0500

    Remove unused/unmaintained postcss-apply

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,6 @@ module.exports = {
   plugins: [
     require('postcss-import')(),
     require('postcss-preset-env')(),
-    require('postcss-apply')(),
     require('postcss-responsive-type')()
   ]
 };
