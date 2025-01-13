2500195dfaba088886665346b1a61c31d99edc3b
Giuseppe Digilio <giuseppe.digilio@4science.com>
2025-01-13 18:00:37 +0100

    [DSC-2142] fix creation of short-lived token during SSR by initializing XSRF token

--- src/app/core/auth/server-auth-request.service.ts
+++ src/app/core/auth/server-auth-request.service.ts
@@ -14,8 +14,9 @@ import {
   XSRF_RESPONSE_HEADER,
   DSPACE_XSRF_COOKIE
 } from '../xsrf/xsrf.constants';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+import { XSRFService } from '../xsrf/xsrf.service';
 
 /**
  * Server side version of the service to send authentication requests
@@ -28,6 +29,7 @@ export class ServerAuthRequestService extends AuthRequestService {
     requestService: RequestService,
     rdbService: RemoteDataBuildService,
     protected httpClient: HttpClient,
+    protected xsrfService: XSRFService
   ) {
     super(halService, requestService, rdbService);
   }
@@ -43,6 +45,7 @@ export class ServerAuthRequestService extends AuthRequestService {
     return this.httpClient.get(this.halService.getRootHref(), { observe: 'response' }).pipe(
       // retrieve the XSRF token from the response header
       map((response: HttpResponse<any>) => response.headers.get(XSRF_RESPONSE_HEADER)),
+      tap(() => this.xsrfService.tokenInitialized$.next(true)),
       // Use that token to create an HttpHeaders object
       map((xsrfToken: string) => new HttpHeaders()
           .set('Content-Type', 'application/json; charset=utf-8')
