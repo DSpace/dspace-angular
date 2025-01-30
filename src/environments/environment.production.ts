290c24b448985c961559f04c01ac43eb4b5bc8f6
Giuseppe Digilio <giuseppe.digilio@4science.com>
2025-01-30 20:41:32 +0100

    [DURACOM-288] Enable URL replacement by default

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -10,7 +10,7 @@ export const environment: Partial<BuildConfig> = {
     time: false,
     inlineCriticalCss: false,
     transferState: true,
-    replaceRestUrl: false,
+    replaceRestUrl: true,
     paths: [ '/home', '/items/', '/entities/', '/collections/', '/communities/', '/bitstream/', '/bitstreams/', '/handle/', '/reload/' ],
     enableSearchComponent: false,
     enableBrowseComponent: false,
