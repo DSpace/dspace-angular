f38de3d89db7f06504af44e5807f46b26bc327ef
Simone Ramundi <simone.ramundi@4science.com>
2025-02-04 16:27:50 +0100

    [DSC-2038] Set property from environment

--- src/app/shared/form/builder/parsers/tag-field-parser.ts
+++ src/app/shared/form/builder/parsers/tag-field-parser.ts
@@ -2,6 +2,8 @@ import { FieldParser } from './field-parser';
 import { FormFieldMetadataValueObject } from '../models/form-field-metadata-value.model';
 import { DynamicTagModel, DynamicTagModelConfig } from '../ds-dynamic-form-ui/models/tag/dynamic-tag.model';
 import {DynamicFormControlLayout} from '@ng-dynamic-forms/core';
+import { environment } from '../../../../../environments/environment';
+
 export class TagFieldParser extends FieldParser {
   public modelFactory(fieldValue?: FormFieldMetadataValueObject | any, label?: boolean): any {
     let clsTag: DynamicFormControlLayout;
@@ -15,6 +17,7 @@ export class TagFieldParser extends FieldParser {
       && this.configData.selectableMetadata[0].controlledVocabulary.length > 0) {
       this.setVocabularyOptions(tagModelConfig, this.parserOptions.collectionUUID);
     }
+    tagModelConfig.minChars = environment.submission.minChars;
     this.setValues(tagModelConfig, fieldValue, null, true);
     tagModelConfig.placeholder = 'Enter the Keywords';
     const tagModel = new DynamicTagModel(tagModelConfig, clsTag);
