f82426b1dc8853b6a984fba2523474f145ecd3eb
Andreas Awouters <andreas.awouters@atmire.com>
2025-02-05 16:05:48 +0100

    125969: Get rid of REQUEST and RESPONSE imports in AuthService

--- src/app/core/auth/server-auth.service.ts
+++ src/app/core/auth/server-auth.service.ts
@@ -1,21 +1,59 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject, Optional } from '@angular/core';
 import { HttpHeaders } from '@angular/common/http';
-
+import { REQUEST, RESPONSE } from '@nguniversal/express-engine/tokens';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-
 import { hasValue, isNotEmpty } from '../../shared/empty.util';
 import { HttpOptions } from '../dspace-rest/dspace-rest.service';
-import { AuthService } from './auth.service';
+import { AuthService, LOGIN_ROUTE } from './auth.service';
 import { AuthStatus } from './models/auth-status.model';
 import { AuthTokenInfo } from './models/auth-token-info.model';
 import { RemoteData } from '../data/remote-data';
+import { NativeWindowService, NativeWindowRef } from '../services/window.service';
+import { AuthRequestService } from './auth-request.service';
+import { EPersonDataService } from '../eperson/eperson-data.service';
+import { Router } from '@angular/router';
+import { RouteService } from '../services/route.service';
+import { CookieService } from '../services/cookie.service';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../app.reducer';
+import { HardRedirectService } from '../services/hard-redirect.service';
+import { NotificationsService } from '../../shared/notifications/notifications.service';
+import { TranslateService } from '@ngx-translate/core';
 
 /**
  * The auth service.
  */
 @Injectable()
 export class ServerAuthService extends AuthService {
+
+  constructor(
+    @Inject(REQUEST) protected req: any,
+    @Optional() @Inject(RESPONSE) private response: any,
+    @Inject(NativeWindowService) protected _window: NativeWindowRef,
+    protected authRequestService: AuthRequestService,
+    protected epersonService: EPersonDataService,
+    protected router: Router,
+    protected routeService: RouteService,
+    protected storage: CookieService,
+    protected store: Store<AppState>,
+    protected hardRedirectService: HardRedirectService,
+    protected notificationService: NotificationsService,
+    protected translateService: TranslateService
+  ) {
+    super(
+      _window,
+      authRequestService,
+      epersonService,
+      router,
+      routeService,
+      storage,
+      store,
+      hardRedirectService,
+      notificationService,
+      translateService
+    );
+  }
 
   /**
    * Returns the authenticated user
@@ -59,5 +97,19 @@ export class ServerAuthService extends AuthService {
     return this.authRequestService.getRequest('status', options).pipe(
       map((rd: RemoteData<AuthStatus>) => Object.assign(new AuthStatus(), rd.payload))
     );
+  }
+
+  override redirectToLoginWhenTokenExpired() {
+    const redirectUrl = LOGIN_ROUTE + '?expired=true';
+    if (this._window.nativeWindow.location) {
+      // Hard redirect to login page, so that all state is definitely lost
+      this._window.nativeWindow.location.href = redirectUrl;
+    } else if (this.response) {
+      if (!this.response._headerSent) {
+        this.response.redirect(302, redirectUrl);
+      }
+    } else {
+      this.router.navigateByUrl(redirectUrl);
+    }
   }
 }
