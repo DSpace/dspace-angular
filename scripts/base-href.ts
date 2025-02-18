2003ef4e9234b8cc158000b5d67edfaf4d224450
Giuseppe Digilio <giuseppe.digilio@4science.com>
2025-02-18 09:28:09 +0100

    [DURACOM-327] Fix start:dev command

--- scripts/base-href.ts
+++ scripts/base-href.ts
@@ -6,31 +6,31 @@ import { buildAppConfig } from '../src/config/config.server';
 
 /**
  * Script to set baseHref as `ui.nameSpace` for development mode. Adds `baseHref` to angular.json build options.
- * 
+ *
  * Usage (see package.json):
- * 
+ *
  * yarn base-href
  */
 
 const appConfig: AppConfig = buildAppConfig();
 
-const angularJsonPath = join(process.cwd(), 'angular.json');
+const projectJsonPath = join(process.cwd(), 'project.json');
 
-if (!existsSync(angularJsonPath)) {
-  console.error(`Error:\n${angularJsonPath} does not exist\n`);
+if (!existsSync(projectJsonPath)) {
+  console.error(`Error:\n${projectJsonPath} does not exist\n`);
   process.exit(1);
 }
 
 try {
-  const angularJson = require(angularJsonPath);
+  const projectJson = require(projectJsonPath);
 
   const baseHref = `${appConfig.ui.nameSpace}${appConfig.ui.nameSpace.endsWith('/') ? '' : '/'}`;
 
-  console.log(`Setting baseHref to ${baseHref} in angular.json`);
+  console.log(`Setting baseHref to ${baseHref} in projects.json`);
 
-  angularJson.projects['dspace-angular'].architect.build.options.baseHref = baseHref;
+  projectJson.targets.build.options.baseHref = baseHref;
 
-  writeFileSync(angularJsonPath, JSON.stringify(angularJson, null, 2) + '\n');
+  writeFileSync(projectJsonPath, JSON.stringify(projectJson, null, 2) + '\n');
 } catch (e) {
   console.error(e);
 }
