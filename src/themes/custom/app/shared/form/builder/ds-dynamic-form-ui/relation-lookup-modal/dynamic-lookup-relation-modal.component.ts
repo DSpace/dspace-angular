1163140e91c7871048a8ddcf5b185e83ca4c3fa0
abhinav <abhinav.sidharthan@atmire.com>
2025-02-19 11:00:54 +0100

    fix ThemeableDsDynamicLookupRelationModalComponent Component decorator

--- src/themes/custom/app/shared/form/builder/ds-dynamic-form-ui/relation-lookup-modal/dynamic-lookup-relation-modal.component.ts
+++ src/themes/custom/app/shared/form/builder/ds-dynamic-form-ui/relation-lookup-modal/dynamic-lookup-relation-modal.component.ts
@@ -2,22 +2,12 @@ import { Component } from '@angular/core';
 import {
   DsDynamicLookupRelationModalComponent as BaseComponent
 } from '../../../../../../../../app/shared/form/builder/ds-dynamic-form-ui/relation-lookup-modal/dynamic-lookup-relation-modal.component';
-import { SEARCH_CONFIG_SERVICE } from '../../../../../../../../app/my-dspace-page/my-dspace-page.component';
-import {
-  SearchConfigurationService
-} from '../../../../../../../../app/core/shared/search/search-configuration.service';
 
 
 @Component({
   selector: 'ds-dynamic-lookup-relation-modal',
   styleUrls: ['../../../../../../../../app/shared/form/builder/ds-dynamic-form-ui/relation-lookup-modal/dynamic-lookup-relation-modal.component.scss'],
-  // templateUrl: '../../../../../../../../app/shared/form/builder/ds-dynamic-form-ui/relation-lookup-modal/dynamic-lookup-relation-modal.component.html',
-  templateUrl: './dynamic-lookup-relation-modal.component.html',
-  providers: [
-    {
-      provide: SEARCH_CONFIG_SERVICE,
-      useClass: SearchConfigurationService
-    }
-  ],
+  templateUrl: '../../../../../../../../app/shared/form/builder/ds-dynamic-form-ui/relation-lookup-modal/dynamic-lookup-relation-modal.component.html',
+  // templateUrl: './dynamic-lookup-relation-modal.component.html',
 })
 export class DsDynamicLookupRelationModalComponent extends BaseComponent {}
