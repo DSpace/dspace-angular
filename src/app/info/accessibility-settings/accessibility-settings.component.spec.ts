4a74a3ac991db9f5cbb54d192f14e6ac32227cb0
Andreas Awouters <andreas.awouters@atmire.com>
2025-02-21 10:10:35 +0100

    119602: Fix existing accessibility-settings.component tests

--- src/app/info/accessibility-settings/accessibility-settings.component.spec.ts
+++ src/app/info/accessibility-settings/accessibility-settings.component.spec.ts
@@ -9,6 +9,8 @@ import { NotificationsServiceStub } from '../../shared/testing/notifications-ser
 import { AuthService } from '../../core/auth/auth.service';
 import { NotificationsService } from '../../shared/notifications/notifications.service';
 import { of } from 'rxjs';
+import { KlaroServiceStub } from '../../shared/cookies/klaro.service.stub';
+import { KlaroService } from '../../shared/cookies/klaro.service';
 
 
 describe('AccessibilitySettingsComponent', () => {
@@ -18,11 +20,13 @@ describe('AccessibilitySettingsComponent', () => {
   let authService: AuthServiceStub;
   let settingsService: AccessibilitySettingsService;
   let notificationsService: NotificationsServiceStub;
+  let klaroService: KlaroServiceStub;
 
   beforeEach(waitForAsync(() => {
     authService = new AuthServiceStub();
     settingsService = getAccessibilitySettingsServiceStub();
     notificationsService = new NotificationsServiceStub();
+    klaroService = new KlaroServiceStub();
 
     TestBed.configureTestingModule({
       imports: [TranslateModule.forRoot()],
@@ -31,6 +35,7 @@ describe('AccessibilitySettingsComponent', () => {
         { provide: AuthService, useValue: authService },
         { provide: AccessibilitySettingsService, useValue: settingsService },
         { provide: NotificationsService, useValue: notificationsService },
+        { provide: KlaroService, useValue: klaroService },
       ],
       schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
