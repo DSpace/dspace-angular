9f259414d22861f48b7e4be1139a29914397a006
FrancescoMolinaro <francesco.molinaro@4science.com>
2025-02-21 09:25:05 +0100

    [DURACOM-327] remove wrong import

--- modules/shared/utils/src/index.ts
+++ modules/shared/utils/src/index.ts
@@ -2,4 +2,3 @@ export * from './lib/utils/date.util';
 export * from './lib/utils/empty.util';
 export * from './lib/utils/numeric.util';
 export * from './lib/utils/object.util';
-export * from '../../../../src/app/core/utilities/remote-data.utils';
