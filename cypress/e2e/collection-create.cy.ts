c9b27173449da1d0b29d73aef1381068da0c0b08
Andreas Awouters <andreas.awouters@atmire.com>
2025-02-25 15:51:12 +0100

    119602: Accept cookies in specs where cookie window covers other elements

--- cypress/e2e/collection-create.cy.ts
+++ cypress/e2e/collection-create.cy.ts
@@ -1,6 +1,9 @@
 beforeEach(() => {
   cy.visit('/collections/create?parent='.concat(Cypress.env('DSPACE_TEST_COMMUNITY')));
   cy.loginViaForm(Cypress.env('DSPACE_TEST_ADMIN_USER'), Cypress.env('DSPACE_TEST_ADMIN_PASSWORD'));
+
+  // Accept all cookies to make sure cookie window does not block other elements
+  cy.get('.orejime-Button--save').click();
 });
 
 it('should show loading component while saving', () => {
