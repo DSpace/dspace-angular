8d7b4cd101c046e4044a0953a8d1d17fd2eaea50
Alan Orth <alan.orth@gmail.com>
2025-02-25 22:13:00 +0300

    src/app/core: add citation_doi tag to head meta
    This is used by harvesters like Altmetric and was present in DSpace
    version 6 and previous.

--- src/app/core/metadata/metadata.service.ts
+++ src/app/core/metadata/metadata.service.ts
@@ -161,6 +161,7 @@ export class MetadataService {
     this.setCitationKeywordsTag();
 
     this.setCitationAbstractUrlTag();
+    this.setCitationDoiTag();
     this.setCitationPdfUrlTag();
     this.setCitationPublisherTag();
 
@@ -291,6 +292,18 @@ export class MetadataService {
         url = new URLCombiner(this.hardRedirectService.getCurrentOrigin(), this.router.url).toString();
       }
       this.addMetaTag('citation_abstract_html_url', url);
+    }
+  }
+
+  /**
+   * Add <meta name="citation_doi" ... >  to the <head>
+   */
+  private setCitationDoiTag(): void {
+    if (this.currentObject.value instanceof Item) {
+      let doi = this.getMetaTagValue('dc.identifier.doi');
+      if (hasValue(doi)) {
+        this.addMetaTag('citation_doi', doi);
+      }
     }
   }
 
