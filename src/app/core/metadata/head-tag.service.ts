5bf07663ab01d0cafa2dccb1f4517c13833aea3a
Alan Orth <alan.orth@gmail.com>
2025-02-26 08:47:48 +0300

    src/app/core: remove unnecessary comment
    Remove commented out this.setCitationDOITag() since it is not used
    and we use camel case with this.setCitationDoiTag() now anyway.

--- src/app/core/metadata/head-tag.service.ts
+++ src/app/core/metadata/head-tag.service.ts
@@ -199,7 +199,6 @@ export class HeadTagService {
     // this.setCitationIssueTag();
     // this.setCitationFirstPageTag();
     // this.setCitationLastPageTag();
-    // this.setCitationDOITag();
     // this.setCitationPMIDTag();
 
     // this.setCitationFullTextTag();
