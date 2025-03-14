01adc9db6235ece241e02ba95e289c3f04cc702d
Giuseppe Digilio <giuseppe.digilio@4science.com>
2025-03-14 12:47:48 +0100

    [DSC-2207] fix build

--- src/app/core/config/models/config-submission-access.model.ts
+++ src/app/core/config/models/config-submission-access.model.ts
@@ -49,12 +49,6 @@ export class SubmissionAccessModel extends ConfigObject {
   required: boolean;
 
   /**
-   * Whether the field is mandatory
-   */
-  @autoserialize
-  required: boolean;
-
-  /**
    * The links to all related resources returned by the rest api.
    */
   @deserialize
