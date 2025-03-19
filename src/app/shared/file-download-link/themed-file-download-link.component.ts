57d6d510ea65513d96f5908f322b25a5cbf01042
Nicolas Boulay <NIBOU230@ulaval.ca>
2025-03-19 13:20:05 -0400

    Add an input to show/hide the badge on file component

--- src/app/shared/file-download-link/themed-file-download-link.component.ts
+++ src/app/shared/file-download-link/themed-file-download-link.component.ts
@@ -27,7 +27,16 @@ export class ThemedFileDownloadLinkComponent extends ThemedComponent<FileDownloa
 
   @Input() enableRequestACopy: boolean;
 
-  protected inAndOutputNames: (keyof FileDownloadLinkComponent & keyof this)[] = ['bitstream', 'item', 'cssClasses', 'isBlank', 'enableRequestACopy'];
+  @Input() showAccessStatusBadge: boolean;
+
+  protected inAndOutputNames: (keyof FileDownloadLinkComponent & keyof this)[] = [
+    'bitstream',
+    'item',
+    'cssClasses',
+    'isBlank',
+    'enableRequestACopy',
+    'showAccessStatusBadge',
+  ];
 
   protected getComponentName(): string {
     return 'FileDownloadLinkComponent';
