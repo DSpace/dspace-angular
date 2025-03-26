ab9cbea7c3ed10c635ee30da8a12ff4357e3159e
Vincenzo Mecca <vincenzo.mecca@4science.com>
2025-03-26 20:23:21 +0100

    [CST-18964] Fixes empty trackerId for bitstream download feature

--- src/app/statistics/matomo.service.ts
+++ src/app/statistics/matomo.service.ts
@@ -145,7 +145,7 @@ export class MatomoService {
    */
   private appendTrackerId(url: string, visitorId: string) {
     const updatedURL = new URL(url);
-    if (visitorId != null) {
+    if (isNotEmpty(visitorId)) {
       updatedURL.searchParams.append('trackerId', visitorId);
     }
     return updatedURL.toString();
