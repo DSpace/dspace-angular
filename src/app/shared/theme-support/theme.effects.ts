5b35da08f75b5b200b0ae898228234dcd57cb3db
lotte <lotte_hofstede@hotmail.com>
2025-03-26 15:01:33 +0100

    129694: PoC #4099 solution

--- src/app/shared/theme-support/theme.effects.ts
+++ src/app/shared/theme-support/theme.effects.ts
@@ -11,25 +11,14 @@ import { getDefaultThemeConfig } from '../../../config/config.util';
 import { hasValue } from '../empty.util';
 import { SetThemeAction } from './theme.actions';
 import { BASE_THEME_NAME } from './theme.constants';
+import { NoOpAction } from '../ngrx/no-op.action';
 
 @Injectable()
 export class ThemeEffects {
   /**
    * Initialize with a theme that doesn't depend on the route.
    */
-  initTheme$ = createEffect(() =>
-    this.actions$.pipe(
-      ofType(ROOT_EFFECTS_INIT),
-      map(() => {
-        const defaultThemeConfig = getDefaultThemeConfig();
-        if (hasValue(defaultThemeConfig)) {
-          return new SetThemeAction(defaultThemeConfig.name);
-        } else {
-          return new SetThemeAction(BASE_THEME_NAME);
-        }
-      }),
-    ),
-  );
+
 
   constructor(
     private actions$: Actions,
