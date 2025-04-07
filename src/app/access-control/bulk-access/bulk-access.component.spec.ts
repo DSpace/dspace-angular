827c20aaa6c63b5d3191a776d23a5b2d4c9cd7f3
Giuseppe Digilio <giuseppe.digilio@4science.com>
2025-04-07 18:44:02 +0200

    [DSC-2184] Code refactoring in order to align with DSpace code

--- src/app/access-control/bulk-access/bulk-access.component.spec.ts
+++ src/app/access-control/bulk-access/bulk-access.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { TranslateModule } from '@ngx-translate/core';
@@ -14,6 +14,7 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { NotificationsService } from '../../shared/notifications/notifications.service';
 import { NotificationsServiceStub } from '../../shared/testing/notifications-service.stub';
 import { By } from '@angular/platform-browser';
+import { Router, Routes } from '@angular/router';
 
 describe('BulkAccessComponent', () => {
   let component: BulkAccessComponent;
@@ -67,11 +68,14 @@ describe('BulkAccessComponent', () => {
   const expectedIdList = ['1234', '5678'];
 
   const selectableListStateEmpty: SelectableListState = { id: 'test', selection: [] };
+  const routes = [
+    {path: 'home', component: {}},
+  ] as Routes;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule,
+        RouterTestingModule.withRoutes(routes),
         TranslateModule.forRoot()
       ],
       declarations: [ BulkAccessComponent ],
@@ -151,10 +155,14 @@ describe('BulkAccessComponent', () => {
       expect(bulkAccessControlService.executeScript).toHaveBeenCalled();
     });
 
-    it('should have a link to /home', () => {
-      const link = fixture.debugElement.query(By.css('a.btn-outline-primary'));
+    it('should have a link to /home', fakeAsync(() => {
+      const router: Router = TestBed.inject(Router);
+      const link = fixture.debugElement.query(By.css('[data-test="back-btn"]'));
       expect(link).toBeTruthy();
-      expect(link.properties.href).toContain('/home');
-    });
+
+      link.nativeElement.click();
+      tick();
+      expect(router.url).toBe(`/home`);
+    }));
   });
 });
