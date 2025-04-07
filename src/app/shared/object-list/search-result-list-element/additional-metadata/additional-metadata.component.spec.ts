6ebe3edac0811f8ee3396da23641b242aa39acc4
Andrea Barbasso <andrea.barbasso@4science.com>
2025-04-07 12:05:51 +0200

    [DSC-2298] remove whitespaces by default in identifier links

--- src/app/shared/object-list/search-result-list-element/additional-metadata/additional-metadata.component.spec.ts
+++ src/app/shared/object-list/search-result-list-element/additional-metadata/additional-metadata.component.spec.ts
@@ -4,6 +4,8 @@ import { AdditionalMetadataComponent } from './additional-metadata.component';
 import { Item } from '../../../../core/shared/item.model';
 import { VocabularyService } from '../../../../core/submission/vocabularies/vocabulary.service';
 import { VocabularyServiceStub } from '../../../testing/vocabulary-service.stub';
+import { APP_CONFIG } from '../../../../../config/app-config.interface';
+import { environment } from '../../../../../environments/environment';
 
 describe('AdditionalMetadataComponent', () => {
   let component: AdditionalMetadataComponent;
@@ -24,7 +26,8 @@ describe('AdditionalMetadataComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [ AdditionalMetadataComponent ],
       providers: [
-        { provide: VocabularyService, useValue: new VocabularyServiceStub() }
+        { provide: VocabularyService, useValue: new VocabularyServiceStub() },
+        { provide: APP_CONFIG, useValue: environment }
       ],
     })
     .compileComponents();
@@ -39,5 +42,19 @@ describe('AdditionalMetadataComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should keep white space in metadata value if shouldKeepWhiteSpaces is true', () => {
+    expect(component.composeLink('keep my white spaces', 'keepMyWhiteSpaces')).toEqual({
+      href: 'https://keepmywhitespaces.com/keep my white spaces',
+      text: 'keep my white spaces'
+    });
+  });
+
+  it('should not keep white space in metadata value if shouldKeepWhiteSpaces is false', () => {
+    expect(component.composeLink('do not keep my white spaces', 'doi')).toEqual({
+      href: 'https://doi.org/donotkeepmywhitespaces',
+      text: 'do not keep my white spaces'
+    });
   });
 });
