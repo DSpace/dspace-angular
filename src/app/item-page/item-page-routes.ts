b3e5da733773e117a8bf20f8179fb34f7f548f14
Andrea Barbasso <andrea.barbasso@4science.com>
2025-04-11 14:42:38 +0200

    [DSC-2210] fix statistics menus

--- src/app/item-page/item-page-routes.ts
+++ src/app/item-page/item-page-routes.ts
@@ -78,7 +78,8 @@ export const ROUTES: Route[] = [
         public: [{
           id: 'statistics_item_:id',
           active: true,
-          visible: false,
+          visible: true,
+          parentID: 'statistics',
           index: 2,
           model: {
             type: MenuItemType.LINK,
