f5fcc309086cf5164b05e14dd9cd34a84e77e475
Linus Gasser <linus.gasser@epfl.ch>
2025-04-17 09:56:13 +0200

    More propagation of 'advanced', and correct replacement of ':'

--- src/app/shared/search-form/themed-search-form.component.ts
+++ src/app/shared/search-form/themed-search-form.component.ts
@@ -22,6 +22,8 @@ export class ThemedSearchFormComponent extends ThemedComponent<SearchFormCompone
 
   @Input() query: string;
 
+  @Input() advanced: boolean;
+
   @Input() inPlaceSearch: boolean;
 
   @Input() scope: string;
@@ -42,6 +44,7 @@ export class ThemedSearchFormComponent extends ThemedComponent<SearchFormCompone
 
   protected inAndOutputNames: (keyof SearchFormComponent & keyof this)[] = [
     'query',
+    'advanced',
     'inPlaceSearch',
     'scope',
     'hideScopeInUrl',
