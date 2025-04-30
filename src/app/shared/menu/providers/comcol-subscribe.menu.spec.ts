4f7dbb8a6962fc74f49b02dd703016d2907c2009
guillermo2519 <escalante.guillermo2519@gmail.com>
2025-04-30 09:58:09 -0600

    Fix: provide missing AuthRequestService - 4195

--- src/app/shared/menu/providers/comcol-subscribe.menu.spec.ts
+++ src/app/shared/menu/providers/comcol-subscribe.menu.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateService } from '@ngx-translate/core';
 import { of as observableOf } from 'rxjs';
+import { AuthService } from 'src/app/core/auth/auth.service';
+import { AuthRequestService } from 'src/app/core/auth/auth-request.service';
 
 import { AuthorizationDataService } from '../../../core/data/feature-authorization/authorization-data.service';
 import { Collection } from '../../../core/shared/collection.model';
+import { SubscriptionsDataService } from '../../subscriptions/subscriptions-data.service';
 import { MenuItemType } from '../menu-item-type.model';
 import { PartialMenuSection } from '../menu-provider.model';
 import { SubscribeMenuProvider } from './comcol-subscribe.menu';
@@ -43,6 +47,20 @@ describe('SubscribeMenuProvider', () => {
         SubscribeMenuProvider,
         { provide: AuthorizationDataService, useValue: authorizationService },
         { provide: NgbModal, useValue: modalService },
+        {
+          provide: AuthService,
+          useValue: {
+            getAuthenticatedUserFromStore: jasmine.createSpy().and.returnValue(observableOf({ id: 'mock-user-id' })),
+          },
+        },
+        {
+          provide: SubscriptionsDataService,
+          useValue: {
+            getSubscriptionsByPersonDSO: jasmine.createSpy().and.returnValue(observableOf([])),
+          },
+        },
+        { provide: TranslateService, useValue: {} },
+        { provide: AuthRequestService, useValue: {} },
       ],
     });
     provider = TestBed.inject(SubscribeMenuProvider);
