d1a876a1b192517cc3e41445891acd00a6b552e7
guillermo2519 <escalante.guillermo2519@gmail.com>
2025-05-01 20:13:03 -0600

    Fix : Button still says "subscribe" after subscription - 4195

--- cypress/e2e/collection-statistics.cy.ts
+++ cypress/e2e/collection-statistics.cy.ts
@@ -6,7 +6,8 @@ describe('Collection Statistics Page', () => {
 
   it('should load if you click on "Statistics" from a Collection page', () => {
     cy.visit('/collections/'.concat(Cypress.env('DSPACE_TEST_COLLECTION')));
-    cy.get('ds-navbar ds-link-menu-item a[data-test="link-menu-item.menu.section.statistics"]').click();
+    cy.get('ds-navbar', { timeout: 10000 }).should('be.visible');
+    cy.get('a[data-test="link-menu-item.menu.section.statistics"]', { timeout: 10000 }).should('be.visible').click();
     cy.location('pathname').should('eq', COLLECTIONSTATISTICSPAGE);
   });
 
