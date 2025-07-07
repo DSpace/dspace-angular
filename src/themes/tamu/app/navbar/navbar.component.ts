cfb9726cb83a3e2c7ac7f4bd228462b43497da66
William Welling <wwelling@tamu.edu>
2025-07-07 15:54:12 -0500

    Upgrade `tamu` theme basic branding and theming
    This aligns Data@TAMU and OAKTrust basic `tamu` theme in addition to upgrades for dspace-angular 9.0.

--- src/themes/tamu/app/navbar/navbar.component.ts
+++ src/themes/tamu/app/navbar/navbar.component.ts
@@ -1,6 +1,15 @@
+import {
+  AsyncPipe,
+  NgClass,
+  NgComponentOutlet,
+} from '@angular/common';
 import { Component } from '@angular/core';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateModule } from '@ngx-translate/core';
+
 import { NavbarComponent as BaseComponent } from '../../../../app/navbar/navbar.component';
 import { slideMobileNav } from '../../../../app/shared/animations/slide';
+import { ThemedUserMenuComponent } from '../../../../app/shared/auth-nav-menu/user-menu/themed-user-menu.component';
 
 /**
  * Component representing the public navbar
@@ -9,7 +18,16 @@ import { slideMobileNav } from '../../../../app/shared/animations/slide';
   selector: 'ds-navbar',
   styleUrls: ['./navbar.component.scss'],
   templateUrl: './navbar.component.html',
-  animations: [slideMobileNav]
+  animations: [slideMobileNav],
+  standalone: true,
+  imports: [
+    AsyncPipe,
+    NgbDropdownModule,
+    NgClass,
+    NgComponentOutlet,
+    ThemedUserMenuComponent,
+    TranslateModule,
+  ],
 })
 export class NavbarComponent extends BaseComponent {
 }
