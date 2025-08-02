84c31388faf1884dc3699150da177c96eed96dc6
Alan Orth <alan.orth@gmail.com>
2025-08-01 22:54:04 -0700

    Upgrade express-rate-limit to v8.0.1

--- src/config/config.util.spec.ts
+++ src/config/config.util.spec.ts
@@ -9,7 +9,7 @@ describe('Config Util', () => {
       const appConfig = new DefaultAppConfig();
       expect(appConfig.cache.msToLive.default).toEqual(15 * 60 * 1000); // 15 minute
       expect(appConfig.ui.rateLimiter.windowMs).toEqual(1 * 60 * 1000); // 1 minute
-      expect(appConfig.ui.rateLimiter.max).toEqual(500);
+      expect(appConfig.ui.rateLimiter.limit).toEqual(500);
       expect(appConfig.ui.useProxies).toEqual(true);
 
       expect(appConfig.submission.autosave.metadata).toEqual([]);
@@ -23,7 +23,7 @@ describe('Config Util', () => {
 
       const rateLimiter = {
         windowMs: 5 * 50 * 1000, // 5 minutes
-        max: 1000,
+        limit: 1000,
       };
       appConfig.ui.rateLimiter = rateLimiter;
 
@@ -47,7 +47,7 @@ describe('Config Util', () => {
 
       expect(environment.cache.msToLive.default).toEqual(msToLive);
       expect(environment.ui.rateLimiter.windowMs).toEqual(rateLimiter.windowMs);
-      expect(environment.ui.rateLimiter.max).toEqual(rateLimiter.max);
+      expect(environment.ui.rateLimiter.limit).toEqual(rateLimiter.limit);
       expect(environment.ui.useProxies).toEqual(false);
       expect(environment.submission.autosave.metadata[0]).toEqual(autoSaveMetadata[0]);
       expect(environment.submission.autosave.metadata[1]).toEqual(autoSaveMetadata[1]);
