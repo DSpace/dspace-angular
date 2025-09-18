a7b67e4357fc442b0d360e456b11d9fd1da514de
Kim Shepherd <kim@shepherd.nz>
2025-09-18 15:05:15 +0200

    [TLC-1202] Centralise getTypeBindRelations to single static method
    (cherry picked from commit 9244c24d118f3a6af1d30ee97f15f6ee43909650)

--- src/app/shared/form/builder/ds-dynamic-form-ui/ds-dynamic-type-bind-relation.service.ts
+++ src/app/shared/form/builder/ds-dynamic-form-ui/ds-dynamic-type-bind-relation.service.ts
@@ -13,7 +13,6 @@ import {
   DynamicFormControlModel,
   DynamicFormControlRelation,
   DynamicFormRelationService,
-  MATCH_DISABLED,
   MATCH_ENABLED,
   MATCH_VISIBLE,
   OR_OPERATOR,
@@ -35,6 +34,48 @@ import { DYNAMIC_FORM_CONTROL_TYPE_RELATION_GROUP } from './ds-dynamic-form-cons
  */
 @Injectable({ providedIn: 'root' })
 export class DsDynamicTypeBindRelationService {
+
+  /**
+   * Get the type bind values from the REST data for a specific field
+   * The return value is any[] in the method signature but in reality it's
+   * returning the 'relation' that'll be used for a dynamic matcher when filtering
+   * fields in type bind, made up of a 'match' outcome (make this field visible), an 'operator'
+   * (OR) and a 'when' condition (the bindValues array).
+   * @param configuredTypeBindValues  array of types from the submission definition (CONFIG_DATA)
+   * @param typeField
+   * @private
+   * @return DynamicFormControlRelation[] array with one relation in it, for type bind matching to show a field
+   */
+  public static getTypeBindRelations(configuredTypeBindValues: string[], typeField: string): DynamicFormControlRelation[] {
+    const bindValues = [];
+    configuredTypeBindValues.forEach((value) => {
+      bindValues.push({
+        id: typeField,
+        value: value,
+      });
+    });
+    // match: MATCH_VISIBLE means that if true, the field / component will be visible
+    // operator: OR means that all the values in the 'when' condition will be compared with OR, not AND
+    // when: the list of values to match against, in this case the list of strings from <type-bind>...</type-bind>
+    // Example: Field [x] will be VISIBLE if item type = book OR item type = book_part
+    //
+    // The opposing match value will be the dc.type for the workspace item
+    //
+    // MATCH_ENABLED is now also returned, so that hidden type-bound fields that are 'required'
+    // do not trigger false validation errors
+    return [
+      {
+        match: MATCH_ENABLED,
+        operator: OR_OPERATOR,
+        when: bindValues,
+      },
+      {
+        match: MATCH_VISIBLE,
+        operator: OR_OPERATOR,
+        when: bindValues,
+      },
+    ];
+  }
 
   constructor(@Optional() @Inject(DYNAMIC_MATCHERS) private dynamicMatchers: DynamicFormControlMatcher[],
               protected dynamicFormRelationService: DynamicFormRelationService,
@@ -216,32 +257,6 @@ export class DsDynamicTypeBindRelationService {
     });
 
     return subscriptions;
-  }
-
-  /**
-    * Helper function to construct a typeBindRelations array
-  * @param configuredTypeBindValues
-  */
-  public getTypeBindRelations(configuredTypeBindValues: string[]): DynamicFormControlRelation[] {
-    const bindValues = [];
-    configuredTypeBindValues.forEach((value) => {
-      bindValues.push({
-        id: 'dc.type',
-        value: value,
-      });
-    });
-    return [
-      {
-        match: MATCH_ENABLED,
-        operator: OR_OPERATOR,
-        when: bindValues,
-      },
-      {
-        match: MATCH_VISIBLE,
-        operator: OR_OPERATOR,
-        when: bindValues,
-      },
-    ];
   }
 
 }
