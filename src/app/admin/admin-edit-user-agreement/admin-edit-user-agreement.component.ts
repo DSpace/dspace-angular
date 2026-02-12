10780484e69fd96b7d6b3cddacc3f504d0e86854
Maciej Kleban dawnwender@gmail.com <dawnwender@gmail.com>
2026-02-12 15:20:11 +0100

    [CRIS-Merger] Port User Agreement edit feature from CRIS

---
+++ src/app/admin/admin-edit-user-agreement/admin-edit-user-agreement.component.ts
@@ -0,0 +1,153 @@
+import {
+  KeyValuePipe,
+  NgForOf,
+} from '@angular/common';
+import {
+  Component,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {
+  TranslateModule,
+  TranslateService,
+} from '@ngx-translate/core';
+import { Operation } from 'fast-json-patch';
+import { Subscription } from 'rxjs';
+
+import { environment } from '../../../environments/environment';
+import { ScriptDataService } from '../../core/data/processes/script-data.service';
+import { SiteDataService } from '../../core/data/site-data.service';
+import { getFirstCompletedRemoteData } from '../../core/shared/operators';
+import { Site } from '../../core/shared/site.model';
+import { AlertComponent } from '../../shared/alert/alert.component';
+import { NotificationsService } from '@dspace/core/notification-system/notifications.service';
+
+/**
+ * Component that represents the user agreement edit page for administrators.
+ */
+@Component({
+  selector: 'ds-admin-edit-user-agreement',
+  templateUrl: './admin-edit-user-agreement.component.html',
+  imports: [
+    AlertComponent,
+    TranslateModule,
+    KeyValuePipe,
+    FormsModule,
+  ],
+  standalone: true,
+})
+export class AdminEditUserAgreementComponent implements OnInit, OnDestroy {
+
+  userAgreementTexts: Map<string,UserAgreementText> = new Map();
+  site: Site;
+
+  subs: Subscription[] = [];
+
+  USER_AGREEMENT_TEXT_METADATA = 'dc.rights';
+
+  USER_AGREEMENT_METADATA = 'dspace.agreements.end-user';
+
+  constructor(private siteService: SiteDataService,
+              private modalService: NgbModal,
+              private translateService: TranslateService,
+              private notificationsService: NotificationsService,
+              private scriptDataService: ScriptDataService ) {
+
+  }
+
+  ngOnInit(): void {
+
+    environment.languages.filter((language) => language.active)
+      .forEach((language) => {
+        this.userAgreementTexts.set( language.code, {
+          languageLabel: language.label,
+          text: '',
+        });
+      });
+
+    this.subs.push(this.siteService.find().subscribe((site) => {
+      this.site = site;
+      for (const metadata of site.metadataAsList) {
+        if (metadata.key === this.USER_AGREEMENT_TEXT_METADATA) {
+          const userAgreementText = this.userAgreementTexts.get(metadata.language);
+          if (userAgreementText != null) {
+            userAgreementText.text = metadata.value;
+          }
+        }
+      }
+    }));
+  }
+
+  /**
+   * Show the confirm modal to choose if  all users must be forced to accept the new user agreement or not.
+   * @param content the modal content
+   */
+  confirmEdit(content: any) {
+    this.modalService.open(content).result.then( (result) => {
+      if (result === 'cancel') {
+        return;
+      }
+      const operations = this.getOperationsToEditText();
+      this.subs.push(this.siteService.patch(this.site, operations).pipe(
+        getFirstCompletedRemoteData(),
+      ).subscribe((restResponse) => {
+        if (restResponse.hasSucceeded) {
+          this.notificationsService.success(this.translateService.get('admin.edit-user-agreement.success'));
+          if ( result === 'edit-with-reset' ) {
+            this.deleteAllUserAgreementMetadataValues();
+          }
+        } else {
+          this.notificationsService.error(this.translateService.get('admin.edit-user-agreement.error'));
+        }
+      }));
+    });
+  }
+
+  /**
+   * Returns the operations to update the user agreement text metadata.
+   */
+  private getOperationsToEditText(): Operation[] {
+    const firstLanguage = this.userAgreementTexts.keys().next().value;
+    const operations = [];
+    operations.push({
+      op: 'replace',
+      path: '/metadata/' + this.USER_AGREEMENT_TEXT_METADATA,
+      value: {
+        value: this.userAgreementTexts.get(firstLanguage).text,
+        language: firstLanguage,
+      },
+    });
+    this.userAgreementTexts.forEach((value, key) => {
+      if (key !== firstLanguage) {
+        operations.push({
+          op: 'add',
+          path: '/metadata/' + this.USER_AGREEMENT_TEXT_METADATA,
+          value: {
+            value: value.text,
+            language: key,
+          },
+        });
+      }
+    });
+    return operations;
+  }
+
+  /**
+   * Invoke the script to delete all the the user agreement text metadata values.
+   */
+  private deleteAllUserAgreementMetadataValues() {
+    this.subs.push(this.scriptDataService.invoke('metadata-deletion', [{ name: '-metadata', value: this.USER_AGREEMENT_METADATA }], []).subscribe());
+  }
+
+  ngOnDestroy(): void {
+    this.subs.forEach((sub) => sub.unsubscribe());
+  }
+
+}
+
+interface UserAgreementText {
+  languageLabel: string;
+  text: string;
+}
--- src/app/info/end-user-agreement/end-user-agreement-content/end-user-agreement-content.component.spec.ts
+++ src/app/info/end-user-agreement/end-user-agreement-content/end-user-agreement-content.component.spec.ts
@@ -8,6 +8,27 @@ import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { EndUserAgreementContentComponent } from './end-user-agreement-content.component';
+import { provideMockStore } from '@ngrx/store/testing';
+import { APP_DATA_SERVICES_MAP } from '@dspace/core/data-services-map-type';
+import { ResourceType } from '@dspace/core/shared/resource-type';
+import { environment } from '../../../../environments/environment.test';
+import { APP_CONFIG } from '@dspace/config/app-config.interface';
+import { LocaleService } from '@dspace/core/locale/locale.service';
+import { of } from 'rxjs';
+
+let localeService: any;
+
+const TEST_MODEL = new ResourceType('testmodel');
+
+const mockDataServiceMap: any = new Map([
+  [TEST_MODEL.value, () => import('../../../core/testing/test-data-service.mock').then(m => m.TestDataService)],
+]);
+
+const languageList = ['en;q=1', 'de;q=0.8'];
+const mockLocaleService = jasmine.createSpyObj('LocaleService', {
+    getCurrentLanguageCode: jasmine.createSpy('getCurrentLanguageCode'),
+    getLanguageCodeList: of(languageList),
+});
 
 describe('EndUserAgreementContentComponent', () => {
   let component: EndUserAgreementContentComponent;
@@ -16,18 +37,32 @@ describe('EndUserAgreementContentComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [TranslateModule.forRoot(), EndUserAgreementContentComponent],
+      providers: [
+        EndUserAgreementContentComponent,
+        provideMockStore({
+          initialState: {
+            index: {
+            }
+          }
+        }),
+        { provide: APP_DATA_SERVICES_MAP, useValue: mockDataServiceMap },
+        { provide: APP_CONFIG, useValue: environment },
+        { provide: LocaleService, useValue: mockLocaleService },
+      ],
       schemas: [NO_ERRORS_SCHEMA],
     })
-      .overrideComponent(EndUserAgreementContentComponent, {
-        remove: {
-          imports: [RouterLink],
-        },
-      })
-      .compileComponents();
+    .overrideComponent(EndUserAgreementContentComponent, {
+      remove: {
+        imports: [RouterLink],
+      },
+    })
+    .compileComponents();
   }));
-
+  
   beforeEach(() => {
     fixture = TestBed.createComponent(EndUserAgreementContentComponent);
+    localeService = TestBed.inject(LocaleService);
+    localeService.getCurrentLanguageCode.and.returnValue(of('en'));
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
