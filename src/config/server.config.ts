117a60734283077b39e5e474a8c6ffe67d94fbb1
Amy Pillow <amypillow@lavache.com>
2026-02-14 23:25:08 +0100

    Move baseurl to server config

--- src/config/server.config.ts
+++ src/config/server.config.ts
@@ -17,11 +17,27 @@ export class ServerConfig extends Config {
   @Config.env('DSPACE_REST_NAMESPACE')
   nameSpace: string;
 
+  private _baseUrl: string;
   @Config.publish()
-  baseUrl?: string;
+  get baseUrl() {
+    return this._baseUrl || [
+      this.ssl ? 'https://' : 'http://',
+      this.host,
+      this.port && this.port !== 80 && this.port !== 443
+        ? `:${this.port}`
+        : '',
+      this.nameSpace && this.nameSpace.startsWith('/')
+        ? this.nameSpace
+        : `/${this.nameSpace}`,
+    ].join('');
+  }
+  set baseUrl(val: string) {
+    this._baseUrl = val;
+  }
 
   @Config.env('DSPACE_REST_SSRBASEURL')
   ssrBaseUrl?: string;
+
   // This boolean will be automatically set on server startup based on whether "baseUrl" and "ssrBaseUrl"
   // have different values.
   hasSsrBaseUrl?: boolean;
