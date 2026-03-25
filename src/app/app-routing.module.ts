d42b9548d851c59c01e51b88960152f323b76fa9
Oscar Chacón <oscar@escire.lat>
2026-03-24 21:38:25 -0600

    backport PR 5053 to Dspace 7.x

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -47,7 +47,7 @@ import { notAuthenticatedGuard } from './core/auth/not-authenticated.guard';
 @NgModule({
   imports: [
     RouterModule.forRoot([
-      { path: INTERNAL_SERVER_ERROR, component: ThemedPageInternalServerErrorComponent },
+      { path: INTERNAL_SERVER_ERROR, component: ThemedPageInternalServerErrorComponent, data: { title: '500.page-internal-server-error' } },
       { path: ERROR_PAGE , component: ThemedPageErrorComponent },
       {
         path: '',
@@ -226,7 +226,8 @@ import { notAuthenticatedGuard } from './core/auth/not-authenticated.guard';
           },
           {
             path: FORBIDDEN_PATH,
-            component: ThemedForbiddenComponent
+            component: ThemedForbiddenComponent,
+            data: { title: '403.forbidden' },
           },
           {
             path: 'statistics',
@@ -250,7 +251,7 @@ import { notAuthenticatedGuard } from './core/auth/not-authenticated.guard';
               .then((m) => m.SubscriptionsPageRoutingModule),
             canActivate: [AuthenticatedGuard]
           },
-          { path: '**', pathMatch: 'full', component: ThemedPageNotFoundComponent },
+          { path: '**', pathMatch: 'full', component: ThemedPageNotFoundComponent, data: { title: '404.page-not-found' } },
         ]
       }
     ], {
