3ea49445eaa91e320a3bbcdeb14b8e24495a50b4
Art Lowel <art.lowel@gmail.com>
2026-05-08 16:44:56 +0200

    fix an issue where search facets wouldn't open in safari

--- src/app/shared/search/search-filters/search-filter/search-facet-filter-wrapper/search-facet-filter-wrapper.component.ts
+++ src/app/shared/search/search-filters/search-filter/search-facet-filter-wrapper/search-facet-filter-wrapper.component.ts
@@ -14,6 +14,7 @@ import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'ds-search-facet-filter-wrapper',
+  styleUrls: ['./search-facet-filter-wrapper.component.scss'],
   templateUrl: './search-facet-filter-wrapper.component.html'
 })
 
