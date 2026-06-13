4a3a53ff11eb7b1e6e9fee274abd7e0efed12cfa
Eike Martin Löhden <eike.loehden@staff.uni-marburg.de>
2026-06-13 10:04:49 +0200

    Added appMenu entries.

--- src/app/app.menus.ts
+++ src/app/app.menus.ts
@@ -9,6 +9,8 @@ import { buildMenuStructure } from './shared/menu/menu.structure';
 import { MenuID } from './shared/menu/menu-id.model';
 import { MenuRoute } from './shared/menu/menu-route.model';
 import { AccessControlMenuProvider } from './shared/menu/providers/access-control.menu';
+import { AddSubCollectionMenu } from './shared/menu/providers/add-sub-collection.menu';
+import { AddSubCommunityMenu } from './shared/menu/providers/add-sub-community.menu';
 import { AdminSearchMenuProvider } from './shared/menu/providers/admin-search.menu';
 import { AuditLogsMenuProvider } from './shared/menu/providers/audit-item.menu';
 import { AuditOverviewMenuProvider } from './shared/menu/providers/audit-overview.menu';
@@ -35,6 +37,7 @@ import { NotificationsMenuProvider } from './shared/menu/providers/notifications
 import { ProcessesMenuProvider } from './shared/menu/providers/processes.menu';
 import { RegistriesMenuProvider } from './shared/menu/providers/registries.menu';
 import { StatisticsMenuProvider } from './shared/menu/providers/statistics.menu';
+import { SubmitNewItemMenu } from './shared/menu/providers/submit-new-item.menu';
 import { SystemWideAlertMenuProvider } from './shared/menu/providers/system-wide-alert.menu';
 import { WithdrawnReinstateItemMenuProvider } from './shared/menu/providers/withdrawn-reinstate-item.menu';
 import { WorkflowMenuProvider } from './shared/menu/providers/workflow.menu';
@@ -94,6 +97,15 @@ export const MENUS = buildMenuStructure({
         MenuRoute.COMMUNITY_PAGE,
         MenuRoute.COLLECTION_PAGE,
         MenuRoute.ITEM_PAGE,
+      ),
+      AddSubCommunityMenu.onRoute(
+        MenuRoute.COMMUNITY_PAGE,
+      ),
+      AddSubCollectionMenu.onRoute(
+        MenuRoute.COMMUNITY_PAGE,
+      ),
+      SubmitNewItemMenu.onRoute(
+        MenuRoute.COLLECTION_PAGE,
       ),
       WithdrawnReinstateItemMenuProvider.onRoute(
         MenuRoute.ITEM_PAGE,
