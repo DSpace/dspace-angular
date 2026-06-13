468f0d5f12babe8df234377a902da5a01b987f0a
Eike Martin Löhden <eike.loehden@staff.uni-marburg.de>
2026-06-13 10:04:36 +0200

    Created new menu providers for collection and community page.

---
+++ src/app/shared/menu/providers/submit-new-item.menu.ts
@@ -0,0 +1,56 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+import { Injectable } from '@angular/core';
+import { AuthorizationDataService } from '@dspace/core/data/feature-authorization/authorization-data.service';
+import { FeatureID } from '@dspace/core/data/feature-authorization/feature-id';
+import { DSpaceObject } from '@dspace/core/shared/dspace-object.model';
+import {
+  combineLatest,
+  Observable,
+} from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { LinkMenuItemModel } from '../menu-item/models/link.model';
+import { MenuItemType } from '../menu-item-type.model';
+import { PartialMenuSection } from '../menu-provider.model';
+import { DSpaceObjectPageMenuProvider } from './helper-providers/dso.menu';
+
+/**
+ * Menu provider to create the "Edit" option in the DSO edit menu
+ */
+@Injectable()
+export class SubmitNewItemMenu extends DSpaceObjectPageMenuProvider {
+  constructor(
+    protected authorizationDataService: AuthorizationDataService,
+  ) {
+    super();
+  }
+
+  public getSectionsForContext(dso: DSpaceObject): Observable<PartialMenuSection[]> {
+    return combineLatest([
+      this.authorizationDataService.isAuthorized(FeatureID.CanSubmit, dso.self),
+    ]).pipe(
+      map(([canSubmitItem]) => {
+        return [
+          {
+            visible: canSubmitItem,
+            model: {
+              type: MenuItemType.LINK,
+              text: 'collection.submit.item',
+              link: '/submit',
+              queryParams: {
+                collection: dso.uuid,
+              },
+            } as LinkMenuItemModel,
+            icon: 'plus',
+          },
+        ] as PartialMenuSection[];
+      }),
+    );
+  }
+}
