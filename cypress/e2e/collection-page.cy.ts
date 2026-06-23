1ff5c3e85d8767ef8f1a527f0f2d1d93d2d61418
Eike Martin Löhden <eike.loehden@staff.uni-marburg.de>
2026-06-23 06:47:27 +0200

    Created spec and e2e tests.

--- cypress/e2e/collection-page.cy.ts
+++ cypress/e2e/collection-page.cy.ts
@@ -1,5 +1,7 @@
 import { testA11y } from 'cypress/support/utils';
 
+const COLLECTION_PAGE = '/collections/'.concat(Cypress.expose('DSPACE_TEST_COLLECTION'));
+
 describe('Collection Page', () => {
 
   it('should pass accessibility tests', () => {
@@ -10,5 +12,20 @@ describe('Collection Page', () => {
 
     // Analyze <ds-collection-page> for accessibility issues
     testA11y('ds-collection-page');
+  });
+});
+
+describe('Collection Page -> Collection-edit menu', () => {
+  beforeEach(() => {
+    // All tests start with visiting the Collection Page
+    cy.visit(COLLECTION_PAGE);
+
+    // These page elements are restricted, so we will be shown the login form. Fill it out & submit.
+    cy.loginViaForm(Cypress.expose('DSPACE_TEST_ADMIN_USER'), Cypress.expose('DSPACE_TEST_ADMIN_PASSWORD'));
+  });
+
+  it('Edit menu should exist for admins.', () => {
+    // <dso-edit-menu> tag must be loaded
+    cy.get('ds-dso-edit-menu').should('be.visible');
   });
 });
