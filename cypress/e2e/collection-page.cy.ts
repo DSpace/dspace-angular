12f4d2533499a5bf399d22e6ecebfbc2283f640f
Eike Martin Löhden <eike.loehden@staff.uni-marburg.de>
2026-06-23 06:47:27 +0200

    Created spec and e2e tests.

--- cypress/e2e/collection-page.cy.ts
+++ cypress/e2e/collection-page.cy.ts
@@ -1,14 +1,31 @@
 import { testA11y } from 'cypress/support/utils';
 
+const COLLECTION_PAGE = '/collections/'.concat(Cypress.expose('DSPACE_TEST_COLLECTION'));
+
 describe('Collection Page', () => {
 
   it('should pass accessibility tests', () => {
-    cy.visit('/collections/'.concat(Cypress.env('DSPACE_TEST_COLLECTION')));
+    cy.visit('/collections/'.concat(Cypress.expose('DSPACE_TEST_COLLECTION')));
 
     // <ds-collection-page> tag must be loaded
     cy.get('ds-collection-page').should('be.visible');
 
     // Analyze <ds-collection-page> for accessibility issues
     testA11y('ds-collection-page');
+  });
+});
+
+describe('Collection Page -> Collection-edit menu', () => {
+  beforeEach(() => {
+    // All tests start with visiting the Collection Page
+    cy.visit(COLLECTION_PAGE);
+
+    // These page elements are restricted, so we will be shown the login form. Fill it out & submit.
+    cy.loginViaForm(Cypress.expose('DSPACE_TEST_ADMIN_USER'), Cypress.expose('DSPACE_TEST_ADMIN_PASSWORD'));
+  });
+
+  it('Edit menu should exist for admins.', () => {
+    // <dso-edit-menu> tag must be loaded
+    cy.get('ds-dso-edit-menu').should('be.visible');
   });
 });
---
+++ src/app/shared/menu/providers/submit-new-item.menu.spec.ts
@@ -0,0 +1,67 @@
+import { TestBed } from '@angular/core/testing';
+import { AuthorizationDataService } from '@dspace/core/data/feature-authorization/authorization-data.service';
+import { Collection } from '@dspace/core/shared/collection.model';
+import { COLLECTION } from '@dspace/core/shared/collection.resource-type';
+import { of } from 'rxjs';
+
+import { MenuItemType } from '../menu-item-type.model';
+import { PartialMenuSection } from '../menu-provider.model';
+import { DSpaceObjectEditMenuProvider } from './dso-edit.menu';
+
+describe('DSpaceObjectEditMenuProvider', () => {
+
+  const expectedSections: PartialMenuSection[] = [
+    {
+      visible: true,
+      model: {
+        type: MenuItemType.LINK,
+        text: 'collection.submit.item',
+        link: '/submit',
+        queryParams: {
+          collection: 'test-uuid',
+        },
+      },
+      icon: 'plus',
+    },
+  ];
+
+  let provider: DSpaceObjectEditMenuProvider;
+
+  const dso: Collection = Object.assign(new Collection(), {
+    type: COLLECTION.value,
+    uuid: 'test-uuid',
+    _links: { self: { href: 'self-link' } },
+  });
+
+
+  let authorizationService;
+
+  beforeEach(() => {
+
+    authorizationService = jasmine.createSpyObj('authorizationService', {
+      'isAuthorized': of(true),
+    });
+
+    TestBed.configureTestingModule({
+      providers: [
+        DSpaceObjectEditMenuProvider,
+        { provide: AuthorizationDataService, useValue: authorizationService },
+      ],
+    });
+    provider = TestBed.inject(DSpaceObjectEditMenuProvider);
+  });
+
+  it('should be created', () => {
+    expect(provider).toBeTruthy();
+  });
+
+  describe('getSectionsForContext', () => {
+    it('should return the expected sections', (done) => {
+      provider.getSectionsForContext(dso).subscribe((sections) => {
+        expect(sections).toEqual(expectedSections);
+        done();
+      });
+    });
+  });
+
+});
